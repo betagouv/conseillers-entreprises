14de0a9710f7ef6e19c2b47a12b44520b6893f06
Arnaud Doucerain <adoucerain@octo.com>
2017-05-10 18:08:28 +0200

    [12_diagnosis] Display Question and Answer lists in Diagnosis index

--- app/controllers/diagnosis_controller.rb
+++ app/controllers/diagnosis_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class DiagnosisController < ApplicationController
-  def index; end
+  def index
+    @questions = Question.all.includes(:answers)
+  end
 end
--- spec/views/diagnosis/index.html.haml_spec.rb
+++ spec/views/diagnosis/index.html.haml_spec.rb
@@ -3,9 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe 'diagnosis/index.html.haml', type: :view do
+  let(:question) { create :question }
+  let!(:answer) { create :answer, question: question }
+
+  before { assign :questions, [question] }
+
   it 'displays a title' do
     render
+    expect(rendered).to match(/Diagnostic/)
+  end
 
-    expect(rendered).to match(/Diagnosis#index/)
+  it 'displays two list elements' do
+    render
+    assert_select 'li', count: 2
   end
 end
