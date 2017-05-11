f5c96e6aabe89ece27ef2e44131463b4eec19e38
Arnaud Doucerain <adoucerain@octo.com>
2017-05-11 18:06:47 +0200

    [12_diagnosis] Schema fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170510150258) do
+ActiveRecord::Schema.define(version: 20170511103406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 20170510150258) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "questions", force: :cascade do |t|
+    t.string   "label"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "searches", force: :cascade do |t|
     t.string   "query"
     t.integer  "user_id"
@@ -37,11 +43,6 @@ ActiveRecord::Schema.define(version: 20170510150258) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_searches_on_user_id", using: :btree
-  end
-  create_table "questions", force: :cascade do |t|
-    t.string   "label"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -65,6 +66,6 @@ ActiveRecord::Schema.define(version: 20170510150258) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  add_foreign_key "searches", "users"
   add_foreign_key "answers", "questions"
+  add_foreign_key "searches", "users"
 end
