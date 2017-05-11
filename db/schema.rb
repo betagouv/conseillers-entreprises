2ed70767ee44c74d2f63b1d5df4507814055e607
Arnaud Doucerain <adoucerain@octo.com>
2017-05-11 17:11:55 +0200

    [2_searchsiret] Adding Search model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420210630) do
+ActiveRecord::Schema.define(version: 20170511103406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20170420210630) do
     t.string   "siren"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.string   "query"
+    t.integer  "user_id"
+    t.string   "label"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_searches_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,7 +45,6 @@ ActiveRecord::Schema.define(version: 20170420210630) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
@@ -44,4 +52,5 @@ ActiveRecord::Schema.define(version: 20170420210630) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "searches", "users"
 end
