179ce287dc0709b006a92c154d22bd7e53436d85
Antoine Boileau <apem.boileau@gmail.com>
2017-05-16 17:12:03 +0200

    [5_admin] Added answers and questions in active admin

---
+++ app/admin/answer.rb
@@ -0,0 +1,14 @@
+ActiveAdmin.register Answer do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+  permit_params do
+    permitted = [:label, :question_id]
+    permitted << :other if params[:action] == 'create' && current_user.is_admin?
+    permitted
+  end
+end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,8 @@
 
 class Question < ApplicationRecord
   has_many :answers
+
+  def to_s
+    label
+  end
 end
