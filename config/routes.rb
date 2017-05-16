85477da77f9fd0d5a2fc33c1ceb3046c7aa54472
Antoine Boileau <apem.boileau@gmail.com>
2017-05-16 18:43:37 +0200

    [5_admin] Cleaned routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users #, ActiveAdmin::Devise.config
+  devise_for :users
   ActiveAdmin.routes(self)
   root to: 'home#index'
   get 'home/about'
