9497f5baacf2f415d563e5874c817f30181c84bc
Antoine Boileau <apem.boileau@gmail.com>
2017-05-24 11:49:51 +0200

    [6_User_profile] Added back the sign_up feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  devise :database_authenticatable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :confirmable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 end
