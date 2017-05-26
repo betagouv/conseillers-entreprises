d3a76ba2c57620eb3fdda80fa274bbd15e8228a6
Antoine Boileau <apem.boileau@gmail.com>
2017-05-26 15:26:27 +0200

    [6_User_profile] Added update first name feature test

---
+++ spec/features/users/registrations_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'the profile update', type: :feature do
+  before do
+    user = create :user, first_name: 'first name', last_name: 'last name', password: 'password'
+    login_as user, scope: :user
+  end
+
+  it 'updates the profile' do
+    visit edit_user_registration_path
+
+    fill_in id: 'user_first_name', with: 'John'
+    fill_in id: 'user_last_name', with: 'Doe'
+    fill_in id: 'user_current_password', with: 'password'
+
+    click_button 'Update'
+
+    expect(User.first.first_name).to eq 'John'
+    expect(User.first.last_name).to eq 'Doe'
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,6 +21,7 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
   config.extend ControllerMacros, type: :controller
+  config.include Warden::Test::Helpers
 
   config.infer_spec_type_from_file_location!
 
