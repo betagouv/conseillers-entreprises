c30b7dea8693fb9b0584ba03efaad1fabb696e70
Arnaud Doucerain <adoucerain@octo.com>
2017-05-31 17:09:02 +0200

    [6_User_profile] Temp

---
+++ app/javascript/packs/user_profile/profile_item.vue
@@ -0,0 +1,29 @@
+<template>
+    <tr>
+        <td></td>
+        <td>
+            <div>
+                <div class="ui form inline field">
+                    <input type="text"/>
+
+                </div>
+            </div>
+        </td>
+    </tr>
+</template>
+
+<script>
+    export default {
+//        props: ['user'],
+        data () {
+            return {
+                msg: 'Hello world!'
+            }
+        }
+    }
+</script>
+
+<style lang="sass">
+    .example
+        font-weight: bold
+</style>
--- app/javascript/packs/user_profile/user_profile.js
+++ app/javascript/packs/user_profile/user_profile.js
@@ -1,13 +1,21 @@
 import Vue from 'vue/dist/vue.esm'
 import axios from 'axios'
+import ProfileItem from './profile_item.vue'
 
 const profile = new Vue({
     el: '#profile',
     data: {
         user: {},
-        editMode: false
+        editMode: false,
     },
-    components: {},
+    components: {
+        ProfileItem
+    },
+    computed: {
+        profileFields: function() {
+            return [{key: 'email', value: 1}]
+        }
+    },
     mounted: function () {
         var that;
         that = this;
