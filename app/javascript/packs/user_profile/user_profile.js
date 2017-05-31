07abf78caa91a0fc97968a5f64f918c40bdb2ac8
Arnaud Doucerain <adoucerain@octo.com>
2017-05-31 14:09:10 +0200

    [6_User_profile] Removing Vue.js experiments

--- app/javascript/packs/user_profile/user_profile.js
+++ app/javascript/packs/user_profile/user_profile.js
@@ -1,13 +1,5 @@
 import Vue from 'vue/dist/vue.esm'
 import axios from 'axios'
-import Profile from './profile.vue'
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const profile = new Vue(Profile).$mount('profile');
-//     console.log(profile);
-// });
-
 
 const profile = new Vue({
     el: '#profile',
@@ -21,13 +13,10 @@ const profile = new Vue({
         that = this;
         axios.get('/users/edit.json')
             .then(function (response) {
-                console.log(response.data);
                 that.user = response.data;
-                console.log('axios.get success');
             })
             .catch(function (error) {
                 console.log('axios.get error');
-                console.log(error);
             });
     }
 });
