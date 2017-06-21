050c0d37c005f6d0568277200000fb039b9d1595
Arnaud Doucerain <adoucerain@octo.com>
2017-06-21 18:20:30 +0200

    Use relevant Qwant API endpoint for news

--- app/services/qwant_api_service.rb
+++ app/services/qwant_api_service.rb
@@ -3,7 +3,7 @@
 module QwantApiService
   def self.results_for_query(query)
     raise ParameterMissingError, 'Query is missing' if query.blank?
-    qwant_api_url = "https://api.qwant.com/egp/search/web?q=#{query}"
+    qwant_api_url = "https://api.qwant.com/egp/search/news?q=#{query}"
     JSON.parse open(qwant_api_url).read
   end
 
--- spec/services/qwant_api_service_spec.rb
+++ spec/services/qwant_api_service_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 describe QwantApiService do
   describe 'results_for_query' do
-    let(:url) { "https://api.qwant.com/egp/search/web?q=#{query}" }
+    let(:url) { "https://api.qwant.com/egp/search/news?q=#{query}" }
     let(:query) { '123456789' }
 
     let(:qwant_json) do
@@ -13,13 +13,28 @@ describe QwantApiService do
           'result' => {
             'items' => [
               {
-                'title' => 'Rejoins <b>OCTO</b>',
-                'favicon' => '//s.qwant.com/fav/o/c/rejoins_octo_com.ico',
-                'url' => 'http =>//rejoins.octo.com/',
-                'source' => 'rejoins.<b>octo</b>.com',
-                'desc' => 'Nos tribus - Devenir un <b>Octo</b> - Nos offres d\'emploi - Nos offres de stage.',
-                '_id' => '1234567890',
-                'position' => 8
+                'title' => "Mais comment passe-t-on des startups d'Etat à l'Etat plateforme",
+                'url' => 'http://www.internetactu.net/2017/04/13/mais-comment-passe-t-on-des-startups-detat-a-letat-plateforme/',
+                'desc' => 'Lentrepreneur Pierre Pezziardi (@ppezziardi) était linvité dune récente matinée organisée par le Liberté Living Lab sur la',
+                'date' => 1_492_059_620,
+                'domain' => 'internetactu.net',
+                '_id' => '342a28b3d4af89967a8415e06c28081d',
+                'media' => [
+                  {
+                    'pict' => {
+                      'url' => '//s2.qwant.com/thumbr/0x110/7/9/422b040372a405923aa0e377dca148/b_1_q_0_p_0.jpg?u=http%3A%2F%2Fwww.internetactu.net%2Fwp-content%2Fuploads%2F2017%2F04%2F096-PEZZIARDI_COUV_web-197x300.jpg&q=0&b=1&p=0&a=0',
+                      'width' => 0,
+                      'height' => 110,
+                      'type' => 'image'
+                    },
+                    'pict_big' => {
+                      'url' => '//s2.qwant.com/thumbr/0x240/f/f/ab2f89103a10abb94e9f2cbe327015/b_1_q_0_p_0.jpg?u=http%3A%2F%2Fwww.internetactu.net%2Fwp-content%2Fuploads%2F2017%2F04%2F096-PEZZIARDI_COUV_web-197x300.jpg&q=0&b=1&p=0&a=0',
+                      'width' => 0,
+                      'height' => 240,
+                      'type' => 'image'
+                    }
+                  }
+                ]
               }
             ]
           }
