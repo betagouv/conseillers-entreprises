eed0d7e49255c00ab00378a8ee35958bda61ff87
Arnaud Doucerain <adoucerain@octo.com>
2017-06-21 19:21:24 +0200

    Rubocop fix

--- spec/services/qwant_api_service_spec.rb
+++ spec/services/qwant_api_service_spec.rb
@@ -22,13 +22,13 @@ describe QwantApiService do
                 'media' => [
                   {
                     'pict' => {
-                      'url' => '//s2.qwant.com/thumbr/0x110/7/9/422b040372a405923aa0e377dca148/b_1_q_0_p_0.jpg?u=http%3A%2F%2Fwww.internetactu.net%2Fwp-content%2Fuploads%2F2017%2F04%2F096-PEZZIARDI_COUV_web-197x300.jpg&q=0&b=1&p=0&a=0',
+                      'url' => 'pierre.jpg',
                       'width' => 0,
                       'height' => 110,
                       'type' => 'image'
                     },
                     'pict_big' => {
-                      'url' => '//s2.qwant.com/thumbr/0x240/f/f/ab2f89103a10abb94e9f2cbe327015/b_1_q_0_p_0.jpg?u=http%3A%2F%2Fwww.internetactu.net%2Fwp-content%2Fuploads%2F2017%2F04%2F096-PEZZIARDI_COUV_web-197x300.jpg&q=0&b=1&p=0&a=0',
+                      'url' => 'pierre_big.jpg',
                       'width' => 0,
                       'height' => 240,
                       'type' => 'image'
