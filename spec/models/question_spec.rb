d8c935958ba7002d584406ef1f15cafeb0b4dcae
Antoine Boileau <apem.boileau@gmail.com>
2017-06-21 18:04:00 +0200

    [44_visit_process_improuvement] Fixed inconsistency of category presence

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -5,4 +5,5 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it { is_expected.to have_many :assistances }
   it { is_expected.to belong_to :category }
+  it { is_expected.to validate_presence_of :category }
 end
