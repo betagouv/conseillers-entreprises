49cecbb74e7a2ae2aaf201ad92ef816aa45d8c92
Arnaud Doucerain <adoucerain@octo.com>
2017-06-23 11:07:23 +0200

    Remove Unaccent extension for now

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622170908) do
+ActiveRecord::Schema.define(version: 20170623090437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "unaccent"
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
