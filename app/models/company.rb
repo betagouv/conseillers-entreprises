66c01811ba3a00633dcda0412141aaf6980edf54
Arnaud Doucerain <adoucerain@octo.com>
2017-06-23 14:50:52 +0200

    Visit + Facility - Form patterns and labels

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Company < ApplicationRecord
-  NUMBER_PATTERN = '[0-9]{9,14}'
-
   validates :name, presence: true
 
   def to_s
--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Facility < ApplicationRecord
+  NUMBER_PATTERN = '[0-9]{14}'
+
   belongs_to :company
 
   validates :company, :siret, :postal_code, presence: true
