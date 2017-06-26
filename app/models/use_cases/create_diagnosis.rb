acffc8e2bbd5cba24aed108f552b17ffd6ebc4a1
Antoine Boileau <apem.boileau@gmail.com>
2017-06-26 18:51:55 +0200

    [44_visit_process_improuvement] Corrected Rubocop offense

--- app/models/use_cases/create_diagnosis.rb
+++ app/models/use_cases/create_diagnosis.rb
@@ -6,7 +6,12 @@ module UseCases
       def create_with_params(params)
         diagnosis = Diagnosis.create visit_id: params['visit_id']
         return diagnosis unless params['diagnosed_needs']
-        params['diagnosed_needs'].each do |need|
+        create_needs(diagnosis: diagnosis, needs_json_array: params['diagnosed_needs'])
+        diagnosis
+      end
+
+      def create_needs(needs_json_array:, diagnosis:)
+        needs_json_array.each do |need|
           next unless need['selected'] == 'on'
           DiagnosedNeed.create(
             diagnosis: diagnosis,
@@ -14,7 +19,6 @@ module UseCases
             question_id: need['question_id']
           )
         end
-        diagnosis
       end
     end
   end
