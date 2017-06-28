9ecf943a941fd1328443b60a831ccbaad73a70dd
Antoine Boileau <apem.boileau@gmail.com>
2017-06-28 17:02:22 +0200

    [55_localization_v1] Corrected rubocop warnings

--- spec/models/use_cases/localize_city_code_spec.rb
+++ spec/models/use_cases/localize_city_code_spec.rb
@@ -4,80 +4,80 @@ require 'rails_helper'
 
 describe UseCases::LocalizeCityCode do
   describe 'in_maubeuge?' do
-    let(:in_maubeuge) { UseCases::LocalizeCityCode.new(city_code).in_maubeuge? }
+    let(:in_maubeuge) { described_class.new(city_code).in_maubeuge? }
 
     context 'is not in maubeuge perimeter' do
-      let(:city_code) { 75108 }
+      let(:city_code) { 75_108 }
 
-      it 'should return false' do
+      it 'returns false' do
         expect(in_maubeuge).to be_falsey
       end
     end
 
     context 'is in maubeuge perimeter' do
-      let(:city_code) { 59169 }
+      let(:city_code) { 59_169 }
 
-      it 'should return true' do
+      it 'returns true' do
         expect(in_maubeuge).to be_truthy
       end
     end
   end
 
   describe 'in_valencienne_cambrai?' do
-    let(:in_valencienne_cambrai) { UseCases::LocalizeCityCode.new(city_code).in_valencienne_cambrai? }
+    let(:in_valencienne_cambrai) { described_class.new(city_code).in_valencienne_cambrai? }
 
     context 'is not in in_valencienne_cambrai perimeter' do
-      let(:city_code) { 75108 }
+      let(:city_code) { 75_108 }
 
-      it 'should return false' do
+      it 'returns false' do
         expect(in_valencienne_cambrai).to be_falsey
       end
     end
 
     context 'is in in_valencienne_cambrai perimeter' do
-      let(:city_code) { 59603 }
+      let(:city_code) { 59_603 }
 
-      it 'should return true' do
+      it 'returns true' do
         expect(in_valencienne_cambrai).to be_truthy
       end
     end
   end
 
   describe 'in_lens?' do
-    let(:in_lens) { UseCases::LocalizeCityCode.new(city_code).in_lens? }
+    let(:in_lens) { described_class.new(city_code).in_lens? }
 
     context 'is not in in_lens perimeter' do
-      let(:city_code) { 75108 }
+      let(:city_code) { 75_108 }
 
-      it 'should return false' do
+      it 'returns false' do
         expect(in_lens).to be_falsey
       end
     end
 
     context 'is in in_lens perimeter' do
-      let(:city_code) { 62573 }
+      let(:city_code) { 62_573 }
 
-      it 'should return true' do
+      it 'returns true' do
         expect(in_lens).to be_truthy
       end
     end
   end
 
   describe 'in_calais?' do
-    let(:in_calais) { UseCases::LocalizeCityCode.new(city_code).in_calais? }
+    let(:in_calais) { described_class.new(city_code).in_calais? }
 
     context 'is not in in_lens perimeter' do
-      let(:city_code) { 75108 }
+      let(:city_code) { 75_108 }
 
-      it 'should return false' do
+      it 'returns false' do
         expect(in_calais).to be_falsey
       end
     end
 
     context 'is in in_lens perimeter' do
-      let(:city_code) { 62769 }
+      let(:city_code) { 62_769 }
 
-      it 'should return true' do
+      it 'returns true' do
         expect(in_calais).to be_truthy
       end
     end
