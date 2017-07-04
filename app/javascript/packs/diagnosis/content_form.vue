35a06234c10f08048dd5233fe817034861744253
Antoine Boileau <apem.boileau@gmail.com>
2017-07-04 17:03:02 +0200

    [58_visit_process_amelioration] Added simple text content form for diagnosis

--- app/javascript/packs/diagnosis/content_form.vue
+++ app/javascript/packs/diagnosis/content_form.vue
@@ -1,6 +1,19 @@
 <template>
     <div id="content-form">
-        <h1>{{ diagnosis.content }}</h1>
+        <div class="ui dimmable segment shadow-less" v-bind:class="{ dimmed: disabled }">
+            <div class="ui simple inverted dimmer">
+                <div class="ui text loader">{{ pageData.locales.save_loader }}</div>
+            </div>
+            <div class="ui form">
+                <div class="field">
+                    <label>{{ pageData.locales.description }}</label>
+                    <textarea rows="2" v-model="diagnosis.content"></textarea>
+                </div>
+                <button class="small ui button" v-on:click="saveButtonClicked">
+                    {{ pageData.locales.save_description }}
+                </button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,33 +24,77 @@
         name: 'content-form',
         data () {
             return {
-                diagnosis: { content: 'Hello world!'}
+                diagnosis: { content: ''},
+                disabled: true,
+                pageData: {
+                    diagnosis_id: '',
+                    locales: {
+                        description: '',
+                        save_description: '',
+                        save_loader: ''
+                    }
+                }
             }
         },
         mounted: function () {
-            this.getDiagnosis();
-
+            this.mountPageData();
+        },
+        watch: {
+            pageData: function (val) {
+                console.log('diagnosis_id: ' + this.pageData.diagnosis_id );
+                if(val.diagnosis_id) {
+                    this.getDiagnosis();
+                }
+            }
         },
         methods: {
             getDiagnosis: function () {
                 var that;
                 that = this;
                 console.log('getDiagnosis');
-                axios.get('/api/diagnoses/1.json')
+                axios.get('/api/diagnoses/' + this.pageData.diagnosis_id  + '.json')
                     .then(function (response) {
                         that.$nextTick(() => {
                             that.diagnosis = response.data;
+                            that.disabled = false;
                         })
                     })
                     .catch(function (error) {
                         console.log('axios.get error' + error);
                     });
+            },
+            updateDiagnosis: function () {
+                var that;
+                that = this;
+                console.log('updateDiagnosis with content: ' + this.diagnosis.content);
+
+                axios.patch('/api/diagnoses/' + this.pageData.diagnosis_id, { diagnosis: this.diagnosis } )
+                    .then(function (response) {
+                        that.$nextTick(() => {
+                            that.diagnosis = response.data;
+                            that.disabled = false;
+                        })
+                    })
+                    .catch(function (error) {
+                        console.log('axios.get error' + error);
+                    });
+            },
+            mountPageData: function () {
+                this.$nextTick(() => {
+                    console.log('launchData' + launchData.locales.description)
+                    this.pageData = launchData;
+                })
+            },
+            saveButtonClicked: function () {
+                this.$nextTick(() => {
+                    console.log('saveButtonClicked')
+                    this.disabled = true;
+                })
+                this.updateDiagnosis();
             }
         }
     }
 </script>
 
 <style lang="sass">
-    .example
-        font-weight: bold
 </style>
