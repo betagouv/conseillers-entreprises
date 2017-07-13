4c868fefe4c05d1ab2716793504a221bb35f5854
Arnaud Doucerain <adoucerain@octo.com>
2017-07-13 11:31:29 +0200

    [78_statemail] Send statistics email each week with Clockwork

--- app/models/clockwork.rb
+++ app/models/clockwork.rb
@@ -6,5 +6,8 @@ require './config/environment'
 
 module Clockwork
   handler do |_job, _time|
+    AdminMailersService.send_statistics_email
   end
+
+  every(1.week, 'send_statistics_email', at: 'Monday 09:30')
 end
