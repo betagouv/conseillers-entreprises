d2601d16247592c22b53c3e5718a9480ef910468
Antoine Boileau <apem.boileau@gmail.com>
2017-07-21 17:31:52 +0200

    [92_process_2_need_selection] Add vue initial data setter component

--- app/javascript/packs/step-2/step2App.js
+++ app/javascript/packs/step-2/step2App.js
@@ -2,6 +2,8 @@ import Vue from 'vue/dist/vue.esm'
 import store from './store'
 import axios from 'axios'
 
+import appDataSetter from './appDataSetter.vue.erb'
+
 var token = document.getElementsByName('csrf-token')[0].getAttribute('content');
 axios.defaults.headers.common['X-CSRF-Token'] = token;
 axios.defaults.headers.common['Accept'] = 'application/json';
@@ -10,5 +12,6 @@ new Vue({
     el: '#step2-app',
     store,
     components: {
+        'app-data-setter': appDataSetter
     }
 });
