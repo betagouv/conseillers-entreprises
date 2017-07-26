b4904a4af614a00f73e8819a7e06b7520f7ee9fa
Arnaud Doucerain <adoucerain@octo.com>
2017-07-26 16:21:46 +0200

    [91_process_1_company_selection] step1.vue tests - editCompanyForm & displayCompanyForm & displayCompanyInfo & formHasError & removeErrors & watch SIRET

--- app/javascript/spec/step-1/selectCompanySpec.js
+++ app/javascript/spec/step-1/selectCompanySpec.js
@@ -78,4 +78,143 @@ describe('selectCompany', () => {
             expect(selectCompany.siretFormatValid()).toBeFalsy()
         })
     })
+
+    describe('editCompanyForm', () => {
+        let selectCompany
+
+        beforeEach(function () {
+            const vueApp = Vue.extend(SelectCompany)
+            selectCompany = new vueApp().$mount()
+        })
+
+        it('removes errors and displays company form', function () {
+            spyOn(selectCompany, 'removeErrors')
+            spyOn(selectCompany, 'displayCompanyForm')
+
+            selectCompany.editCompanyForm()
+
+            expect(selectCompany.removeErrors).toHaveBeenCalled()
+            expect(selectCompany.displayCompanyForm).toHaveBeenCalled()
+        })
+    })
+
+    describe('displayCompanyForm', () => {
+        let selectCompany
+
+        beforeEach(function () {
+            const vueApp = Vue.extend(SelectCompany)
+            selectCompany = new vueApp().$mount()
+
+            selectCompany.companyFormDisplayed = false
+            selectCompany.companyInfoDisplayed = true
+            selectCompany.nextStepButtonDisabled = false
+        })
+
+        it('activates company form and disables next step button', function () {
+            selectCompany.displayCompanyForm()
+
+            expect(selectCompany.companyFormDisplayed).toBe(true)
+            expect(selectCompany.companyInfoDisplayed).toBe(false)
+            expect(selectCompany.nextStepButtonDisabled).toBe(true)
+        })
+    })
+
+    describe('displayCompanyInfo', () => {
+        let selectCompany
+
+        beforeEach(function () {
+            const vueApp = Vue.extend(SelectCompany)
+            selectCompany = new vueApp().$mount()
+
+            selectCompany.companyFormDisplayed = true
+            selectCompany.companyInfoDisplayed = false
+            selectCompany.nextStepButtonDisabled = true
+        })
+
+        it('activates company info and enables next step button', function () {
+            selectCompany.displayCompanyInfo()
+
+            expect(selectCompany.companyFormDisplayed).toBe(false)
+            expect(selectCompany.companyInfoDisplayed).toBe(true)
+            expect(selectCompany.nextStepButtonDisabled).toBe(false)
+        })
+    })
+
+    describe('formHasError', () => {
+        let selectCompany
+
+        beforeEach(function () {
+            const vueApp = Vue.extend(SelectCompany)
+            selectCompany = new vueApp().$mount()
+        })
+
+        it('assumes the form has no error by default', function () {
+            selectCompany.formHasError()
+
+            expect(selectCompany.formHasError()).toBe(false)
+        })
+
+        it('assumes the form has an error when there is a SIRET format error', function () {
+            selectCompany.siretFormatError = true
+
+            selectCompany.formHasError()
+
+            expect(selectCompany.formHasError()).toBe(true)
+        })
+
+        it('assumes the form has an error when company is not found', function () {
+            selectCompany.companyNotFoundError = true
+
+            selectCompany.formHasError()
+
+            expect(selectCompany.formHasError()).toBe(true)
+        })
+
+        it('assumes the form has an error when diagnosis was not saved', function () {
+            selectCompany.diagnosisSaveError = true
+
+            selectCompany.formHasError()
+
+            expect(selectCompany.formHasError()).toBe(true)
+        })
+    })
+
+    describe('removeErrors', () => {
+        let selectCompany
+
+        beforeEach(function () {
+            const vueApp = Vue.extend(SelectCompany)
+            selectCompany = new vueApp().$mount()
+
+            selectCompany.siretFormatError = true
+            selectCompany.companyNotFoundError = true
+            selectCompany.diagnosisSaveError = true
+        })
+
+        it('removes all errors', function () {
+            selectCompany.removeErrors()
+
+            expect(selectCompany.siretFormatError).toBe(false)
+            expect(selectCompany.companyNotFoundError).toBe(false)
+            expect(selectCompany.diagnosisSaveError).toBe(false)
+        })
+    })
+
+    describe('watch SIRET', () => {
+        let selectCompany
+
+        beforeEach(function () {
+            const vueApp = Vue.extend(SelectCompany)
+            selectCompany = new vueApp().$mount()
+        })
+
+        it('removes all errors', async function () {
+            spyOn(selectCompany, 'removeErrors')
+
+            selectCompany.siret = '1234'
+            await Vue.nextTick()
+
+            expect(selectCompany.removeErrors).toHaveBeenCalled()
+        })
+    })
 })
