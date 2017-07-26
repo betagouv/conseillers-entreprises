365bb7ceae9d2204f5494c43436176e4effd09a7
Arnaud Doucerain <adoucerain@octo.com>
2017-07-26 19:07:02 +0200

    [visit_singleton] Adding Diagnosis.step

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170720134933) do
+ActiveRecord::Schema.define(version: 20170726170511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 20170720134933) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "step", default: 1
     t.index ["visit_id"], name: "index_diagnoses_on_visit_id"
   end
 
