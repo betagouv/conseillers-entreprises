a9bb7701a45dd37ebec912e7c63a074ccd29fc29
Arnaud Doucerain <adoucerain@octo.com>
2017-07-28 14:34:17 +0200

    [94_process_4_experts_selection] Select All/Unselect All

--- app/javascript/packs/step-4/step4App.js
+++ app/javascript/packs/step-4/step4App.js
@@ -3,11 +3,31 @@ import Vue from 'vue/dist/vue.esm'
 import TurbolinksAdapter from 'vue-turbolinks'
 Vue.use(TurbolinksAdapter)
 
-import SelectExperts from './selectExperts.vue.erb'
-
 new Vue({
     el: '#step4-app',
-    components: {
-        'select-experts': SelectExperts
+    data: {
+        selectedAssistanceExperts: [],
+        selectedAssistanceExpertsCount: 0
+    },
+    methods: {
+        selectAll: function() {
+            this.changeCheckboxesFor(true)
+        },
+        unSelectAll: function() {
+            this.changeCheckboxesFor(false)
+        },
+        changeCheckboxesFor: function(newValue) {
+            let checkboxes = document.getElementsByTagName('input')
+            for(let i = 0; i < checkboxes.length; i++) {
+                if(checkboxes[i].type === 'checkbox') {
+                    checkboxes[i].checked = newValue
+                }
+            }
+        }
+    },
+    watch: {
+        selectedAssistanceExperts: function(_newValue) {
+            this.selectedAssistanceExpertsCount = this.selectedAssistanceExperts.length
+        }
     }
 })
