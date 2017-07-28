d48ccc4357ec8c195f7920dc4967cdcdd49f61ab
Antoine Boileau <apem.boileau@gmail.com>
2017-07-28 12:15:06 +0200

    [93_process_3_complementary_data] fix rubocop warnings

--- app/controllers/api/visits_controller.rb
+++ app/controllers/api/visits_controller.rb
@@ -2,13 +2,11 @@
 
 module Api
   class VisitsController < ApplicationController
-
     def update
       visit = Visit.find params[:id]
 
       validate_happened_at update_params[:happened_at]
       visit.update update_params
-
     rescue StandardError
       render body: nil, status: :bad_request
     end
@@ -19,7 +17,7 @@ module Api
       params.require(:visit).permit(%i[happened_at])
     end
 
-    def validate_happened_at happened_at
+    def validate_happened_at(happened_at)
       DateTime.iso8601(happened_at)
     end
   end
