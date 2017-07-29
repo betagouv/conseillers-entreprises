e063ad8ccb978394b968c66f471048a25c664747
Arnaud Doucerain <adoucerain@octo.com>
2017-07-29 05:01:25 +0200

    [94_process_4_experts_selection] Adding a useful comment

--- app/controllers/diagnoses_controller.rb
+++ app/controllers/diagnoses_controller.rb
@@ -36,6 +36,7 @@ class DiagnosesController < ApplicationController
     diagnosis = Diagnosis.find params[:id]
     assistance_expert_ids = ExpertMailersService.filter_assistances_experts(params[:assistances_experts])
     UseCases::CreateSelectedAssistancesExperts.perform(diagnosis, assistance_expert_ids)
+    # TODO: Use Delayed Jobs to perform email sending ; http://doc.scalingo.com/languages/ruby/delayed-job.html
     ExpertMailersService.send_assistances_email(advisor: current_user, diagnosis: diagnosis,
                                                 assistance_expert_ids: assistance_expert_ids)
     redirect_to step_5_diagnosis_path(diagnosis)
