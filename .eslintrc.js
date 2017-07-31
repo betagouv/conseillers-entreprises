169f534fe459e5c27c827142cd31ca8e88d4f5bf
Xavier J <xjulien@octo.com>
2017-07-31 13:00:35 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: [
+        "standard",
+        'plugin:vue/base',
+        'vue'
+    ],
+    "parserOptions": {
+        "ecmaVersion": 6
+    },
+    rules: {
+        // override/add rules' settings here
+        'vue/no-invalid-v-if': 'error'
+    }
+};
+
