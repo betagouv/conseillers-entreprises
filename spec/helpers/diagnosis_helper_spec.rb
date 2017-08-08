70406d0b2ef2e2e73dddf65ecd5e6575fb4331b8
Antoine Boileau <apem.boileau@gmail.com>
2017-08-08 18:00:10 +0200

    [105_visit_page_amelioration] Change step layout to consider the diagnosis real step

--- spec/helpers/diagnosis_helper_spec.rb
+++ spec/helpers/diagnosis_helper_spec.rb
@@ -4,24 +4,34 @@ require 'rails_helper'
 
 describe DiagnosisHelper, type: :helper do
   describe 'classes_for_step' do
-    subject { helper.classes_for_step(displayed_step, current_step) }
+    subject { helper.classes_for_step(displayed_step, current_step, diagnosis_step) }
 
     let(:displayed_step) { 2 }
 
-    context 'displayed step < current_step' do
+    context 'displayed step < diagnosis step' do
       let(:current_step) { 3 }
+      let(:diagnosis_step) { 3 }
 
       it { is_expected.to eq 'completed' }
     end
 
-    context 'displayed step = current_step' do
+    context 'displayed step = current step && displayed step => diagnosis step' do
       let(:current_step) { 2 }
+      let(:diagnosis_step) { 2 }
 
       it { is_expected.to eq 'active' }
     end
 
-    context 'displayed step > current_step' do
+    context 'displayed step = current_step && displayed step < diagnosis_step' do
+      let(:current_step) { 2 }
+      let(:diagnosis_step) { 3 }
+
+      it { is_expected.to eq 'completed active' }
+    end
+
+    context 'displayed step > current_step && displayed step > diagnosis_step' do
       let(:current_step) { 1 }
+      let(:diagnosis_step) { 1 }
 
       it { is_expected.to be_nil }
     end
