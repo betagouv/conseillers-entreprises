180fb2d24c4a5387649bc2237a6f669d2ddeb25d
Antoine Boileau <apem.boileau@gmail.com>
2017-08-16 15:19:26 +0200

    [110_process_5_summary-amelioration] Add data in selected assistances experts

---
+++ db/migrate/20170816130145_add_informations_to_selected_assistance_expert.rb
@@ -0,0 +1,7 @@
+class AddInformationsToSelectedAssistanceExpert < ActiveRecord::Migration[5.1]
+  def change
+    add_column :selected_assistances_experts, :expert_full_name, :string
+    add_column :selected_assistances_experts, :expert_institution_name, :string
+    add_column :selected_assistances_experts, :assistance_title, :string
+  end
+end
--- spec/models/use_cases/create_selected_assistances_experts_spec.rb
+++ spec/models/use_cases/create_selected_assistances_experts_spec.rb
@@ -20,6 +20,12 @@ describe UseCases::CreateSelectedAssistancesExperts do
         expect(SelectedAssistanceExpert.first.diagnosed_need).to eq diagnosed_need
         expect(SelectedAssistanceExpert.first.assistance_expert).to eq assistance_expert
       end
+
+      it 'stores the right data' do
+        expect(SelectedAssistanceExpert.first.expert_full_name).to eq assistance_expert.expert.full_name
+        expect(SelectedAssistanceExpert.first.expert_institution_name).to eq assistance_expert.expert.institution.name
+        expect(SelectedAssistanceExpert.first.assistance_title).to eq assistance_expert.assistance.title
+      end
     end
   end
 end
