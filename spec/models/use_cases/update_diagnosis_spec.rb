628f7990c1a0a7aefe8e1d27580ea80961fec1ac
Antoine Boileau <apem.boileau@gmail.com>
2017-08-16 12:33:55 +0200

    [110_process_5_summary-amelioration] Fix diagnosis update bug

--- spec/models/use_cases/update_diagnosis_spec.rb
+++ spec/models/use_cases/update_diagnosis_spec.rb
@@ -11,40 +11,73 @@ describe UseCases::UpdateDiagnosis do
     context 'regular case as an integer' do
       let(:params) { { content: 'content', step: 3 } }
 
-      it('returns params without the keys that are blanks') do
-        expect(cleaned_params).to eq content: 'content', step: 3
+      it('returns params without modifications') do
+        hash = { content: 'content', step: 3 }
+        expect(cleaned_params).to eq hash
+
+        expect(cleaned_params.key?(:content)).to be_truthy
+        expect(cleaned_params.key?(:step)).to be_truthy
       end
     end
 
     context 'regular case as a string' do
       let(:params) { { content: 'content', step: '3' } }
 
-      it('returns params without the step key') do
-        expect(cleaned_params).to eq content: 'content', step: 3
+      it('returns params without modifications') do
+        hash = { content: 'content', step: 3 }
+        expect(cleaned_params).to eq hash
+
+        expect(cleaned_params.key?(:content)).to be_truthy
+        expect(cleaned_params.key?(:step)).to be_truthy
       end
     end
 
-    context 'content is blank' do
-      let(:params) { { content: '', step: 3 } }
+    context 'content is blank and there is no step' do
+      let(:params) { { content: '' } }
 
-      it('returns params without the keys that are blanks') do
-        expect(cleaned_params).to eq step: 3
+      it('returns an empty hash') do
+        hash = {}
+        expect(cleaned_params).to eq hash
+
+        expect(cleaned_params.key?(:content)).to be_falsey
+        expect(cleaned_params.key?(:step)).to be_falsey
       end
     end
 
     context 'the step is smaller than current step' do
-      let(:params) { { content: 'content', step: 1 } }
+      let(:params) { { content: 'content' } }
 
       it('returns params without the step key') do
-        expect(cleaned_params).to eq content: 'content'
+        hash = { content: 'content' }
+        expect(cleaned_params).to eq hash
+
+        expect(cleaned_params.key?(:content)).to be_truthy
+        expect(cleaned_params.key?(:step)).to be_falsey
       end
     end
 
-    context 'the step is a string beginning with an integer' do
-      let(:params) { { content: 'content', step: '5abcd' } }
+    context 'there is no step and content is not blank' do
+      let(:params) { { content: 'content' } }
 
       it('returns params without the step key') do
-        expect(cleaned_params).to eq content: 'content', step: 5
+        hash = { content: 'content' }
+        expect(cleaned_params).to eq hash
+
+        expect(cleaned_params.key?(:content)).to be_truthy
+        expect(cleaned_params.key?(:step)).to be_falsey
+      end
+    end
+
+    context 'the step is smaller than current step' do
+      let(:current_step) { 5 }
+      let(:params) { { step: 3 } }
+
+      it('returns empty params') do
+        hash = {}
+        expect(cleaned_params).to eq hash
+
+        expect(cleaned_params.key?(:content)).to be_falsey
+        expect(cleaned_params.key?(:step)).to be_falsey
       end
     end
   end
