2253acb77753b26ddd9bb221f0c7cac65536c03b
Antoine Boileau <apem.boileau@gmail.com>
2017-08-18 15:27:28 +0200

    [79_company_data_from_index_page] Refactor and mutualise Axios configuration

--- app/javascript/packs/step-1/step1App.js
+++ app/javascript/packs/step-1/step1App.js
@@ -1,22 +1,12 @@
 import Vue from 'vue/dist/vue.esm'
-import axios from 'axios'
 
 import TurbolinksAdapter from 'vue-turbolinks'
 
 Vue.use(TurbolinksAdapter)
 
 import SelectCompany from './selectCompany.vue.erb'
-
-var token
-try {
-    token = document.getElementsByName('csrf-token')[0].getAttribute('content')
-}
-catch (e) {
-    token = ''
-}
-
-axios.defaults.headers.common['X-CSRF-Token'] = token
-axios.defaults.headers.common['Accept'] = 'application/json'
+import AxiosConfigurator from '../common/axiosConfigurator'
+AxiosConfigurator.configure()
 
 new Vue({
     el: '#step1-app',
