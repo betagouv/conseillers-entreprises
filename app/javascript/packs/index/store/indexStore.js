a6b20c5360ef5d7baa008d4aa0f8cd742bd454a1
Antoine Boileau <apem.boileau@gmail.com>
2017-08-21 17:40:28 +0200

    [79_company_data_from_index_page] Add company siret search on index page

--- app/javascript/packs/index/store/indexStore.js
+++ app/javascript/packs/index/store/indexStore.js
@@ -1,16 +1,39 @@
-// import Step3APIService from './step3APIService'
+import IndexAPIService from '../utils/indexAPIService'
 import * as types from './indexMutationTypes'
+import * as errors from '../utils/indexErrorTypes'
 
 const state = {
     isRequestInProgress: false,
-    showFormErrorMessage: false,
-    siret: ''
+    formErrorType: '',
+    siret: '',
+    companyData: {},
 }
 
-const getters = {
-}
+const getters = {}
 
 const actions = {
+    fetchCompany({commit, state, indexAPIServiceDependency}) {
+        var indexAPIService = indexAPIServiceDependency
+        if (typeof indexAPIService === 'undefined') {
+            indexAPIService = IndexAPIService
+        }
+
+        commit(types.REQUEST_IN_PROGRESS, true)
+        return indexAPIService.fetchCompany(state.siret)
+            .then((data) => {
+                commit(types.COMPANY_DATA, {
+                    name: data.company_name,
+                    location: data.facility_location,
+                    siret: state.siret
+                })
+            })
+            .catch(() => {
+                commit(types.FORM_ERROR_TYPE, errors.NOT_FOUND_ERROR)
+            })
+            .then(() => {
+                commit(types.REQUEST_IN_PROGRESS, false)
+            })
+    }
 }
 
 const mutations = {
@@ -18,13 +41,19 @@ const mutations = {
         state.isRequestInProgress = isRequestInProgress
     },
 
-    [types.FORM_ERROR_MESSAGE](state, showFormErrorMessage) {
-        state.showFormErrorMessage = showFormErrorMessage
+    [types.FORM_ERROR_TYPE](state, formErrorType) {
+        state.formErrorType = formErrorType
+        state.companyData = {}
     },
 
     [types.SIRET](state, siret) {
         state.siret = siret
-    }
+    },
+
+    [types.COMPANY_DATA](state, {name, location, siret}) {
+        state.companyData = {name: name, location: location, siret: siret}
+        state.formErrorType = ''
+    },
 }
 
 export default {
--- app/javascript/spec/index/store/indexStoreSpec.js
+++ app/javascript/spec/index/store/indexStoreSpec.js
@@ -24,12 +24,24 @@ describe('IndexStore', () => {
             })
         })
 
-        describe('FORM_ERROR_MESSAGE', function () {
+        describe('FORM_ERROR_TYPE', function () {
 
-            it('updates the showFormErrorMessage value', function () {
-                const state = {showFormErrorMessage: undefined}
-                mutations.FORM_ERROR_MESSAGE(state, true)
-                expect(state.showFormErrorMessage).toBeTruthy()
+            let state
+
+            beforeEach(function () {
+                state = {
+                    formErrorType: '',
+                    companyData: {name: 'Café Bourgeois', location: 'somewhere', siret: 'siret number'}
+                }
+            })
+
+            it('updates the formErrorType value', function () {
+                mutations.FORM_ERROR_TYPE(state, 'ERROR')
+                expect(state.formErrorType).toEqual('ERROR')
+            })
+            it('clears the company values', function () {
+                mutations.FORM_ERROR_TYPE(state, 'ERROR')
+                expect(state.companyData).toEqual({})
             })
         })
 
@@ -41,11 +53,107 @@ describe('IndexStore', () => {
                 expect(state.siret).toEqual('48245813000010')
             })
         })
+
+        describe('COMPANY_DATA', function () {
+
+            let state
+
+            beforeEach(function () {
+                state = {
+                    formErrorType: 'ERROR',
+                    companyData: {},
+                }
+            })
+
+            it('updates the companyData value', function () {
+                mutations.COMPANY_DATA(state, {name: 'Café Bourgeois', location: 'somewhere', siret: 'siret number'})
+                expect(state.companyData.name).toEqual('Café Bourgeois')
+                expect(state.companyData.location).toEqual('somewhere')
+                expect(state.companyData.siret).toEqual('siret number')
+            })
+            it('clears the error value', function () {
+                mutations.COMPANY_DATA(state, {name: 'Café Bourgeois', location: 'somewhere'})
+                expect(state.formErrorType).toEqual('')
+            })
+        })
     })
 
     describe('actions', () => {
 
         const actions = IndexStore.actions
 
+        const indexAPIServiceMock = {
+            fetchCompany: function () {
+            }
+        }
+
+        var commit
+        var dispatch
+        var getters = {}
+        var state = {}
+
+        let apiServiceContext = function (dispatch, commit, state, getters) {
+            return {
+                dispatch: dispatch,
+                commit: commit,
+                state: state,
+                getters: getters,
+                indexAPIServiceDependency: indexAPIServiceMock
+            }
+        }
+
+        describe('fetchCompany', function () {
+
+            const positivePromise = Promise.resolve({
+                company_name: 'Fun Company',
+                facility_location: '59350 Lille'
+            })
+
+            beforeEach(function () {
+                spyOn(indexAPIServiceMock, 'fetchCompany').and.returnValue(positivePromise)
+                commit = jasmine.createSpy()
+
+                state.siret = 'siret number'
+            })
+
+            it('returns a promise', function () {
+                var promise = actions.fetchCompany(apiServiceContext(dispatch, commit, state, getters))
+                expect(typeof promise.then).toBe('function')
+            })
+
+            it('calls commit REQUEST_IN_PROGRESS with true at start of action', function () {
+                actions.fetchCompany(apiServiceContext(dispatch, commit, state, getters))
+
+                expect(commit.calls.argsFor(0)).toEqual([
+                    'REQUEST_IN_PROGRESS',
+                    true
+                ])
+            })
+
+            it('calls IndexAPIService with the siret', async function () {
+                await actions.fetchCompany(apiServiceContext(dispatch, commit, state, getters))
+
+                expect(indexAPIServiceMock.fetchCompany.calls.count()).toEqual(1)
+                expect(indexAPIServiceMock.fetchCompany.calls.argsFor(0)).toEqual(['siret number'])
+            })
+
+            it('calls commit COMPANY_DATA with the company data', async function () {
+                await actions.fetchCompany(apiServiceContext(dispatch, commit, state, getters))
+
+                expect(commit.calls.argsFor(1)).toEqual([
+                    'COMPANY_DATA',
+                    {name: 'Fun Company', location: '59350 Lille', siret: 'siret number'}
+                ])
+            })
+
+            it('calls commit REQUEST_IN_PROGRESS with false at end of action', async function () {
+                await actions.fetchCompany(apiServiceContext(dispatch, commit, state, getters))
+
+                expect(commit.calls.argsFor(2)).toEqual([
+                    'REQUEST_IN_PROGRESS',
+                    false
+                ])
+            })
+        })
     })
 })
