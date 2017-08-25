32763fd23544181ae5b65f6fa03e0bb42262c3d6
Antoine Boileau <apem.boileau@gmail.com>
2017-08-25 16:41:25 +0200

    [97_process_2_enable_modifications] Add bulk modification for diagnosed_needs api endpoint

--- app/controllers/api/diagnosed_needs_controller.rb
+++ app/controllers/api/diagnosed_needs_controller.rb
@@ -11,7 +11,37 @@ module Api
       render body: nil, status: :bad_request
     end
 
+    def bulk
+      DiagnosedNeed.transaction do
+        DiagnosedNeed.create bulk_create_param_array
+        DiagnosedNeed.update(bulk_update_param_hash.keys, bulk_update_param_hash.values)
+        DiagnosedNeed.destroy bulk_delete_param_array
+      end
+
+      render body: nil
+    rescue StandardError
+      render body: nil, status: :bad_request
+    end
+
     private
+
+    def bulk_create_param_array
+      create_param = params.require(:bulk_params).permit(create: %i[question_id question_label content])
+      create_param.fetch(:create, []).map { |need_params| need_params.merge!(diagnosis_id: params[:diagnosis_id]) }
+    end
+
+    def bulk_update_param_hash
+      update_param = params.require(:bulk_params).permit(update: %i[id content])
+      update_param.fetch(:update, [])
+                  .delete_if { |udpate_item| udpate_item[:id].nil? }
+                  .group_by { |udpate_item| udpate_item[:id] }
+                  .each_with_object({}) { |(k, v), hash| hash[k] = { content: v.first&.fetch('content', '') } }
+    end
+
+    def bulk_delete_param_array
+      delete_param = params.require(:bulk_params).permit(delete: %i[id])
+      delete_param.fetch(:delete, []).map(&:values).map(&:first)
+    end
 
     def create_param_array
       param_array = params.permit(diagnosed_needs: %i[question_id question_label content]).require(:diagnosed_needs)
--- spec/controllers/api/diagnosed_needs_controller_spec.rb
+++ spec/controllers/api/diagnosed_needs_controller_spec.rb
@@ -2,6 +2,7 @@
 
 require 'rails_helper'
 
+# rubocop:disable Metrics/BlockLength
 RSpec.describe Api::DiagnosedNeedsController, type: :controller do
   login_user
 
@@ -60,4 +61,74 @@ RSpec.describe Api::DiagnosedNeedsController, type: :controller do
       end
     end
   end
+
+  describe 'POST #bulk' do
+    let(:id1) { 1 }
+    let(:id2) { 2 }
+    let(:id3) { 3 }
+    let!(:diagnosed_need1) { create :diagnosed_need, id: id1, diagnosis: diagnosis, content: 'Not random content' }
+    let!(:diagnosed_need2) { create :diagnosed_need, id: id2, diagnosis: diagnosis }
+    let!(:diagnosed_need3) { create :diagnosed_need, id: id3, diagnosis: diagnosis }
+
+    describe 'in case of success' do
+      let(:bulk_params) do
+        {
+          create: [{ question_id: question1.id, question_label: question1.label, content: 'Random content' }],
+          update: [{ id: diagnosed_need1.id, content: 'New content' }],
+          delete: [{ id: diagnosed_need2.id }, { id: diagnosed_need3.id }]
+        }
+      end
+
+      before do
+        post :bulk, format: :json, params: { diagnosis_id: diagnosis.id, bulk_params: bulk_params }
+      end
+
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'creates one new diagnosed need' do
+        expect(DiagnosedNeed.all.count).to eq(2)
+        expect(DiagnosedNeed.last.question_id).to eq(question1.id)
+        expect(DiagnosedNeed.last.content).to eq('Random content')
+        expect(DiagnosedNeed.last.question_label).to eq(question1.label)
+      end
+
+      it 'updates the flagged toUpdate diagnosed need' do
+        diagnosed_need1_content = DiagnosedNeed.find(diagnosed_need1.id).content
+        expect(diagnosed_need1_content).to eq('New content')
+      end
+
+      it 'deletes the flagged toDelete diagnosed need' do
+        expect(DiagnosedNeed.where(id: id2)).not_to exist
+        expect(DiagnosedNeed.where(id: id3)).not_to exist
+      end
+    end
+
+    describe 'in case of error' do
+      let(:bulk_params) do
+        {
+          create: [{ question_id: question1.id, question_label: question1.label, content: 'Random content' }],
+          update: [{ id: nil, content: 'New content' }],
+          delete: [{ id: diagnosed_need2.id }, { id: diagnosed_need3.id }]
+        }
+      end
+
+      before do
+        post :bulk, format: :json, params: { diagnosis_id: diagnosis.id, bulk_params: bulk_params }
+      end
+
+      it 'returns http bad_request' do
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'does not modify anything' do
+        expect(DiagnosedNeed.all.count).to eq(3)
+        expect(DiagnosedNeed.where(id: id2)).to exist
+        expect(DiagnosedNeed.where(id: id3)).to exist
+        expect(DiagnosedNeed.find(id1).content).to eq 'Not random content'
+      end
+    end
+  end
 end
+# rubocop:enable Metrics/BlockLength
