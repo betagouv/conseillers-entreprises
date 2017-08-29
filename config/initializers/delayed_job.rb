50fb94bc7ea94f741a3d3436eab0830e4ac82a78
Arnaud Doucerain <adoucerain@octo.com>
2017-08-29 18:08:47 +0200

    [ard_code_review] Delayed Jobs - Error handlers are back

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -5,3 +5,16 @@ Delayed::Worker.max_run_time = 5.minutes
 Delayed::Worker.max_attempts = 1
 
 Delayed::Worker.default_priority = 10
+
+# Fail at startup if method does not exist instead of later in a background job
+[[ExceptionNotifier, :notify_exception]].each do |object, method_name|
+  next if object.respond_to?(method_name, true)
+  raise NoMethodError, "undefined method `#{method_name}' for #{object.inspect}"
+end
+
+# Call exception notification
+Delayed::Job.class_eval do
+  def error(_job, exception)
+    ExceptionNotifier.notify_exception(exception)
+  end
+end
