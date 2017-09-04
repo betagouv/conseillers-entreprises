ddc73ea1949fd669cc894cfd92f36863172436bd
Arnaud Doucerain <adoucerain@octo.com>
2017-09-04 18:17:20 +0200

    [pknoth-master] Test for ApiEntrepriseError

--- spec/controllers/api/facilities_controller_spec.rb
+++ spec/controllers/api/facilities_controller_spec.rb
@@ -8,21 +8,36 @@ RSpec.describe Api::FacilitiesController, type: :controller do
   describe 'POST #search_by_siret' do
     let(:siret) { '12345678901234' }
 
-    before do
-      company_json = JSON.parse(File.read(Rails.root.join('spec/fixtures/api_entreprise_get_entreprise.json')))
-      entreprises_instance = ApiEntreprise::EntrepriseWrapper.new(company_json)
-      allow(UseCases::SearchCompany).to receive(:with_siret).with(siret) { entreprises_instance }
+    context 'when facility is found' do
+      before do
+        company_json = JSON.parse(File.read(Rails.root.join('spec/fixtures/api_entreprise_get_entreprise.json')))
+        entreprises_instance = ApiEntreprise::EntrepriseWrapper.new(company_json)
+        allow(UseCases::SearchCompany).to receive(:with_siret).with(siret) { entreprises_instance }
 
-      facility_json = JSON.parse(File.read(Rails.root.join('spec/fixtures/api_entreprise_get_etablissement.json')))
-      facility_instance = ApiEntreprise::EtablissementWrapper.new(facility_json)
-      allow(UseCases::SearchFacility).to receive(:with_siret).with(siret) { facility_instance }
+        facility_json = JSON.parse(File.read(Rails.root.join('spec/fixtures/api_entreprise_get_etablissement.json')))
+        facility_instance = ApiEntreprise::EtablissementWrapper.new(facility_json)
+        allow(UseCases::SearchFacility).to receive(:with_siret).with(siret) { facility_instance }
+      end
+
+      it 'returns http success' do
+        post :search_by_siret, params: { siret: siret }, format: :js
+
+        expect(response).to have_http_status(:success)
+        expect(response.body).to eq({ company_name: 'Octo Technology', facility_location: '75008 Paris 8' }.to_json)
+      end
     end
 
-    it 'returns http success' do
-      post :search_by_siret, params: { siret: siret }, format: :js
+    context 'when facility is not found' do
+      before do
+        allow(UseCases::SearchFacility).to receive(:with_siret).with(siret).and_raise ApiEntreprise::ApiEntrepriseError
 
-      expect(response).to have_http_status(:success)
-      expect(response.body).to eq({ company_name: 'Octo Technology', facility_location: '75008 Paris 8' }.to_json)
+        post :search_by_siret, params: { siret: siret }, format: :js
+      end
+
+      it 'returns http unprocessable entity' do
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.body).to be_blank
+      end
     end
   end
 end
