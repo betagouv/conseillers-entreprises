4700a992a26abe87af42c3a9e215fc3ac88514af
Arnaud Doucerain <adoucerain@octo.com>
2017-09-05 16:16:46 +0200

    [102_firmapi] Removing old Visit and Search by name/siren files

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -15,10 +15,6 @@ class Visit < ApplicationRecord
   scope :of_siret, (->(siret) { joins(:facility).where(facilities: { siret: siret }) })
   scope :with_completed_diagnosis, (-> { joins(:diagnosis).merge(Diagnosis.completed) })
 
-  def to_s
-    I18n.t('visits.to_s', company_name: company_name)
-  end
-
   def happened_at_localized
     I18n.l happened_at if happened_at
   end
