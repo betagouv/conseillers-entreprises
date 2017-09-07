dce42d27af879fd506a78dfce3d1c38f43d96ac3
Arnaud Doucerain <adoucerain@octo.com>
2017-09-07 16:28:45 +0200

    [102_firmapi] Actions before mutations

--- app/javascript/spec/index/store/indexStoreSpec.js
+++ app/javascript/spec/index/store/indexStoreSpec.js
@@ -11,100 +11,6 @@ describe('IndexStore', () => {
 
     })
 
-    describe('mutations', () => {
-
-        const mutations = IndexStore.mutations
-
-        describe('REQUEST_IN_PROGRESS', function () {
-
-            it('updates the isRequestInProgress value', function () {
-                const state = {isRequestInProgress: undefined}
-                mutations.REQUEST_IN_PROGRESS(state, true)
-                expect(state.isRequestInProgress).toBeTruthy()
-            })
-        })
-
-        describe('FORM_ERROR_TYPE', function () {
-
-            let state
-
-            beforeEach(function () {
-                state = {
-                    formErrorType: '',
-                    companyData: {name: 'Café Bourgeois', location: 'somewhere', siret: 'siret number'}
-                }
-            })
-
-            it('updates the formErrorType value', function () {
-                mutations.FORM_ERROR_TYPE(state, 'ERROR')
-                expect(state.formErrorType).toEqual('ERROR')
-            })
-            it('clears the company values', function () {
-                mutations.FORM_ERROR_TYPE(state, 'ERROR')
-                expect(state.companyData).toEqual({})
-            })
-        })
-
-        describe('SIRET', function () {
-
-            it('updates the siret value', function () {
-                const state = {siret: ''}
-                mutations.SIRET(state, '48245813000010')
-                expect(state.siret).toEqual('48245813000010')
-            })
-        })
-
-        describe('NAME', function () {
-
-            it('updates the name value', function () {
-                const state = {name: ''}
-                mutations.NAME(state, 'Octo')
-                expect(state.name).toEqual('Octo')
-            })
-        })
-
-        describe('COUNTY', function () {
-
-            it('updates the county value', function () {
-                const state = {county: ''}
-                mutations.COUNTY(state, '59')
-                expect(state.county).toEqual('59')
-            })
-        })
-
-        describe('COMPANIES', function () {
-
-            it('updates the companies array', function () {
-                const state = {companies: []}
-                mutations.COMPANIES(state, ['123'])
-                expect(state.companies).toEqual(['123'])
-            })
-        })
-
-        describe('COMPANY_DATA', function () {
-
-            let state
-
-            beforeEach(function () {
-                state = {
-                    formErrorType: 'ERROR',
-                    companyData: {},
-                }
-            })
-
-            it('updates the companyData value', function () {
-                mutations.COMPANY_DATA(state, {name: 'Café Bourgeois', location: 'somewhere', siret: 'siret number'})
-                expect(state.companyData.name).toEqual('Café Bourgeois')
-                expect(state.companyData.location).toEqual('somewhere')
-                expect(state.companyData.siret).toEqual('siret number')
-            })
-            it('clears the error value', function () {
-                mutations.COMPANY_DATA(state, {name: 'Café Bourgeois', location: 'somewhere'})
-                expect(state.formErrorType).toEqual('')
-            })
-        })
-    })
-
     describe('actions', () => {
 
         const actions = IndexStore.actions
@@ -237,6 +143,100 @@ describe('IndexStore', () => {
                     'REQUEST_IN_PROGRESS',
                     false
                 ])
+            })
+        })
+    })
+
+    describe('mutations', () => {
+
+        const mutations = IndexStore.mutations
+
+        describe('REQUEST_IN_PROGRESS', function () {
+
+            it('updates the isRequestInProgress value', function () {
+                const state = {isRequestInProgress: undefined}
+                mutations.REQUEST_IN_PROGRESS(state, true)
+                expect(state.isRequestInProgress).toBeTruthy()
+            })
+        })
+
+        describe('FORM_ERROR_TYPE', function () {
+
+            let state
+
+            beforeEach(function () {
+                state = {
+                    formErrorType: '',
+                    companyData: {name: 'Café Bourgeois', location: 'somewhere', siret: 'siret number'}
+                }
+            })
+
+            it('updates the formErrorType value', function () {
+                mutations.FORM_ERROR_TYPE(state, 'ERROR')
+                expect(state.formErrorType).toEqual('ERROR')
+            })
+            it('clears the company values', function () {
+                mutations.FORM_ERROR_TYPE(state, 'ERROR')
+                expect(state.companyData).toEqual({})
+            })
+        })
+
+        describe('SIRET', function () {
+
+            it('updates the siret value', function () {
+                const state = {siret: ''}
+                mutations.SIRET(state, '48245813000010')
+                expect(state.siret).toEqual('48245813000010')
+            })
+        })
+
+        describe('NAME', function () {
+
+            it('updates the name value', function () {
+                const state = {name: ''}
+                mutations.NAME(state, 'Octo')
+                expect(state.name).toEqual('Octo')
+            })
+        })
+
+        describe('COUNTY', function () {
+
+            it('updates the county value', function () {
+                const state = {county: ''}
+                mutations.COUNTY(state, '59')
+                expect(state.county).toEqual('59')
+            })
+        })
+
+        describe('COMPANIES', function () {
+
+            it('updates the companies array', function () {
+                const state = {companies: []}
+                mutations.COMPANIES(state, ['123'])
+                expect(state.companies).toEqual(['123'])
+            })
+        })
+
+        describe('COMPANY_DATA', function () {
+
+            let state
+
+            beforeEach(function () {
+                state = {
+                    formErrorType: 'ERROR',
+                    companyData: {},
+                }
+            })
+
+            it('updates the companyData value', function () {
+                mutations.COMPANY_DATA(state, {name: 'Café Bourgeois', location: 'somewhere', siret: 'siret number'})
+                expect(state.companyData.name).toEqual('Café Bourgeois')
+                expect(state.companyData.location).toEqual('somewhere')
+                expect(state.companyData.siret).toEqual('siret number')
+            })
+            it('clears the error value', function () {
+                mutations.COMPANY_DATA(state, {name: 'Café Bourgeois', location: 'somewhere'})
+                expect(state.formErrorType).toEqual('')
             })
         })
     })
