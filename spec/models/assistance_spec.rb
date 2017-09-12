60975155d73f5daf5c05e3ff728b7796720e3eb7
Arnaud Doucerain <adoucerain@octo.com>
2017-09-12 12:33:30 +0200

    Adding missing tests

--- spec/models/assistance_spec.rb
+++ spec/models/assistance_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Assistance, type: :model do
     it do
       is_expected.to belong_to :question
       is_expected.to belong_to :institution
-      is_expected.to have_many :assistances_experts
+      is_expected.to have_many(:assistances_experts).dependent(:destroy)
       is_expected.to have_many :experts
       is_expected.to validate_presence_of :title
       is_expected.to validate_presence_of :question
--- spec/models/expert_spec.rb
+++ spec/models/expert_spec.rb
@@ -5,9 +5,13 @@ require 'rails_helper'
 RSpec.describe Expert, type: :model do
   describe 'validations' do
     it do
+      is_expected.to belong_to :institution
+      is_expected.to have_many(:assistances_experts).dependent(:destroy)
+      is_expected.to have_many :assistances
       is_expected.to validate_presence_of(:last_name)
       is_expected.to validate_presence_of(:role)
       is_expected.to validate_presence_of(:institution)
+      is_expected.to validate_presence_of(:email)
     end
   end
 
