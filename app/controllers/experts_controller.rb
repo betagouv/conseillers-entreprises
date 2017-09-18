e623c3c14a3c7b657686df918323ea1ec3a6a67c
Antoine Boileau <apem.boileau@gmail.com>
2017-09-18 15:36:30 +0200

    [139_expert_page] Created a person partial

--- app/controllers/experts_controller.rb
+++ app/controllers/experts_controller.rb
@@ -4,7 +4,7 @@ class ExpertsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def diagnosis
-    associations = [visit: [:visitee, facility: [:company]], diagnosed_needs: [:selected_assistance_experts]]
+    associations = [visit: [:visitee, :advisor, facility: [:company]], diagnosed_needs: [:selected_assistance_experts]]
     @diagnosis = Diagnosis.includes(associations).find params[:diagnosis_id]
   end
 end
