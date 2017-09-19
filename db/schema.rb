db68ef707104796f899bae1e2d055e84d25b542c
Arnaud Doucerain <adoucerain@octo.com>
2017-09-19 14:03:23 +0200

    [139_expert_page] oops

--- db/schema.rb
+++ db/schema.rb
@@ -181,7 +181,7 @@ ActiveRecord::Schema.define(version: 20170919085828) do
   create_table "users", id: :serial, force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_ee"
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
