ffcf043d83a92db0516574a218bfe7a9e27e450b
Arnaud Doucerain <adoucerain@octo.com>
2017-09-19 13:02:18 +0200

    [139_expert_page] Rubocop fix

--- spec/routing/routing_spec.rb
+++ spec/routing/routing_spec.rb
@@ -4,6 +4,6 @@ require 'rails_helper'
 
 RSpec.describe 'some routes of application', type: :routing do
   it do
-    expect(get '/experts/diagnoses/32').to route_to(controller: 'experts', action: 'diagnosis', diagnosis_id: '32')
+    expect(get('/experts/diagnoses/32')).to route_to(controller: 'experts', action: 'diagnosis', diagnosis_id: '32')
   end
 end
