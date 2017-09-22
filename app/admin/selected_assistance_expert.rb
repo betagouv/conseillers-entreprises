f82362dc7c9aba40ea5f5168cfad29da73bc4780
Arnaud Doucerain <adoucerain@octo.com>
2017-09-22 10:59:48 +0200

    Admin - Improve SelectedAssistanceExpert index page

--- app/admin/selected_assistance_expert.rb
+++ app/admin/selected_assistance_expert.rb
@@ -3,19 +3,19 @@
 ActiveAdmin.register SelectedAssistanceExpert do
   menu parent: :diagnoses, priority: 2
   actions :index, :show
-  includes :diagnosed_need
+  includes diagnosed_need: [diagnosis: [visit: :advisor]]
 
   index do
     selectable_column
     id_column
+    column('Date de contact', :created_at)
     column :diagnosed_need
-    column :created_at
-    column :updated_at
+    column('Conseiller') { |sae| sae.diagnosed_need.diagnosis.visit.advisor.full_name }
     column :expert_full_name
     column :expert_institution_name
     column :assistance_title
     column :expert_viewed_page_at
-    column :status
+    column(:status) { |sae| t("activerecord.attributes.selected_assistance_expert.statuses.#{sae.status}") }
     actions
   end
 end
