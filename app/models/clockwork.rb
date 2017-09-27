66d729637018bf740e239414a0feff3d6c27cf95
Arnaud Doucerain <adoucerain@octo.com>
2017-09-27 19:25:38 +0200

    [141_expert_reminder] Using Clockwork to send Expert reminders each week

--- app/models/clockwork.rb
+++ app/models/clockwork.rb
@@ -5,7 +5,6 @@ require './config/boot'
 require './config/environment'
 
 module Clockwork
-  handler { |_job, _time| AdminMailersService.send_statistics_email }
-
-  every(1.week, 'send_statistics_email', at: 'Monday 09:30')
+  every(1.week, 'send_statistics_email', at: 'Monday 09:30') { AdminMailersService.delay.send_statistics_email }
+  every(1.week, 'send_experts_reminders', at: 'Monday 09:30') { ExpertReminderService.delay.send_reminders }
 end
