e06f648ab3092ade1189e99c1a9220a355a01086
Arnaud Doucerain <adoucerain@octo.com>
2017-09-27 16:59:36 +0200

    [141_expert_reminder] Reminder weekly email to Expert

--- app/mailers/expert_mailer.rb
+++ app/mailers/expert_mailer.rb
@@ -5,13 +5,23 @@ class ExpertMailer < ApplicationMailer
   default from: SENDER, template_path: 'mailers/expert_mailer'
 
   def notify_company_needs(expert, params)
-    @params = params
     @access_token = expert.access_token
+    @params = params
 
     mail(
       to: "#{expert.full_name} <#{expert.email}>",
       cc: "#{params[:advisor].full_name} <#{params[:advisor].email}>",
       subject: t('mailers.expert_mailer.notify_company_needs.subject', company_name: params[:company_name])
+    )
+  end
+
+  def remind_involvement(expert, selected_assistances_experts_hash)
+    @access_token = expert.access_token
+    @selected_assistances_experts_hash = selected_assistances_experts_hash
+
+    mail(
+      to: "#{expert.full_name} <#{expert.email}>",
+      subject: t('mailers.expert_mailer.remind_involvement.subject')
     )
   end
 end
