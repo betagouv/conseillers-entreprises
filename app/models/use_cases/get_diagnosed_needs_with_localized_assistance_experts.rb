91140a0e79ac354fc1c913b6d780c48183fa9adf
Arnaud Doucerain <adoucerain@octo.com>
2017-10-03 14:57:02 +0200

    [cleaner_step4_usecase] Working usecase! :)

--- app/models/use_cases/get_diagnosed_needs_with_localized_assistance_experts.rb
+++ app/models/use_cases/get_diagnosed_needs_with_localized_assistance_experts.rb
@@ -4,21 +4,14 @@ module UseCases
   class GetDiagnosedNeedsWithLocalizedAssistanceExperts
     class << self
       def of_diagnosis(diagnosis)
-        territories = Territory.joins(:territory_cities)
-                        .where(territory_cities: { city_code: diagnosis.visit.location })
         associations = [question: [assistances: [assistances_experts: [expert: :institution]]]]
-        query = { question:
-                    { assistances_experts:
-                        { expert:
-                            { expert_territories: { territory_id: territories.map(&:id) } }
-                        }
-                    }
-        }
-
-          DiagnosedNeed.of_diagnosis(diagnosis)
-            .includes(associations)
-            .where(query)
-            .references(associations)
+        diagnosed_needs = DiagnosedNeed.of_diagnosis(diagnosis).joins(associations).includes(associations)
+        localized_assistances_experts = AssistanceExpert.of_city_code(diagnosis.visit.location).of_diagnosis(diagnosis)
+        diagnosed_needs.each do |diagnosed_need|
+          diagnosed_need.question.assistances.each do |assistance|
+            assistance.assistances_experts &= localized_assistances_experts
+          end
+        end
       end
     end
   end
--- spec/models/use_cases/get_diagnosed_needs_with_localized_assistance_experts_spec.rb
+++ spec/models/use_cases/get_diagnosed_needs_with_localized_assistance_experts_spec.rb
@@ -4,12 +4,11 @@ require 'rails_helper'
 
 describe UseCases::GetDiagnosedNeedsWithLocalizedAssistanceExperts do
   describe 'of_diagnosis' do
-    subject { described_class.of_diagnosis(diagnosis) }
+    subject(:diagnosed_needs) { described_class.of_diagnosis(diagnosis) }
 
-    let(:facility) { create :facility, city_code: 75_001}
+    let(:facility) { create :facility, city_code: 75_001 }
     let(:visit) { create :visit, facility: facility }
     let!(:territory) { create :territory }
-    let!(:territory_city) { create :territory_city, territory: territory, city_code: 75_001 }
 
     let!(:diagnosis) { create :diagnosis, visit: visit }
     let!(:question1) { create :question }
@@ -27,20 +26,18 @@ describe UseCases::GetDiagnosedNeedsWithLocalizedAssistanceExperts do
     before do
       create :diagnosed_need
       create :assistance_expert, assistance: assistance2
-      # allow(AssistanceExpert).to receive(:of_city_code).and_return(assistance_expert1)
+      create :territory_city, territory: territory, city_code: 75_001
     end
 
     it 'gets the right diagnosed needs' do
-      expect(subject).to contain_exactly(diagnosed_need1, diagnosed_need2)
+      expect(diagnosed_needs).to contain_exactly(diagnosed_need1, diagnosed_need2)
     end
 
     it 'includes the rightly localized assistance experts' do
-      returned_assistance_experts = described_class.of_diagnosis(diagnosis)
-                                                   .map(&:question)
+      returned_assistance_experts = diagnosed_needs.map(&:question)
                                                    .map(&:assistances).flatten
                                                    .map(&:assistances_experts).flatten
       expect(returned_assistance_experts).to contain_exactly(assistance_expert1)
-      # expect(returned_assistance_experts).to eq(expected_assistance_experts)
     end
   end
 end
