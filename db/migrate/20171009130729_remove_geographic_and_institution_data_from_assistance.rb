356754d4571f6641b0c712f94a0e0de32fbe07bf
Antoine Boileau <apem.boileau@gmail.com>
2017-10-09 15:12:18 +0200

    [180_clean_and_change_wording] Remove county, geographic_scope and institution reference from Assistance db object

---
+++ db/migrate/20171009130729_remove_geographic_and_institution_data_from_assistance.rb
@@ -0,0 +1,7 @@
+class RemoveGeographicAndInstitutionDataFromAssistance < ActiveRecord::Migration[5.1]
+  def change
+    remove_column :assistances, :county, :integer
+    remove_column :assistances, :geographic_scope, :integer
+    remove_reference :assistances, :institution, foreign_key: true
+  end
+end
