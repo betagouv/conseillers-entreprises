ba1b7bfceafdfb60a630d0425891834a4c91aa40
Arnaud Doucerain <adoucerain@octo.com>
2017-10-26 18:24:02 +0200

    Admin TerritoryCity fix

--- app/admin/territory_city.rb
+++ app/admin/territory_city.rb
@@ -2,6 +2,6 @@
 
 ActiveAdmin.register TerritoryCity do
   menu parent: :territories, priority: 2
-  permit_params :territory, :city_code
+  permit_params :territory_id, :city_code
   includes :territory
 end
