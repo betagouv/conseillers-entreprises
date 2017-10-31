1e8d48fb1f437e77f62cbb56a82bb18197730eff
Arnaud Doucerain <adoucerain@octo.com>
2017-10-31 18:57:07 +0100

    Admin - Display deleted diagnoses

--- app/admin/diagnosis.rb
+++ app/admin/diagnosis.rb
@@ -5,9 +5,16 @@ ActiveAdmin.register Diagnosis do
   actions :index, :show
   includes visit: [facility: :company]
 
+  controller do
+    def scoped_collection
+      Diagnosis.unscoped
+    end
+  end
+
   filter :visit, collection: -> { Visit.includes(facility: :company).joins(facility: :company).order('companies.name') }
   filter :content
   filter :created_at
   filter :updated_at
+  filter :deleted_at
   filter :step
 end
