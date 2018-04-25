f1d43d9d38473a8b8b519ec9f487c2ffed0a41ba
gregoirenovel <log@kinko.fr>
2018-04-25 15:44:58 +0200

    Do one thing per line

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -12,8 +12,15 @@ class CompaniesController < ApplicationController
 
   def create_diagnosis_from_siret
     facility = UseCases::SearchFacility.with_siret_and_save params[:siret]
-    visit = Visit.create advisor: current_user, facility: facility if facility
-    diagnosis = Diagnosis.new visit: visit, step: '2' if visit
+
+    if facility
+      visit = Visit.create advisor: current_user, facility: facility
+    end
+
+    if visit
+      diagnosis = Diagnosis.new visit: visit, step: '2'
+    end
+
     if facility && visit && diagnosis.save
       redirect_to step_2_diagnosis_path(diagnosis)
     else
