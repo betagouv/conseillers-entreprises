f65ef92886b6914102fa14e49c1f882e92398d3d
gregoirenovel <log@kinko.fr>
2018-04-25 15:29:50 +0200

    Simplify code

--- app/validators/selected_assistance_expert_validator.rb
+++ app/validators/selected_assistance_expert_validator.rb
@@ -1,6 +1,7 @@
 class SelectedAssistanceExpertValidator < ActiveModel::Validator
   def validate(selected_assistance_expert)
-    return nil unless selected_assistance_expert.assistance_expert && selected_assistance_expert.territory_user
-    selected_assistance_expert.errors.add(:assistance_expert, :can_not_be_set_with_territory_user)
+    if selected_assistance_expert.assistance_expert && selected_assistance_expert.territory_user
+      selected_assistance_expert.errors.add(:assistance_expert, :can_not_be_set_with_territory_user)
+    end
   end
 end
