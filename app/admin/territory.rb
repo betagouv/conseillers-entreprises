ad57f49fc99ea5e4268eba49bff2c8bcd53554e6
Nicolas Bouilleaud <nico@bou.io>
2018-05-23 16:16:20 +0200

    Display the full list of contacted_experts in the territory admin page

--- app/admin/territory.rb
+++ app/admin/territory.rb
@@ -34,6 +34,32 @@ ActiveAdmin.register Territory do
         column :institution
       end
     end
+
+    panel I18n.t('active_admin.territories.contacted_experts') do
+      table_for territory.selected_assistance_experts
+                    .includes(diagnosed_need: [diagnosis: [visit: [facility: :company]]])
+                    .includes(:expert)
+                    .order(created_at: :desc) do
+        column(:id) do |selected_expert|
+          link_to(selected_expert.id, admin_selected_assistance_expert_path(selected_expert))
+        end
+        column :created_at
+        column(I18n.t('activerecord.attributes.visit.facility')) do |selected_expert|
+          selected_expert.diagnosed_need&.diagnosis&.visit&.facility
+        end
+        column(:diagnosed_need) do |selected_expert|
+          need = selected_expert.diagnosed_need
+          link_to(need.question_label, admin_diagnosed_need_path(need))
+        end
+        column :expert_full_name do |selected_expert|
+          link_to("#{selected_expert.expert_full_name} (#{selected_expert.expert_institution_name})", admin_expert_path(selected_expert.expert))
+        end
+        column :status do |selected_expert|
+          I18n.t("activerecord.attributes.selected_assistance_expert.statuses.#{selected_expert.status}")
+        end
+      end
+    end
+
   end
 
   form do |f|
--- app/models/territory.rb
+++ app/models/territory.rb
@@ -4,6 +4,7 @@ class Territory < ApplicationRecord
   has_many :territory_cities, dependent: :destroy
   has_many :expert_territories
   has_many :experts, through: :expert_territories
+  has_many :selected_assistance_experts, through: :experts
   has_many :territory_users
   has_many :users, through: :territory_users
 
