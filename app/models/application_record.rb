84958cf5ec2cb8a7838b3e7938b8032707d500d9
Nicolas Bouilleaud <nico@bou.io>
2018-05-24 16:23:27 +0200

    Move the “created_*” scopes and move them to a mixin

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,9 +2,6 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  scope :created_last_week, (-> { where("#{table_name}.created_at >= ?", 1.week.ago) })
-  scope :created_before_last_week, (-> { where("#{table_name}.created_at < ?", 1.week.ago) })
-  scope :updated_last_week, (-> { where("#{table_name}.updated_at >= ?", 1.week.ago) })
-  scope :updated_yesterday, (-> { where("#{table_name}.updated_at >= ?", 1.day.ago) })
+  include CreatedWithin
+  
 end
---
+++ app/models/concerns/created_within.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module CreatedWithin
+  extend ActiveSupport::Concern
+
+  included do
+
+    scope :created_last_week, (-> { where("#{table_name}.created_at >= ?", 1.week.ago) })
+    scope :created_before_last_week, (-> { where("#{table_name}.created_at < ?", 1.week.ago) })
+    scope :updated_last_week, (-> { where("#{table_name}.updated_at >= ?", 1.week.ago) })
+    scope :updated_yesterday, (-> { where("#{table_name}.updated_at >= ?", 1.day.ago) })
+
+  end
+end
