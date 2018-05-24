163bb65a7b12c3434777a600d61c553bef124f74
Nicolas Bouilleaud <nico@bou.io>
2018-05-24 16:23:27 +0200

    Get rid of the “useful links” in the dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,11 +4,6 @@ ActiveAdmin.register_page 'Dashboard' do
   menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
   content title: proc { I18n.t('active_admin.dashboard') } do
-    panel I18n.t('active_admin.dashboard_welcome.useful_links') do
-      span link_to 'Trello', 'https://trello.com/b/TdTq4e5P/web', class: 'button'
-      span link_to 'Mailtrap', 'https://mailtrap.io', class: 'button'
-    end
-
     columns do
       column do
         panel t('active_admin.dashboard_welcome.invite_users') do
