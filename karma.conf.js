2c73041d9f47ea673ae01fd98fcb926fa2767db8
Nicolas Bouilleaud <nico@bou.io>
2018-05-28 14:07:38 +0200

    remove karma-nyan-reporter

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['nyan'],
+    reporters: ['dots'],
 
     // web server port
     port: 9876,
