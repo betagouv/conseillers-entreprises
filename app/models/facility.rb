b2debd5b3adae9f9b0fd9688826d64dda64f7b24
Nicolas Bouilleaud <nico@bou.io>
2018-06-13 13:45:58 +0200

    Fallback to city_code in Facility.to_s

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -10,6 +10,6 @@ class Facility < ApplicationRecord
   scope :in_territory, (->(territory) { where(city_code: territory.city_codes) })
 
   def to_s
-    "#{company.name_short} (#{readable_locality})"
+    "#{company.name_short} (#{readable_locality || city_code})"
   end
 end
