c3327ecc8d3e5aefad657189f525bb9192e78ff4
Nicolas Bouilleaud <nico@bou.io>
2018-07-02 17:45:27 +0200

    Tweak and refactor the ActiveAdmin page for Expert

--- app/admin/expert.rb
+++ app/admin/expert.rb
@@ -16,100 +16,112 @@ ActiveAdmin.register Expert do
     assistances_experts_attributes: %i[id assistance_id _create _update _destroy]
   ]
 
+  # Index
+  #
   index do
     selectable_column
     id_column
-    column :first_name
+    column :full_name
     column :last_name
+    column :institution
+    column(:users) { |expert| expert.users.length }
     column :role
     column :email
-    column :institution
-    column t('active_admin.experts.assistances_count'), (proc { |expert| expert.assistances.length })
-    column :users, (proc { |expert| expert.users.length })
-    column(:territories) do |expert|
-      safe_join(expert.territories.map do |territory|
-        link_to territory.name, admin_territory_path(territory)
-      end, ', '.html_safe)
-    end
+    column(:assistances) { |expert| expert.assistances.length }
+    column(:territories) { |expert| expert.territories.length }
     actions
   end
 
+  filter :territories, as: :ajax_select, data: { url: :admin_territories_path, search_fields: [:name] }
+  filter :institution, as: :ajax_select, data: { url: :admin_institutions_path, search_fields: [:name] }
+  filter :assistances, as: :ajax_select, data: { url: :admin_assistances_path, search_fields: [:title, :description] }
+  filter :first_name
+  filter :last_name
+  filter :email
+  filter :phone_number
+  filter :role
+
+  ## Show
+  #
   show do
     attributes_table do
-      row :first_name
-      row :last_name
-      row :role
+      row :full_name
       row :institution
+      row :role
       row :email
       row :phone_number
-      row :access_token
-      row(:users) do |expert|
-        safe_join(expert.users.map do |user|
-          link_to user.full_name, admin_user_path(user)
-        end, ', '.html_safe)
-      end
+    end
+    attributes_table do
       row(:territories) do |expert|
         safe_join(expert.territories.map do |territory|
           link_to territory.name, admin_territory_path(territory)
         end, ', '.html_safe)
       end
     end
-
-    panel I18n.t('active_admin.experts.assistances') do
+    panel I18n.t('activerecord.attributes.expert.assistances') do
       table_for expert.assistances do
-        column :title, (proc { |assistance| link_to(assistance.title, admin_assistance_path(assistance)) })
+        column(:title) { |assistance| link_to(assistance.title, admin_assistance_path(assistance)) }
         column :question
       end
     end
   end
 
+  sidebar I18n.t('activerecord.models.user.other'), only: :show do
+    attributes_table do
+      row(:users) do |expert|
+        safe_join(expert.users.map do |user|
+          link_to user.full_name, admin_user_path(user)
+        end, ', '.html_safe)
+      end
+    end
+  end
+
+  sidebar I18n.t('active_admin.experts.access'), only: :show do
+    attributes_table do
+      row :access_token
+    end
+  end
+
+  ## Form
+  #
   form do |f|
     f.inputs do
       f.input :first_name
       f.input :last_name
+      f.input :institution, as: :ajax_select, data: {
+        url: :admin_institutions_path,
+        search_fields: [:name],
+        limit: 999,
+      }
       f.input :role
-      f.input :institution, collection: Institution.ordered_by_name
       f.input :email
       f.input :phone_number
     end
 
-    f.inputs t('activerecord.models.user.other') do
-      f.input :users, as: :ajax_select, data: {
-        url: :filter_admin_users_path,
-        search_fields: [:first_name, :last_name, :role, :email],
+    f.inputs t('activerecord.attributes.expert.users') do
+      f.input :users, label: t('activerecord.models.user.other'), as: :ajax_select, data: {
+        url: :admin_users_path,
+        search_fields: [:full_name],
         limit: 999,
       }
     end
 
-    f.inputs I18n.t('active_admin.experts.territories') do
-      f.has_many :expert_territories,
-        heading: false,
-        new_record: I18n.t('active_admin.experts.add_territory'),
-        allow_destroy: true do |expert_territory|
-        expert_territory.input :territory, label: I18n.t('active_admin.experts.territory')
-      end
+    f.inputs t('activerecord.attributes.expert.territories') do
+      f.input :territories, as: :ajax_select, data: {
+        url: :admin_territories_path,
+        search_fields: [:name],
+        limit: 999,
+      }
     end
 
-    f.inputs I18n.t('active_admin.experts.assistances') do
-      f.has_many :assistances_experts,
-        heading: false,
-        new_record: I18n.t('active_admin.experts.add_assistance'),
-        allow_destroy: true do |assistance_expert|
-        assistance_expert.input :assistance, label: I18n.t('active_admin.experts.assistance')
-      end
+    f.inputs t('activerecord.attributes.expert.assistances') do
+      f.input :assistances, as: :ajax_select, data: {
+        url: :admin_assistances_path,
+        search_fields: [:title, :description],
+        limit: 999,
+      }
     end
 
     f.actions
   end
-
-  filter :territories, collection: -> { Territory.order(:name) }
-  filter :institution, collection: -> { Institution.ordered_by_name }
-  filter :assistances, collection: -> { Assistance.order(:title).map { |a| ["#{a.title} (#{a.id})", a.id] } }
-  filter :first_name
-  filter :last_name
-  filter :email
-  filter :phone_number
-  filter :role
-  filter :created_at
-  filter :updated_at
 end
