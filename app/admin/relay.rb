dc13bf7d9c6ce9d82ad04254c49c9619a900a08b
Nicolas Bouilleaud <nico@bou.io>
2018-07-10 15:32:02 +0200

    Display the matches in admin/expert and admin/relay
    (copy-pasted from territory)

--- app/admin/relay.rb
+++ app/admin/relay.rb
@@ -22,4 +22,45 @@ ActiveAdmin.register Relay do
     actions
   end
 
+  ## Show
+  #
+  show do
+    default_main_content
+
+    panel I18n.t('active_admin.territories.contacted_experts') do
+      table_for Match.of_relay(relay)
+                  .includes(diagnosed_need: [diagnosis: [visit: [facility: :company]]])
+                  .includes(diagnosed_need: [diagnosis: [visit: :advisor]])
+                  .includes(:expert)
+                  .order(created_at: :desc) do
+        column(:id) do |match|
+          link_to(match.id, admin_match_path(match))
+        end
+        column :created_at
+        column(I18n.t('activerecord.attributes.visit.advisor')) do |match|
+          advisor = match.diagnosed_need.diagnosis.visit.advisor
+          link_to(advisor.full_name_with_role, admin_user_path(advisor))
+        end
+        column(I18n.t('activerecord.attributes.visit.facility')) do |match|
+          match.diagnosed_need&.diagnosis&.visit&.facility
+        end
+        column(:diagnosed_need) do |match|
+          need = match.diagnosed_need
+          link_to(need.question_label, admin_diagnosed_need_path(need))
+        end
+        column :expert_full_name do |match|
+          expert = match.expert
+          if expert.present?
+            link_to(match.expert_description, admin_expert_path(expert))
+          else
+            I18n.t('active_admin.matches.deleted', expert: match.expert_description)
+          end
+        end
+        column :status do |match|
+          I18n.t("activerecord.attributes.match.statuses.#{match.status}")
+        end
+      end
+    end
+
+  end
 end
