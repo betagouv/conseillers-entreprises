ff81900089b11f0301cc9b6b2baac593a3ece7f1
Nicolas Bouilleaud <nico@bou.io>
2018-09-11 13:48:36 +0200

    Improve ActiveAdmin operations for Visit/Diagnosis
    Allow editing and deleting visits and diagnoses from ActiveAdmin, properly cascade deletions, and display diagnosed needs from the Diagnosis details view.

--- app/admin/diagnosis.rb
+++ app/admin/diagnosis.rb
@@ -2,8 +2,7 @@
 
 ActiveAdmin.register Diagnosis do
   menu priority: 7
-  actions :index, :show
-  includes visit: [facility: :company]
+  includes visit: [:advisor, facility: :company]
 
   filter :visit, collection: -> { Visit.includes(facility: :company).joins(facility: :company).order('companies.name') }
   filter :content
@@ -11,4 +10,24 @@ ActiveAdmin.register Diagnosis do
   filter :updated_at
   filter :archived_at
   filter :step
+
+  show do
+    attributes_table do
+      row :visit
+      row :created_at
+      row :updated_at
+      row :archived_at
+      row(:advisor) { |d| d.visit.advisor }
+      row :step
+      row :description
+    end
+    panel I18n.t('activerecord.models.diagnosed_need.other') do
+      table_for diagnosis.diagnosed_needs do
+        column(:id) { |n| link_to(n.id, admin_diagnosed_need_path(n)) }
+        column(:category) { |n| n.question&.category&.label }
+        column :question_label
+        column(:content) { |n| link_to(n.content, admin_diagnosed_need_path(n)) }
+      end
+    end
+  end
 end
