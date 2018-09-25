311688e6c178ff06e0e310b71aface8f40759ff1
Nicolas Bouilleaud <nico@bou.io>
2018-09-25 15:26:02 +0200

    Add Feedback access checking

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,3 +1,7 @@
 class Feedback < ApplicationRecord
   belongs_to :match
+
+  def can_be_viewed_by?(role)
+    self.match.can_be_viewed_by?(role)
+  end
 end
--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -4,4 +4,27 @@ RSpec.describe Feedback, type: :model do
   it do
     is_expected.to belong_to :match
   end
+
+  describe 'can_be_viewed_by?' do
+    subject { feedback.can_be_viewed_by?(expert) }
+
+    let(:feedback) { create :feedback }
+    let(:expert) { create :expert }
+
+    context 'expert is the expert of the match' do
+      before do
+        feedback.match.expert = expert
+      end
+
+      it { is_expected.to eq true }
+    end
+
+    context 'expert is unrelated' do
+      before do
+        feedback.match.expert = create :expert
+      end
+
+      it { is_expected.to eq false }
+    end
+  end
 end
