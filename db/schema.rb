cd721b7ca6121f94bf5edee55137f36e0c92c848
Nicolas Bouilleaud <nico@bou.io>
2018-10-11 16:05:21 +0200

    Migration: remove Institutions email and phone_number

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_25_131649) do
+ActiveRecord::Schema.define(version: 2018_10_11_135725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -169,8 +169,6 @@ ActiveRecord::Schema.define(version: 2018_09_25_131649) do
 
   create_table "institutions", force: :cascade do |t|
     t.string "name"
-    t.string "email"
-    t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "qualified_for_commerce", default: true, null: false
