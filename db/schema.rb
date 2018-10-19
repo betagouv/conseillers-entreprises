7f8ed08c2c170e68174f6d4cd5613a04c4e4b166
Nicolas Bouilleaud <nico@bou.io>
2018-10-19 11:50:07 +0200

    Migration: Add index to experts.email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_25_131649) do
+ActiveRecord::Schema.define(version: 2018_10_19_094404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -138,6 +138,7 @@ ActiveRecord::Schema.define(version: 2018_09_25_131649) do
     t.string "access_token"
     t.string "full_name"
     t.index ["access_token"], name: "index_experts_on_access_token"
+    t.index ["email"], name: "index_experts_on_email"
     t.index ["institution_id"], name: "index_experts_on_institution_id"
   end
 
