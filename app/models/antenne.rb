38b7d3641c858148c85df9ec015388ee18b5fd4f
Nicolas Bouilleaud <nico@bou.io>
2018-10-31 09:33:14 +0100

    Add Antenne.sent_matches and Antenne.received_matches

--- app/models/antenne.rb
+++ app/models/antenne.rb
@@ -1,7 +1,21 @@
 class Antenne < ApplicationRecord
+  ## Relations and Validations
+  #
   belongs_to :institution
   has_and_belongs_to_many :communes, join_table: :intervention_zones
+  has_many :experts
+  has_many :users
 
   validates :name, presence: true, uniqueness: true
   validates :institution, presence: true
+
+  ## Matches, Sent and Received
+  #
+  def sent_matches
+    Match.sent_by(users)
+  end
+
+  def received_matches
+    Match.of_relay_or_expert(experts)
+  end
 end
