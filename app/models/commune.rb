20babba26f2931022989a3437774030644503da4
Nicolas Bouilleaud <nico@bou.io>
2018-11-08 18:35:38 +0100

    Reorder commune.rb

--- app/models/commune.rb
+++ app/models/commune.rb
@@ -1,8 +1,10 @@
 class Commune < ApplicationRecord
+  ## Constants
+  #
   INSEE_CODE_FORMAT = /\A[0-9AB]{5}\z/
 
-  validates :insee_code, presence: true, uniqueness: true, format: { with: INSEE_CODE_FORMAT }
-
+  ## Relations and Validations
+  #
   has_and_belongs_to_many :territories
   has_many :facilities
   has_and_belongs_to_many :antennes
@@ -12,11 +14,18 @@ class Commune < ApplicationRecord
   has_many :antenne_experts, through: :antennes, source: :experts
   has_many :relays, through: :territories
 
+  validates :insee_code, presence: true, uniqueness: true, format: { with: INSEE_CODE_FORMAT }
+
+  ##
+  #
   def all_experts
     # Direct or through Antennes; returns an ActiveRecord Relation rather than an array.
     Expert.where(id: direct_experts)
       .or(Expert.where(id: antenne_experts).where(id: Expert.without_custom_zone)) # Experts of the antennes on this Commune, who don’t override their Antenne zone.
   end
+
+  ##
+  #
   def to_s
     insee_code
   end
