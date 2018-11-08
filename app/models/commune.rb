9299ae76d856617c155b0a23f4463f795587a3aa
Nicolas Bouilleaud <nico@bou.io>
2018-11-08 18:35:38 +0100

    Add Commune.all_experts

--- app/models/commune.rb
+++ app/models/commune.rb
@@ -9,8 +9,14 @@ class Commune < ApplicationRecord
 
   has_many :experts, through: :territories # this is the current state of data and will be `through: :antennes`
   has_and_belongs_to_many :direct_experts, class_name: 'Expert'
+  has_many :antenne_experts, through: :antennes, source: :experts
   has_many :relays, through: :territories
 
+  def all_experts
+    # Direct or through Antennes; returns an ActiveRecord Relation rather than an array.
+    Expert.where(id: direct_experts)
+      .or(Expert.where(id: antenne_experts).where(id: Expert.without_custom_zone)) # Experts of the antennes on this Commune, who don’t override their Antenne zone.
+  end
   def to_s
     insee_code
   end
