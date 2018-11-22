1b7184dd6ac27eaf5fb79b88d9f22676ef04b318
Nicolas Bouilleaud <nico@bou.io>
2018-11-22 16:27:19 +0100

    Cleanup Assistance/Questions/Categories models

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,14 +1,28 @@
 # frozen_string_literal: true
 
 class Question < ApplicationRecord
-  has_many :assistances, dependent: :nullify
-  has_many :diagnosed_needs, dependent: :nullify
-  belongs_to :category
+  ## Associations
+  #
+  belongs_to :category, inverse_of: :questions
 
+  has_many :assistances, dependent: :nullify, inverse_of: :question
+  has_many :diagnosed_needs, dependent: :nullify, inverse_of: :question
+
+  ## Validations
+  #
   validates :category, presence: true
 
+  ## Through Associations
+  #
+  # :diagnosed_needs
+  has_many :diagnoses, through: :diagnosed_needs, inverse_of: :questions
+
+  ## Scopes
+  #
   default_scope { order(:interview_sort_order, :id) }
 
+  ##
+  #
   def to_s
     label
   end
