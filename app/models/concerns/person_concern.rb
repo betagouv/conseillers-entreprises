a69566105fb4210e80f9c54c3ba66f2222782e97
Nicolas Bouilleaud <nico@bou.io>
2018-11-24 00:01:09 +0100

    Codestyle in concerns
    Don’t just put everything under `included do`

--- app/models/concerns/person_concern.rb
+++ app/models/concerns/person_concern.rb
@@ -6,47 +6,47 @@ module PersonConcern
   included do
     validates :full_name, :role, presence: true
     validates :email, format: { with: /\A.+@.+\..+\z/ }, allow_blank: true
+  end
 
-    def to_s
-      full_name
-    end
+  def to_s
+    full_name
+  end
 
-    def email_with_display_name
-      if email.nil?
-        nil
-      elsif full_name.nil?
-        email
-      else
-        "\"#{full_name}\" <#{email}>"
-      end
+  def email_with_display_name
+    if email.nil?
+      nil
+    elsif full_name.nil?
+      email
+    else
+      "\"#{full_name}\" <#{email}>"
     end
+  end
 
-    def normalize_values!
-      normalize_name
-      normalize_phone_number
-      normalize_email
-      normalize_role
-      save!
-    end
+  def normalize_values!
+    normalize_name
+    normalize_phone_number
+    normalize_email
+    normalize_role
+    save!
+  end
 
-    def normalize_name
-      self.full_name = self.full_name.squish.titleize
-    end
+  def normalize_name
+    self.full_name = self.full_name.squish.titleize
+  end
 
-    def normalize_email
-      self.email = self.email.strip.downcase
-    end
+  def normalize_email
+    self.email = self.email.strip.downcase
+  end
 
-    def normalize_phone_number
-      number = self.phone_number.gsub(/[^0-9]/,'')
-      if number.length == 10
-        number = number.gsub(/(.{2})(?=.)/, '\1 \2')
-        self.phone_number = number
-      end
+  def normalize_phone_number
+    number = self.phone_number.gsub(/[^0-9]/,'')
+    if number.length == 10
+      number = number.gsub(/(.{2})(?=.)/, '\1 \2')
+      self.phone_number = number
     end
+  end
 
-    def normalize_role
-      self.role = self.role.squish.titleize
-    end
+  def normalize_role
+    self.role = self.role.squish.titleize
   end
 end
