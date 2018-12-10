74522bbb36f64f7d67b5f51917eec54c3d1c333e
Nicolas Bouilleaud <nico@bou.io>
2018-12-10 17:24:18 +0100

    Use rails_ujs instead of jquery_ujs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,9 +9,11 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
+
 // = require jquery
-// = require jquery_ujs
+
+// = require rails-ujs
 // = require turbolinks
 // = require semantic-ui/transition
 // = require semantic-ui/popup
