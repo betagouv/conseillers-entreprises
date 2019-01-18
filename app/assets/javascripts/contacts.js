68f58337a9db814470ed58a43086317d3e7fe973
Nicolas Bouilleaud <nico@bou.io>
2019-01-18 17:17:15 +0100

    Use a slimmer js manifest for the contact page
    no need to include jquery, semantic-ui, or highcharts.

---
+++ app/assets/javascripts/contacts.js
@@ -0,0 +1,8 @@
+// Manifest file for the / page.
+// We currently have basically two separate sites.
+// * / (contact), based on the datagouv template
+// * the rest of the app, using semantic-ui.
+// This is hopefully a workaround until we migrate the rest of the app to the datagouv template
+
+// = require rails-ujs
+// = require turbolinks
