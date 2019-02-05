1dc41b9ae57a52e57467a24911822cf670594961
Nicolas Bouilleaud <nico@bou.io>
2019-02-05 10:07:13 +0100

    Fix diagnosed_needs per-status scopes
    Add proper tests for `with_some_matches_in_status` and `with_matches_only_in_status`

--- app/models/diagnosed_need.rb
+++ app/models/diagnosed_need.rb
@@ -90,11 +90,11 @@ class DiagnosedNeed < ApplicationRecord
   scope :rejected, -> { with_matches_only_in_status(:not_for_me) }
   scope :being_taken_care_of, -> { with_some_matches_in_status(:taking_care).where.not(id: done) }
 
-  scope :with_matches_only_in_status, -> (status) do # can be an array
-    where.not(matches: Match.where.not(status: status)).distinct
-  end
   scope :with_some_matches_in_status, -> (status) do # can be an array
-    where(matches: Match.where(status: status)).distinct
+    joins(:matches).where(matches: Match.where(status: status)).distinct
+  end
+  scope :with_matches_only_in_status, -> (status) do # can be an array
+    joins(:matches).where.not(matches: Match.where.not(status: status)).distinct
   end
 
   scope :abandoned, -> { rejected.not_archived }
