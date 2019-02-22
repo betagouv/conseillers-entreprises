2887d916bc0294ad7fe68beb65576dce16bcddb1
Nicolas Bouilleaud <nico@bou.io>
2019-02-22 18:23:57 +0100

    Fix Needs description in solicitation form on the homepage

--- app/helpers/solicitation_helper.rb
+++ app/helpers/solicitation_helper.rb
@@ -1,6 +1,6 @@
 module SolicitationHelper
   def all_needs_cards(f)
-    localized_needs_keypath = 'solicitations.needs.'
+    localized_needs_keypath = 'solicitations.needs.long'
     all_needs = I18n.t(localized_needs_keypath)
     tags = all_needs.map do |value, text|
       needs_card_tag(f, value, text)
@@ -16,7 +16,7 @@ module SolicitationHelper
 
   def needs_description(solicitation)
     needs_keys = solicitation.needs.select{ |_,v| v == "1" }.keys.sort
-    localized_needs = needs_keys.map{ |key| I18n.t("solicitations.needs.#{key}") }
+    localized_needs = needs_keys.map{ |key| I18n.t("solicitations.needs.short.#{key}") }
     tag.ul do
       localized_needs.map{ |need| tag.li(need) }.join.html_safe
     end
