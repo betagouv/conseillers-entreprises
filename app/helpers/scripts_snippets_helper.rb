a83d883b51a46e74c12eae480adc280f0e9c7f09
Nicolas Bouilleaud <nico@bou.io>
2019-03-05 16:33:30 +0100

    Add sentry integration to JS

--- app/helpers/scripts_snippets_helper.rb
+++ app/helpers/scripts_snippets_helper.rb
@@ -4,4 +4,10 @@ module ScriptsSnippetsHelper
       render partial: 'scripts_snippets/matomo'
     end
   end
+
+  def sentry_script
+    if Rails.env.production?
+      render partial: 'scripts_snippets/sentry'
+    end
+  end
 end
