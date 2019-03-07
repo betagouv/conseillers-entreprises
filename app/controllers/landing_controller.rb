68da03bd27ef1bb4a3f972033b45b5a4c2433408
Nicolas Bouilleaud <nico@bou.io>
2019-03-07 15:11:13 +0100

    Do not add pk params when redirecting from landing pages
    That’s pointless if we have proper pageviews tracking.

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -9,18 +9,6 @@ class LandingController < ApplicationController
 
     redirect_to root_path if @landing.nil?
 
-    @url_to_root = url_to_root(slug)
-  end
-
-  private
-
-  def url_to_root(slug)
-    tracking_params = { pk_source: 'landing', pk_campaign: 'entreprise', pk_content: slug }
-    tracking_params.merge! existing_tracking_params
-    root_path(tracking_params)
-  end
-
-  def existing_tracking_params
-    params.permit(Solicitation::TRACKING_KEYS).to_h.symbolize_keys
+    @url_to_root = root_path
   end
 end
