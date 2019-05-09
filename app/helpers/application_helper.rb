3ed2fd451bf0bd670b2069a46d6784fb4b4eeae1
Nicolas Bouilleaud <nico@bou.io>
2019-05-09 14:19:13 +0200

    Remove models_human_description

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,11 +4,4 @@ module ApplicationHelper
   def random_color
     %w[red orange yellow olive green teal blue violet pink].sample
   end
-
-  def models_human_description(objects)
-    return '-' if objects.blank?
-    klass = objects.first.class
-    count = objects.count
-    "#{count} #{klass.model_name.human(count: count).downcase}"
-  end
 end
