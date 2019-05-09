7b9b941b7c643af22026d7d8bdfa4632db05cff6
Nicolas Bouilleaud <nico@bou.io>
2019-05-09 15:12:41 +0200

    Add CoreExtension::Relation::HumanCount

--- config/initializers/core_extensions.rb
+++ config/initializers/core_extensions.rb
@@ -1,3 +1,5 @@
 require 'core_extensions/active_record/created_within'
+require 'core_extensions/relation/human_count'
 
 ActiveRecord::Base.include CoreExtensions::ActiveRecord::CreatedWithin
+ActiveRecord::Relation.include CoreExtensions::Relation::HumanCount
---
+++ spec/lib/core_extensions/relation/human_count_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+require 'core_extensions/relation/human_count'
+
+describe CoreExtensions::Relation::HumanCount, type: :lib do
+  describe 'human_count' do
+    subject { user.searches.human_count }
+
+    let(:user) { create :user, searches: create_list(:search, search_count) }
+
+    context 'one object' do
+      let(:search_count) { 1 }
+
+      it { is_expected.to eq '1 recherche' }
+    end
+
+    context 'zero objects' do
+      let(:search_count) { 0 }
+
+      it { is_expected.to eq '0 recherche' }
+    end
+
+    context 'several objects' do
+      let(:search_count) { 4 }
+
+      it { is_expected.to eq '4 recherches' }
+    end
+  end
+end
