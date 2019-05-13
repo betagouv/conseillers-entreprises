e3cc8c5cf3175eb2f7dd111b5b05c63c906280c2
Nicolas Bouilleaud <nico@bou.io>
2019-05-13 11:34:43 +0200

    Add solicitations_stats

---
+++ app/services/stats/solicitations_stats.rb
@@ -0,0 +1,29 @@
+module Stats
+  class SolicitationsStats
+    include BaseStats
+
+    def main_query
+      Solicitation.all
+    end
+
+    def date_group_attribute
+      'created_at'
+    end
+
+    def filtered(query)
+      query
+    end
+
+    def category_group_attribute
+      Arel.sql('true')
+    end
+
+    def category_name(_)
+      I18n.t('activerecord.models.solicitation.other')
+    end
+
+    def category_order_attribute
+      Arel.sql('true')
+    end
+  end
+end
