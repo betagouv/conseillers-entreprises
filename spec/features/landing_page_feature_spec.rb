7fc4ecd1a29556ee22cfad9fe03adb6bb5485a60
Nicolas Bouilleaud <nico@bou.io>
2019-06-14 22:10:18 +0200

    Fix the landing page redirect button not keeping piwik params
    Also, add a non-regression test

---
+++ spec/features/landing_page_feature_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Landing Page Feature', type: :feature do
+  before do
+    create :landing, slug: 'landing', button: 'Go to Home'
+  end
+
+  describe 'get solicitation with pk params' do
+    before do
+      visit '/aide/landing?pk_campaign=foo&pk_kwd=bar'
+      click_link 'Go to Home'
+    end
+
+    it { expect(page).to have_current_path '/?pk_campaign=foo&pk_kwd=bar' }
+  end
+end
