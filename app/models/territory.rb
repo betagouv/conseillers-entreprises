576230a9112d18198f72f31c42aaba861de64ba0
Nicolas Bouilleaud <nico@bou.io>
2019-06-25 14:35:48 +0200

    Add Territory.regions (draft) and Territory.all_experts

--- app/models/territory.rb
+++ app/models/territory.rb
@@ -25,6 +25,9 @@ class Territory < ApplicationRecord
 
   has_many :facilities, through: :communes, inverse_of: :territories
 
+  has_many :bassins_emploi, -> { distinct.bassins_emploi }, through: :communes, source: :territories
+  has_many :regions, -> { distinct.regions }, through: :communes, source: :territories
+
   # :facilities
   has_many :diagnoses, through: :facilities, inverse_of: :facility_territories
   has_many :needs, through: :facilities, inverse_of: :facility_territories
@@ -34,10 +37,17 @@ class Territory < ApplicationRecord
   ## Scopes
   #
   scope :bassins_emploi, -> { where(bassin_emploi: true) }
+  scope :regions, -> { where(bassin_emploi: false) }
 
   ##
   #
   def to_s
     name
+  end
+
+  def all_experts
+    Expert.where(id: direct_experts)
+      .or(Expert.where(id: antenne_experts).where(id: Expert.without_custom_communes)) # Experts of the antennes on this Territory, who don’t override their Antenne zone.
+      .or(Expert.with_global_zone)
   end
 end
