87c16e8b69572a569cace83c0f06560308d59398
Nicolas Bouilleaud <nico@bou.io>
2019-09-09 16:22:32 +0200

    Add Cache-Control headers for static assets

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -19,6 +19,9 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, s-maxage=31536000, max-age=15552000'
+  }
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
