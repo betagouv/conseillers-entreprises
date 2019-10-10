cc231190f74d08b6f992be2b369e80c7a2137bfc
Nicolas Bouilleaud <nico@bou.io>
2019-10-10 16:51:34 +0200

    Add the “Invite a new user” form

---
+++ app/controllers/users/invitations_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Users
+  class InvitationsController < Devise::InvitationsController
+    before_action :configure_permitted_parameters, only: %i[create update]
+
+    def after_invite_path_for(inviter, invitee = nil)
+      new_user_invitation_path
+    end
+
+    def configure_permitted_parameters
+      editable_attributes = %i[email full_name role phone_number antenne_id]
+      devise_parameter_sanitizer.permit(:invite, keys: editable_attributes)
+      devise_parameter_sanitizer.permit(:accept_invitation, keys: editable_attributes)
+    end
+  end
+end
