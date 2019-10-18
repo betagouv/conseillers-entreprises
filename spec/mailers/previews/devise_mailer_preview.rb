340e05fbf435ead315d06e089398a0fc11978ae7
Nicolas Bouilleaud <nico@bou.io>
2019-10-18 09:49:54 +0200

    Notes re: unused Devise emails

--- spec/mailers/previews/devise_mailer_preview.rb
+++ spec/mailers/previews/devise_mailer_preview.rb
@@ -9,6 +9,12 @@ class DeviseMailerPreview < ActionMailer::Preview
     Devise::Mailer.reset_password_instructions(user, 'faketoken')
   end
 
+  # Other Devise emails never used:
+  # confirmation_instructions: Signup is invite-only, and we don’t let users change their email themselves. If we allow it, we’ll use send_reconfirmation_instructions.
+  # email_changed: config.send_email_change_notification is false
+  # password_change: config.send_password_change_notification is false
+  # unlock_instructions: User is not :lockable.
+
   private
 
   def user
