c02d7d3392923fe9c58afa47b3d17b9c8e8df62f
Nicolas Bouilleaud <nico@bou.io>
2019-11-16 00:11:44 +0100

    Run `rake app:update`: changes in environments/test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,7 +6,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
@@ -22,6 +22,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+  config.cache_store = :null_store
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
