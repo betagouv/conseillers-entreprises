60e964ef74746155094f8eef34ac5d1342206ba7
Lucien <mollard.lucien@gmail.com>
2019-11-28 16:26:35 +0100

    create page for experts to edit his subjects

---
+++ app/controllers/experts_controller.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class ExpertsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_expert
+  before_action :check_user
+
+  def edit
+    @institutions_subjects = @expert.antenne.institution.institutions_subjects
+    @es_by_theme = @institutions_subjects.group_by { |is| is.subject.theme }
+    @themes = Theme.all.ordered_for_interview
+  end
+
+  def update
+    @expert.update(expert_params)
+    redirect_to profile_path
+  end
+
+  private
+
+  def expert_params
+    params.require(:expert).permit(:experts_subjects_attributes => {})
+  end
+
+  def find_expert
+    @expert = Expert.find(params[:id])
+  end
+
+  def check_user
+    check_current_user_access_to(@expert)
+  end
+end
