7fd78ba7afb3a053c7d016cca020ca3ea3f09e89
Nicolas Bouilleaud <nico@bou.io>
2019-12-16 14:46:03 +0100

    Disable the flash to review subjects for now
    first, get some feedback from users on the expert page

--- app/controllers/concerns/flash_to_review_subjects.rb
+++ app/controllers/concerns/flash_to_review_subjects.rb
@@ -6,6 +6,8 @@ module FlashToReviewSubjects
   end
 
   def flash_to_review_subjects_if_needed
+    return
+
     expert_to_review = current_user.experts.find(&:should_review_subjects?)
     if expert_to_review.present?
       message = I18n.t('experts.flash.review_your_subjects_html',
