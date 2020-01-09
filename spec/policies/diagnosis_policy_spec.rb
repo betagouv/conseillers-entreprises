b75a6f3c300a74e175bf81d1908a49d2adaaff5a
Nicolas Bouilleaud <nico@bou.io>
2020-01-09 17:59:48 +0100

    Make rubocop happy
    🤷🏻‍♂️

--- spec/policies/diagnosis_policy_spec.rb
+++ spec/policies/diagnosis_policy_spec.rb
@@ -32,13 +32,13 @@ RSpec.describe DiagnosisPolicy, type: :policy do
       let!(:expert_subject) { create :expert_subject, expert: expert, institution_subject: institution_subject }
       let(:institution_subject) { create :institution_subject, subject: support_subject }
 
-      it { expect(subject).to permit(user, diagnosis) }
+      it { is_expected.to permit(user, diagnosis) }
     end
 
     context "denies access if user is another user" do
       let(:user) { create :user, antenne: create(:antenne) }
 
-      it { expect(subject).not_to permit(user, diagnosis) }
+      it { is_expected.not_to permit(user, diagnosis) }
     end
 
     context "denies access if user is another support_user" do
@@ -48,85 +48,85 @@ RSpec.describe DiagnosisPolicy, type: :policy do
       let(:institution_subject) { create :institution_subject, subject: support_subject }
       let(:support_subject) { create :subject, is_support: true }
 
-      it { expect(subject).not_to permit(user, diagnosis) }
+      it { is_expected.not_to permit(user, diagnosis) }
     end
   end
 
-   permissions :create? do
+  permissions :create? do
      context "all user can create a diagnosis" do
-       let(:user) { create :user }
+        let(:user) { create :user }
 
-       it { is_expected.to permit(user, diagnosis) }
-     end
+        it { is_expected.to permit(user, diagnosis) }
+      end
    end
 
-   permissions :update? do
+  permissions :update? do
      context "grants access if user is diagnosis advisor" do
-       let(:user) { diagnosis.advisor }
+        let(:user) { diagnosis.advisor }
 
-       it { is_expected.to permit(user, diagnosis) }
-     end
+        it { is_expected.to permit(user, diagnosis) }
+      end
 
      context "grants access if user is admin" do
-       let(:user) { create :user, is_admin: true }
+        let(:user) { create :user, is_admin: true }
 
-       it { is_expected.to permit(user, diagnosis) }
-     end
+        it { is_expected.to permit(user, diagnosis) }
+      end
 
      context "grants access if user is support" do
-       let(:user) { create :user, experts: [expert] }
-       let(:expert) { create :expert, communes: [diagnosis.facility.commune] }
-       let(:support_subject) { create :subject, is_support: true }
-       let!(:expert_subject) { create :expert_subject, expert: expert, institution_subject: institution_subject }
-       let(:institution_subject) { create :institution_subject, subject: support_subject }
+        let(:user) { create :user, experts: [expert] }
+        let(:expert) { create :expert, communes: [diagnosis.facility.commune] }
+        let(:support_subject) { create :subject, is_support: true }
+        let!(:expert_subject) { create :expert_subject, expert: expert, institution_subject: institution_subject }
+        let(:institution_subject) { create :institution_subject, subject: support_subject }
 
-       it { expect(subject).to permit(user, diagnosis) }
-     end
+        it { is_expected.to permit(user, diagnosis) }
+      end
 
      context "denies access if user is another user" do
-       let(:user) { create :user, antenne: create(:antenne) }
+        let(:user) { create :user, antenne: create(:antenne) }
 
-       it { expect(subject).not_to permit(user, diagnosis) }
-     end
+        it { is_expected.not_to permit(user, diagnosis) }
+      end
 
      context "denies access if user is another support_user" do
-       let(:user) { create :user, antenne: create(:antenne) }
-       let(:another_expert) { create :expert, users: [user] }
-       let!(:expert_subject2) { create :expert_subject, expert: another_expert, institution_subject: institution_subject }
-       let(:institution_subject) { create :institution_subject, subject: support_subject }
-       let(:support_subject) { create :subject, is_support: true }
+        let(:user) { create :user, antenne: create(:antenne) }
+        let(:another_expert) { create :expert, users: [user] }
+        let!(:expert_subject2) { create :expert_subject, expert: another_expert, institution_subject: institution_subject }
+        let(:institution_subject) { create :institution_subject, subject: support_subject }
+        let(:support_subject) { create :subject, is_support: true }
 
-       it { expect(subject).not_to permit(user, diagnosis) }
-     end
+        it { is_expected.not_to permit(user, diagnosis) }
+      end
    end
 
-   permissions :destroy? do
+  permissions :destroy? do
      context "denie access if user is diagnosis advisor" do
-       let(:user) { diagnosis.advisor }
+        let(:user) { diagnosis.advisor }
 
-       it { is_expected.not_to permit(user, diagnosis) }
-     end
+        it { is_expected.not_to permit(user, diagnosis) }
+      end
 
      context "grants access if user is admin" do
-       let(:user) { create :user, is_admin: true }
+        let(:user) { create :user, is_admin: true }
 
-       it { is_expected.to permit(user, diagnosis) }
-     end
+        it { is_expected.to permit(user, diagnosis) }
+      end
 
      context "denies access if user is support" do
-       let(:user) { create :user, experts: [expert] }
-       let(:expert) { create :expert, communes: [diagnosis.facility.commune] }
-       let(:support_subject) { create :subject, is_support: true }
-       let!(:expert_subject) { create :expert_subject, expert: expert, institution_subject: institution_subject }
-       let(:institution_subject) { create :institution_subject, subject: support_subject }
+        let(:user) { create :user, experts: [expert] }
+        let(:expert) { create :expert, communes: [diagnosis.facility.commune] }
+        let(:support_subject) { create :subject, is_support: true }
+        let!(:expert_subject) { create :expert_subject, expert: expert, institution_subject: institution_subject }
+        let(:institution_subject) { create :institution_subject, subject: support_subject }
 
-       it { expect(subject).not_to permit(user, diagnosis) }
-     end
+        it { is_expected.not_to permit(user, diagnosis) }
+      end
 
      context "denies access if user is another user" do
-       let(:user) { create :user, antenne: create(:antenne) }
+        let(:user) { create :user, antenne: create(:antenne) }
 
-       it { expect(subject).not_to permit(user, diagnosis) }
-     end
+        it { is_expected.not_to permit(user, diagnosis) }
+      end
    end
 end
--- spec/policies/match_policy_spec.rb
+++ spec/policies/match_policy_spec.rb
@@ -6,23 +6,23 @@ RSpec.describe MatchPolicy, type: :policy do
 
   subject { described_class }
 
-   permissions :update? do
-     context "grants access if user is an admin" do
-       let(:user) { create :user, is_admin: true }
+  permissions :update? do
+    context "grants access if user is an admin" do
+      let(:user) { create :user, is_admin: true }
 
-       it { is_expected.to permit(user, match) }
-     end
+      it { is_expected.to permit(user, match) }
+    end
 
-     context "grants access if user is a contacted user" do
-       let(:user) { match.contacted_users.first }
+    context "grants access if user is a contacted user" do
+      let(:user) { match.contacted_users.first }
 
-       it { is_expected.to permit(user, match) }
-     end
+      it { is_expected.to permit(user, match) }
+    end
 
-     context "denies access if user is another user" do
-       let(:user) { create :user }
+    context "denies access if user is another user" do
+      let(:user) { create :user }
 
-       it { expect(subject).not_to permit(user, match) }
-     end
-   end
+      it { is_expected.not_to permit(user, match) }
+    end
+  end
 end
