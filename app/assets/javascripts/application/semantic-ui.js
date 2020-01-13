84be275bd3d83a3514e4a6fa771f51d3a9897e82
Nicolas Bouilleaud <nico@bou.io>
2020-01-13 15:53:57 +0100

    Tweak semantic-ui popup activation js
    So that it can be used easily anywhere.

--- app/assets/javascripts/application/semantic-ui.js
+++ app/assets/javascripts/application/semantic-ui.js
@@ -1,5 +1,5 @@
 addEventListener('turbolinks:load', function(event) {
-  $('.need-section .feed .event .user').popup({ hoverable: true });
+  $('.popup-hover').popup({ hoverable: true });
   $('select.ui.selection.search.dropdown').dropdown({ fullTextSearch: 'exact', ignoreDiacritics: true });
   $('select.ui.selection.dropdown').dropdown();
 });
