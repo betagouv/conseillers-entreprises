22f723a9f2139d9657c0cc934e38a07716a73bc6
Nicolas Bouilleaud <nico@bou.io>
2020-01-24 09:41:41 +0100

    Reorder routes.rb for simplicity
    Distinct sections for Pages, Application, and Devise, ActiveAdmin routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
-  mount UserImpersonate::Engine => '/impersonate', as: 'impersonate_engine'
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-
-  ActiveAdmin.routes(self)
-  devise_for :users, controllers: { registrations: 'users/registrations', invitations: 'users/invitations' }, skip: [:registrations]
-  devise_scope :user do
-    get 'users/edit' => 'users/registrations#edit', :as => 'edit_user_registration'
-    put 'users' => 'users/registrations#update', :as => 'user_registration'
-  end
-
+  # Pages
   root to: 'landings#index'
-
-  get 'profile' => 'users#show'
 
   resource :stats, only: [:show] do
     collection do
@@ -32,6 +21,7 @@ Rails.application.routes.draw do
 
   resource :solicitation, only: %i[create]
 
+  # Application
   resources :diagnoses, only: %i[index new show], path: 'analyses' do
     collection do
       get :archives
@@ -74,8 +64,8 @@ Rails.application.routes.draw do
   end
 
   resources :matches, only: %i[update]
-
   resources :feedbacks, only: %i[create destroy]
+  resources :experts, only: %i[edit update]
 
   resources :relances, as: 'reminders', controller: 'reminders', only: %i[index show] do
     member do
@@ -83,7 +73,28 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :experts, only: %i[edit update]
-
   get '/diagnoses', to: redirect('/analyses')
+
+  # Devise
+  devise_for :users,
+             controllers: {
+               registrations: 'users/registrations',
+               invitations: 'users/invitations'
+             },
+             skip: [:registrations]
+  devise_scope :user do
+    get 'users/edit' => 'users/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'users/registrations#update', :as => 'user_registration'
+  end
+
+  get 'profile' => 'users#show'
+
+  # ActiveAdmin
+  ActiveAdmin.routes(self)
+
+  # Impersonate
+  mount UserImpersonate::Engine => '/impersonate', as: 'impersonate_engine'
+
+  # LetterOpener
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
