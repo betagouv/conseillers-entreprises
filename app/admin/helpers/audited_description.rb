8571baa7147e7614c4350e7adda09a1f4bc3050a
Nicolas Bouilleaud <nico@bou.io>
2020-01-27 15:39:12 +0100

    Fix audited description for deleted subjects

--- app/admin/helpers/audited_description.rb
+++ app/admin/helpers/audited_description.rb
@@ -8,9 +8,11 @@ module Admin
 
     module AuditedExpertSubject
       def admin_description
-        "<div><a href='subjects/#{subject.id}'>#{subject}</a></div>
-         <div>#{institution_subject.description}</div>
-         <div>#{description}</div>".html_safe
+        if persisted?
+          "<div><a href='subjects/#{subject.id}'>#{subject}</a></div>
+           <div>#{institution_subject.description}</div>
+           <div>#{description}</div>".html_safe
+        end
       end
     end
 
