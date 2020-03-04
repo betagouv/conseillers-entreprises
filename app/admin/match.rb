3960753cfa8387d9e94dc6edf629aa938e1e5839
Lucien <mollard.lucien@gmail.com>
2020-03-04 15:01:08 +0100

    add experts_subjects to match csv

--- app/admin/match.rb
+++ app/admin/match.rb
@@ -85,6 +85,9 @@ ActiveAdmin.register Match do
     column :subject
     column(:content) { |m| m.need.content }
     column :expert
+    column 'Champs d’intervention' do |m|
+      m.expert&.experts_subjects&.find { |x| x.subject == m.subject }&.description
+    end
     column :expert_antenne
     column :expert_institution
     column(:status) { |m| m.status_short_description }
