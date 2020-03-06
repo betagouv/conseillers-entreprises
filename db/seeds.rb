1c1fc43bff16076f2228f0ed6cdbf7d03f06ecdc
Nicolas Bouilleaud <nico@bou.io>
2020-03-06 17:08:48 +0100

    Make seeds.rb usable for initial setup
    Do not rely on first running import_dump :)
    
    * db:seed can now be used on an empty DB (db:setup and db:reset work)
    * db:seed is idempotent
    * The app is usable after a db:setup
    
    fixes #874

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,33 @@
 # frozen_string_literal: true
 
-if User.find_by(email: "a@a.a").blank?
-  antenne = Antenne.first
-  User.create!(
-    email: "a@a.a",
-    password: "1234567",
-    is_admin: true,
-    phone_number: "0612345678",
-    role: "Admin",
-    full_name: "Edith Piaf",
-    antenne: antenne,
-    experts: [antenne.experts.first]
-  )
-  p "Admin created"
+TEST_PASSWORD = '1234567'
+TEST_EMAIL = 'a@a.a'
+
+## Landing pages
+landing = Landing.find_or_create_by!(slug: 'test-landing', home_sort_order: 0)
+topic = LandingTopic.find_or_create_by!(title: 'Test Topic', landing: landing)
+
+## Theme and Subject
+theme = Theme.find_or_create_by!(label: 'Test Theme')
+subject = Subject.find_or_create_by!(theme: theme, label: 'Test Subject')
+
+## Institution and Antenne
+institution = Institution.find_or_create_by!(name: 'Test Institution')
+antenne = Antenne.find_or_create_by!(name: 'Test Antenne', institution: institution)
+institution_subject = InstitutionSubject.find_or_create_by!(institution: institution, subject: subject)
+
+## User and Expert
+person_params = {
+  email: TEST_EMAIL,
+  phone_number: '0612345678',
+  role: 'Test User',
+  full_name: 'Edith Piaf',
+  antenne: antenne
+}
+
+## User and Expert
+user = User.find_or_create_by!(person_params) do |user|
+  user.update!(password: TEST_PASSWORD, is_admin: true)
+  # users.experts.first is created implicitely
+  user.experts.first.experts_subjects.find_or_create_by!(institution_subject: institution_subject)
 end
