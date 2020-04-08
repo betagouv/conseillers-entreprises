betagouv/conseillers-entreprises

commit 88b66e87daf629e1f59e72634672f9a562a1e70c

Activate all .ui.dropdown in semantic-ui

app/assets/javascripts/application/semantic-ui.js
@@ -2,7 +2,7 @@ addEventListener('turbolinks:load', function(event) {
   $('.ui.modal').modal( { closable: false }).modal('show'); // Show modal before activating other elements that may be inside the modal
   $('.popup-hover').popup({ hoverable: true });
   $('select.ui.selection.search.dropdown').dropdown({ fullTextSearch: 'exact', ignoreDiacritics: true });
-  $('select.ui.selection.dropdown').dropdown();
+  $('.ui.dropdown').dropdown();
   $('.tabular.menu .item').tab();
   $('.ui.accordion').accordion();
 });

commit b947b030d18dbd559ba3f552c67acab5547ff421

Do not enable js for “simple” dropdown
The dropdown menus in the navbar are “simple” in fomantic-ui, which means the work without js.

(This glitch was introduced in 88b66e87daf629e1f59e72634672f9a562a1e70c)

app/assets/javascripts/application/semantic-ui.js
@@ -2,7 +2,7 @@ addEventListener('turbolinks:load', function(event) {
   $('.ui.modal').modal( { closable: false }).modal('show'); // Show modal before activating other elements that may be inside the modal
   $('.popup-hover').popup({ hoverable: true });
   $('select.ui.selection.search.dropdown').dropdown({ fullTextSearch: 'exact', ignoreDiacritics: true });
-  $('.ui.dropdown').dropdown();
+  $('.ui.dropdown').not('.simple').dropdown();
   $('.tabular.menu .item').tab();
   $('.ui.accordion').accordion();
 });