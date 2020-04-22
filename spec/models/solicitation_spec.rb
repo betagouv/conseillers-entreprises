d6158d089bbde1be33a412aacc7cccc19a20f02c
Nicolas Bouilleaud <nico@bou.io>
2020-04-22 11:08:42 +0200

    Add missing tests for Solicitation.landing_options
    and fix LandingOptions factory

--- spec/models/solicitation_spec.rb
+++ spec/models/solicitation_spec.rb
@@ -14,4 +14,24 @@ RSpec.describe Solicitation, type: :model do
     it { is_expected.to validate_presence_of :phone_number }
     it { is_expected.to validate_presence_of :email }
   end
+
+  describe '#landing_options' do
+    let(:solicitation) { create :solicitation, landing_options_slugs: slugs }
+    let!(:option1) { create :landing_option, slug: 'option1' }
+    let!(:option2) { create :landing_option, slug: 'option2' }
+
+    subject { solicitation.landing_options }
+
+    context 'slugs are known' do
+      let(:slugs) { %w[option1 option2] }
+
+      it { is_expected.to match_array [option1, option2] }
+    end
+
+    context 'slugs are unknown' do
+      let(:slugs) { %w[option3 option4] }
+
+      it { is_expected.to be_empty }
+    end
+  end
 end
