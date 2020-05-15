eabd1e479ea06c8600152f82f3c81cc2c5e1c218
Nicolas Bouilleaud <nico@bou.io>
2020-05-15 15:48:36 +0200

    Split ExpertSubject#relevant_for scope and add tests

--- spec/models/expert_subject_spec.rb
+++ spec/models/expert_subject_spec.rb
@@ -7,4 +7,38 @@ RSpec.describe ExpertSubject, type: :model do
       is_expected.to belong_to :institution_subject
     end
   end
+
+  describe 'scopes' do
+    describe 'relevant_for' do
+      subject{ described_class.relevant_for(need) }
+
+      let(:need) { create :need }
+      let!(:expert_subject) do
+        create :expert_subject,
+               institution_subject: create(:institution_subject, subject: the_subject),
+               expert: create(:expert, communes: communes)
+      end
+
+      context 'when the expert isn’t on the commune' do
+        let(:the_subject) { need.subject }
+        let(:communes) { [create(:commune)] }
+
+        it{ is_expected.to be_blank }
+      end
+
+      context 'when the institution doesn’t handle that subject' do
+        let(:the_subject) { create :subject }
+        let(:communes) { [need.facility.commune] }
+
+        it{ is_expected.to be_blank }
+      end
+
+      context 'when both subject and institution match' do
+        let(:the_subject) { need.subject }
+        let(:communes) { [need.facility.commune] }
+
+        it{ is_expected.to eq [expert_subject] }
+      end
+    end
+  end
 end
