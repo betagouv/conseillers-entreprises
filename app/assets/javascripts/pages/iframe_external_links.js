8168d40a3cb1e031d663c94f6b18a7120d8987ad
Nicolas Bouilleaud <nico@bou.io>
2020-06-19 12:43:14 +0200

    Make external links in iframe target _parent

---
+++ app/assets/javascripts/pages/iframe_external_links.js
@@ -0,0 +1,31 @@
+(function() {
+  // Make external links work properly when the site is inside an iframe.
+  // This adds target="_parent" to all <a> whose url isn't ours.
+
+  this.onload = function() {
+    if(inIframe()) {
+      makeExternalLinksOpenInParent();
+    }
+  }
+
+  function inIframe() {
+    try {
+      return window.self !== window.top;
+    } catch (e) {
+      return true;
+    }
+  }
+
+  function makeExternalLinksOpenInParent() {
+    var links = document.getElementsByTagName("a")
+    for (var i = 0; i < links.length; ++i) {
+      targetParentIfExternal(links[i]);
+    }
+  }
+
+  function targetParentIfExternal(link) {
+    if(link.host !== window.location.host) {
+      link.target = "_parent";
+    }
+  }
+})();
