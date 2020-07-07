63ffce6d9a7b963daa4f31354811cdc1648a0c20
Lucien <mollard.lucien@gmail.com>
2020-07-07 11:32:22 +0200

    use session[:region] only for region banner

--- app/controllers/concerns/geocoder_ip.rb
+++ app/controllers/concerns/geocoder_ip.rb
@@ -9,16 +9,12 @@ module GeocoderIp
     return if Rails.env.test?
 
     ip = ENV['IP_OVERRIDE'] || request.remote_ip
-    if session[:region_in_territory].blank? || session[:region].blank?
+    if session[:region].blank?
       results = Rails.cache.fetch(ip, expires_in: 12.hours) do
         Geocoder.search(ip)
       end
       region = results.first&.region || '' # to prevent "Geocoding API error: 429 Too Many Requests"
-      all_regions = ["Hauts-de-France"]
-      unless all_regions.include? region
-        session[:region_in_territory] = t('pages.alert_region.alert_region_html')
-        session[:region] = region
-      end
+      session[:region] = region
     end
   end
 end
