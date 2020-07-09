2d6782348f64200f5ab0b9d7e34175e3a8c288e7
Lucien <mollard.lucien@gmail.com>
2020-07-09 12:19:56 +0200

    reduce cache expires time

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -6,7 +6,7 @@ class AboutController < PagesController
   def mentions_legales; end
 
   def comment_ca_marche
-    Rails.cache.fetch("institutions-#{Institution.maximum(:updated_at)}") do
+    Rails.cache.fetch("institutions-#{Institution.maximum(:updated_at)}", expires_in: 1.minute) do
       institutions = Institution.where(show_on_list: true).pluck(:name).sort
       @institutions = institutions.each_slice((institutions.count.to_f / 4).ceil).to_a
     end
