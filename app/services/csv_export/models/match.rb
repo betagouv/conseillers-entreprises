348a09b9a8b7af8db16dd9cec53db4e086a80d1a
Nicolas Bouilleaud <nico@bou.io>
2020-08-28 09:23:33 +0200

    Include the visitee email in the matches.csv export

--- app/services/csv_export/models/match.rb
+++ app/services/csv_export/models/match.rb
@@ -8,6 +8,7 @@ module CsvExport
             id: :id,
             need: :need_id,
             company: :company,
+            visitee: -> { diagnosis.visitee.email },
             siret: -> { facility.siret },
             commune: -> { facility.commune },
             created_at: :created_at,
@@ -23,7 +24,7 @@ module CsvExport
             status: -> { need.human_attribute_value(:status, context: :short) },
             taken_care_of_at: :taken_care_of_at,
             closed_at: :closed_at,
-            page_analyse: :need_url_route
+            page_analyse: :need_url_route,
           }
         end
 
@@ -33,7 +34,8 @@ module CsvExport
             :advisor, :advisor_antenne, :advisor_institution,
             :expert, :expert_antenne, :expert_institution,
             :subject, :theme,
-            facility: :commune
+            facility: :commune,
+            diagnosis: :visitee,
           ]
         end
       end
