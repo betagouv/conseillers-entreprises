40889b2b53f79686c387c6381a50ebcb60f81f41
Lucien <mollard.lucien@gmail.com>
2020-09-16 10:19:16 +0200

    find needs by territory

--- app/controllers/reminders/needs_controller.rb
+++ app/controllers/reminders/needs_controller.rb
@@ -2,24 +2,45 @@ module Reminders
   class NeedsController < ApplicationController
     before_action :authenticate_admin!
     before_action :maybe_review_expert_subjects
+    before_action :find_territories
 
     layout 'side_menu'
 
     def index
-      @needs = Need.reminder_quo_not_taken.page params[:page]
-      @status = t('reminders.needs.menu.treat').downcase
+      retrieve_needs :abandoned_quo_not_taken
     end
 
     def in_progress
-      @needs = Need.reminder_quo_not_taken_in_progress.page params[:page]
-      @status = t('reminders.needs.menu.in_progress').downcase
+      retrieve_needs :reminder_in_progress
       render :index
     end
 
     def abandoned
-      @needs = Need.abandoned_without_taking_care.page params[:page]
-      @status = t('reminders.needs.menu.abandoned').downcase
+      retrieve_needs :abandoned_without_taking_care
       render :index
+    end
+
+    private
+
+    def find_territories
+      @territories = Territory.regions.order(:name)
+      @territory = retrieve_territory
+    end
+
+    def retrieve_territory
+      safe_params = params.permit(:territory)
+      if safe_params[:territory].present?
+        Territory.find(safe_params[:territory])
+      end
+    end
+
+    def retrieve_needs(scope)
+      @needs = if @territory.present?
+        Need.send(scope).joins(:diagnosis).where(diagnoses: { facility: @territory&.facilities }).page(params[:page])
+      else
+        Need.send(scope).page(params[:page])
+      end
+      @status = t("reminders.needs.menu.#{scope}").downcase
     end
   end
 end
