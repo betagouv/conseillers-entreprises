bc297bc54e4ffa2e7056dfcb968adbf7a6f3328d
Lucien <mollard.lucien@gmail.com>
2020-09-24 11:28:16 +0200

    update page helper for collections with pages

--- app/helpers/page_infos_helper.rb
+++ app/helpers/page_infos_helper.rb
@@ -2,8 +2,17 @@
 
 module PageInfosHelper
   def collection_count(collection)
-    size = collection.size.is_a?(Hash) ? collection.size.keys.count : collection.size
-    collection_name = size == 1 ? collection.model_name.human : collection.model_name.human.pluralize
-    t('helpers.page_infos.collection_count_html', size: size, name: collection_name)
+    collection_size = collection.total_count
+    collection_name = collection.model_name.human(count: collection_size)
+
+    if collection.total_pages < 2
+      t('helpers.page_infos.collection_count_html', size: collection_size, name: collection_name)
+    else
+      t('helpers.page_entries_info.many_pages_html',
+        total: collection_size,
+        start: collection.offset_value + 1,
+        end: collection.offset_value + collection.length,
+        name: collection_name)
+    end
   end
 end
