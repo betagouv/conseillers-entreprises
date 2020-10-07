6cb6286bca69c10647d4510da058ae0d288cff77
Nicolas Bouilleaud <nicolas.bouilleaud@beta.gouv.fr>
2020-10-07 17:05:19 +0200

    Fix crash when importing users with nested errors

--- app/helpers/nested_errors_helper.rb
+++ app/helpers/nested_errors_helper.rb
@@ -9,7 +9,7 @@ module NestedErrorsHelper
     sub_messages = errors.details.values.flatten.flat_map do |hash|
       case value = hash[:value]
       when ActiveRecord::Base
-        nested_errors_messages(value.errors, level + 1)
+        nested_errors_messages(value, level + 1)
       when ActiveRecord::Relation
         value.map { |object| nested_errors_messages(object, level + 1) }
       else nil
