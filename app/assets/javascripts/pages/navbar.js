a321bbdaf3575070b1fee3f00a15a59576beb5e5
clairezed <clairezuliani@gmail.com>
2020-10-23 10:35:37 +0200

    Fix toggle navbar on mobile #1235

--- app/assets/javascripts/pages/navbar.js
+++ app/assets/javascripts/pages/navbar.js
@@ -4,9 +4,15 @@
   function setupToggleNavButton() {
     let ToggleNavButton = document.querySelector("[data-action='toggle-nav']");
     let nav = document.querySelector("[data-target='toggle-nav']")
-    ToggleNavButton.addEventListener("click", (event) => {
-      event.preventDefault();
-      nav.classList.toggle('active');
-    }, false);
+    if (exists(ToggleNavButton) && exists(nav)) {
+      ToggleNavButton.addEventListener("click", (event) => {
+        event.preventDefault();
+        nav.classList.toggle('active');
+      }, false);
+    }
+  }
+
+  function exists(element) {
+    return (typeof element != "undefined" && element != null);
   }
 })();
