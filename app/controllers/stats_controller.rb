6e10fbc1be9a887a4bebd18486e4bded7450bc07
Lucien <mollard.lucien@gmail.com>
2020-10-27 15:39:49 +0100

    display only last 6 months

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -34,7 +34,10 @@ class StatsController < PagesController
   private
 
   def stats_params
-    params.permit(:territory, :institution, :start_date, :end_date)
+    permit_params = params.permit(:territory, :institution, :start_date, :end_date)
+    permit_params[:start_date] ||= 6.months.ago.beginning_of_month.to_date
+    permit_params[:end_date] ||= Date.today
+    permit_params
   end
 
   def users_stats
