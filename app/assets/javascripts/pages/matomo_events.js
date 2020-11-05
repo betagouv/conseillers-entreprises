b8a16352b35b4c072e5d8b92cc2bce7e7859316d
Lucien <mollard.lucien@gmail.com>
2020-11-05 15:48:03 +0100

    add event to link for find siret on annuaire.entreprise.data.gouv

--- app/assets/javascripts/pages/matomo_events.js
+++ app/assets/javascripts/pages/matomo_events.js
@@ -5,3 +5,7 @@ function click_on_landing() {
 function click_on_landing_option() {
   _paq.push(['trackEvent', 'formulaire', "success"]);
 }
+
+function click_on_find_siret() {
+  _paq.push(['trackEvent', 'chercher-SIRET', "success"]);
+}
