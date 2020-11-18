98291a49e13f1d797353bebdb2f99829a22ebc69
clairezed <clairezuliani@gmail.com>
2020-11-18 10:51:43 +0100

    Fix semantic ui init

--- app/front/javascripts/application/semantic_ui_calendar_datepicker_params.js
+++ app/front/javascripts/application/semantic_ui_calendar_datepicker_params.js
@@ -1,28 +1,45 @@
-function semantic_ui_calendar_datepicker_params() {
-  // This can’t just be in semantic-ui.js with the other initializer, as we need to dynamically set the text from rails i18n.
-  return {
-    type: 'date',
-    firstDayOfWeek: 1,
-    disableMonth: true,
-    disableYear: true,
-    formatter: {
-      date: function (date, settings) {
-        var d = date.getDate();
-        var m = date.getMonth() + 1;
-        var y = date.getFullYear();
-        return ("0"+d).slice(-2) + "/" + ("0"+m).slice(-2) + "/" + y;
-      }
-    },
-    parser: {
-      date: function (text, settings) {
-        var parts = text.split('-');
-        if(parts.length !== 3){
-          parts = text.split('/').reverse();
+(function () {
+  addEventListener("turbolinks:load", setupSemanticUiCalendar);
+
+  function setupSemanticUiCalendar() {
+    const semanticUiCalendars = document.querySelectorAll("[data-calendar='semantic-ui']");
+
+    for (let calendar of semanticUiCalendars) {
+      let params = semantic_ui_calendar_datepicker_params()
+      params['text'] = {
+          days: JSON.parse(calendar.dataset.days),
+          months: JSON.parse(calendar.dataset.months)
         }
-        if(parts.length === 3){
-          return new Date(parts[0], parts[1] - 1, parts[2]);
-        }
-      }
-    },
-  };
-}
+      $('.ui.calendar#datepicker').calendar(params);
+    }
+  }
+
+  function semantic_ui_calendar_datepicker_params() {
+    // This can’t just be in semantic-ui.js with the other initializer, as we need to dynamically set the text from rails i18n.
+    return {
+      type: "date",
+      firstDayOfWeek: 1,
+      disableMonth: true,
+      disableYear: true,
+      formatter: {
+        date: function (date, settings) {
+          var d = date.getDate();
+          var m = date.getMonth() + 1;
+          var y = date.getFullYear();
+          return ("0" + d).slice(-2) + "/" + ("0" + m).slice(-2) + "/" + y;
+        },
+      },
+      parser: {
+        date: function (text, settings) {
+          var parts = text.split("-");
+          if (parts.length !== 3) {
+            parts = text.split("/").reverse();
+          }
+          if (parts.length === 3) {
+            return new Date(parts[0], parts[1] - 1, parts[2]);
+          }
+        },
+      },
+    };
+  }
+})();
