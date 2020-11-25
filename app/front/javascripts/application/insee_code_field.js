cfff0e7b4727fdfc88e93315db1b7d1327650771
clairezed <clairezuliani@gmail.com>
2020-11-25 10:27:43 +0100

    Add  eslint, closes #1411

--- app/front/javascripts/application/insee_code_field.js
+++ app/front/javascripts/application/insee_code_field.js
@@ -1,38 +1,38 @@
-(function(){
+(function () {
   // See also views/application/_insee_code_field.html.haml
   // Uses the api-adresse to load communes insee codes for a postal code, and fill the <option>s of a <select>.
-  addEventListener("turbolinks:load", setupInseeCodeFields);
+  addEventListener('turbolinks:load', setupInseeCodeFields)
 
-  function setupInseeCodeFields(event) {
-    var containers = document.querySelectorAll('.insee-code-field');
-    containers.forEach( container => setupInseeCodeField(container) )
+  function setupInseeCodeFields () {
+    const containers = document.querySelectorAll('.insee-code-field')
+    containers.forEach(container => setupInseeCodeField(container))
   }
 
-  function setupInseeCodeField(container) {
-    var text_field = container.querySelector('input#postal_code');
-    text_field.addEventListener('input', (event) => loadInseeCodes(container, text_field));
+  function setupInseeCodeField (container) {
+    const text_field = container.querySelector('input#postal_code')
+    text_field.addEventListener('input', () => loadInseeCodes(container, text_field))
   }
 
-  function loadInseeCodes(container, text_field) {
+  function loadInseeCodes (container, text_field) {
     if (!text_field.validity.valid) {
       return
     }
 
-    var loader = container.querySelector('.insee-code-loader');
-    var select = container.querySelector('select#insee_code');
+    const loader = container.querySelector('.insee-code-loader')
+    const select = container.querySelector('select#insee_code')
 
-    loader.classList.add('active');
+    loader.classList.add('active')
 
-    var url = `https://api-adresse.data.gouv.fr/search/?type=municipality&q=${text_field.value}`;
-    var request = new XMLHttpRequest();
-    request.open('GET', url);
+    const url = `https://api-adresse.data.gouv.fr/search/?type=municipality&q=${text_field.value}`
+    const request = new XMLHttpRequest()
+    request.open('GET', url)
     request.addEventListener('load', function () {
-      loader.classList.remove('active');
-      select.length = 0;
-      var json = JSON.parse(this.response);
-      var cities = json['features'];
-      cities.forEach(hash => select.add(new Option(hash['properties']['name'], hash['properties']['citycode'])));
-    });
-    request.send();
+      loader.classList.remove('active')
+      select.length = 0
+      const json = JSON.parse(this.response)
+      const cities = json.features
+      cities.forEach(hash => select.add(new Option(hash.properties.name, hash.properties.citycode)))
+    })
+    request.send()
   }
-})();
+})()
--- app/front/javascripts/pages/siret_autocomplete.js
+++ app/front/javascripts/pages/siret_autocomplete.js
@@ -1,48 +1,48 @@
-import { exists, debounce } from "../shared/utils.js";
-import accessibleAutocomplete from "accessible-autocomplete";
+import { exists, debounce } from '../shared/utils.js'
+import accessibleAutocomplete from 'accessible-autocomplete';
 
 (function () {
-  addEventListener("DOMContentLoaded", setupSiretAutocomplete);
+  addEventListener('DOMContentLoaded', setupSiretAutocomplete)
 
-  const NAME_SEARCH_URL = "https://entreprise.data.gouv.fr/api/sirene/v1/full_text/";
-  const SIRET_SEARCH_URL = "https://entreprise.data.gouv.fr/api/sirene/v3/unites_legales/";
+  const NAME_SEARCH_URL = 'https://entreprise.data.gouv.fr/api/sirene/v1/full_text/'
+  const SIRET_SEARCH_URL = 'https://entreprise.data.gouv.fr/api/sirene/v3/unites_legales/'
 
-  function setupSiretAutocomplete() {
-    const targetField = document.querySelector("[data-target='siret-autocomplete']");
-    const autocompleteField = document.querySelector("[data-action='siret-autocomplete']");
+  function setupSiretAutocomplete () {
+    const targetField = document.querySelector("[data-target='siret-autocomplete']")
+    const autocompleteField = document.querySelector("[data-action='siret-autocomplete']")
 
     if (exists(autocompleteField)) {
       accessibleAutocomplete({
         element: autocompleteField,
-        id: "solicitation_siret_autocomplete",
+        id: 'solicitation_siret_autocomplete',
         showNoOptionsFound: false,
         templates: {
           inputValue: inputValueTemplate,
-          suggestion: suggestionTemplate,
+          suggestion: suggestionTemplate
         },
-        tAssistiveHint: () => autocompleteField.dataset["assistiveHint"],
+        tAssistiveHint: () => autocompleteField.dataset.assistiveHint,
         source: debounce(async (query, populateResults) => {
           // fill hidden field in case autocomplete gives no result
-          targetField.value = query;
+          targetField.value = query
           // display autocomplete suggestions
-          let url = getSearchUrl(query);
-          const res = await fetchSource(query, url);
-          const filteredResults = filterResults(res);
+          const url = getSearchUrl(query)
+          const res = await fetchSource(query, url)
+          const filteredResults = filterResults(res)
           if (filteredResults) {
-            populateResults(filteredResults);
+            populateResults(filteredResults)
           }
         }, 300),
         onConfirm: (option) => {
-          fillSiretField(option);
-        },
-      });
+          fillSiretField(option)
+        }
+      })
     }
 
-    function fillSiretField(option) {
-      if (option && option["label"]) {
-        let match = option["label"].match(/\d{14}/)[0];
+    function fillSiretField (option) {
+      if (option && option.label) {
+        const match = option.label.match(/\d{14}/)[0]
         if (match) {
-          targetField.value = match;
+          targetField.value = match
         }
       }
     }
@@ -50,72 +50,70 @@ import accessibleAutocomplete from "accessible-autocomplete";
 
   // Récupération des résultats ----------------------------------------------------
 
-  function getSearchUrl(query) {
-    return isSiretSearch(query) ? SIRET_SEARCH_URL : NAME_SEARCH_URL;
+  function getSearchUrl (query) {
+    return isSiretSearch(query) ? SIRET_SEARCH_URL : NAME_SEARCH_URL
   }
 
-  async function fetchSource(query, url) {
-    if (isSiretSearch(query)) query = query.replace(/\s/g, "");
+  async function fetchSource (query, url) {
+    if (isSiretSearch(query)) query = query.replace(/\s/g, '')
     const res = await fetch(
       `${url}${encodeURIComponent(query)}?per_page=15`
-    );
-    const data = await res.json();
-    return data;
+    )
+    const data = await res.json()
+    return data
   }
 
   // Traitement des résultats --------------------------------------------
 
-  function filterResults(results) {
-    if (results["message"] == "no results found") return;
+  function filterResults (results) {
+    if (results.message == 'no results found') return
     // Recherche par SIRET
-    if (results["unite_legale"]) {
-      let uniteLegale = results["unite_legale"]
-      let etablissement = uniteLegale["etablissement_siege"];
+    if (results.unite_legale) {
+      const uniteLegale = results.unite_legale
+      const etablissement = uniteLegale.etablissement_siege
       return [{
-        label: `${etablissement["siret"]} (${uniteLegale["denomination"]})`,
-        address: etablissement["geo_adresse"]
-      }];
+        label: `${etablissement.siret} (${uniteLegale.denomination})`,
+        address: etablissement.geo_adresse
+      }]
     }
     // Recherche par nom
-    if (results["etablissement"]) {
-      return results["etablissement"]
+    if (results.etablissement) {
+      return results.etablissement
         .filter((etablissement) => {
           // remove Administrations from suggestions
-          return (etablissement["libelle_activite_principale"] != "Administration publique générale");
+          return (etablissement.libelle_activite_principale != 'Administration publique générale')
         })
         .map((etablissement) => {
-          let name = getName(etablissement) .replace(/\*/g, " ");
+          const name = getName(etablissement).replace(/\*/g, ' ')
           return {
-            label: `${name} (${etablissement["siret"]})`,
-            address: etablissement["geo_adresse"],
-            activity: etablissement["libelle_activite_principale"],
-          };
-        });
+            label: `${name} (${etablissement.siret})`,
+            address: etablissement.geo_adresse,
+            activity: etablissement.libelle_activite_principale
+          }
+        })
     }
   }
 
   function suggestionTemplate (result) {
     if (!result) return
-    let activity = result.activity ? `${result.activity} - ` : ''
+    const activity = result.activity ? `${result.activity} - ` : ''
     return result && `<strong> ${result.label} </strong>
         <p><span class="small">${activity}${result.address}</span> </p>`
   }
 
-  function inputValueTemplate(result) {
-    return result && result.label;
+  function inputValueTemplate (result) {
+    return result && result.label
   }
 
-  function getName(etablissement) {
-    if (etablissement["nom"])
-      return `${etablissement["nom"]} ${etablissement["prenom"]}`;
-    if (etablissement["enseigne"]) return etablissement["enseigne"];
-    return etablissement["nom_raison_sociale"];
+  function getName (etablissement) {
+    if (etablissement.nom) { return `${etablissement.nom} ${etablissement.prenom}` }
+    if (etablissement.enseigne) return etablissement.enseigne
+    return etablissement.nom_raison_sociale
   }
 
   // Utilities ----------------------------------------------------------------
 
-  function isSiretSearch(str) {
-    return str.replace(/\s/g, "").match(/^\d+$/g);
+  function isSiretSearch (str) {
+    return str.replace(/\s/g, '').match(/^\d+$/g)
   }
-
-})();
+})()
