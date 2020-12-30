21d8af518f7cbc66149ee74f457989995fc09213
clairezed <clairezuliani@gmail.com>
2020-12-30 10:36:02 +0100

    Add need show feature test

---
+++ spec/features/users/needs_feature_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'experts', type: :feature do
+  describe 'expert update' do
+    let(:expert) { create :expert, users: [current_user] }
+    let!(:match) { create :match, expert: expert }
+    let(:need_subject) { match.need.subject.label }
+
+    login_user
+
+    it 'shows the need to user' do
+      visit needs_path
+      expect(page.html).to include 'Demandes reçues'
+      expect(page.html).to include need_subject
+      page.click_link('', :href => "/besoins/#{match.need.diagnosis.id}")
+      expect(page.html).to include need_subject
+    end
+  end
+end
