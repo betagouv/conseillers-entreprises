38aad3bafb59ad36a6801725d3f84f8a5ce25c7f
Lucien <mollard.lucien@gmail.com>
2021-01-19 14:13:05 +0100

    Update variable for deployed regions

--- app/services/stats/public/solicitations_in_regions_stats.rb
+++ app/services/stats/public/solicitations_in_regions_stats.rb
@@ -7,7 +7,7 @@ module Stats::Public
     end
 
     def territories
-      Territory.where(id: YAML.safe_load(ENV['DEPLOYED_REGIONS_IDS']))
+      Territory.where(region: true, code_region: YAML.safe_load(ENV['DEPLOYED_REGIONS_CODES']))
     end
 
     def in_regions(query)
