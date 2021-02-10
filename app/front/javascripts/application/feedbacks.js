9a45ac8f429bf1f92c5000bd2ef231af567e58f9
clairezed <clairezuliani@gmail.com>
2021-02-10 12:46:05 +0100

    Change feedbacks js loop

--- app/front/javascripts/application/feedbacks.js
+++ app/front/javascripts/application/feedbacks.js
@@ -1,10 +1,11 @@
 (function () {
   addEventListener('turbolinks:load', function() {
     let feedbackLinks = document.getElementsByClassName( 'show-feedbacks-form' )
-    for ( let feedbackLink of feedbackLinks ) {
+    for ( let i = 0; i < feedbackLinks.length; i++ ) {
+      const feedbackLink = feedbackLinks[i];
       feedbackLink.onclick = function() {
-        let feedbackableId = feedbackLink.dataset.feedbackable
-        let form = document.getElementById("feedback-form-" + feedbackableId)
+        const feedbackableId = feedbackLink.dataset.feedbackable
+        const form = document.getElementById("feedback-form-" + feedbackableId)
         form.style.display = 'block'
       }
     }
