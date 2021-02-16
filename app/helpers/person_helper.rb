43300ca8455b82f918bba61723b05eb515bcb39f
Lucien <mollard.lucien@gmail.com>
2021-02-16 16:55:00 +0100

    add "Design System de l'État" and remix icons

--- app/helpers/person_helper.rb
+++ app/helpers/person_helper.rb
@@ -17,6 +17,25 @@ module PersonHelper
     person_block_raw(params.merge(extra_params))
   end
 
+  # Utilisé avec le nouveau design
+  def person_infos(person, extra_params = {})
+    return t('person.deleted_person') if person.nil?
+
+    params = {
+      name: person.full_name,
+      role: detailed_role(person),
+      email: person.email,
+      phone_number: person.phone_number
+    }
+    person_infos_raw(params.merge(extra_params))
+  end
+
+  def person_infos_raw(params)
+    defaults = { name: nil, prefix: nil, suffix: nil, role: nil, email: nil, phone_number: nil }
+    params = defaults.merge!(params)
+    render 'person_infos', params
+  end
+
   private
 
   def detailed_role(person)
