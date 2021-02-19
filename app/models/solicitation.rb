b41985f9266a106cbd4580fd987660d445d7dbd2
Lucien <mollard.lucien@gmail.com>
2021-02-19 10:45:04 +0100

    temporary fix invers_of

--- app/models/solicitation.rb
+++ app/models/solicitation.rb
@@ -40,7 +40,7 @@ class Solicitation < ApplicationRecord
   #
   belongs_to :landing, primary_key: :slug, foreign_key: :landing_slug, inverse_of: :solicitations, optional: true
   has_one :diagnosis, inverse_of: :solicitation
-  has_many :diagnosis_region, -> { regions }, through: :diagnosis, source: :facility_territories, inverse_of: :diagnosis
+  has_many :diagnosis_region, -> { regions }, through: :diagnosis, source: :facility_territories
 
   has_many :feedbacks, as: :feedbackable, dependent: :destroy
   has_many :matches, through: :diagnosis, inverse_of: :solicitation
