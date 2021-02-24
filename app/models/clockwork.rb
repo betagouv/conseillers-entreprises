90c28413f8fdecca4b675a4a4c2480c8c174592a
clairezed <clairezuliani@gmail.com>
2021-02-24 17:15:16 +0100

    Add clockwork task for code_region

--- app/models/clockwork.rb
+++ app/models/clockwork.rb
@@ -6,16 +6,16 @@ module Clockwork
   every(1.week, 'send_app_administrators_statistics_email', at: 'Monday 07:30') do
     AdminMailersService.delay.send_statistics_email
   end
-
   every(1.week, 'send_experts_reminders', at: 'Tuesday 9:00') do
     ExpertReminderService.delay.send_reminders
   end
-
   every(1.day, 'send_newsletter_subscription_emails', at: ('4:41')) do
     CompanyMailerService.delay.send_newsletter_subscription_emails
   end
-
   every(1.day, 'send_satisfaction_emails', at: ('5:41')) do
     CompanyMailerService.delay.send_satisfaction_emails
+  end
+  every(1.day, 'update_solicitations_code_region', at: ('3:41'), :if => lambda { |t| t.day == 1 }) do
+    `rake update_solicitations_code_region:from_diagnosis`
   end
 end
