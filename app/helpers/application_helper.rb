068aa10ab9eea9f342450be6ef2510ae59c395be
clairezed <clairezuliani@gmail.com>
2021-04-02 12:33:16 +0200

    Add canonical url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,18 @@ module ApplicationHelper
      end + tag.span("#{count}", class: "rf-tag rf-ml-2v")
     end
   end
+
+  # Inspiré de https://github.com/jumph4x/canonical-rails
+  def canonical_tags
+    canonical_url = raw "#{ENV['HOST_NAME']}#{path_without_html_extension}"
+    capture do
+      concat tag(:link, href: canonical_url, rel: :canonical)
+      concat tag(:meta, property: 'og:url', content: canonical_url)
+    end
+  end
+
+  def path_without_html_extension
+    return '' if request.path == '/'
+    request.path.sub(/\.html$/, '')
+  end
 end
