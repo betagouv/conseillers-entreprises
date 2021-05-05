330ddc3c4c420acdabe559094597e0727e0ff46d
clairezed <clairezuliani@gmail.com>
2021-05-05 10:38:06 +0200

    Display ld json

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -12,5 +12,22 @@ class AboutController < PagesController
       institutions = Institution.where(show_on_list: true).pluck(:name).sort
       institutions.each_slice((institutions.count.to_f / 4).ceil).to_a
     end
+    @faq = I18n.t('faq').values
+    # Rich snippets
+    faq_to_ld_json = @faq.each_with_object([]) do |item, new_array|
+      new_array << {
+        "@type": "Question",
+        name: item[:question],
+        acceptedAnswer: {
+          "@type": "Answer",
+          text: item[:answer]
+        }
+      }
+    end
+    @ld_json = {
+      "@context": "https://schema.org",
+      "@type": "FAQPage",
+      mainEntity: faq_to_ld_json
+    }.to_json
   end
 end
