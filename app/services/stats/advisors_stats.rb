a8a374618584dba34b3e43e15f60f6205eb42a86
clairezed <clairezuliani@gmail.com>
2021-05-07 22:23:13 +0200

    Remove these files ?

--- app/services/stats/advisors_stats.rb
+++ app/services/stats/advisors_stats.rb
@@ -1,40 +1,40 @@
-module Stats
-  class AdvisorsStats
-    include BaseStats
+# module Stats
+#   class AdvisorsStats
+#     include BaseStats
 
-    def main_query
-      User.all.distinct
-    end
+#     def main_query
+#       User.all.distinct
+#     end
 
-    def additive_values
-      true
-    end
+#     def additive_values
+#       true
+#     end
 
-    def date_group_attribute
-      'users.created_at'
-    end
+#     def date_group_attribute
+#       'users.created_at'
+#     end
 
-    def filtered(query)
-      if territory.present?
-        query.merge! territory.advisors
-      end
-      if institution.present?
-        query.merge! institution.advisors
-      end
+#     def filtered(query)
+#       if territory.present?
+#         query.merge! territory.advisors
+#       end
+#       if institution.present?
+#         query.merge! institution.advisors
+#       end
 
-      query
-    end
+#       query
+#     end
 
-    def category_group_attribute
-      Arel.sql('true')
-    end
+#     def category_group_attribute
+#       Arel.sql('true')
+#     end
 
-    def category_name(_)
-      I18n.t('attributes.advisors.other')
-    end
+#     def category_name(_)
+#       I18n.t('attributes.advisors.other')
+#     end
 
-    def category_order_attribute
-      Arel.sql('true')
-    end
-  end
-end
+#     def category_order_attribute
+#       Arel.sql('true')
+#     end
+#   end
+# end
--- app/services/stats/matches_stats.rb
+++ app/services/stats/matches_stats.rb
@@ -1,37 +1,37 @@
-module Stats
-  class MatchesStats
-    include BaseStats
+# module Stats
+#   class MatchesStats
+#     include BaseStats
 
-    def main_query
-      Match
-        .joins(:advisor)
-    end
+#     def main_query
+#       Match
+#         .joins(:advisor)
+#     end
 
-    def date_group_attribute
-      'matches.created_at'
-    end
+#     def date_group_attribute
+#       'matches.created_at'
+#     end
 
-    def filtered(query)
-      if territory.present?
-        query.merge! territory.matches
-      end
-      if institution.present?
-        query.merge! institution.sent_matches
-      end
+#     def filtered(query)
+#       if territory.present?
+#         query.merge! territory.matches
+#       end
+#       if institution.present?
+#         query.merge! institution.sent_matches
+#       end
 
-      query
-    end
+#       query
+#     end
 
-    def category_group_attribute
-      'matches.status'
-    end
+#     def category_group_attribute
+#       'matches.status'
+#     end
 
-    def category_name(category)
-      Match.human_attribute_value(:status, category)
-    end
+#     def category_name(category)
+#       Match.human_attribute_value(:status, category)
+#     end
 
-    def category_order_attribute
-      'matches.status'
-    end
-  end
-end
+#     def category_order_attribute
+#       'matches.status'
+#     end
+#   end
+# end
