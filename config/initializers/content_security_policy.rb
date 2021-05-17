7f6cd632fd980bd788dee4f5085a5be66bdab9aa
Lucien <mollard.lucien@gmail.com>
2021-05-17 16:36:58 +0200

    fix csp on application side

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -13,6 +13,7 @@ Rails.application.config.content_security_policy do |p|
 
   if Rails.env.development?
     p.script_src :self, :https, :unsafe_eval
+    p.connect_src :self, :https, 'http://localhost:3035', 'ws://localhost:3035'
   else
     p.script_src :self, :https, 'browser.sentry-cdn.com', 'stats.data.gouv.fr'
   end
