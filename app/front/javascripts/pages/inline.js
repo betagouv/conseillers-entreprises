467c5df8308e36d10afe5d5aaf7a2d97134fe8df
Lucien <mollard.lucien@gmail.com>
2021-05-25 15:01:09 +0200

    move js to open cookies box

--- app/front/javascripts/pages/inline.js
+++
@@ -1,7 +0,0 @@
-// Ouvre la boite gestion des cookies au clic sur le lien
-document.addEventListener("DOMContentLoaded", function() {
-  const cookiesLink = document.getElementById("open-cookies-box")
-  if (cookiesLink !== null) {
-    cookiesLink.onclick = function() { tarteaucitron.userInterface.openPanel();return false };
-  }
-});
