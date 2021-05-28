cfa04624cea90beb3d44a3e91c3f0eb79a8b6e57
clairezed <clairezuliani@gmail.com>
2021-05-28 12:17:30 +0200

    Try to customize 360 iframe

---
+++ app/helpers/iframe_custom_css_helper.rb
@@ -0,0 +1,11 @@
+module IframeCustomCssHelper
+  def martinique_custom_css
+    "section.section, section.section .landing-topic, section.section-grey {
+      background-color: white !important;
+    }
+    .card {
+      background-color: #ebeff3 !important;
+    }
+    "
+  end
+end
