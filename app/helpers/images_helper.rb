53aaf87bbcf59d8e2585fd294b353dda6ca78e3e
clairezed <clairezuliani@gmail.com>
2021-06-09 17:03:28 +0200

    Add mailer asset_host

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -7,6 +7,6 @@ module ImagesHelper
     path = possible_paths.find{ |path| resolve_asset_path(path, true) }
     params = { alt: name.strip.titleize.capitalize }
     params.merge! extra_params
-    image_tag(root_url + image_path(path), params) if path
+    image_tag(image_path(path), params) if path
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.action_mailer.asset_host = "http://#{ENV['HOST_NAME']}:#{ENV['PORT']}"
+
   # Raises error for missing translations
   config.action_view.raise_on_missing_translations = false
 
