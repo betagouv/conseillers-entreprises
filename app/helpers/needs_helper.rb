be1b65e7d2b74276be7a4ef71aee6484e3ea6139
Lucien <mollard.lucien@gmail.com>
2021-06-09 17:20:54 +0200

    wip

---
+++ app/helpers/needs_helper.rb
@@ -0,0 +1,13 @@
+
+module NeedsHelper
+  def need_general_context(need)
+    context = ""
+    if need.diagnosis.content.present? || need.solicitation&.description.present?
+      context << simple_format(need.diagnosis.content.presence || need.solicitation&.description, class: 'content')
+    end
+    if need.content.present?
+      context << simple_format(need.content, class: 'content')
+    end
+    context
+  end
+end
