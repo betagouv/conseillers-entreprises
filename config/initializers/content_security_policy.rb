fa57495efe4a14ac1806b72b63f4bfd9a1d9a21a
clairezed <clairezuliani@gmail.com>
2021-06-18 18:01:13 +0200

    open bar

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -12,7 +12,7 @@ Rails.application.config.content_security_policy do |p|
   p.img_src     :self, :data, 'https://voxusagers.numerique.gouv.fr', 'https://stats.data.gouv.fr', 'https://www.google.com', 'https://www.google.fr', 'https://googleads.g.doubleclick.net', 'https://www.googletagmanager.com', 'https://www.googleadservices.com', 'https://www.gstatic.com',	'https://adservice.google.com'
   p.object_src  :none
   p.style_src   :self, :unsafe_inline, 'https://fonts.googleapis.com', 'https://www.ssa.gov'
-  p.script_src  :self, :blob, 'https://browser.sentry-cdn.com', 'sentry.io', 'https://stats.data.gouv.fr', 'https://cdn.jsdelivr.net', 'https://www.googletagmanager.com', 'https://www.googleadservices.com', 'https://googleads.g.doubleclick.net', 'https://www.google.com', 'https://www.ssa.gov'
+  p.script_src  :self, :unsafe_inline, :blob, 'https://browser.sentry-cdn.com', 'sentry.io', 'https://stats.data.gouv.fr', 'https://cdn.jsdelivr.net', 'https://www.googletagmanager.com', 'https://www.googleadservices.com', 'https://googleads.g.doubleclick.net', 'https://www.google.com', 'https://www.ssa.gov'
   p.script_src_elem :self, 'https://browser.sentry-cdn.com', 'sentry.io', 'https://stats.data.gouv.fr', 'https://cdn.jsdelivr.net', 'https://www.googletagmanager.com', 'https://www.googleadservices.com', 'https://googleads.g.doubleclick.net', 'https://www.google.com'
   p.frame_src :self, 'stats.data.gouv.fr', 'https://bid.g.doubleclick.net', 'browser.sentry-cdn.com', 'cdn.jsdelivr.net'
 
