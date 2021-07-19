fa83a7706f424ce19984660a90201f955565e99b
clairezed <clairezuliani@protonmail.com>
2021-07-19 10:23:17 +0200

    Ajout tests sollicitation

--- spec/system/new_solicitation_system_spec.rb
+++ spec/system/new_solicitation_system_spec.rb
@@ -5,34 +5,51 @@ require 'rails_helper'
 describe 'New Solicitation', type: :system, js: true do
   let(:landing) { create :landing, slug: 'test-landing', home_sort_order: 0, home_title: 'Test Landing' }
   let(:landing_option) { create :landing_option, landing: landing, requires_siret: true, requires_email: true }
-  let(:landing_topic) { create :landing_topic, title: 'landing topic test', landing: landing, landing_option_slug: landing_option.slug }
-
-  before do
-    Rails.cache.clear
-    landing_topic
-  end
+  let!(:landing_topic) { create :landing_topic, title: 'landing topic test', landing: landing, landing_option_slug: landing_option.slug }
 
   describe 'post solicitation' do
     let(:solicitation) { Solicitation.last }
 
-    before do
-      visit '/?pk_campaign=FOO&pk_kwd=BAR'
-      click_link 'Test Landing'
-      # Find 'Choose' link
-      find("#section-exemples > div > div.landing-topics > div.landing-topic > h3 > a").click
+    context "from pk campaign" do
+      before do
+        visit '/?pk_campaign=FOO&pk_kwd=BAR'
+        click_link 'Test Landing'
+        # Find 'Choose' link
+        find("#section-exemples > div > div.landing-topics > div.landing-topic > h3 > a").click
 
-      fill_in 'Description', with: 'Ceci est un test'
-      fill_in 'SIRET', with: '123 456 789 00010'
-      fill_in 'E-mail', with: 'user@exemple.com'
-      click_button 'Envoyer ma demande'
+        fill_in 'Description', with: 'Ceci est un test'
+        fill_in 'SIRET', with: '123 456 789 00010'
+        fill_in 'E-mail', with: 'user@exemple.com'
+        click_button 'Envoyer ma demande'
+      end
+
+      it do
+        expect(page).to have_content('Merci')
+        expect(solicitation.landing_slug).to eq 'test-landing'
+        expect(solicitation.siret).to eq '123 456 789 00010'
+        expect(solicitation.pk_campaign).to eq 'FOO'
+        expect(solicitation.pk_kwd).to eq 'BAR'
+      end
     end
 
-    it do
-      expect(page).to have_content('Merci')
-      expect(solicitation.landing_slug).to eq 'test-landing'
-      expect(solicitation.siret).to eq '123 456 789 00010'
-      expect(solicitation.pk_campaign).to eq 'FOO'
-      expect(solicitation.pk_kwd).to eq 'BAR'
+    context "from home page" do
+      before do
+        visit '/'
+        click_link 'Test Landing'
+        click_link 'landing topic test'
+
+        fill_in 'Description', with: 'Ceci est un test'
+        fill_in 'SIRET', with: '123 456 789 00010'
+        fill_in 'E-mail', with: 'user@example.com'
+        click_button 'Envoyer ma demande'
+      end
+
+      it do
+        expect(page).to have_content('Merci')
+        expect(solicitation.landing_slug).to eq 'test-landing'
+        expect(solicitation.siret).to eq '123 456 789 00010'
+        expect(solicitation.pk_campaign).to eq nil
+      end
     end
   end
 
@@ -68,7 +85,42 @@ describe 'New Solicitation', type: :system, js: true do
         expect(option).to have_content('OCTO-TECHNOLOGY')
         page.execute_script("document.querySelector('.autocomplete__option').click()")
 
-        # expect(page).to have_field("solicitation_siret_autocomplete", with: '41816609600051 - OCTO-TECHNOLOGY', wait: 5)
+        expect(page).to have_field("SIRET", with: '41816609600051 - OCTO-TECHNOLOGY', wait: 2)
+
+        click_button 'Envoyer ma demande'
+        expect(solicitation.siret).to eq '41816609600051'
+        expect(solicitation.code_region).to eq 11
+      end
+    end
+  end
+
+  describe 'solicitation with fulltext search' do
+    let(:base_url) { 'https://entreprise.data.gouv.fr/api/sirene/v1/full_text' }
+
+    context 'choose autocomplete choice' do
+      let(:solicitation) { Solicitation.last }
+      let(:search) { 'octo technology' }
+      let(:url) { "#{base_url}/#{search}" }
+
+      before do
+        stub_request(:get, url).to_return(
+          body: file_fixture('entreprise_data_gouv_full_text.json')
+        )
+      end
+
+      # Not working, for now
+      xit 'correctly sets siret and code_region' do
+        visit '/'
+        click_link 'Test Landing'
+        find("#section-exemples > div > div.landing-topics > div.landing-topic > h3 > a").click
+        fill_in 'E-mail', with: 'user@exemple.com'
+        fill_in 'Description', with: 'Ceci est un test'
+        fill_in 'SIRET', with: 'octo technology'
+        option = find(".autocomplete__option")
+        expect(option).to have_content('OCTO-TECHNOLOGY')
+        # page.execute_script("document.querySelector('.autocomplete__option').click()")
+        find(".autocomplete__option", match: :first).click
+
         expect(page).to have_field("SIRET", with: '41816609600051 - OCTO-TECHNOLOGY', wait: 5)
 
         click_button 'Envoyer ma demande'
