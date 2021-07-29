e920d6b18dcfce0976e72dfc127cb826f3362484
clairezed <clairezuliani@protonmail.com>
2021-07-29 15:22:58 +0200

    Add landing layout management

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,6 +1,6 @@
 class LandingsController < PagesController
   before_action :save_form_info, only: %i[index show]
-  before_action :retrieve_landing, except: %i[index home]
+  before_action :retrieve_landing, only: %i[show]
 
   include IframePrefix
 
@@ -21,22 +21,17 @@ class LandingsController < PagesController
 
   def show; end
 
-  def new_solicitation
-    @solicitation = @landing.solicitations.new(landing_options_slugs: [params[:option_slug]].compact)
-  end
-
-  def create_solicitation
-    sanitized_params = sanitize_params(solicitation_params).merge(retrieve_form_info)
-    @solicitation = SolicitationModification::Create.call(sanitized_params)
-    if @solicitation.persisted?
-      CompanyMailer.confirmation_solicitation(@solicitation).deliver_later
-      @solicitation.delay.prepare_diagnosis(nil)
-    end
-
-    render :new_solicitation # rerender the form on error, render the thankyou partial on success
-  end
-
   private
+
+  def retrieve_landing
+    slug = params[:slug]&.to_sym
+    @landing = Rails.cache.fetch("landing-#{slug}", expires_in: 1.minute) do
+      Landing.find_by(slug: slug)
+    end
+    @landing_themes = @landing&.landing_themes&.order(:position)
+
+    redirect_to root_path, status: :moved_permanently if @landing.nil?
+  end
 
   def save_form_info
     form_info = session[:solicitation_form_info] || {}
@@ -45,32 +40,8 @@ class LandingsController < PagesController
     session[:solicitation_form_info] = form_info if form_info.present?
   end
 
-  def retrieve_form_info
-    # Les params ne passent pas en session dans les iframe, raison pour laquelle on check ici aussi les params de l'url
-    form_info = session[:solicitation_form_info] || {}
-    info_params = show_params.slice(*Solicitation::FORM_INFO_KEYS)
-    form_info.merge!(info_params)
-    session.delete(:solicitation_form_info)
-    { form_info: form_info }
-  end
-
-  def retrieve_landing
-    slug = params[:slug]&.to_sym
-    @landing = Rails.cache.fetch("landing-#{slug}", expires_in: 1.minute) do
-      Landing.find_by(slug: slug)
-    end
-
-    redirect_to root_path, status: :moved_permanently if @landing.nil?
-  end
 
   def show_params
     params.permit(:slug, *Solicitation::FORM_INFO_KEYS)
-  end
-
-  def solicitation_params
-    params.require(:solicitation)
-      .permit(:landing_slug, :description, :code_region,
-              *Solicitation::FIELD_TYPES.keys,
-              landing_options_slugs: [])
   end
 end
