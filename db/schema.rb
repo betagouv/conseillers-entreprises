88b9c2ce0404cbd3b49888834826d7dfbf773abc
Lucien <mollard.lucien@gmail.com>
2021-08-17 12:02:50 +0200

    add status reminder for solicitations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_29_125841) do
+ActiveRecord::Schema.define(version: 2021_08_17_084817) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
