a5d9f9a6e6a5b5007aaee01f598c4d5bd089ee79
Lucien <mollard.lucien@gmail.com>
2021-09-14 17:19:10 +0200

    change 'region_image' to 'prefet_region_logo'

--- app/helpers/regions_helper.rb
+++ app/helpers/regions_helper.rb
@@ -1,10 +1,16 @@
 module RegionsHelper
-  def region_image(region_code)
-    slug = I18n.t(region_code, scope: 'regions_codes_to_slugs')
-    name = I18n.t(region_code, scope: 'regions_codes_to_libelles')
-    possible_paths = "regions/#{slug}.png", "regions/#{slug}.svg", "regions/#{slug}.jpg"
+  def prefet_region_logo(region_code)
+    slug, name = find_name_and_slug(region_code)
+    possible_paths = "prefets_regions/#{slug}.png", "prefets_regions/#{slug}.svg", "prefets_regions/#{slug}.jpg"
     path = possible_paths.find{ |path| resolve_asset_path(path, true) }
     params = { alt: t('logos.prefet_region', name: name), class: 'institution_logo' }
     image_tag(path, params) if path
+  end
+  private
+
+  def find_name_and_slug(region_code)
+    slug = I18n.t(region_code, scope: 'regions_codes_to_slugs')
+    name = I18n.t(region_code, scope: 'regions_codes_to_libelles')
+    [slug, name]
   end
 end
