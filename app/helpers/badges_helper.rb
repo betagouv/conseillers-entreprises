23a2d6da4548f7096300d7cfbf40a533daf74aa8
clairezed <clairezuliani@protonmail.com>
2021-10-06 15:55:16 +0200

    Update solicitation card design

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -2,7 +2,7 @@
 
 module BadgesHelper
   def badge_label(badge)
-    tag.div(badge.title, class: 'ui basic label badge',
+    tag.div(badge.title, class: 'label',
                 style: "border: 1px solid #{badge.color}; color: #{badge.color}")
   end
 end
