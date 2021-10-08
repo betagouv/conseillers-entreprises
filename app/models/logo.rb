64c7677877df21de8cb39e21dc39003fe1f04af4
clairezed <clairezuliani@protonmail.com>
2021-10-08 12:09:10 +0200

    Add missing logo institution links

--- app/models/logo.rb
+++ app/models/logo.rb
@@ -2,11 +2,20 @@
 #
 # Table name: logos
 #
-#  id         :bigint(8)        not null, primary key
-#  name       :string
-#  slug       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id             :bigint(8)        not null, primary key
+#  filename       :string
+#  name           :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  institution_id :bigint(8)
+#
+# Indexes
+#
+#  index_logos_on_institution_id  (institution_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (institution_id => institutions.id)
 #
 class Logo < ApplicationRecord
   has_and_belongs_to_many :landing_subjects, inverse_of: :logos
