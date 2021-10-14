db7f5c7948b17b5552668b8ae4ae070cda5e20c7
Lucien <mollard.lucien@gmail.com>
2021-10-14 16:34:00 +0200

    add video

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -14,7 +14,7 @@ Rails.application.config.content_security_policy do |p|
   p.style_src   :self, :unsafe_inline, 'https://www.ssa.gov', 'https://fonts.googleapis.com'
   p.script_src  :self, :blob, 'https://browser.sentry-cdn.com/', 'sentry.io', 'https://stats.data.gouv.fr', 'https://cdn.jsdelivr.net', 'https://www.ssa.gov', 'https://www.googletagmanager.com', 'https://www.googleadservices.com', 'https://googleads.g.doubleclick.net', 'https://www.google.com'
   p.script_src_elem :self, 'https://browser.sentry-cdn.com/', 'sentry.io', 'https://stats.data.gouv.fr', 'https://cdn.jsdelivr.net', 'www.pagespeed-mod.com', 'https://www.googletagmanager.com', 'https://www.googleadservices.com', 'https://googleads.g.doubleclick.net', 'https://www.google.com'
-  p.frame_src :self, 'stats.data.gouv.fr', 'browser.sentry-cdn.com/', 'cdn.jsdelivr.net', 'https://bid.g.doubleclick.net'
+  p.frame_src :self, 'stats.data.gouv.fr', 'browser.sentry-cdn.com/', 'cdn.jsdelivr.net', 'https://bid.g.doubleclick.net', 'https://tube.nocturlab.fr'
   if Rails.env.development?
     p.connect_src :self, 'localhost:3035', 'ws://localhost:3035', 'https://api-adresse.data.gouv.fr/'
   else
