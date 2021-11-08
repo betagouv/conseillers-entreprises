9f550eea8a284c39f50ba44b0bef02024eeb7c66
Lucien <mollard.lucien@gmail.com>
2021-11-08 17:01:32 +0100

    add tac service for facebook

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -9,16 +9,16 @@ Rails.application.config.content_security_policy do |p|
   p.default_src :self
   p.base_uri    :self
   p.font_src    :self, :data, 'https://github.com', 'https://fonts.gstatic.com'
-  p.img_src     :self, :data, 'https://voxusagers.numerique.gouv.fr', 'https://stats.data.gouv.fr', 'https://www.google.com', 'https://www.google.fr', 'https://googleads.g.doubleclick.net', 'https://www.googletagmanager.com', 'https://www.googleadservices.com', 'https://www.gstatic.com',	'https://adservice.google.com'
+  p.img_src     :self, :data, 'https://voxusagers.numerique.gouv.fr', 'https://stats.data.gouv.fr', 'https://www.google.com', 'https://www.google.fr', 'https://googleads.g.doubleclick.net', 'https://www.googletagmanager.com', 'https://www.googleadservices.com', 'https://www.gstatic.com',	'https://adservice.google.com', 'https://www.facebook.com'
   p.object_src  :none
   p.style_src   :self, :unsafe_inline, 'https://www.ssa.gov', 'https://fonts.googleapis.com'
-  p.script_src  :self, :blob, 'https://browser.sentry-cdn.com/', 'sentry.io', 'https://stats.data.gouv.fr', 'https://cdn.jsdelivr.net', 'https://www.ssa.gov', 'https://www.googletagmanager.com', 'https://www.googleadservices.com', 'https://googleads.g.doubleclick.net', 'https://www.google.com'
+  p.script_src  :self, :blob, 'https://browser.sentry-cdn.com/', 'sentry.io', 'https://stats.data.gouv.fr', 'https://cdn.jsdelivr.net', 'https://www.ssa.gov', 'https://www.googletagmanager.com', 'https://www.googleadservices.com', 'https://googleads.g.doubleclick.net', 'https://www.google.com', 'https://connect.facebook.net'
   p.script_src_elem :self, 'https://browser.sentry-cdn.com/', 'sentry.io', 'https://stats.data.gouv.fr', 'https://cdn.jsdelivr.net', 'www.pagespeed-mod.com', 'https://www.googletagmanager.com', 'https://www.googleadservices.com', 'https://googleads.g.doubleclick.net', 'https://www.google.com'
   p.frame_src :self, 'stats.data.gouv.fr', 'browser.sentry-cdn.com/', 'cdn.jsdelivr.net', 'https://bid.g.doubleclick.net', 'https://tube.nocturlab.fr'
   if Rails.env.development?
     p.connect_src :self, 'localhost:3035', 'ws://localhost:3035', 'https://api-adresse.data.gouv.fr/'
   else
-    p.connect_src :self, '*.sentry.io', 'https://api-adresse.data.gouv.fr/', 'https://www.google.com', 'https://adservice.google.com', 'https://stats.data.gouv.fr'
+    p.connect_src :self, '*.sentry.io', 'https://api-adresse.data.gouv.fr/', 'https://www.google.com', 'https://adservice.google.com', 'https://stats.data.gouv.fr', 'https://www.facebook.com'
     if ENV["CSP_REPORT_URI"].present?
       p.report_uri ENV["CSP_REPORT_URI"]
     end
