b1a73f4d3c2fea65448b1581aa5cbabaafbdca6a
Lucien <mollard.lucien@gmail.com>
2021-12-02 15:42:52 +0100

    raise_on_missing_translations for i18n

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_mailer.asset_host = "http://#{ENV['HOST_NAME']}:#{ENV['PORT']}"
 
   # Raises error for missing translations
-  config.i18n.raise_on_missing_translations = false
+  config.i18n.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
