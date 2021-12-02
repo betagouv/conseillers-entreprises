a96e2dcf3c78453d290907bdb4566bdc48dc3755
clairezed <clairezuliani@protonmail.com>
2021-12-02 12:29:11 +0100

    Silence falsy sentry error

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,4 +3,5 @@ Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger]
   config.traces_sample_rate = 0.5
   config.send_default_pii = false
+  config.ignoreErrors = ['Non-Error promise rejection captured']
 end
