fdb2c94984e69df96202a886d0453f297db92929
Lucien <mollard.lucien@gmail.com>
2022-01-04 17:30:11 +0100

    some improvements

--- app/admin/helpers/links.rb
+++ app/admin/helpers/links.rb
@@ -2,18 +2,17 @@ module Admin
   module Helpers
     module Links
       def needs_links(needs)
-        needs_links = needs.map do |need|
+        needs.map do |need|
           I18n.t('active_admin.solicitations.need_show') + link_to(need.subject.label, need_path(need))
-        end
-        needs_links.join('</br>')
+        end.join('</br>')
       end
 
       def diagnosis_link(diagnosis)
-        if diagnosis&.step_completed?
-          "#{I18n.t('activerecord.models.diagnosis.one')} : #{admin_link_to(diagnosis)}"
-        elsif diagnosis.present? && !diagnosis.step_completed?
-          "#{I18n.t('activerecord.models.diagnosis.one')} : #{admin_link_to(diagnosis)} #{I18n.t('active_admin.solicitations.diagnosis_in_progress')}"
+        if diagnosis.present?
+          link = "#{I18n.t('activerecord.models.diagnosis.one')} : #{admin_link_to(diagnosis)}"
+          link += I18n.t('active_admin.solicitations.diagnosis_in_progress') unless diagnosis.step_completed?
         end
+        link
       end
     end
 
