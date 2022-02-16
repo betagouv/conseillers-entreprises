0c5d104226ab79b7e9751e6a49ac7f24665f3415
Lucien <mollard.lucien@gmail.com>
2022-02-16 13:57:56 +0100

    add title for annuaire icons

--- app/helpers/annuaire_helper.rb
+++ app/helpers/annuaire_helper.rb
@@ -12,9 +12,9 @@ module AnnuaireHelper
 
   def build_user_name_cell(user, antenne)
     html = link_to(user.full_name, edit_admin_expert_path(user.relevant_expert), title: t('annuaire_helper.build_user_name_cell.edit_expert', expert_name: user.relevant_expert.full_name, antenne: antenne))
-    html << tag.span(class: 'ri-mail-add-fill blue fr-ml-1v') if user.invitation_sent_at.nil?
-    html << tag.span(class: 'ri-nurse-fill blue fr-ml-1v') if user.role_antenne_manager?
-    html << tag.span(class: 'ri-map-2-line blue fr-ml-1v') if user.relevant_expert.communes.any?
+    html << tag.span(class: 'ri-mail-add-fill blue fr-ml-1v', title: t('annuaire_helper.build_user_name_cell.not_invited')) if user.invitation_sent_at.nil?
+    html << tag.span(class: 'ri-nurse-fill blue fr-ml-1v', title: t('annuaire_helper.build_user_name_cell.antenne_manager')) if user.role_antenne_manager?
+    html << tag.span(class: 'ri-map-2-line blue fr-ml-1v', title: t('annuaire_helper.build_user_name_cell.specific_territories')) if user.relevant_expert.communes.any?
     html
   end
 
