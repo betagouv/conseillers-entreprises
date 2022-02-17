b1e8aed54c4f4697b2cd83b118c28e6edf9ced8b
Lucien <mollard.lucien@gmail.com>
2022-02-17 12:08:22 +0100

    display region for solicitations

---
+++ spec/helpers/solicitations_helper_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe SolicitationHelper, type: :helper do
+  describe 'display_region' do
+    context 'without region filter' do
+      let(:region) { create :territory, :region }
+
+      subject { helper.display_region(region, nil) }
+
+      it 'return region' do
+        is_expected.to eq "<div class=\"item\">#{I18n.t('helpers.solicitation.localisation_html', region: region.name)}</div>"
+      end
+    end
+
+    context 'with region filter' do
+      let(:region) { create :territory, :region }
+
+      subject { helper.display_region(region, 'Region Bretagne') }
+
+      it 'return nothing' do
+        is_expected.to be nil
+      end
+    end
+
+    context 'without region for solicitation' do
+      let(:region) { nil }
+
+      subject { helper.display_region(region, nil) }
+
+      it 'return nothing' do
+        is_expected.to be nil
+      end
+    end
+  end
+end
