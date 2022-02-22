e6e5278f4d8de4a9d50752655f9e05935c40754c
clairezed <clairezuliani@protonmail.com>
2022-02-22 16:33:26 +0100

    Utilise le scope en cours pour export csv des MER

--- app/admin/concerns/csv_exportable.rb
+++ app/admin/concerns/csv_exportable.rb
@@ -9,7 +9,9 @@ module CsvExportable
     ## Using `.send` because the ResourceDSL methods are (wrongly) private
     # See https://github.com/activeadmin/activeadmin/issues/3673#issuecomment-291267819
     dsl.send(:collection_action, :export_csv, method: :post) do
-      ransack_params = params.slice(:q).permit(q: {})[:q]
+      # on integre le scope en cours, a autoriser via self.ransackable_scopes si autre que all
+      scope = current_scope.scope_method
+      ransack_params = params.slice(:q).permit(q: {})[:q].merge({scope => true})
       CsvJob.perform_later(resource_class.name, ransack_params, current_user)
       flash.notice = t('active_admin.csv_export_launched')
       redirect_back fallback_location: admin_root_path
