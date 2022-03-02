c4da87851e87df74a6446b6852c65d4414d609b9
clairezed <clairezuliani@protonmail.com>
2022-03-02 14:23:14 +0100

    Add stats trimestrielles

--- app/services/xlsx_export/antenne_stats_exporter.rb
+++ app/services/xlsx_export/antenne_stats_exporter.rb
@@ -12,92 +12,87 @@ module XlsxExport
 
       create_styles wb.styles
 
-      wb.add_worksheet(name: "stats") do |sheet|
-        sheet.add_row ["#{@antenne.name} - #{@end_date.year}T#{TimeDurationService.find_quarter(@start_date.month, 'to_number')}"], style: @header
-        # sheet.add_row [antenne.name], style: header
+      wb.add_worksheet(name: I18n.t('antenne_stats_exporter.quarter_stats')) do |sheet|
+        sheet.add_row ["#{@antenne.name} - #{@end_date.year}T#{TimeDurationService.find_quarter(@start_date.month, 'to_number')}"], style: @title
+        sheet.add_row [I18n.t('antenne_stats_exporter.subtitle')], style: @subtitle
 
         sheet.add_row
-        sheet.add_row [I18n.t('antenne_stats_exporter.needs'), needs.size]
-        sheet.add_row [I18n.t('antenne_stats_exporter.matches'), matches.size]
-        sheet.add_row [I18n.t('antenne_stats_exporter.facilities'), facilities.size]
+        sheet.add_row [I18n.t('antenne_stats_exporter.needs'), needs.size], style: [@bold, nil]
+        sheet.add_row [I18n.t('antenne_stats_exporter.matches'), matches.size], style: [@bold, nil]
+        sheet.add_row [I18n.t('antenne_stats_exporter.facilities'), facilities.size], style: [@bold, nil]
         sheet.add_row
 
         ordered_match_statuses = [:done_not_reachable, :done, :done_no_help, :not_for_me, :quo, :taking_care]
         ordered_positionning_statuses = [:positionning, :positionning_accepted, :done, :not_for_me, :quo, nil]
 
-        sheet.add_row [I18n.t('attributes.match_status'), I18n.t('antenne_stats_exporter.count'), I18n.t('antenne_stats_exporter.percentage'),nil, I18n.t('antenne_stats_exporter.answer_rate'), I18n.t('antenne_stats_exporter.count'), I18n.t('antenne_stats_exporter.percentage')], style: [@left_header, @right_header, @right_header, nil, @left_header, @right_header, @right_header]
+        sheet.add_row [
+          I18n.t('attributes.match_status'),
+          I18n.t('antenne_stats_exporter.count'),
+          I18n.t('antenne_stats_exporter.percentage'),nil,
+          I18n.t('antenne_stats_exporter.answer_rate'),
+          I18n.t('antenne_stats_exporter.count'),
+          I18n.t('antenne_stats_exporter.percentage')
+        ], style: [@left_header, @right_header, @right_header, nil, @left_header, @right_header, @right_header]
+
         (0...ordered_match_statuses.size).to_a.each do |index|
-          status = ordered_match_statuses[index]
+          match_status = ordered_match_statuses[index]
+          match_status_size = matches.send("status_#{match_status}")&.size
           positionning_status = ordered_positionning_statuses[index]
-          match_status_size = matches.send("status_#{status}")&.size
           positionning_status_size = calculate_positionning_status_size(positionning_status)
           sheet.add_row [
-            I18n.t("activerecord.attributes.match/statuses/short.#{status}"),
+            I18n.t("activerecord.attributes.match/statuses/short.#{match_status}"),
             match_status_size,
-            match_status_rate(match_status_size), nil,
+            match_rate(match_status_size), nil,
             positionning_status.present? ? I18n.t("antenne_stats_exporter.#{positionning_status}_rate") : nil,
             positionning_status_size,
-            match_status_rate(positionning_status_size)
+            match_rate(positionning_status_size)
           ], style: count_rate_row_style
         end
         sheet.add_row
 
+        ordered_need_statuses = [:done, :done_no_help, :done_not_reachable, :quo, :not_for_me, :taking_care]
+        # On ne prend que les thèmes principaux
+        themes = Theme.for_interview.limit(10)
 
-        # sheet.add_row ['Estimated monthly net income', 500, nil, 'Monthly income', '=C9'], style: [@label, @money, nil, @label, @money]
-        # sheet.add_row ['Financial aid', 100, nil, 'Monthly expenses', '=C27'], style: [@label, @money, nil, @label, @money]
-        # sheet.add_row ['Allowance from mom & dad', 20000, nil, 'Semester expenses', '=F19'], style: [@label, @money, nil, @label, @money]
-        # sheet.add_row ['Total', '=SUM(C6:C8)', nil, 'Difference', '=F6 - SUM(F7:F8)'], style: [t_@label, t_@money, nil, t_@label, t_@money]
+        sheet.add_row [
+          I18n.t('attributes.need_status'),
+          I18n.t('antenne_stats_exporter.count'),
+          I18n.t('antenne_stats_exporter.percentage'),nil,
+          Theme.model_name.human,
+          I18n.t('antenne_stats_exporter.count'),
+          I18n.t('antenne_stats_exporter.percentage')
+        ], style: [@left_header, @right_header, @right_header, nil, @left_header, @right_header, @right_header]
 
-        # sheet.add_row
-        # sheet.add_row ["What's going out this month.", nil, nil, 'Semester Costs'], style: tbl_header
-        # sheet.add_row ['Item', 'Amount', nil, 'Item', 'Amount'], style: [@left_header, @right_header, nil, @left_header, @right_header]
-        # sheet.add_row ['Rent', 650, nil, 'Tuition', 200], style: [@label, @money, nil, @label, @money]
-        # sheet.add_row ['Utilities', 120, nil, 'Lab fees', 50], style: [@label, @money, nil, @label, @money]
-        # sheet.add_row ['Cell phone', 100, nil, 'Other fees', 10], style: [@label, @money, nil, @label, @money]
-        # sheet.add_row ['Groceries', 75, nil, 'Books', 150], style: [@label, @money, nil, @label, @money]
-        # sheet.add_row ['Auto expenses', 0, nil, 'Deposits', 0], style: [@label, @money, nil, @label, @money]
-        # sheet.add_row ['Student loans', 0, nil, 'Transportation', 30], style: [@label, @money, nil, @label, @money]
-        # sheet.add_row ['Other loans', 350, nil, 'Total', '=SUM(F13:F18)'], style: [@label, @money, nil, t_@label, t_@money]
-        # sheet.add_row ['Credit cards', 450], style: [@label, @money]
-        # sheet.add_row ['Insurance', 0], style: [@label, @money]
-        # sheet.add_row ['Laundry', 10], style: [@label, @money]
-        # sheet.add_row ['Haircuts', 0], style: [@label, @money]
-        # sheet.add_row ['Medical expenses', 0], style: [@label, @money]
-        # sheet.add_row ['Entertainment', 500], style: [@label, @money]
-        # sheet.add_row ['Miscellaneous', 0], style: [@label, @money]
-        # sheet.add_row ['Total', '=SUM(C13:C26)'], style: [t_@label, t_@money]
+        (0...themes.size).to_a.each do |index|
+          need_status = ordered_need_statuses[index]
+          need_status_size = needs.send("status_#{need_status}")&.size if need_status.present?
+          theme = Theme.find_by(interview_sort_order: index+1)
+          needs_by_theme_size = calculate_needs_by_theme_size(theme)
+          sheet.add_row [
+            need_status.present? ? I18n.t("activerecord.attributes.need/statuses/csv.#{need_status}") : nil,
+            need_status_size || nil,
+            need_rate(need_status_size), nil,
+            theme&.label,
+            needs_by_theme_size,
+            need_rate(needs_by_theme_size)
+          ], style: count_rate_row_style
+        end
+        sheet.add_row
 
-        # [
-        #   'B4:C4',
-        #   'E4:F4',
-        #   'B11:C11',
-        #   'E11:F11',
-        #   'B2:F2'
-        # ].each { |range| sheet.merge_cells(range) }
+        [
+          'A1:G1',
+          'A2:G2',
+        ].each { |range| sheet.merge_cells(range) }
 
-        # sheet.column_widths 2, nil, nil, 2, nil, nil, 2
+        sheet.column_widths 50, 8, 8, 2, 50, 8, 8
       end
 
       p.use_shared_strings = true
       p.to_stream.read
     end
 
-    def create_styles(s)
-      @header     = s.add_style bg_color: 'DD', sz: 16, b: true, alignment: { horizontal: :center }
-      @tbl_header = s.add_style b: true, alignment: { horizontal: :center }
-      @left_header = s.add_style bg_color: 'FFDFDEDF', b: true, alignment: { horizontal: :left }
-      @right_header = s.add_style bg_color: 'FFDFDEDF', b: true, alignment: { horizontal: :right }
-      @label      = s.add_style alignment: { indent: 1 }
-      @rate      = s.add_style format_code: '#0.0%'
-      @t_label    = s.add_style b: true, bg_color: 'FFDFDEDF'
-      @t_money    = s.add_style b: true, num_fmt: 5, bg_color: 'FFDFDEDF'
-      s
-    end
-
-    def count_rate_row_style
-      [@label, nil, @rate, nil, @label, nil, @rate]
-    end
-
+    # Base variables
+    #
     def needs
       @needs ||= @antenne.received_needs.joins(:matches).created_between(@start_date, @end_date).distinct
     end
@@ -110,11 +105,33 @@ module XlsxExport
       @facilities ||= @antenne.received_facilities.joins(diagnoses: :needs).where(diagnoses: { needs: needs }).distinct
     end
 
+    # Style
+    #
+    def create_styles(s)
+      @title        = s.add_style bg_color: 'DD', sz: 16, b: true, alignment: { horizontal: :center, vertical: :center }
+      @subtitle     = s.add_style bg_color: 'DD', sz: 14, b: true, alignment: { horizontal: :center, vertical: :center }
+      @bold         = s.add_style b: true
+      @left_header  = s.add_style bg_color: 'FFDFDEDF', b: true, alignment: { horizontal: :left }
+      @right_header = s.add_style bg_color: 'FFDFDEDF', b: true, alignment: { horizontal: :right }
+      @label      = s.add_style alignment: { indent: 1 }
+      @rate      = s.add_style format_code: '#0.0%'
+      s
+    end
+
+    def count_rate_row_style
+      [@label, nil, @rate, nil, @label, nil, @rate]
+    end
+
     # Calculation
     #
-    def match_status_rate(status_size)
+    def match_rate(status_size)
       return unless status_size
       status_size / matches.size.to_f
+    end
+
+    def need_rate(status_size)
+      return unless status_size
+      status_size / needs.size.to_f
     end
 
     def calculate_positionning_status_size(status)
@@ -129,6 +146,10 @@ module XlsxExport
       else
         matches.send("status_#{status}")&.size
       end
+    end
+
+    def calculate_needs_by_theme_size(theme)
+      needs.joins(subject: :theme).where(subject: {theme: theme}).size
     end
   end
 end
