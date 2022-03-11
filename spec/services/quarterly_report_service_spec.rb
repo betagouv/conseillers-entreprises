771012b0f59b5978270b5536f4c3298341d15e10
clairezed <clairezuliani@protonmail.com>
2022-03-11 12:10:13 +0100

    Fix quarters tests

--- spec/services/quarterly_report_service_spec.rb
+++ spec/services/quarterly_report_service_spec.rb
@@ -3,30 +3,58 @@
 require 'rails_helper'
 describe QuarterlyReportService do
   describe 'last_quarters' do
-    let(:antenne) { create :antenne }
     let(:quarters) { described_class.send(:last_quarters, antenne) }
 
-    context 'with no old matches' do
-      it 'return nothing' do
-        expect(quarters).to be_nil
+    context 'local antenne' do
+      let(:antenne) { create :antenne, :local }
+
+      context 'with no old matches' do
+        it 'return nothing' do
+          expect(quarters).to be_nil
+        end
+      end
+
+      context 'with first matches 4 months ago' do
+        let!(:expert) { create :expert_with_users, antenne: antenne }
+        let!(:a_match) { create :match, expert: expert, need: create(:need, created_at: 3.months.ago) }
+
+        it 'return one past quarters' do
+          expect(quarters.length).to eq 1
+        end
+      end
+
+      context 'with first matches a year ago' do
+        let!(:expert) { create :expert_with_users, antenne: antenne }
+        let!(:a_match) { create :match, expert: expert, need: create(:need, created_at: 1.year.ago) }
+
+        it 'return 4 past quarters' do
+          expect(quarters.length).to eq 4
+        end
       end
     end
 
-    context 'with first matches 4 months ago' do
-      let!(:expert) { create :expert_with_users, antenne: antenne }
-      let!(:a_match) { create :match, expert: expert, created_at: 3.months.ago }
+    context 'national antenne' do
+      let(:institution) { create :institution }
+      let(:antenne) { create :antenne, :national, institution: institution }
+      let(:local_antenne) { create :antenne, :local, institution: institution }
 
-      it 'return one past quarters' do
-        expect(quarters.length).to eq 1
+      context 'with first local matches 4 months ago' do
+        let!(:expert) { create :expert_with_users, antenne: local_antenne }
+        let!(:a_match) { create :match, expert: expert, need: create(:need, created_at: 3.months.ago) }
+
+        it 'return one past quarters' do
+          expect(quarters.length).to eq 1
+        end
       end
-    end
 
-    context 'with first matches a year ago' do
-      let!(:expert) { create :expert_with_users, antenne: antenne }
-      let!(:a_match) { create :match, expert: expert, created_at: 1.year.ago }
+      context 'with first national matches a year half ago' do
+        let!(:expert) { create :expert_with_users, antenne: antenne }
+        let!(:a_match) { create :match, expert: expert, need: create(:need, created_at: 18.months.ago) }
+        let!(:other_match) { create :match, expert: create(:expert, antenne: local_antenne), need: create(:need, created_at: 5.months.ago) }
 
-      it 'return 4 past quarters' do
-        expect(quarters.length).to eq 4
+        it 'return 4 past quarters' do
+          expect(quarters.length).to eq 4
+        end
       end
     end
   end
@@ -34,7 +62,7 @@ describe QuarterlyReportService do
   describe 'destroy_old_report_files' do
     let(:antenne) { create :antenne }
     let!(:expert) { create :expert_with_users, antenne: antenne }
-    let!(:a_match) { create :match, expert: expert, created_at: 2.years.ago }
+    let!(:a_match) { create :match, expert: expert, need: create(:need, created_at: 2.years.ago) }
     let!(:quarterly_report_ok) { create :quarterly_report, :category_matches, antenne: antenne, start_date: 3.months.ago }
     let!(:quarterly_report_ko) { create :quarterly_report, :category_matches, antenne: antenne, start_date: 2.years.ago }
     let(:quarters) { described_class.send(:last_quarters, antenne) }
