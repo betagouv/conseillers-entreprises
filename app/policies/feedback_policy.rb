37b2b26a4596e6f27770de0636ff584987ab5104
clairezed <clairezuliani@protonmail.com>
2022-03-24 09:23:47 +0100

    Merge

--- app/policies/feedback_policy.rb
+++ app/policies/feedback_policy.rb
@@ -8,7 +8,7 @@ class FeedbackPolicy < ApplicationPolicy
   end
 
   def prefill?
-    @user.role_admin? && I18n.t("prefill_feedbacks.#{@record.category}").is_a?(Hash)
+    @user.is_admin? && I18n.t("prefill_feedbacks.#{@record.category}").is_a?(Hash)
   end
 
   class Scope < Scope
