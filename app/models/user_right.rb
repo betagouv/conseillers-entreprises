60180ed03591af3cf066391218d0f5bf8db3e93e
clairezed <clairezuliani@protonmail.com>
2022-04-21 11:07:26 +0200

    Force manager to have managed_antennes

--- app/models/user_right.rb
+++ app/models/user_right.rb
@@ -30,5 +30,18 @@ class UserRight < ApplicationRecord
     manager: 'manager'
   }, _prefix: true
 
-  validates :user_id, uniqueness: { scope: :antenne_id }
+  validates :user_id, uniqueness: { scope: [:right, :antenne_id] }
+  validate :manager_has_managed_antennes
+
+  before_validation :add_default_managed_antenne
+
+  private
+
+  def manager_has_managed_antennes
+    self.errors.add(:antenne_id, I18n.t('errors.manager_without_managed_antennes')) if (right_manager? && antenne.blank?)
+  end
+
+  def add_default_managed_antenne
+    self.antenne = user.antenne if (right_manager? && antenne.blank?)
+  end
 end
