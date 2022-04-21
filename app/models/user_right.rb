e06ba5d0171d961829bb60c994743fc791eeafba
Lucien <mollard.lucien@gmail.com>
2022-04-21 16:41:46 +0200

    Revert "Merge pull request #2427 from betagouv/improve-user-rights"
    e5622d9a7f3e36cb99b6296c1a8f7a13d539d947

--- app/models/user_right.rb
+++ app/models/user_right.rb
@@ -3,7 +3,7 @@
 # Table name: user_rights
 #
 #  id         :bigint(8)        not null, primary key
-#  right      :enum             not null
+#  right      :enum             default("advisor"), not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  antenne_id :bigint(8)
@@ -11,9 +11,9 @@
 #
 # Indexes
 #
-#  index_user_rights_on_antenne_id                        (antenne_id)
-#  index_user_rights_on_user_id                           (user_id)
-#  index_user_rights_on_user_id_and_antenne_id_and_right  (user_id,antenne_id,right) UNIQUE
+#  index_user_rights_on_antenne_id              (antenne_id)
+#  index_user_rights_on_user_id                 (user_id)
+#  index_user_rights_on_user_id_and_antenne_id  (user_id,antenne_id) UNIQUE
 #
 # Foreign Keys
 #
@@ -25,22 +25,10 @@ class UserRight < ApplicationRecord
   belongs_to :user, inverse_of: :user_rights
 
   enum right: {
+    advisor: 'advisor',
     admin: 'admin',
     manager: 'manager'
   }, _prefix: true
 
-  validates :user_id, uniqueness: { scope: [:right, :antenne_id] }
-  validate :manager_has_managed_antennes
-
-  before_validation :add_default_managed_antenne
-
-  private
-
-  def manager_has_managed_antennes
-    self.errors.add(:antenne_id, I18n.t('errors.manager_without_managed_antennes')) if (right_manager? && antenne.blank?)
-  end
-
-  def add_default_managed_antenne
-    self.antenne = user.antenne if (right_manager? && antenne.blank?)
-  end
+  validates :user_id, uniqueness: { scope: :antenne_id }
 end
