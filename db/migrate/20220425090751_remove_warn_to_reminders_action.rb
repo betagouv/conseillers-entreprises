95174cba06e0c078d31dfd3f91aca91cb444ecb6
Lucien <mollard.lucien@gmail.com>
2022-04-26 10:46:40 +0200

    change postgres enum to integer

--- db/migrate/20220425090751_remove_warn_to_reminders_action.rb
+++ db/migrate/20220425090751_remove_warn_to_reminders_action.rb
@@ -1,6 +1,30 @@
 class RemoveWarnToRemindersAction < ActiveRecord::Migration[6.1]
-  def change
-    RemindersAction.where(category: 'warn').destroy_all
-    remove_enum_value :actions_categories, "warn"
+  def up
+    rename_column :reminders_actions, :category, :old_category
+    change_column_null :reminders_actions, :old_category, true
+    add_column :reminders_actions, :category, :integer
+
+    RemindersAction.where(old_category: 'warn').destroy_all
+    RemindersAction.where(old_category: 'poke').update_all(category: 1)
+    RemindersAction.where(old_category: 'recall').update_all(category: 2)
+
+    change_column_null :reminders_actions, :category, false
+    add_index :reminders_actions, [:need_id, :category], unique: true
+    remove_column :reminders_actions, :old_category
+    drop_enum :actions_categories
+  end
+
+  def down
+    create_enum "actions_categories", %w[poke recall warn]
+    add_column :reminders_actions, :old_category, :actions_categories
+    change_column_null :reminders_actions, :category, true
+
+    RemindersAction.where(category: 1).update_all(old_category: 'poke')
+    RemindersAction.where(category: 2).update_all(old_category: 'recall')
+
+    remove_column :reminders_actions, :category
+    rename_column :reminders_actions, :old_category, :category
+    change_column_null :reminders_actions, :category, false
+    add_index :reminders_actions, [:need_id, :category], unique: true
   end
 end
