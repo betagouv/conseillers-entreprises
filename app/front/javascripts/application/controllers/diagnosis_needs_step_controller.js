a7a2805e1dce954511346a1ae2ceb81531bdc446
clairezed <clairezuliani@protonmail.com>
2022-04-26 09:38:36 +0200

    Décoche automatiquement Autre besoin

---
+++ app/front/javascripts/application/controllers/diagnosis_needs_step_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = [ "checkboxes" ]
+
+
+  autoUncheck(event) {
+    let currentCheckbox = event.currentTarget
+    if (this.isNotAutoUncheckable(currentCheckbox)) {
+      for(let checkbox of this.uncheckableCheckboxes()) {
+        checkbox.checked = false
+      }
+    }
+  }
+
+  isNotAutoUncheckable(checkbox) {
+    return checkbox.dataset.uncheck == "false"
+  }
+
+  uncheckableCheckboxes() {
+    this.checkboxesTargets.filter(e => console.log(e))
+    return this.checkboxesTargets.filter(e => e.dataset.uncheck == 'true')
+  }
+
+}
