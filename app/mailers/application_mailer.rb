fe89a864d8fe701d6a411844591bf2a9da2e80a5
clairezed <clairezuliani@protonmail.com>
2022-04-29 12:31:11 +0200

    Change default sender

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,8 +2,10 @@
 
 class ApplicationMailer < ActionMailer::Base
   SENDER_EMAIL = ENV['APPLICATION_EMAIL']
+  REPLY_TO_EMAIL = ENV['APPLICATION_REPLY_TO_EMAIL']
   SENDER = "#{I18n.t('app_name')} <#{SENDER_EMAIL}>"
-  default from: SENDER
+  REPLY_TO = "#{I18n.t('app_name')} <#{REPLY_TO_EMAIL}>"
+  default from: SENDER, reply_to: REPLY_TO
   helper :mailto
 
   layout 'mailers'
