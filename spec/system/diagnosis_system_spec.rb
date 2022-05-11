5b662041c8072dc2e3986f63ce67c90c1d15e8c7
clairezed <clairezuliani@protonmail.com>
2022-05-11 06:40:06 +0200

    diagnosis feature test in progress

--- spec/system/diagnosis_system_spec.rb
+++ spec/system/diagnosis_system_spec.rb
@@ -15,12 +15,9 @@ describe 'diagnosis', type: :system, js: true do
                institution_subject: create(:institution_subject, subject: need.subject),
                expert: create(:expert, communes: [need.facility.commune])
       end
-      # let!(:match) { create :match, expert: expert_subject.expert, need: need }
 
       before do
         subjects = create_list(:subject, 4)
-        # diagnosis.needs = create_list(:need, 1, diagnosis: diagnosis)
-        # diagnosis.save
       end
 
       it 'display all steps' do
@@ -31,22 +28,30 @@ describe 'diagnosis', type: :system, js: true do
         expect(page).to have_selector 'h1', text: "Besoin exprimé"
         expect(page).to have_current_path(needs_diagnosis_path(diagnosis))
 
+        # On ne peut sélectionner qu'un seul besoin
+        expect(page).to have_css('input[type=checkbox]:checked', count: 1, visible: false)
+        find('input[type=checkbox]', visible: false, match: :first).set(true)
         expect(page).to have_css('input[type=checkbox]:checked', count: 1, visible: false)
         # check('diagnosis_needs_attributes_1__destroy', allow_label_click: true)
-        # # find('label[for="diagnosis_needs_attributes_1__destroy"]').click
-        # # find('label', text: Subject.first.label, visible: false).click
+        # find('label[for="diagnosis_needs_attributes_1__destroy"]').click
+        # find('label', text: Subject.first.label, visible: false).click
+        # find('#diagnosis_needs_attributes_1__destroy', visible: false).set(true)
         # click 'diagnosis_needs_attributes_1__destroy'
         # expect(page).to have_css('input[type=checkbox]:checked', count: 1)
-        # Match.create(expert_id: expert_subject.expert.id, need_id: need.id)
+        # check(Subject.second.label, allow_label_click: true)
+        # expect(page).to have_css('input[type=checkbox]:checked', count: 1, visible: false)
         click_button('diagnosis_submit', match: :first)
 
         expect(page).to have_selector 'h2', text: diagnosis.needs.first.subject.label
-        # check(expert_subject.expert.full_name)
-        # page.execute_script("document.querySelector('[for=\"diagnosis_needs_attributes_0_matches_attributes_0__destroy\"]').click()")
+        # find('input[type=checkbox]', visible: false, match: :first).set(true)
+        # find(:css, '.experts_subject_checkbox', match: :first)
 
-        # save_and_open_page
-        # click_button('diagnosis_submit', match: :first)
-        # expect(page).to have_current_path(needs_diagnosis_path(diagnosis))
+        # check(expert_subject.expert.full_name)
+        # page.execute_script("document.querySelector('#diagnosis_needs_attributes_0_matches_attributes_0__destroy').checked = true")
+        save_and_open_page
+
+        click_button('diagnosis_submit', match: :first)
+        expect(page).to have_current_path(needs_diagnosis_path(diagnosis))
       end
     end
   end
