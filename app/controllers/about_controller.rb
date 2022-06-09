e38fc740743426271bc3aa24428a77cacd9afb66
Lucien <mollard.lucien@gmail.com>
2022-06-09 13:45:11 +0200

    don't display deleted institutions

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -11,7 +11,7 @@ class AboutController < PagesController
 
   def comment_ca_marche
     @institutions = Rails.cache.fetch("institutions-#{Institution.maximum(:updated_at)}") do
-      institutions = Institution.where(show_on_list: true).pluck(:name).sort
+      institutions = Institution.not_deleted.where(show_on_list: true).pluck(:name).sort
       institutions.each_slice((institutions.count.to_f / 4).ceil).to_a
     end
     @ld_json = FaqGenerator.new(I18n.t('faq').values).to_ld_json
