e598754e9727ea4590d0a5a76e9ce8bb870aafc5
Lucien <mollard.lucien@gmail.com>
2022-06-15 14:44:51 +0200

    prefill additional questions in iframe

--- app/controllers/landings/base_controller.rb
+++ app/controllers/landings/base_controller.rb
@@ -6,12 +6,12 @@ class Landings::BaseController < PagesController
   def save_query_params
     saved_params = session[:solicitation_form_info] || {}
     # siret : peut être transmis via l'url (iframe)
-    query_params = view_params.slice(*Solicitation::FORM_INFO_KEYS + [:siret])
+    query_params = view_params.slice(*Solicitation::FORM_INFO_KEYS + [:siret] + AdditionalSubjectQuestion.pluck(:key))
     saved_params.merge!(query_params)
     session[:solicitation_form_info] = saved_params if saved_params.present?
   end
 
   def view_params
-    params.permit(:landing_slug, :slug, :siret, *Solicitation::FORM_INFO_KEYS)
+    params.permit(:landing_slug, :slug, :siret, *Solicitation::FORM_INFO_KEYS, AdditionalSubjectQuestion.pluck(:key))
   end
 end
