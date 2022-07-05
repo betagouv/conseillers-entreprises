26ef12fd82620c706151afaf6c82cfec92365809
Lucien <mollard.lucien@gmail.com>
2022-07-05 13:30:24 +0200

    little improvements for landings

--- app/helpers/breadcrumbs_helper.rb
+++ app/helpers/breadcrumbs_helper.rb
@@ -5,7 +5,7 @@ module BreadcrumbsHelper
   def breadcrumbs_landing(landing, landing_theme, landing_subject = nil, params = {})
     html = content_tag('li', home_link(landing, params))
     if landing_subject.present?
-      html << content_tag('li', link_to(landing_theme.title, landing_theme_path(landing, landing_theme, params), class: 'fr-breadcrumb__link'))
+      html << content_tag('li', link_to(landing_theme.title, landing_theme_path(landing, landing_theme, params), class: 'fr-breadcrumb__link blue'))
       html << content_tag('li', link_to(landing_subject.title, '#', class: 'fr-breadcrumb__link', 'aria-current': 'page'))
     else
       html << content_tag('li', link_to(landing_theme.title, '#', class: 'fr-breadcrumb__link', 'aria-current': 'page'))
@@ -21,19 +21,19 @@ module BreadcrumbsHelper
   # Breadcrumbs used for other pages
   # Ex : "Déposer une demande › Comment ça marche ?"
   def breadcrumbs_page(title)
-    content_tag('li', link_to(t('breadcrumbs_helper.home_link.home'), root_path, class: 'fr-breadcrumb__link'))
+    content_tag('li', link_to(t('breadcrumbs_helper.home_link.home'), root_path, class: 'fr-breadcrumb__link blue'))
   end
 
   private
 
   def home_link(landing, params = {})
     if landing.iframe? && landing.integral_iframe?
-      link_to(t('breadcrumbs_helper.home_link.pde'), landing, class: 'fr-breadcrumb__link')
+      link_to(t('breadcrumbs_helper.home_link.pde'), landing, class: 'fr-breadcrumb__link blue')
     elsif landing.iframe?
       ''
     else
       params = params.permit(:landing_slug, :pk_campaign, :pk_kwd) if params.present?
-      link_to(t('breadcrumbs_helper.home_link.home'), root_path(params), class: 'fr-breadcrumb__link')
+      link_to(t('breadcrumbs_helper.home_link.home'), root_path(params), class: 'fr-breadcrumb__link blue')
     end
   end
 end
