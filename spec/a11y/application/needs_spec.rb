51369d3832955dd5f9aba2bf5d9ad41ac77916b8
Lucien <mollard.lucien@gmail.com>
2022-07-20 16:49:01 +0200

    add tests for needs show

--- spec/a11y/application/needs_spec.rb
+++ spec/a11y/application/needs_spec.rb
@@ -15,4 +15,34 @@ describe 'needs', type: :feature, js: true do
 
     it { is_expected.to be_accessible }
   end
+
+  describe '/besoins/:need' do
+    before do
+      visit "/besoins/#{a_match.need.id}"
+    end
+
+    context 'match quo' do
+      let(:a_match) { create :match, expert: current_user.experts.first, status: :quo }
+
+      it { is_expected.to be_accessible }
+    end
+
+    context 'match taking_care' do
+      let(:a_match) { create :match, expert: current_user.experts.first, status: :taking_care }
+
+      it { is_expected.to be_accessible }
+    end
+
+    context 'match done' do
+      let(:a_match) { create :match, expert: current_user.experts.first, status: :done }
+
+      it { is_expected.to be_accessible }
+    end
+
+    context 'match not_for_me' do
+      let(:a_match) { create :match, expert: current_user.experts.first, status: :not_for_me }
+
+      it { is_expected.to be_accessible }
+    end
+  end
 end
