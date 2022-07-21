29fc58f31209ad81eb9edecf9650a67e4f2b1ebb
Lucien <mollard.lucien@gmail.com>
2022-07-21 17:28:04 +0200

    add tests for user account pages accessibility

--- spec/a11y/application/devise_spec.rb
+++ spec/a11y/application/devise_spec.rb
@@ -10,4 +10,36 @@ describe 'devise', type: :feature, js: true do
 
     it { is_expected.to be_accessible }
   end
+
+  describe '/mon_compte' do
+    login_user
+    let!(:expert) { create :expert_with_users, users: [current_user], antenne: current_user.antenne }
+    let!(:expert2) { create :expert_with_users, users: [current_user], antenne: current_user.antenne }
+    let!(:expert_subject) { create :expert_subject, expert: expert }
+    let!(:institution_subject) { create :institution_subject, institution: expert.antenne.institution }
+
+    context '/mon_compte/informations' do
+      before { visit '/mon_compte/informations' }
+
+      it { is_expected.to be_accessible }
+    end
+
+    context '/mon_compte/mot_de_passe' do
+      before { visit '/mon_compte/mot_de_passe' }
+
+      it { is_expected.to be_accessible }
+    end
+
+    context '/mon_compte/antenne' do
+      before { visit '/mon_compte/antenne' }
+
+      it { is_expected.to be_accessible }
+    end
+
+    context '/mon_compte/referents/:expert_id/domaines' do
+      before { visit "/mon_compte/referents/#{expert.id}/domaines" }
+
+      it { is_expected.to be_accessible }
+    end
+  end
 end
