5eb45abe176f5c0a5cdb080d4ca5d7eeb0c673ab
clairezed <clairezuliani@protonmail.com>
2022-08-01 15:51:15 +0200

    Fix a11y test

--- spec/a11y/pages/solicitation_form_spec.rb
+++ spec/a11y/pages/solicitation_form_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'api_helper'
 
 describe 'solicitation_form', type: :feature, js: true do
   subject { page }
@@ -10,9 +11,23 @@ describe 'solicitation_form', type: :feature, js: true do
     let!(:landing) { create :landing, slug: 'accueil', title: 'Test Landing' }
     let(:landing_theme) { create :landing_theme, title: "Test Landing Theme" }
     let!(:landing_subject) {
-  create :landing_subject, landing_theme: landing_theme, subject: pde_subject,
-                                title: "Super sujet", description: "Description LS", requires_siret: true
-}
+    create :landing_subject, landing_theme: landing_theme, subject: pde_subject,
+                                  title: "Super sujet", description: "Description LS", requires_siret: true
+    }
+    let!(:additional_question_1) { create :additional_subject_question, subject: pde_subject, key: 'recrutement_poste_cadre' }
+    let!(:additional_question_2) { create :additional_subject_question, subject: pde_subject, key: 'recrutement_en_apprentissage' }
+    let(:siret) { '41816609600069' }
+    let(:solicitation) { Solicitation.last }
+    let(:api_url) { "https://api.insee.fr/entreprises/sirene/V3/siret/?q=siret:#{query}" }
+    let(:fixture_file) { 'api_insee_siret.json' }
+    let(:query) { siret }
+
+    before do
+      authorize_insee_token
+      stub_request(:get, api_url).to_return(
+        body: file_fixture(fixture_file)
+      )
+    end
 
     it do
       # First step
@@ -23,10 +38,14 @@ describe 'solicitation_form', type: :feature, js: true do
       fill_in 'Téléphone', with: '0123456789'
       click_button 'Suivant'
       # Second step
+      ## Search company
       is_expected.to be_accessible
-      fill_in 'Votre numéro SIRET', with: '12345678900010'
-      fill_in 'solicitation_siret', with: '12345678900010'
-      click_button 'Suivant'
+      fill_in 'Recherchez votre entreprise', with: query
+      click_button 'Rechercher'
+      ## Search Facility
+      is_expected.to be_accessible
+      click_button "#{query} - Octo Technology"
+
       # Third step
       is_expected.to be_accessible
       fill_in 'Description', with: 'Ceci n\'est pas un test'
