02b0493c2c767a09a377359c941cf3bdddbbe5a4
Lucien <mollard.lucien@gmail.com>
2022-08-22 12:00:20 +0200

    add required fields

--- app/admin/duplicate_user.rb
+++ app/admin/duplicate_user.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register_page 'Duplicate user' do
   belongs_to :user
+  Formtastic::FormBuilder.perform_browser_validations = true
 
   page_action :duplicate, method: :post do
     old_user = User.find(params[:user_id])
@@ -20,15 +21,15 @@ ActiveAdmin.register_page 'Duplicate user' do
         new_user = User.new
         user = User.find(params[:user_id])
         active_admin_form_for new_user, url: admin_user_duplicate_user_duplicate_path do |f|
-          f.input :full_name
+          f.input :full_name, input_html: { required: true }
           li do
             f.label :job
             f.text_field :job, value: user.job
           end
-          f.input :email
+          f.input :email, input_html: { required: true }
           f.input :phone_number
           if user.experts.map(&:custom_communes?).any?
-            f.input :specifiques_territories, as: :boolean, label: I18n.t('active_admin.user.duplicate_territories')
+            f.input :specifics_territories, as: :boolean, label: I18n.t('active_admin.user.duplicate_territories')
           end
           f.submit
         end
