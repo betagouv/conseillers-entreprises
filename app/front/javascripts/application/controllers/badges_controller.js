d2079fc9a5aac83ab5d7dd865b3cf0e7fe1c63de
Lucien <mollard.lucien@gmail.com>
2022-09-27 15:53:22 +0200

    move partial and linter

--- app/front/javascripts/application/controllers/badges_controller.js
+++ app/front/javascripts/application/controllers/badges_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = [ 'form', 'list' ]
 
-  toggle(event) {
+  toggle() {
     this.formTarget.classList.toggle("hide")
     this.listTarget.classList.toggle("hide")
   }
