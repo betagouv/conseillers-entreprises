21adeaaba087e5b8008a7a6118d7a8fd7996f070
clairezed <clairezuliani@protonmail.com>
2022-10-07 11:32:09 +0200

    Quick fix landing layout single page

--- app/controllers/landings/landing_themes_controller.rb
+++ app/controllers/landings/landing_themes_controller.rb
@@ -5,7 +5,7 @@ class Landings::LandingThemesController < Landings::BaseController
     @landing_theme = LandingTheme.not_archived.find_by(slug: params[:slug])
     redirect_to root_path and return if @landing_theme.nil?
     # pas de page "theme" pour les landing `single_page`
-    redirect_to({ controller: "landings/landings", action: "show", landing_slug: @landing.slug }.merge(query_params)) if (@landing.layout_single_page? && !@landing.iframe)
+    redirect_to({ controller: "landings/landings", action: "show", landing_slug: @landing.slug }.merge(query_params)) if (@landing.layout_single_page? && !@landing.iframe?)
 
     @landing_subjects = @landing_theme.landing_subjects.not_archived.order(:position)
   end
