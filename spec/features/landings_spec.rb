3bc0f39ec5bb25e8a4885fb98645f31283ac1d98
clairezed <clairezuliani@protonmail.com>
2022-10-07 14:56:00 +0200

    Add non regression test

---
+++ spec/features/landings_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'api_helper'
+
+describe 'Landings', type: :feature, js: true, flaky: true do
+  let(:pde_subject) { create :subject }
+  let(:landing_theme) { create :landing_theme, title: "Test Landing Theme" }
+  let!(:landing_subject) { create :landing_subject, landing_theme: landing_theme, subject: pde_subject, title: "Super sujet", description: "Description LS", requires_siret: true }
+
+  describe 'single_page_layout' do
+    let!(:landing) { create :landing, integration: :intern, slug: 'intern', layout: 'single_page' }
+
+    before do
+      landing.landing_themes << landing_theme
+    end
+
+    context "root url" do
+      it do
+        visit "/aide-entreprise/#{landing.slug}"
+        expect(page).to have_selector('h2', text: landing_theme.title)
+        expect(page).to have_link(landing_subject.title)
+      end
+    end
+
+    context "theme url" do
+      it do
+        visit "/aide-entreprise/#{landing.slug}/theme/#{landing_theme.slug}"
+        expect(page).to have_selector('h2', text: landing_theme.title)
+        expect(page).to have_link(landing_subject.title)
+      end
+    end
+  end
+end
