ea0550d683f0ee853504f71f088213e230550dd2
Lucien <mollard.lucien@gmail.com>
2022-10-12 11:57:57 +0200

    reversible migration

--- db/migrate/20221010154842_change_require_siret_for_landing_subjects.rb
+++ db/migrate/20221010154842_change_require_siret_for_landing_subjects.rb
@@ -1,5 +1,9 @@
 class ChangeRequireSiretForLandingSubjects < ActiveRecord::Migration[7.0]
-  def change
+  def up
     change_column :landing_subjects, :requires_siret, :boolean, default: true
+  end
+
+  def down
+    change_column :landing_subjects, :requires_siret, :boolean, default: false
   end
 end
