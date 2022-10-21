8e129a5b99d11fa20f4b7bcf7563295766d81503
clairezed <clairezuliani@protonmail.com>
2022-10-21 16:04:51 +0200

    Ajout Plan du site

---
+++ app/services/sitemap_generator.rb
@@ -0,0 +1,73 @@
+module SitemapGenerator
+  def self.perform
+    @content = []
+
+    # Landing pages
+    landings = Landing.intern.not_archived.preload(:landing_themes)
+
+    # Pour afficher en premier la landing 'accueil'
+    landings.where(slug: 'accueil').each do |landing|
+      create_landing_hash(landing)
+    end
+    landings.where.not(slug: 'accueil').each do |landing|
+      create_landing_hash(landing)
+    end
+
+    # Misc static pages
+    static_pages = [
+      { url: Rails.application.routes.url_helpers.comment_ca_marche_url, title: I18n.t('about.comment_ca_marche.title') },
+      { url: Rails.application.routes.url_helpers.public_index_url, title: I18n.t('usage_stats') },
+      { url: ENV['BLOG_URL'], title: I18n.t('about.blog.title') },
+    ]
+    static_pages.each_with_index do |page, idx|
+      page_elt = {
+        loc: page[:url],
+        priority: 0.5,
+        title: page[:title],
+        href: true,
+        changefreq: 'monthly'
+      }
+      @content << { "page_#{idx}": page_elt }
+    end
+
+    @content
+  end
+
+  def self.create_landing_hash(landing)
+    landing_elt = {
+      loc: Rails.application.routes.url_helpers.landing_url(landing),
+      priority: 0.9,
+      lasmod: landing.updated_at.iso8601,
+      title: landing.title,
+      href: true,
+      elements: [],
+      changefreq: 'weekly'
+    }
+
+    landing.landing_themes.not_archived.preload(:landing_subjects).each do |landing_theme|
+      landing_theme_elt = {
+        loc: Rails.application.routes.url_helpers.landing_theme_url(landing, landing_theme),
+        priority: 0.7,
+        lasmod: landing_theme.updated_at.iso8601,
+        title: landing_theme.title,
+        href: true,
+        elements: [],
+        changefreq: 'weekly'
+      }
+
+      landing_theme.landing_subjects.not_archived.each do |landing_subject|
+        landing_subject_elt = {
+          loc: Rails.application.routes.url_helpers.new_solicitation_url(landing.slug, landing_subject.slug),
+          priority: 0.7,
+          lasmod: landing_subject.updated_at.iso8601,
+          title: landing_subject.title,
+          href: true,
+          changefreq: 'weekly'
+        }
+        landing_theme_elt[:elements] << { child_page: landing_subject_elt }
+      end
+      landing_elt[:elements] << { child_page: landing_theme_elt }
+    end
+    @content << { "#{landing.slug}": landing_elt }
+  end
+end
--- app/views/sitemap/sitemap.xml.builder
+++ app/views/sitemap/sitemap.xml.builder
@@ -1,37 +1,28 @@
 # Human-readable spec: https://www.sitemaps.org/fr/protocol.html
-xml.instruct!
-xml.urlset xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9" do
-  # Landing pages
-  Landing.intern.preload(:landing_themes).each do |landing|
-    xml.url do
-      xml.loc landing_url(landing)
-      xml.lastmod landing.updated_at.iso8601
-      xml.changefreq 'weekly'
-    end
-
-    landing.landing_themes.preload(:landing_subjects).each do |landing_theme|
-      xml.url do
-        xml.loc landing_theme_url(landing, landing_theme)
-        xml.lastmod landing_theme.updated_at.iso8601
+def site_map_xml(element, xml)
+  xml_return = ""
+  element.each do |el|
+    e = el.last
+    if e[:href] == true
+      xml_return << xml.tag!("url") do
+        xml.loc e[:loc]
+        xml.lastmod e[:lastmod]
+        xml.priority e[:priority]
         xml.changefreq 'weekly'
       end
-
-      landing_theme.landing_subjects.each do |landing_subject|
-        xml.url do
-          xml.loc new_solicitation_path(landing.slug, landing_subject.slug)
-          xml.lastmod landing_subject.updated_at.iso8601
-          xml.changefreq 'weekly'
-        end
+    end
+    if e[:elements]
+      e[:elements].each do |elem|
+        xml_return << site_map_xml(elem, xml)
       end
     end
   end
+  xml_return
+end
 
-  # Misc static pages
-  static_pages = [comment_ca_marche_url, public_index_url, ENV['BLOG_URL']]
-  static_pages.each do |page|
-    xml.url do
-      xml.loc page
-      xml.changefreq 'monthly'
-    end
+xml.instruct!
+xml.urlset xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9" do
+  @site_map.each do |element|
+    site_map_xml(element, xml)
   end
 end
