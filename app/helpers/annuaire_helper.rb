88f4956c2b857bf380d9c17f6404960321b34dce
clairezed <clairezuliani@protonmail.com>
2022-11-29 11:34:34 +0100

    Add missing aria-hidden

--- app/helpers/annuaire_helper.rb
+++ app/helpers/annuaire_helper.rb
@@ -12,9 +12,9 @@ module AnnuaireHelper
 
   def build_user_name_cell(user, antenne)
     html = link_to(user.full_name, edit_admin_expert_path(user.relevant_expert), title: t('annuaire_helper.build_user_name_cell.edit_expert', expert_name: user.relevant_expert.full_name, antenne: antenne))
-    html << tag.span(class: 'ri-mail-add-fill blue fr-ml-1v', title: t('annuaire_helper.build_user_name_cell.not_invited')) if user.invitation_sent_at.nil?
-    html << tag.span(class: 'ri-nurse-fill blue fr-ml-1v', title: t('annuaire_helper.build_user_name_cell.antenne_manager')) if user.is_manager?
-    html << tag.span(class: 'ri-map-2-line blue fr-ml-1v', title: t('annuaire_helper.build_user_name_cell.specific_territories')) if user.relevant_expert.communes.any?
+    html << tag.span(class: 'ri-mail-add-fill blue fr-ml-1v', aria: {hidden: true}, title: t('annuaire_helper.build_user_name_cell.not_invited')) if user.invitation_sent_at.nil?
+    html << tag.span(class: 'ri-nurse-fill blue fr-ml-1v', aria: {hidden: true}, title: t('annuaire_helper.build_user_name_cell.antenne_manager')) if user.is_manager?
+    html << tag.span(class: 'ri-map-2-line blue fr-ml-1v', aria: {hidden: true}, title: t('annuaire_helper.build_user_name_cell.specific_territories')) if user.relevant_expert.communes.any?
     html
   end
 
@@ -43,13 +43,13 @@ module AnnuaireHelper
   def set_title_and_icon(experts_count, anomalie_less, anomalie_more_specific, anomalie_more)
     if anomalie_less
       title = t('helpers.annuaire.anomalie_less')
-      icon = tag.span(class: 'red ri-error-warning-line fr-ml-1v')
+      icon = tag.span(class: 'red ri-error-warning-line fr-ml-1v', aria: {hidden: true})
     elsif anomalie_more_specific
       title = t('helpers.annuaire.anomalie_more_specific', count: experts_count)
-      icon = tag.span(class: 'orange ri-map-2-line fr-ml-1v')
+      icon = tag.span(class: 'orange ri-map-2-line fr-ml-1v', aria: {hidden: true})
     elsif anomalie_more
       title = t('helpers.annuaire.experts_on_subject', count: experts_count)
-      icon = tag.span(class: 'orange ri-error-warning-line fr-ml-1v')
+      icon = tag.span(class: 'orange ri-error-warning-line fr-ml-1v', aria: {hidden: true})
     else
       title = t('helpers.annuaire.experts_on_subject', count: experts_count)
       icon = ''
