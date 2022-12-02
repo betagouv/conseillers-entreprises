d01bc1d3a8af447c9ee77033ce53c5db8904cadd
clairezed <clairezuliani@protonmail.com>
2022-12-02 17:14:02 +0100

    Add basic test

---
+++ spec/controllers/solicitations_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe SolicitationsController do
+
+  describe 'GET #redirect_to_solicitation_step' do
+    subject(:request) { get :redirect_to_solicitation_step, params: { uuid: solicitation.uuid, relaunch: 'relance-sollicitation' } }
+
+    let!(:solicitation) { create :solicitation, full_name: "JJ Goldman", email: 'test@example.com', phone_number: 'xx', completed_at: nil, status: status, siret: siret }
+
+    context 'step_company solicitation' do
+      let(:status) { :step_company }
+      let(:siret) { nil }
+
+      it 'returns http success' do
+        expect(response).to be_successful
+      end
+    end
+
+    context 'step_description solicitation' do
+
+      let!(:solicitation) { create :solicitation, full_name: "JJ Goldman", email: 'test@example.com', phone_number: 'xx', completed_at: nil, status: :step_description, siret: '41816609600069' }
+
+      it 'returns http success' do
+        expect(response).to be_successful
+      end
+    end
+
+  end
+end
