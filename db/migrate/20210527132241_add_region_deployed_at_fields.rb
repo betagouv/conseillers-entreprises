b50e28782412dc7a0f099301e0980acd8dd75fba
clairezed <clairezuliani@protonmail.com>
2023-01-17 23:30:46 +0100

    Fix Api solicitation controller tests

--- db/migrate/20210527132241_add_region_deployed_at_fields.rb
+++ db/migrate/20210527132241_add_region_deployed_at_fields.rb
@@ -6,7 +6,14 @@ class AddRegionDeployedAtFields < ActiveRecord::Migration[6.1]
     up_only do
       Territory.find_by(code_region: 32).update(deployed_at: "2017-07-01".to_datetime)
       Territory.find_by(code_region: 11).update(deployed_at: "2020-12-01".to_datetime)
-      SolicitationModification::UpdateAllCreatedInDeployedRegion.new(Solicitation.where.not(code_region: nil).where(created_in_deployed_region: false)).call!
+      solicitations = Solicitation.where.not(code_region: nil).where(created_in_deployed_region: false)
+      solicitations.find_each do |solicitation|
+        region = solicitation.region
+        next unless region
+        if region.deployed? && (solicitation.created_at > region.deployed_at)
+          solicitation.update(created_in_deployed_region: true)
+        end
+      end
     end
   end
 end
--- spec/controllers/solicitations_controller_spec.rb
+++ spec/controllers/solicitations_controller_spec.rb
@@ -1,6 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe SolicitationsController do
+
+  describe 'POST #create' do
+    let(:landing) { create(:landing) }
+    let(:landing_subject) { create(:landing_subject) }
+    let(:request) { post :create,
+      params: { landing_slug: landing.slug, landing_subject_slug: landing_subject.slug,
+        solicitation: {full_name: full_name, email: email, phone_number: phone_number, landing_id: landing.id, landing_subject_id: landing_subject.id } } }
+
+    context 'with good params' do
+      let(:full_name) { 'Louise Michel' }
+      let(:email) { 'louise@michel.org' }
+      let(:phone_number) { '0606060606' }
+
+      it 'creates a solicitation' do
+        request
+        solicitation = Solicitation.last
+        expect(solicitation.full_name).to eq('Louise Michel')
+        expect(solicitation.status).to eq('step_company')
+      end
+    end
+  end
+
+  describe 'PATCH #update_step_company' do
+    let(:solicitation) { create(:solicitation, full_name: "L Michel", email: 'l@michel.org', phone_number: 'xx', status: 'step_company', siret: nil) }
+    let(:request) { patch :update_step_company, params: { uuid: solicitation.uuid, solicitation: { siret: siret } } }
+
+    context 'with blank siret' do
+      let(:siret) { "       " }
+
+      it "invalidates solicitation" do
+        request
+        expect(solicitation.siret).to eq(nil)
+        expect(solicitation.reload.status).to eq('step_company')
+      end
+    end
+
+    context 'with incorrect siret' do
+      let(:siret) { "123 456 789 00011" }
+
+      it "invalidates solicitation" do
+        request
+        expect(solicitation.siret).to eq(nil)
+        expect(solicitation.reload.status).to eq('step_company')
+      end
+    end
+
+    context 'with correct siret' do
+      let(:siret) { "41816609600069" }
+
+      it "updates solicitation" do
+        request
+        expect(solicitation.valid?).to be(true)
+        expect(solicitation.reload.siret).to eq("41816609600069")
+        expect(solicitation.reload.status).to eq('step_description')
+      end
+    end
+  end
+
   describe 'GET #redirect_to_solicitation_step' do
     subject(:request) { get :redirect_to_solicitation_step, params: { uuid: solicitation.uuid, relaunch: 'relance-sollicitation' } }
 
