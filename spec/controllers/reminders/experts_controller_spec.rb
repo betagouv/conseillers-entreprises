3fad72130efd9449530ef40c029f7248b12d94a3
Lucien <mollard.lucien@gmail.com>
2023-02-15 11:47:06 +0100

    improve tests

--- spec/controllers/reminders/experts_controller_spec.rb
+++ spec/controllers/reminders/experts_controller_spec.rb
@@ -11,30 +11,21 @@ RSpec.describe Reminders::ExpertsController do
     before { RemindersService.create_reminders_registers }
 
     describe '#GET many_pending_needs' do
-      subject(:request) { get :many_pending_needs }
+      before { get :many_pending_needs }
 
-      it do
-        request
-        expect(assigns(:active_experts)).to match_array([expert_with_many_old_quo_matches])
-      end
+      it { expect(assigns(:active_experts)).to match_array([expert_with_many_old_quo_matches]) }
     end
 
     describe '#GET medium_pending_needs' do
-      subject(:request) { get :medium_pending_needs }
+      before { get :medium_pending_needs }
 
-      it do
-        request
-        expect(assigns(:active_experts)).to match_array([expert_with_medium_old_quo_matches])
-      end
+      it { expect(assigns(:active_experts)).to match_array([expert_with_medium_old_quo_matches]) }
     end
 
     describe '#GET one_pending_need' do
-      subject(:request) { get :one_pending_need }
+      before { get :one_pending_need }
 
-      it do
-        request
-        expect(assigns(:active_experts)).to match_array([expert_with_one_quo_match_1, expert_with_one_old_quo_match])
-      end
+      it { expect(assigns(:active_experts)).to match_array([expert_with_one_quo_match_1, expert_with_one_old_quo_match]) }
     end
   end
 
@@ -44,21 +35,15 @@ RSpec.describe Reminders::ExpertsController do
     before { RemindersService.create_reminders_registers }
 
     describe '#GET inputs' do
-      subject(:request) { get :inputs }
+      before { get :inputs }
 
-      it do
-        request
-        expect(assigns(:active_experts)).to match_array([expert_input])
-      end
+      it { expect(assigns(:active_experts)).to match_array([expert_input]) }
     end
 
     describe '#GET outputs' do
-      subject(:request) { get :outputs }
+      before { get :outputs }
 
-      it do
-        request
-        expect(assigns(:active_experts)).to match_array([expert_output])
-      end
+      it { expect(assigns(:active_experts)).to match_array([expert_output]) }
     end
 
     describe '#POST process_register' do
