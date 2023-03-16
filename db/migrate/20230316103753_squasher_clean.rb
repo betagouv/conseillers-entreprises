41cfc76f53e5a5efa73bc8fdb51a31a0d7f7e151
Lucien <mollard.lucien@gmail.com>
2023-03-16 11:42:02 +0100

    squash migrations until august 2022

--- db/migrate/20230316103753_squasher_clean.rb
+++ db/migrate/20230316103753_squasher_clean.rb
@@ -1,12 +1,13 @@
 class SquasherClean < ActiveRecord::Migration[7.0]
-  class SchemaMigration < ApplicationRecord
+  class SchemaMigration < ActiveRecord::Base
   end
 
   def up
     migrations = Dir.glob(File.join(File.dirname(__FILE__), '*.rb'))
     versions = migrations.map { |file| File.basename(file)[/\A\d+/] }
-    SchemaMigration.where.not(version: versions).delete_all
+    SchemaMigration.where("version NOT IN (?)", versions).delete_all
   end
 
-  def down; end
+  def down
+  end
 end
