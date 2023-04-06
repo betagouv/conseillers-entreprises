dcc1da5b4b1f34d89b359055dc4ca502ff927c83
Lucien <mollard.lucien@gmail.com>
2023-04-06 17:30:23 +0200

    improve mailers preview

--- spec/mailers/previews/company_mailer_preview.rb
+++ spec/mailers/previews/company_mailer_preview.rb
@@ -1,10 +1,10 @@
 class CompanyMailerPreview < ActionMailer::Preview
   def confirmation_solicitation_from_pde
-    CompanyMailer.confirmation_solicitation(Solicitation.where(institution: nil).sample)
+    CompanyMailer.confirmation_solicitation(Solicitation.where(institution: nil).where.associated(:landing_subject).sample)
   end
 
   def confirmation_solicitation_from_iframe
-    CompanyMailer.confirmation_solicitation(Solicitation.where.not(institution: nil).sample)
+    CompanyMailer.confirmation_solicitation(Solicitation.where.not(institution: nil).where.associated(:landing_subject).sample)
   end
 
   def taking_care_solicitation
