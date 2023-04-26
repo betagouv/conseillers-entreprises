99370d49cc2f70cebe0d71f3c364affbc2284e03
Lucien <mollard.lucien@gmail.com>
2023-04-26 16:37:42 +0200

    create columns for antennes, experts and users

--- app/helpers/annuaire_helper.rb
+++ app/helpers/annuaire_helper.rb
@@ -21,6 +21,14 @@ module AnnuaireHelper
     end
   end
 
+  def build_user_name_cell2(user, antenne)
+    html = link_to(user[:name], admin_user_path(user[:id]), title: t('annuaire_helper.build_user_name_cell.edit_user', user_name: user[:name], antenne: antenne[:name]))
+    html << tag.span(class: 'ri-mail-add-fill blue fr-ml-1v', aria: { hidden: true }, title: t('annuaire_helper.build_user_name_cell.not_invited')) if user[:invitation_sent_at].nil?
+    html << tag.span(class: 'ri-nurse-fill blue fr-ml-1v', aria: { hidden: true }, title: t('annuaire_helper.build_user_name_cell.antenne_manager')) if user[:responsable]
+    html << tag.span(class: 'ri-map-2-line blue fr-ml-1v', aria: { hidden: true }, title: t('annuaire_helper.build_user_name_cell.specific_territories')) if user[:has_specific_territories]
+    html
+  end
+
   def build_user_name_cell(user, antenne)
     html = link_to(user.full_name, edit_admin_expert_path(user.relevant_expert), title: t('annuaire_helper.build_user_name_cell.edit_expert', expert_name: user.relevant_expert.full_name, antenne: antenne))
     html << tag.span(class: 'ri-mail-add-fill blue fr-ml-1v', aria: { hidden: true }, title: t('annuaire_helper.build_user_name_cell.not_invited')) if user.invitation_sent_at.nil?
