88b02359403d81b0cacc2bd3e35b6b10a86ace99
clairezed <clairezuliani@protonmail.com>
2023-05-12 16:09:53 +0200

    Fix message recherche besoins si plusieurs pages

--- app/helpers/page_infos_helper.rb
+++ app/helpers/page_infos_helper.rb
@@ -18,6 +18,7 @@ module PageInfosHelper
 
   def search_collection_count(collection)
     collection_size = collection.total_count
+    collection_name = collection.model_name.human(count: collection_size)
 
     if collection.total_pages < 2
       t('helpers.page_infos.searched_collection_count_html', size: collection_size)
@@ -25,7 +26,8 @@ module PageInfosHelper
       t('helpers.page_entries_info.many_pages_html',
         total: collection_size,
         start: collection.offset_value + 1,
-        end: collection.offset_value + collection.length)
+        end: collection.offset_value + collection.length,
+        name: collection_name)
     end
   end
 end
