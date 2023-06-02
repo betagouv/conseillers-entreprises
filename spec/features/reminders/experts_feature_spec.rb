f5f3c469fdfe5704f4d2a73b6e872b576af98496
clairezed <clairezuliani@protonmail.com>
2023-06-02 15:41:50 +0200

    test test

--- spec/features/reminders/experts_feature_spec.rb
+++ spec/features/reminders/experts_feature_spec.rb
@@ -15,18 +15,18 @@ describe 'reminders experts', js: true do
       expect(page.html).to include 'Paniers qualité'
       p "many_pending_needs expected count : 1"
       page.click_link(href: "/relances/experts/superieur-a-cinq-besoins")
-      p "many_pending_needs given count : #{Expert.many_pending_needs.distinct.size}"
-      expect(page).to have_css('.card', count: 1)
+      p "many_pending_needs real count : #{Expert.many_pending_needs.distinct.size}"
+      expect(page).to have_css('.card', count: 1, wait: 10)
       expect(page).to be_accessible
       p "medium_pending_needs expected count : 1 "
       page.click_link(href: "/relances/experts/entre-deux-et-cinq-besoins")
-      p "medium_pending_needs given count : #{Expert.medium_pending_needs.distinct.size}"
-      expect(page).to have_css('.card', count: 1)
+      p "medium_pending_needs real count : #{Expert.medium_pending_needs.distinct.size}"
+      expect(page).to have_css('.card', count: 1, wait: 10)
       expect(page).to be_accessible
       p "one_pending_need expected count : 2"
       page.click_link(href: "/relances/experts/un-besoin-recent")
-      p "one_pending_need given count : #{Expert.one_pending_need.distinct.size}"
-      expect(page).to have_css('.card', count: 2)
+      p "one_pending_need real count : #{Expert.one_pending_need.distinct.size}"
+      expect(page).to have_css('.card', count: 2, wait: 10)
       expect(page).to be_accessible
       click_link("1 boite de réception", match: :first)
       expect(page).to have_text("Boite de réception")
@@ -50,8 +50,8 @@ describe 'reminders experts', js: true do
       it 'displays page' do
         p "inputs expected count : 2"
         visit inputs_reminders_experts_path
-        p "inputs given count : #{Expert.inputs.distinct.size}"
-        expect(page).to have_css('.card', count: 2)
+        p "inputs real count : #{Expert.inputs.distinct.size}"
+        expect(page).to have_css('.card', count: 2, wait: 10)
         expect(page).to be_accessible
       end
     end
@@ -60,8 +60,8 @@ describe 'reminders experts', js: true do
       it 'displays page' do
         p "outputs expected count : 3"
         visit outputs_reminders_experts_path
-        p "outputs given count : #{Expert.outputs.distinct.size}"
-        expect(page).to have_css('.card', count: 3)
+        p "outputs real count : #{Expert.outputs.distinct.size}"
+        expect(page).to have_css('.card', count: 3, wait: 10)
         expect(page).to be_accessible
       end
     end
