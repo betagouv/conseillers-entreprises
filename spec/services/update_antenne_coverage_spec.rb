9350929926ba5df7b0b767b7fc046b93e3999df1
Lucien <mollard.lucien@gmail.com>
2023-06-08 10:02:09 +0200

    add tests for local coverage

---
+++ spec/services/update_antenne_coverage_spec.rb
@@ -0,0 +1,127 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+describe UpdateAntenneCoverage do
+  # bonne couverture
+  # missing_insee_codes
+  # extra_insee_codes
+  # no_expert
+
+  let(:institution) { create(:institution) }
+  let!(:local_antenne) { create(:antenne, :local, institution: institution, communes: communes) }
+  let!(:regional_antenne) { create(:antenne, :regional, institution: institution, communes: communes) }
+  let(:beaufay) { create(:commune, insee_code: '72026') }
+  let(:bonnétable) { create(:commune, insee_code: '72039') }
+  let(:briosne) { create(:commune, insee_code: '72048') }
+  let(:jauzé) { create(:commune, insee_code: '72148') }
+  let(:communes) { [beaufay, bonnétable, briosne, jauzé] }
+  let!(:region) { create(:territory, :region, communes: communes) }
+  let!(:institution_subject) { create(:institution_subject, institution: institution) }
+
+  before do
+    regional_antenne.reload
+    local_antenne.reload
+  end
+
+  subject { described_class.new(local_antenne) }
+
+  context 'local' do
+    # ok : un ou plusieurs experts au niveau local couvrent tous les codes insee
+    # ok : un ou plusieurs experts au niveau local avec des territoires spécifiques couvrent tous les codes insee
+    # ko : pas d'experts sur ce sujet
+    # ko : des experts au niveau local couvrent le sujet mais il manque des codes insee qui ne sont pas couverts
+    # ko : des experts au niveau local avec des territoire spécifiques couvrent le sujet mais il manque des codes insee qui ne sont pas couverts
+    # ko : des experts au niveau local sans territoire spécifique sont plusieurs à couvrir le même sujet
+    # ko : des experts au niveau local avec des territoires spécifique sont plusieurs à couvrir le même sujet
+
+    context 'un ou plusieurs experts au niveau local couvrent tous les codes insee' do
+      let!(:local_expert) { create(:expert_with_users, antenne: local_antenne, experts_subjects: [create(:expert_subject, institution_subject: institution_subject)]) }
+
+      before { subject.call }
+
+      it do
+        expect(local_antenne.referencement_coverages.count).to eq(1)
+        expect(local_antenne.referencement_coverages.first.antenne).to eq(local_antenne)
+        expect(local_antenne.referencement_coverages.first.anomalie).to eq('no_anomalie')
+        expect(local_antenne.referencement_coverages.first.coverage).to eq('local')
+        expect(local_antenne.referencement_coverages.first.anomalie_details).to be_nil
+      end
+    end
+
+    context 'un ou plusieurs experts au niveau local avec des territoires spécifiques couvrent tous les codes insee' do
+      let!(:local_expert) { create(:expert_with_users, antenne: local_antenne, communes: communes, experts_subjects: [create(:expert_subject, institution_subject: institution_subject)]) }
+
+      before { subject.call }
+
+      it do
+        expect(local_antenne.referencement_coverages.first.anomalie).to eq('no_anomalie')
+        expect(local_antenne.referencement_coverages.first.coverage).to eq('local')
+        expect(local_antenne.referencement_coverages.first.anomalie_details).to be_nil
+      end
+    end
+
+    context 'pas d’experts sur ce sujet' do
+      before { subject.call }
+
+      it do
+        expect(local_antenne.referencement_coverages.first.anomalie).to eq('no_expert')
+        expect(local_antenne.referencement_coverages.first.coverage).to be_nil
+        expect(local_antenne.referencement_coverages.first.anomalie_details).to be_nil
+      end
+    end
+
+    context 'des experts au niveau local couvrent le sujet mais il manque des codes insee qui ne sont pas couverts' do
+      let!(:local_expert) { create(:expert, antenne: local_antenne, communes: [bonnétable], experts_subjects: [create(:expert_subject, institution_subject: institution_subject)]) }
+
+      before { subject.call }
+
+      it do
+        expect(local_antenne.referencement_coverages.first.anomalie).to eq('missing_insee_codes')
+        expect(local_antenne.referencement_coverages.first.coverage).to eq('local')
+        expect(local_antenne.referencement_coverages.first.anomalie_details['missing_insee_codes']).to contain_exactly("#{beaufay.insee_code}", "#{briosne.insee_code}", "#{jauzé.insee_code}")
+      end
+    end
+
+    context 'des experts au niveau local avec des territoire spécifiques couvrent le sujet mais il manque des codes insee qui ne sont pas couverts' do
+      let!(:local_expert1) { create(:expert, antenne: local_antenne, communes: [bonnétable], experts_subjects: [create(:expert_subject, institution_subject: institution_subject)]) }
+      let!(:local_expert2) { create(:expert, antenne: local_antenne, communes: [beaufay], experts_subjects: [create(:expert_subject, institution_subject: institution_subject)]) }
+
+      before { subject.call }
+
+      it do
+        expect(local_antenne.referencement_coverages.first.anomalie).to eq('missing_insee_codes')
+        expect(local_antenne.referencement_coverages.first.coverage).to eq('local')
+        expect(local_antenne.referencement_coverages.first.anomalie_details['missing_insee_codes']).to contain_exactly("#{briosne.insee_code}", "#{jauzé.insee_code}")
+      end
+    end
+
+    context 'des experts au niveau local sans territoire spécifique sont plusieurs à couvrir le même sujet' do
+      let!(:local_expert1) { create(:expert, antenne: local_antenne, experts_subjects: [create(:expert_subject, institution_subject: institution_subject)]) }
+      let!(:local_expert2) { create(:expert, antenne: local_antenne, experts_subjects: [create(:expert_subject, institution_subject: institution_subject)]) }
+
+      before { subject.call }
+
+      it do
+        expect(local_antenne.referencement_coverages.first.anomalie).to eq('extra_insee_codes')
+        expect(local_antenne.referencement_coverages.first.coverage).to eq('local')
+        expect(local_antenne.referencement_coverages.first.anomalie_details['experts']).to contain_exactly(local_expert1.id, local_expert2.id)
+        expect(local_antenne.referencement_coverages.first.anomalie_details['extra_insee_codes']).to contain_exactly("#{bonnétable.insee_code}", "#{beaufay.insee_code}", "#{jauzé.insee_code}", "#{briosne.insee_code}")
+      end
+    end
+
+    context 'des experts au niveau local avec des territoires spécifique sont plusieurs à couvrir le même sujet' do
+      let!(:local_expert1) { create(:expert, antenne: local_antenne, communes: communes, experts_subjects: [create(:expert_subject, institution_subject: institution_subject)]) }
+      let!(:local_expert2) { create(:expert, antenne: local_antenne, communes: communes, experts_subjects: [create(:expert_subject, institution_subject: institution_subject)]) }
+
+      before { subject.call }
+
+      it do
+        expect(local_antenne.referencement_coverages.first.anomalie).to eq('extra_insee_codes')
+        expect(local_antenne.referencement_coverages.first.coverage).to eq('local')
+        expect(local_antenne.referencement_coverages.first.anomalie_details['experts']).to contain_exactly(local_expert1.id, local_expert2.id)
+        expect(local_antenne.referencement_coverages.first.anomalie_details['extra_insee_codes']).to contain_exactly("#{bonnétable.insee_code}", "#{beaufay.insee_code}", "#{jauzé.insee_code}", "#{briosne.insee_code}")
+      end
+    end
+  end
+
+end
