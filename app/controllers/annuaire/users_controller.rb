b077b0666690cd8259c2dae249b8956eb568e4fa
clairezed <clairezuliani@protonmail.com>
2023-06-21 12:50:42 +0200

    Optimize user view loading time

--- app/controllers/annuaire/users_controller.rb
+++ app/controllers/annuaire/users_controller.rb
@@ -5,14 +5,19 @@ module  Annuaire
     before_action :retrieve_users, only: :index
 
     def index
-      institutions_subjects_by_theme = @institution.institutions_subjects
-        .preload(:subject, :theme, :experts_subjects, :not_deleted_experts)
-        .group_by(&:theme)
-      institutions_subjects_exportable = institutions_subjects_by_theme.values.flatten
+      puts "============================="
+      time = Benchmark.measure do
+        institutions_subjects_by_theme = @institution.institutions_subjects
+          .includes(:subject, :theme, :experts_subjects, :not_deleted_experts)
+          .group_by(&:theme)
+        institutions_subjects_exportable = institutions_subjects_by_theme.values.flatten
 
-      @grouped_subjects = institutions_subjects_by_theme.transform_values{ |is| is.group_by(&:subject) }
+        @grouped_subjects = institutions_subjects_by_theme.transform_values{ |is| is.group_by(&:subject) }
+        @grouped_users = @users.select(:antennes).group_by(&:antenne).transform_values{ |users| users.group_by(&:relevant_expert) }
 
-      @not_invited_users = not_invited_users
+        @not_invited_users = not_invited_users
+      end
+      puts "END time : #{time} ======================"
 
       respond_to do |format|
         format.html
@@ -73,17 +78,17 @@ module  Annuaire
     end
 
     def retrieve_users
-      @users = advisors
+      @users = base_users
         .relevant_for_skills
         .order('antennes.name', 'team_name', 'users.full_name')
-        .preload(:antenne, relevant_expert: [:users, :antenne, :experts_subjects])
+        .preload(:antenne, :user_rights_manager, relevant_expert: [:users, :antenne, :experts_subjects, :communes])
 
       if params[:region_id].present?
         @users = @users.in_region(params[:region_id])
       end
     end
 
-    def advisors
+    def base_users
       if params[:advisor].present?
         searched_advisor = User.find(params[:advisor])
         flash[:table_highlighted_ids] = [searched_advisor.id]
