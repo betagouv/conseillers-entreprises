b3a97f368d2650a095e13417aa20f2d0282414fa
Julien Dussart <julien.dussart@leikir.io>
2023-06-22 16:50:20 +0200

    clean solicitations exporter by removing logs, benchmark and dead code

--- app/services/csv_export/solicitation_exporter.rb
+++ app/services/csv_export/solicitation_exporter.rb
@@ -1,88 +1,57 @@
-require 'benchmark'
-
 module CsvExport
   class SolicitationExporter < BaseExporter
     # Ici, il s'agit du big fichier qui présente l'historique des solicitations jusqu'aux fin d'histoire.
     # Il y a un donc un mélange de solicitations et de matchs
     def initialize(relation, options = {})
-      puts 'YOLOOOOOOOOOOOOOOOO ---------------------------'
-      ActiveRecord::Base.logger.silence do
-        time = Benchmark.measure do
-          # time_matches_old = Benchmark.measure do
-          #   matches_id = relation.preload(:matches).map{ |s| s.matches.pluck(:id) }.flatten
-          #   @matches = Match.where(id: matches_id)
-          # puts "------ time_matches old : #{time_matches_old}"
+      solicitations_ids = relation.pluck(:id)
+      @matches = Match.joins(diagnosis: :solicitation)
+                      .where(solicitation: { id: solicitations_ids })
 
-          time_matches_new = Benchmark.measure do
-            solicitations_ids = relation.pluck(:id)
-            @matches = Match
-                        .joins(diagnosis: :solicitation)
-                        .where(solicitation: { id: solicitations_ids })
-          end
-          puts "------ time_matches NEW : #{time_matches_new}"
+      @solicitations = relation.without_matches
 
-          time_solicitations = Benchmark.measure do
-            @solicitations = relation.without_matches
-          end
-          puts "------ time_solicitations : #{time_solicitations}"
-          # Pour le nom du fichier
-          @relation = @solicitations
+      # Pour le nom du fichier
+      @relation = @solicitations
 
-          @options = options
-        end
-        puts "SolicitationExporter::initialize time : #{time}"
-      end
+      @options = options
     end
 
     def csv
-      all_csv = nil
-      time = Benchmark.measure do
-        # /!\ les fields de MatchExporter et SolicitationExporter doivent correspondre pour garantir la cohérence du fichier
-        matches_exporter = MatchExporter.new(@matches, @options)
-        match_attributes = matches_exporter.fields
-        solicitation_attributes = fields
+      # /!\ les fields de MatchExporter et SolicitationExporter doivent correspondre pour garantir la cohérence du fichier
+      matches_exporter = MatchExporter.new(@matches, @options)
+      match_attributes = matches_exporter.fields
+      solicitation_attributes = fields
 
-        all_csv = CSV.generate do |csv|
-          csv << match_attributes.keys.map{ |attr| Match.human_attribute_name(attr, default: attr) }
+      CSV.generate do |csv|
+        csv << match_attributes.keys.map{ |attr| Match.human_attribute_name(attr, default: attr) }
 
-          time_solicitations = Benchmark.measure do
-            solicitation_row = solicitation_attributes.values
-            sorted_solicitation_relation = sort_relation(@solicitations)
-            while sorted_solicitation_relation.count > 0
-              object = sorted_solicitation_relation.shift
-              csv << solicitation_row.map do |val|
-                if val.respond_to? :call
-                  lambda = val
-                  object.instance_exec(&lambda)
-                else
-                  object.send(val)
-                end
-              end
+        solicitation_row = solicitation_attributes.values
+        sorted_solicitation_relation = sort_relation(@solicitations)
+        while sorted_solicitation_relation.count > 0
+          object = sorted_solicitation_relation.shift
+          csv << solicitation_row.map do |val|
+            if val.respond_to? :call
+              lambda = val
+              object.instance_exec(&lambda)
+            else
+              object.send(val)
             end
           end
-          puts "----- generate csv with solicitations time : #{time_solicitations}"
-
-          time_matches = Benchmark.measure do
-            match_row = match_attributes.values
-            sorted_match_relation = matches_exporter.sort_relation(@matches)
-            while sorted_match_relation.count > 0
-              object = sorted_match_relation.shift
-              csv << match_row.map do |val|
-                if val.respond_to? :call
-                  lambda = val
-                  object.instance_exec(&lambda)
-                else
-                  object.send(val)
-                end
-              end
-            end
-          end
-          puts "----- generate csv with matches time : #{time_matches}"
         end
 
+        match_row = match_attributes.values
+        sorted_match_relation = matches_exporter.sort_relation(@matches)
+        while sorted_match_relation.count > 0
+          object = sorted_match_relation.shift
+          csv << match_row.map do |val|
+            if val.respond_to? :call
+              lambda = val
+              object.instance_exec(&lambda)
+            else
+              object.send(val)
+            end
+          end
+        end
       end
-      puts "SolicitationExporter::csv time : #{time}"
-      all_csv
     end
 
     def write_row(csv, row, object)
