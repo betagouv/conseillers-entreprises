bebdcf9631047e64a73301ff1e88d9da5bae5c93
clairezed <clairezuliani@protonmail.com>
2023-06-22 15:11:17 +0200

    Use expert link annuaire helper

--- app/helpers/annuaire_helper.rb
+++ app/helpers/annuaire_helper.rb
@@ -12,4 +12,15 @@ module AnnuaireHelper
     details = t('application.modal.see_details')
     [anomalie_title, details].join(' - ')
   end
+
+  def referencement_coverage_anomalie(anomalie_type, value)
+    case anomalie_type
+    when 'experts'
+      ids = value
+      experts = Expert.where(id: value)
+      experts.map{|e| link_to(e.full_name, edit_admin_expert_path(e), title: t('annuaire_helper.build_user_name_cell.edit_expert', expert_name: e.full_name, antenne: e.antenne)) }.join(", ").html_safe
+    else
+      value
+    end
+  end
 end
