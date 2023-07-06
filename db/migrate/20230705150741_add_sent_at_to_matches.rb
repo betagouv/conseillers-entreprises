c6e0038c9bc7dd0f034638bd3a3fc078d795f904
clairezed <clairezuliani@protonmail.com>
2023-07-06 14:24:36 +0200

    Improve batch task

--- db/migrate/20230705150741_add_sent_at_to_matches.rb
+++ db/migrate/20230705150741_add_sent_at_to_matches.rb
@@ -3,7 +3,7 @@ class AddSentAtToMatches < ActiveRecord::Migration[7.0]
     add_column :matches, :sent_at, :timestamp
 
     up_only do
-      # on fait le reste en batch, en console
+      # on fait le reste en batch, avec tâche rake init_match_sent_at
       Match.sent.created_between(3.months.ago, Time.zone.now).where(sent_at: nil).find_each do |match|
         match.update_columns(sent_at: match.diagnosis.completed_at)
       end
