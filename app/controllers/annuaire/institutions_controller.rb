1b0a2a42df41a4cdbc443130be42bf41dcbe65d4
Julien Dussart <julien.dussart@leikir.io>
2023-07-06 16:50:24 +0200

    WIP refine sql query to fetch antennes count and users count

--- app/controllers/annuaire/institutions_controller.rb
+++ app/controllers/annuaire/institutions_controller.rb
@@ -26,11 +26,9 @@ module  Annuaire
       antennes_count = Antenne.select('COUNT(DISTINCT antennes.id) AS antennes_count, antennes.institution_id AS institution_id')
         .left_joins(:regions, :experts)
         .not_deleted
-        # .where(territories: { id: params[:region_id] })
-        .or(Antenne.where(experts: { is_global_zone: true }).where(deleted_at: nil))
+        .where(territories: { id: params[:region_id] })
+        .or(Antenne.where(deleted_at: nil, experts: { is_global_zone: true }))
         .group('antennes.institution_id')
-
-      antennes_count = antennes_count.where(territories: { id: params[:region_id] }) if params[:region_id].present?
 
       @antennes_count = antennes_count.each_with_object({}) do |institution, hash|
         hash[institution.institution_id] = institution.antennes_count
@@ -41,11 +39,9 @@ module  Annuaire
       users_count = User.select('COUNT(DISTINCT users.id) AS users_count, antennes.institution_id AS institution_id')
         .not_deleted
         .left_joins(:antenne_regions, :experts)
-        # .where(antennes: { territories: { id: params[:region_id] } })
-        .or(Antenne.where(experts: { is_global_zone: true }).where(users: { deleted_at: nil }))
+        .where(antennes: { territories: { id: params[:region_id] } })
+        .or(Antenne.where(users: { deleted_at: nil }, experts: { is_global_zone: true }))
         .group('antennes.institution_id')
-
-      users_count = users_count.where(antennes: { territories: { id: params[:region_id] } }) if params[:region_id].present?
 
       @users_count = users_count.each_with_object({}) do |institution, hash|
         hash[institution.institution_id] = institution.users_count
