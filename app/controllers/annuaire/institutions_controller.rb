a2c68292f35362dc62d1065806fe0d4eaaa3e2ec
Julien Dussart <julien.dussart@leikir.io>
2023-07-11 17:50:16 +0200

    polish fetch of antennes, users and institutions to display annuaire on institutions page

--- app/controllers/annuaire/institutions_controller.rb
+++ app/controllers/annuaire/institutions_controller.rb
@@ -6,10 +6,6 @@ module  Annuaire
       authorize Institution, :index?
       get_antennes_count
       get_users_count
-      # if params[:region_id].present?
-      #   get_antennes_count
-      #   get_users_count
-      # end
     end
 
     def show
@@ -19,16 +15,23 @@ module  Annuaire
     private
 
     def retrieve_institutions
-      @institutions = Institution.retrieve_institutions(params[:region_id])
+      @institutions = Institution.includes(:logo, :themes).not_deleted.order(:slug)
+      @institutions = @institutions.in_region(params[:region_id]) if params[:region_id].present?
     end
 
     def get_antennes_count
-      antennes_count = Antenne.select('COUNT(DISTINCT antennes.id) AS antennes_count, antennes.institution_id AS institution_id')
-        .left_joins(:regions, :experts)
-        .not_deleted
-        .where(territories: { id: params[:region_id] })
-        .or(Antenne.where(deleted_at: nil, experts: { is_global_zone: true }))
-        .group('antennes.institution_id')
+      antennes_count = if params[:region_id].present?
+        Antenne.select('COUNT(DISTINCT antennes.id) AS antennes_count, antennes.institution_id AS institution_id')
+          .not_deleted
+          .left_joins(:regions, :experts)
+          .where(territories: { id: params[:region_id] })
+          .or(Antenne.where(deleted_at: nil, experts: { is_global_zone: true }))
+          .group('antennes.institution_id')
+      else
+        Antenne.select('COUNT(DISTINCT antennes.id) AS antennes_count, antennes.institution_id AS institution_id')
+          .not_deleted
+          .group('antennes.institution_id')
+      end
 
       @antennes_count = antennes_count.each_with_object({}) do |institution, hash|
         hash[institution.institution_id] = institution.antennes_count
@@ -36,12 +39,20 @@ module  Annuaire
     end
 
     def get_users_count
-      users_count = User.select('COUNT(DISTINCT users.id) AS users_count, antennes.institution_id AS institution_id')
+      users_count = if params[:region_id].present?
+        User.select('COUNT(DISTINCT users.id) AS users_count, antennes.institution_id AS institution_id')
+          .not_deleted
+          .left_joins(:antenne_regions, :experts)
+          .where(antennes: { territories: { id: params[:region_id] } })
+          .or(Antenne.where(users: { deleted_at: nil }, experts: { is_global_zone: true }))
+          .group('antennes.institution_id')
+      else
+        User.select('COUNT(DISTINCT users.id) AS users_count, antennes.institution_id AS institution_id')
+        .joins(:antenne)
         .not_deleted
-        .left_joins(:antenne_regions, :experts)
-        .where(antennes: { territories: { id: params[:region_id] } })
-        .or(Antenne.where(users: { deleted_at: nil }, experts: { is_global_zone: true }))
+        .where(antennes: { deleted_at: nil })
         .group('antennes.institution_id')
+      end
 
       @users_count = users_count.each_with_object({}) do |institution, hash|
         hash[institution.institution_id] = institution.users_count
