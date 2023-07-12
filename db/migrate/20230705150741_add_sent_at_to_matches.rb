4c80abff6603ed4965d775fa0b4d091af1c2889a
clairezed <clairezuliani@protonmail.com>
2023-07-12 14:22:30 +0200

    Fix sent scope use

--- db/migrate/20230705150741_add_sent_at_to_matches.rb
+++ db/migrate/20230705150741_add_sent_at_to_matches.rb
@@ -4,7 +4,7 @@ class AddSentAtToMatches < ActiveRecord::Migration[7.0]
 
     up_only do
       # on fait le reste en batch, avec tâche rake init_match_sent_at
-      Match.sent.created_between(3.months.ago, Time.zone.now).where(sent_at: nil).find_each do |match|
+      Match.where(id: Match.joins(:diagnosis).merge(Diagnosis.step_completed)).created_between(3.months.ago, Time.zone.now).where(sent_at: nil).find_each do |match|
         match.update_columns(sent_at: match.diagnosis.completed_at)
       end
     end
--- lib/tasks/init_match_sent_at.rake
+++ lib/tasks/init_match_sent_at.rake
@@ -1,5 +1,6 @@
+desc 'Init match sent_at'
 task init_match_sent_at: :environment do
-  Match.sent.where(sent_at: nil).find_in_batches(batch_size: 1000) do |matches|
+  Match.where(id: Match.joins(:diagnosis).merge(Diagnosis.step_completed)).where(sent_at: nil).find_in_batches(batch_size: 1000) do |matches|
     InitMatchSentAt.new(matches).delay(queue: :low_priority).call
   end
 end
