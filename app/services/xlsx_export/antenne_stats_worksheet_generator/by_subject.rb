e89522f43014561176ac6574e3132987e2b4382f
Lucien <mollard.lucien@gmail.com>
2023-07-18 15:41:56 +0200

    order subjects by needs.count

--- app/services/xlsx_export/antenne_stats_worksheet_generator/by_subject.rb
+++ app/services/xlsx_export/antenne_stats_worksheet_generator/by_subject.rb
@@ -6,10 +6,15 @@ module XlsxExport
 
         add_agglomerate_headers
 
+        needs_by_subjects = {}
+
         @antenne.institution.subjects.each do |subject|
-          needs = @needs.where(subject: subject)
+          needs_by_subjects[subject.label] = @needs.where(subject: subject)
+        end
+
+        needs_by_subjects.sort_by { |_, needs| -needs.count }.each do |subject_label, needs|
           ratio = calculate_rate(needs.count, @needs)
-          add_agglomerate_rows(needs, ratio, subject.label)
+          add_agglomerate_rows(needs, ratio, subject_label)
         end
 
         finalise_agglomerate_style
