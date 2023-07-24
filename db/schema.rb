46e074fb1b203e872bd4c2f854cf5db362ee4d90
clairezed <clairezuliani@protonmail.com>
2023-07-24 16:21:59 +0200

    Fix missing referencement_coverages in schema

--- db/schema.rb
+++ db/schema.rb
@@ -512,6 +512,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_07_152426) do
     t.index ["category"], name: "index_quarterly_reports_on_category"
   end
 
+  create_table "referencement_coverages", force: :cascade do |t|
+    t.bigint "antenne_id", null: false
+    t.bigint "institution_subject_id", null: false
+    t.string "coverage"
+    t.integer "anomalie"
+    t.json "anomalie_details"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["antenne_id"], name: "index_referencement_coverages_on_antenne_id"
+    t.index ["institution_subject_id"], name: "index_referencement_coverages_on_institution_subject_id"
+  end
+
   create_table "reminders_actions", force: :cascade do |t|
     t.bigint "need_id", null: false
     t.datetime "created_at", null: false
@@ -701,6 +713,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_07_152426) do
   add_foreign_key "needs", "diagnoses"
   add_foreign_key "needs", "subjects"
   add_foreign_key "quarterly_reports", "antennes"
+  add_foreign_key "referencement_coverages", "antennes"
+  add_foreign_key "referencement_coverages", "institutions_subjects"
   add_foreign_key "reminders_actions", "needs"
   add_foreign_key "reminders_registers", "experts"
   add_foreign_key "searches", "users"
