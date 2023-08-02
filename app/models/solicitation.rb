b3272ca95c2d5ae644a5f8cd1e38e6dd3932c06d
clairezed <clairezuliani@protonmail.com>
2023-08-02 16:54:45 +0200

    Remplacement mail retraite -> no_expert_juridique

--- app/models/solicitation.rb
+++ app/models/solicitation.rb
@@ -75,7 +75,7 @@ class Solicitation < ApplicationRecord
 
   paginates_per 25
 
-  GENERIC_EMAILS_TYPES = %i[bad_quality particular_retirement creation employee_labor_law siret moderation independent_tva intermediary recruitment_foreign_worker no_expert carsat tns_training]
+  GENERIC_EMAILS_TYPES = %i[bad_quality creation employee_labor_law siret moderation independent_tva intermediary recruitment_foreign_worker no_expert no_expert_juridique carsat tns_training]
 
   ## Status
   #
