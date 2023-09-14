089767c0f06bf349077045808e8be97182371b05
Lucien <mollard.lucien@gmail.com>
2023-09-14 17:47:18 +0200

    test received matches

--- app/services/stats/filters_stats.rb
+++ app/services/stats/filters_stats.rb
@@ -62,9 +62,8 @@ module Stats
     end
 
     def filtered_matches(query)
-      query = query.joins(:need)
       query.merge! query.in_region(territory) if territory.present?
-      query.merge! antenne_or_institution.perimeter_received_matches if antenne_or_institution.present?
+      query.merge! antenne_or_institution.received_matches if antenne_or_institution.present?
       query.merge! Match.joins(:subject).where(subjects: subject) if subject.present?
       query.merge! Match.joins(need: { solicitation: :landing })
         .where(solicitations: { landings: { integration: integration } }) if integration.present?
