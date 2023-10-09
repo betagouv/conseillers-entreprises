0485f4487a232afad9dc53969e767b0570e1ad88
Lucien <mollard.lucien@gmail.com>
2023-10-09 16:43:34 +0200

    managers can modify match status

--- app/policies/match_policy.rb
+++ app/policies/match_policy.rb
@@ -4,6 +4,16 @@ class MatchPolicy < ApplicationPolicy
   end
 
   def update_status?
+    admin? ||
+      (@user.is_manager? && @record.in?(@user.managed_antennes.map(&:perimeter_received_needs))) ||
+      (@user.is_manager? && (@user.managed_antennes.include?(@record.expert.antenne)))
+  end
+
+  def taking_care_by?
+    admin?
+  end
+
+  def show_inbox?
     admin?
   end
 end
--- spec/policies/match_policy_spec.rb
+++ spec/policies/match_policy_spec.rb
@@ -33,6 +33,39 @@ RSpec.describe MatchPolicy, type: :policy do
       it { is_expected.to permit(user, match) }
     end
 
+    context "grant access for manager if match is in a managed antenne" do
+      let(:antenne) { match.expert.antenne }
+      let(:user) { create :user, :manager, antenne: antenne }
+
+      it { is_expected.to permit(user, match) }
+    end
+
+    context "denies access for manager if match is not in a managed antenne" do
+      let(:user) { create :user, :manager, antenne: create(:antenne) }
+
+      it { is_expected.not_to permit(user, match) }
+    end
+
+    context "denies access if user is a contacted user" do
+      let(:user) { match.contacted_users.first }
+
+      it { is_expected.not_to permit(user, match) }
+    end
+
+    context "denies access if user is another user" do
+      let(:user) { create :user }
+
+      it { is_expected.not_to permit(user, match) }
+    end
+  end
+
+  permissions :taking_care_by?, :show_inbox? do
+    context "grants access if user is an admin" do
+      let(:user) { create :user, :admin }
+
+      it { is_expected.to permit(user, match) }
+    end
+
     context "denies access if user is a contacted user" do
       let(:user) { match.contacted_users.first }
 
