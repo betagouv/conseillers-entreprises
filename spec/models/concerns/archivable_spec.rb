72c8cc728136099174cffbb999e8a55d7aaa2631
Lucien <mollard.lucien@gmail.com>
2023-10-12 10:04:35 +0200

    rubocop RedundantActiveRecordAllMethod

--- spec/models/concerns/archivable_spec.rb
+++ spec/models/concerns/archivable_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe Archivable do
 
       it('archives the diagnosis') do
         expect(diagnosis.is_archived).to be_truthy
-        expect(Diagnosis.all.count).to eq 1
+        expect(Diagnosis.count).to eq 1
         expect(Diagnosis.archived(false).count).to eq 0
         expect(Diagnosis.archived(true).count).to eq 1
       end
