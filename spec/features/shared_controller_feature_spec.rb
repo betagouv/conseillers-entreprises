3a1cbe9706dbbfd808f2965b960fb7d27b16be2f
Lucien <mollard.lucien@gmail.com>
2023-10-17 10:07:02 +0200

    fix errors pages tests

--- spec/features/shared_controller_feature_spec.rb
+++ spec/features/shared_controller_feature_spec.rb
@@ -6,11 +6,10 @@ describe 'SharedController features' do
   describe 'render_error' do
     login_admin
 
-    # TODO : à mettre à jour
     before do
       ENV['TEST_ERROR_RENDERING'] = 'true'
-      # allow_any_instance_of(User).to receive(:received_needs).and_raise(raised_error)
-      # # Je comprend pas pourquoi ce test visite une url de diagnoses et si on rien ne passe
+      allow_any_instance_of(User).to receive(:received_needs).and_raise(raised_error)
+      visit quo_active_needs_path
     end
 
     after do
@@ -18,19 +17,15 @@ describe 'SharedController features' do
     end
 
     describe '404 error' do
-      it do
-        # visit edit_user_path
-        expect(page.html).to include I18n.t('shared.errors.404.message')
-      end
+      let(:raised_error) { ActiveRecord::RecordNotFound }
+
+      it { expect(page.html).to include(I18n.t('shared.errors.404.message')) }
     end
 
     describe '500 error' do
       let(:raised_error) { ArgumentError }
 
-      it do
-        visit need_path(id: 'wrong_id')
-        expect(page.html).to include I18n.t('shared.errors.500.message')
-      end
+      it { expect(page.html).to include I18n.t('shared.errors.500.message') }
     end
   end
 end
