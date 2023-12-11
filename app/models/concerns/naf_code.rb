2bd6512de5bcc2c80c5890c8eb790d95a29b0409
Lucien <mollard.lucien@gmail.com>
2023-12-11 12:12:29 +0100

    moves translation to I18n

--- app/models/concerns/naf_code.rb
+++ app/models/concerns/naf_code.rb
@@ -5,12 +5,12 @@ module NafCode
 
   def self.naf_libelle(naf_code, level = 'a10')
     return I18n.t('no_data') if naf_code.nil?
-    I18n.t(naf_code, scope: "naf_libelle_#{level}", default: 'code non trouvé')
+    I18n.t(naf_code, scope: "naf_libelle_#{level}", default: t('naf_libelle.code_not_found'))
   end
 
   def self.code_a10(naf_code)
     return if naf_code.nil?
-    I18n.t(level2_code(naf_code), scope: 'naf_level2_to_naf_a10', default: 'code non trouvé')
+    I18n.t(level2_code(naf_code), scope: 'naf_level2_to_naf_a10', default: t('naf_libelle.code_not_found'))
   end
 
   def self.level2_code(naf_code)
