30429e9e18d379aaa9d0f18bacd080972a050c79
Lucien <mollard.lucien@gmail.com>
2023-12-11 11:58:26 +0100

    prevent undefined naf code

--- app/models/concerns/naf_code.rb
+++ app/models/concerns/naf_code.rb
@@ -5,12 +5,12 @@ module NafCode
 
   def self.naf_libelle(naf_code, level = 'a10')
     return I18n.t('no_data') if naf_code.nil?
-    I18n.t(naf_code, scope: "naf_libelle_#{level}")
+    I18n.t(naf_code, scope: "naf_libelle_#{level}", default: 'code non trouvé')
   end
 
   def self.code_a10(naf_code)
     return if naf_code.nil?
-    I18n.t(level2_code(naf_code), scope: 'naf_level2_to_naf_a10')
+    I18n.t(level2_code(naf_code), scope: 'naf_level2_to_naf_a10', default: 'code non trouvé')
   end
 
   def self.level2_code(naf_code)
--- app/services/stats/companies/by_naf_code.rb
+++ app/services/stats/companies/by_naf_code.rb
@@ -28,7 +28,7 @@ module Stats::Companies
     end
 
     def colors
-      %w[#DDDDDD #9F3BCA #F15C80 #E78016 #F2DD68 #2D908F #62E0D3 #88c479 #A7FF96 #946D47 #64609B #63DDDB #F45A5A]
+      %w[#DDDDDD #9F3BCA #F15C80 #E78016 #F2DD68 #2D908F #62E0D3 #88c479 #A7FF96 #946D47 #64609B #DDDDDD #F45A5A]
     end
 
     def count
