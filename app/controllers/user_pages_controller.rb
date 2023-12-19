76344573cf3ead17849d86ecc9f9938f193a5e9d
clairezed <clairezuliani@protonmail.com>
2023-12-19 11:30:33 +0100

    Update footer and remove tutoriel unconnected

--- app/controllers/user_pages_controller.rb
+++ app/controllers/user_pages_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class UserPagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:tutoriels]
   before_action :fetch_themes
 
   def tutoriels; end
