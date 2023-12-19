b9536b3b7f76fbf65a088124cd6a77bb57b9aac6
clairezed <clairezuliani@protonmail.com>
2023-12-19 21:08:17 +0100

    Ajout des infos page d'origine dans les exports

--- app/services/csv_export/solicitation_exporter.rb
+++ app/services/csv_export/solicitation_exporter.rb
@@ -72,6 +72,7 @@ module CsvExport
         solicitation_provenance_category: -> { I18n.t(provenance_category, scope: %i(solicitation provenance_categories)) if provenance_category.present? },
         solicitation_provenance_title: -> { provenance_title },
         solicitation_provenance_detail: -> { provenance_detail },
+        solicitation_origin_id: -> { solicitation&.origin_id },
         solicitation_gclid: -> { gclid },
         landing_theme_title: -> { landing_theme&.title },
         landing_subject_title: -> { subject&.label },
