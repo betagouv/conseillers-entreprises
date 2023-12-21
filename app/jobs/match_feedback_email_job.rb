298dc5ca358d6f385acf30091108d4901a36960f
Lucien <mollard.lucien@gmail.com>
2023-12-21 11:36:31 +0100

    fix bug notify feedback

--- app/jobs/match_feedback_email_job.rb
+++ app/jobs/match_feedback_email_job.rb
@@ -1,9 +1,9 @@
 class MatchFeedbackEmailJob < ApplicationJob
-  def perform(feedback_id, person_id)
+  def perform(feedback_id, expert_id)
     feedback = Feedback.find_by(id: feedback_id)
     if feedback.present?
-      user = User.find(person_id)
-      UserMailer.match_feedback(feedback, user).deliver_now
+      expert = Expert.find(expert_id)
+      UserMailer.match_feedback(feedback, expert).deliver_now
     end
   end
 end
