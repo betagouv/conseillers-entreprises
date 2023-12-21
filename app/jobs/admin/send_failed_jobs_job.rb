de1ce973ecd0a68d65b4883ffe3e9de593261236
Lucien <mollard.lucien@gmail.com>
2023-12-21 13:55:15 +0100

    fix failed jobs mailer

--- app/jobs/admin/send_failed_jobs_job.rb
+++ app/jobs/admin/send_failed_jobs_job.rb
@@ -2,7 +2,7 @@ class Admin::SendFailedJobsJob < ApplicationJob
   queue_as :low_priority
 
   def perform
-    failed_jobs = Sidekiq::DeadSet.new.map{ |job| job }.as_json
+    failed_jobs = Sidekiq::FailureSet.new.map{ |job| job }.as_json
     AdminMailer.failed_jobs(failed_jobs).deliver_later(queue: 'low_priority')
   end
 end
