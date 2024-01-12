9d40318214a2583629b66dcf2f48776899267b10
Claire Zuliani <clairezuliani@gmail.com>
2024-01-12 16:58:30 +0100

    debug migration

--- db/migrate/20240109123620_add_children_antennes.rb
+++ db/migrate/20240109123620_add_children_antennes.rb
@@ -4,17 +4,20 @@ class AddChildrenAntennes < ActiveRecord::Migration[7.0]
 
     up_only do
       Antenne.not_deleted.territorial_level_regional.find_each do |regional_antenne|
+        p "antenne regionale : #{regional_antenne.name} (#{regional_antenne.id}) ========================"
         institution_id = regional_antenne.institution_id
-
+        
         territorial_antennes = Antenne.not_deleted.where(institution_id: institution_id, territorial_level: Antenne.territorial_levels[:local])
-          .left_joins(:communes, :experts)
-          .where(communes: { id: regional_antenne.commune_ids })
-          .or(Antenne.not_deleted.where(institution_id: institution_id, territorial_level: Antenne.territorial_levels[:local]).where(experts: { is_global_zone: true }))
-          .distinct
-        territorial_antennes.update_all(parent_antenne_id: regional_antenne.id)
-
+        .left_joins(:communes, :experts)
+        .where(communes: { id: regional_antenne.commune_ids })
+        .or(Antenne.not_deleted.where(institution_id: institution_id, territorial_level: Antenne.territorial_levels[:local]).where(experts: { is_global_zone: true }))
+        .distinct
+        p "Problème antennes territoriale !!!" if territorial_antennes.empty?
+        territorial_antennes.update_all(parent_antenne_id: regional_antenne.id) if territorial_antennes.any?
+        
         national_antenne = Antenne.not_deleted.where(institution_id: regional_antenne.institution_id, territorial_level: :national).first
-        regional_antenne.update(parent_antenne_id: national_antenne.id)
+        p "Problème antenne nationale !!!" if national_antenne.blank?
+        regional_antenne.update(parent_antenne_id: national_antenne.id) if national_antenne.present?
       end
     end
   end
