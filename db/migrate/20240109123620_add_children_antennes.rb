a6cd08fc92f4d2d64aaddd7f9b34459016886e8e
Claire Zuliani <clairezuliani@gmail.com>
2024-01-15 09:01:48 +0100

    Lint

--- db/migrate/20240109123620_add_children_antennes.rb
+++ db/migrate/20240109123620_add_children_antennes.rb
@@ -3,20 +3,17 @@ class AddChildrenAntennes < ActiveRecord::Migration[7.0]
     add_reference :antennes, :parent_antenne, references: :antennes, index: true
 
     up_only do
-      Antenne.not_deleted.territorial_level_regional.find_each do |regional_antenne|
-        p "antenne regionale : #{regional_antenne.name} (#{regional_antenne.id}) ========================"
+      Antenne.not_deleted.territorial_level_regional.order(:institution_id).find_each do |regional_antenne|
         institution_id = regional_antenne.institution_id
-        
+
         territorial_antennes = Antenne.not_deleted.where(institution_id: institution_id, territorial_level: Antenne.territorial_levels[:local])
-        .left_joins(:communes, :experts)
-        .where(communes: { id: regional_antenne.commune_ids })
-        .or(Antenne.not_deleted.where(institution_id: institution_id, territorial_level: Antenne.territorial_levels[:local]).where(experts: { is_global_zone: true }))
-        .distinct
-        p "Problème antennes territoriale !!!" if territorial_antennes.empty?
+          .left_joins(:communes, :experts)
+          .where(communes: { id: regional_antenne.commune_ids })
+          .or(Antenne.not_deleted.where(institution_id: institution_id, territorial_level: Antenne.territorial_levels[:local]).where(experts: { is_global_zone: true }))
+          .distinct
         territorial_antennes.update_all(parent_antenne_id: regional_antenne.id) if territorial_antennes.any?
-        
+
         national_antenne = Antenne.not_deleted.where(institution_id: regional_antenne.institution_id, territorial_level: :national).first
-        p "Problème antenne nationale !!!" if national_antenne.blank?
         regional_antenne.update(parent_antenne_id: national_antenne.id) if national_antenne.present?
       end
     end
