32658289be010b2a3809b2d1889064fb85184c06
Claire Zuliani <clairezuliani@gmail.com>
2024-01-25 13:24:55 +0100

    Prevent duplicate deleted_user

--- app/admin/duplicate_user.rb
+++ app/admin/duplicate_user.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register_page 'Duplicate user' do
   page_action :duplicate, method: :post do
     old_user = User.find(params[:user_id])
     if old_user.deleted?
-      flash[:alert] = 'utilisateur supprimé'
+      flash[:alert] = I18n.t('active_admin.duplicate_user.deleted_user')
       redirect_to admin_users_path and return
     end
     user_params = params.require(:user).permit(:full_name, :email, :phone_number, :job, :specifics_territories)
