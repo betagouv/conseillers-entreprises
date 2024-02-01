b3e44caff326db51f1177ad90147635c83ac815e
Lucien <mollard.lucien@gmail.com>
2024-02-01 14:13:33 +0100

    linter is coming

--- spec/features/application_controller_feature_spec.rb
+++ spec/features/application_controller_feature_spec.rb
@@ -77,7 +77,7 @@ describe 'ApplicationController specific features' do
       visit new_user_session_path
       fill_in I18n.t('attributes.email'), with: user.email
       fill_in I18n.t('activerecord.attributes.user.password'), with: password
-      click_on I18n.t('sign_in')
+      click_on I18n.t('sign_in'), class: 'fr-btn'
     end
 
     context 'first connexion' do
