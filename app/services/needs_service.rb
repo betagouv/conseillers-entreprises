e9d911627944aa5b8c6cf1e355ca6a8b6ea58f7d
Lucien <mollard.lucien@gmail.com>
2024-02-08 16:31:24 +0100

    remove unused code

--- app/services/needs_service.rb
+++ app/services/needs_service.rb
@@ -6,17 +6,4 @@ class NeedsService
       .where(sent_at: ..Need::ARCHIVE_DELAY.ago)
       .update_all(archived_at: Time.zone.now)
   end
-
-  def self.abandon_needs
-    Need.status_quo
-      .without_action('abandon')
-      .where(created_at: ..Need::REMINDERS_DAYS[:abandon].days.ago).find_each do |need|
-      # Envoie de l'email d'abandon a l’entreprise si le besoin a aucun email envoyé et qu'il a plus de 45 jours
-      if need.abandoned_email_sent == false
-        CompanyMailer.abandoned_need(need).deliver_later
-        need.update(abandoned_email_sent: true)
-      end
-      need.reminders_actions.create(category: 'abandon')
-    end
-  end
 end
--- spec/services/needs_service_spec.rb
+++ spec/services/needs_service_spec.rb
@@ -23,41 +23,4 @@ describe NeedsService do
       expect(match5.reload.archived_at).to be_nil
     end
   end
-
-  describe 'abandon_needs' do
-    context 'without last chance email' do
-      # Besoin quo done_no_help done_not_reachable de moins de 45 jours   ko
-      # Besoin quo done_no_help done_not_reachable de plus de 45 jours    ok
-      # Besoin done de moins de 45 jours                                  ko
-      # Besoin done de plus de 45 jours                                   ko
-      # Besoin done_no_help de moins de 45 jours                          ko
-      # Besoin done_not_reachable de moins de 45 jours                    ko
-
-      let!(:need1) { create :need, matches: [match1] }
-      let(:match1) { create :match, status: :quo }
-      let!(:need2) { create :need, matches: [match2], created_at: 45.days.ago }
-      let(:match2) { create :match, status: :quo, created_at: 45.days.ago }
-      let!(:need3) { create :need, matches: [match3] }
-      let(:match3) { create :match, status: :done }
-      let!(:need4) { create :need, matches: [match4], created_at: 45.days.ago }
-      let(:match4) { create :match, status: :done, created_at: 45.days.ago }
-      let!(:need5) { create :need, matches: [match5] }
-      let(:match5) { create :match, status: :done_no_help }
-      let!(:need6) { create :need, matches: [match6] }
-      let(:match6) { create :match, status: :done_not_reachable }
-
-      before { described_class.abandon_needs }
-
-      it 'abandon only old needs without help and send' do
-        expect(need1.reload.is_abandoned?).to be false
-        expect(need2.reload.is_abandoned?).to be true
-        expect(need3.reload.is_abandoned?).to be false
-        expect(need4.reload.is_abandoned?).to be false
-        expect(need5.reload.is_abandoned?).to be false
-        expect(need6.reload.is_abandoned?).to be false
-        assert_enqueued_with(job: ActionMailer::MailDeliveryJob)
-        expect(enqueued_jobs.count).to eq 1
-      end
-    end
-  end
 end
