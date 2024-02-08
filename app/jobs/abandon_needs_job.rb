7787d409a2d6727b5393ac44b92700a96db7c410
Lucien <mollard.lucien@gmail.com>
2024-02-08 14:51:49 +0100

    improve condition

--- app/jobs/abandon_needs_job.rb
+++ app/jobs/abandon_needs_job.rb
@@ -4,12 +4,13 @@ class AbandonNeedsJob < ApplicationJob
   def perform
     Need.status_quo
       .without_action('abandon')
-      .where(created_at: ..Need::REMINDERS_DAYS[:last_chance].days.ago).find_each do |need|
-      # Envoie de l'email d'abandon a l’entreprise si le besoin a plus de 45 jours
-      if need.created_at <= Need::REMINDERS_DAYS[:abandon].days.ago
+      .where(created_at: ..Need::REMINDERS_DAYS[:abandon].days.ago).find_each do |need|
+      # Envoie de l'email d'abandon a l’entreprise si le besoin a aucun email envoyé et qu'il a plus de 45 jours
+      if need.abandoned_email_sent == false
         CompanyMailer.abandoned_need(need).deliver_later(queue: 'low_priority')
-        need.reminders_actions.create(category: 'abandon')
+        need.update(abandoned_email_sent: true)
       end
+      need.reminders_actions.create(category: 'abandon')
     end
   end
 end
--- app/services/needs_service.rb
+++ app/services/needs_service.rb
@@ -10,14 +10,13 @@ class NeedsService
   def self.abandon_needs
     Need.status_quo
       .without_action('abandon')
-      .where(created_at: ..Need::REMINDERS_DAYS[:last_chance].days.ago).find_each do |need|
-      # Envoie de l'email d'abandon a l’entreprise si :
-      # le besoin a aucun email envoyé et qu'il a plus de 45 jours
-      # ou si le besoin a un email envoyé depuis plus de 10 jours et que le besoin a plus de 21 jours
-      if need.created_at <= Need::REMINDERS_DAYS[:abandon].days.ago
+      .where(created_at: ..Need::REMINDERS_DAYS[:abandon].days.ago).find_each do |need|
+      # Envoie de l'email d'abandon a l’entreprise si le besoin a aucun email envoyé et qu'il a plus de 45 jours
+      if need.abandoned_email_sent == false
         CompanyMailer.abandoned_need(need).deliver_later
-        need.reminders_actions.create(category: 'abandon')
+        need.update(abandoned_email_sent: true)
       end
+      need.reminders_actions.create(category: 'abandon')
     end
   end
 end
