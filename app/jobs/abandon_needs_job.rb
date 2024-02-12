36824da2ce76a96e1a998d592f6e48605055beae
Lucien <mollard.lucien@gmail.com>
2024-02-12 13:48:16 +0100

    improvements

--- app/jobs/abandon_needs_job.rb
+++ app/jobs/abandon_needs_job.rb
@@ -4,6 +4,7 @@ class AbandonNeedsJob < ApplicationJob
   def perform
     Need.status_quo
       .without_action('abandon')
+      .without_action('refused')
       .where(created_at: ..Need::REMINDERS_DAYS[:abandon].days.ago).find_each do |need|
       CompanyMailer.failed_need(need).deliver_later(queue: 'low_priority')
       need.reminders_actions.create(category: 'abandon')
