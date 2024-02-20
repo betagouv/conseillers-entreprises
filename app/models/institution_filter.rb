48fd8f3190aed05f153dda2b106f7e1f3b91f4a5
Lucien <mollard.lucien@gmail.com>
2024-02-20 16:48:10 +0100

    add comments

--- app/models/institution_filter.rb
+++ app/models/institution_filter.rb
@@ -27,4 +27,9 @@ class InstitutionFilter < ApplicationRecord
   validates :institution_filtrable_id, uniqueness: { scope: %i[institution_filtrable_type additional_subject_question_id] }
 
   delegate :key, to: :additional_subject_question
+
+  ## institution_filtrable_type
+  # Institution : Lié à une institution il sert à l'inclure ou non en fonction de la réponse
+  # Solicitation : Sert pour sauvegarder la réponse à la question additionnelle
+  # Need : Sert pour sauvegarder la réponse au niveau du besoin et la comparer à l'institution
 end
