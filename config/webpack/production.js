401cf94c39428ef4a176123c7f032cc57fbfb1ec
Lucien <mollard.lucien@gmail.com>
2024-02-28 14:03:33 +0100

    add jquery plugin

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,27 +1,3 @@
-// process.env.NODE_ENV = process.env.NODE_ENV || 'production'
-// const webpack = require('webpack');
-
-// const base = require('./base')
-
-// // Config specifique pour eviter une erreur CSP
-// // cf https://github.com/webpack/webpack/issues/5627#issuecomment-394309966
-// const extraConfig = {
-//   node: {
-//     global: false,
-//   },
-//   plugins: [
-//     new webpack.DefinePlugin({
-//       global: "window", // Placeholder for global used in any node_modules
-//     }),
-//   ],
-// };
-
-// base.config.merge(extraConfig);
-
-// module.exports = base.toWebpackConfig()
-
-// const { EsbuildPlugin } = require('esbuild-loader')
-// const CompressionPlugin = require('compression-webpack-plugin')
 const webpack = require("webpack")
 
 module.exports = (webpackConfig) => {
@@ -30,32 +6,16 @@ module.exports = (webpackConfig) => {
   webpackConfig.node = {
     global: false,
   },
-  // webpackConfig.bail = true
 
-  // webpackConfig.plugins.push(
-  //   new CompressionPlugin({
-  //     filename: '[path][base].gz[query]',
-  //     algorithm: 'gzip',
-  //     test: /\.(js|css|html|json|ico|svg|eot|otf|ttf|map)$/
-  //   })
-  // )
   webpackConfig.plugins.push(
     new webpack.DefinePlugin({
       global: "window", // Placeholder for global used in any node_modules
     }),
+    new webpack.ProvidePlugin({
+      $: 'jquery/src/jquery',
+      jQuery: 'jquery/src/jquery'
+    })
   )
-
-  // const prodOptimization = {
-  //   minimize: true,
-  //   minimizer: [
-  //     new EsbuildPlugin({
-  //       target: 'es2015',
-  //       css: true  // Apply minification to CSS assets
-  //     })
-  //   ]
-  // }
-
-  // Object.assign(webpackConfig.optimization, prodOptimization);
 
   return webpackConfig;
 }
