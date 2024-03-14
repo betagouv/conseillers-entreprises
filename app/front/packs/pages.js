9b4418357e0f0763d2cdfcfd5f362b086f182a2d
Claire Zuliani <clairezuliani@gmail.com>
2024-03-14 16:40:29 +0100

    Internalisation de remixicon

--- app/front/packs/pages.js
+++ app/front/packs/pages.js
@@ -20,11 +20,6 @@ import "whatwg-fetch";
 import "@hotwired/turbo-rails";
 
 require("@rails/ujs").start();
-// require('remixicon/fonts/remixicon.css');
-// require('@gouvfr/dsfr/dist/utility/icons/icons-communication/icons-communication.css')
-// require('@gouvfr/dsfr/dist/dsfr/dsfr.css')
-// require('../stylesheets/pages.sass')
-
 require("jquery");
 import "../javascripts/shared";
 import "../javascripts/pages";
