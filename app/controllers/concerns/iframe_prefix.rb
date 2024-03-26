85feefbb963a00ab75a9209964b0688bd412a46e
Claire Zuliani <clairezuliani@gmail.com>
2024-03-26 12:30:51 +0100

    lint

--- app/controllers/concerns/iframe_prefix.rb
+++ app/controllers/concerns/iframe_prefix.rb
@@ -38,15 +38,11 @@ module IframePrefix
   end
 
   def fetch_query_params
-    p "fetch_query_params ---------------------------"
-    p "session"
-    p session[:solicitation_form_info]
     saved_params = session[:solicitation_form_info] || {}
     # pas de session dans les iframe, on recupere les params dans l'url
     url_query_params = view_params.slice(*Solicitation::FORM_INFO_KEYS + [:siret, :redirected] + AdditionalSubjectQuestion.pluck(:key))
     # on supprime les params matomo anciens si doublon
     saved_params.except!(*Solicitation::MATOMO_KEYS.map(&:to_s)) if double_matomo_params(saved_params, url_query_params)
-    p "fetch_query_params END ----------------------------"
     saved_params.with_indifferent_access.merge!(url_query_params)
   end
 
