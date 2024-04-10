e17882cdb82f8fcb9789bc340c2d74a1be6ee376
Lucien <mollard.lucien@gmail.com>
2024-04-10 11:57:39 +0200

    uses persist search controller

--- app/controllers/annuaire/institutions_controller.rb
+++ app/controllers/annuaire/institutions_controller.rb
@@ -18,17 +18,17 @@ module  Annuaire
 
     def retrieve_institutions
       @institutions = Institution.expert_provider.includes(:logo, :themes).not_deleted.order(:slug)
-      @institutions = @institutions.by_region(session[:annuaire_region]) if session[:annuaire_region].present?
-      @institutions = @institutions.joins(:themes).where(themes: { id: session[:annuaire_theme] }) if session[:annuaire_theme].present?
-      @institutions = @institutions.joins(:subjects).where(subjects: { id: session[:annuaire_subject] }) if session[:annuaire_subject].present?
+      @institutions = @institutions.by_region(index_search_params[:region]) if index_search_params[:region].present?
+      @institutions = @institutions.joins(:themes).where(themes: { id: index_search_params[:theme] }) if index_search_params[:theme].present?
+      @institutions = @institutions.joins(:subjects).where(subjects: { id: index_search_params[:subject] }) if index_search_params[:subject].present?
     end
 
     def get_antennes_count
       antennes_count = Antenne.select('COUNT(DISTINCT antennes.id) AS antennes_count, antennes.institution_id AS institution_id')
         .not_deleted
-        .by_region(session[:annuaire_region])
-        .by_subject(session[:annuaire_subject])
-        .by_theme(session[:annuaire_theme])
+        .by_region(index_search_params[:region])
+        .by_subject(index_search_params[:subject])
+        .by_theme(index_search_params[:theme])
         .group('antennes.institution_id')
 
       @antennes_count = antennes_count.each_with_object({}) do |institution, hash|
@@ -41,9 +41,9 @@ module  Annuaire
         .joins(:antenne)
         .not_deleted
         .where(antennes: { deleted_at: nil })
-        .by_region(session[:annuaire_region])
-        .by_subject(session[:annuaire_subject])
-        .by_theme(session[:annuaire_theme])
+        .by_region(index_search_params[:region])
+        .by_subject(index_search_params[:subject])
+        .by_theme(index_search_params[:theme])
         .group('antennes.institution_id')
 
       @users_count = users_count.each_with_object({}) do |institution, hash|
