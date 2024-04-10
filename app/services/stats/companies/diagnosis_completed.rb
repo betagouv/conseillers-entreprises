7b2be5e3cff66e7e32236ab1584db90549cb1a7c
Claire Zuliani <clairezuliani@gmail.com>
2024-04-10 18:42:43 +0200

    Mises à jour pages publiques

--- app/services/stats/companies/diagnosis_completed.rb
+++ app/services/stats/companies/diagnosis_completed.rb
@@ -3,9 +3,11 @@ module Stats::Companies
     include ::Stats::MiniStats
 
     def main_query
+      diagnosis_done = Diagnosis.from_solicitation
+        .joins(:needs).merge(Need.status_done)
       Company
         .joins(facilities: :diagnoses)
-        .where(facilities: { diagnoses: { step: :completed } })
+        .where(facilities: { diagnoses: diagnosis_done })
         .distinct
     end
   end
