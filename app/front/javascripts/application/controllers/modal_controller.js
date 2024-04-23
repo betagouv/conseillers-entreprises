141abb4c3cb4aadd39f423b825c4d60765f9d640
Claire Zuliani <clairezuliani@gmail.com>
2024-04-24 00:04:50 +0200

    Fix lint

--- app/front/javascripts/application/controllers/modal_controller.js
+++ app/front/javascripts/application/controllers/modal_controller.js
@@ -4,16 +4,16 @@ export default class extends Controller {
   static targets = [  ]
 
   connect() {
+    // eslint-disable-next-line no-undef
     dsfr(this.element).modal.disclose()
-    this.element.addEventListener('dsfr.conceal', (e) => {
+    this.element.addEventListener('dsfr.conceal', () => {
       this.markAsSeen();
-  })
+    })
   }
 
   async markAsSeen() {
-    console.log("markAsSeen")
     let url = this.element.dataset.url
-    const response = await fetch(url, {
+    await fetch(url, {
       credentials: "same-origin"
     })
   }
