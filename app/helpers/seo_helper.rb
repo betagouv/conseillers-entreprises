d17eaf9af680b51206a92a22f0e8f54d16b22133
Claire Zuliani <clairezuliani@gmail.com>
2024-05-15 15:26:08 +0200

    lint !

--- app/helpers/seo_helper.rb
+++ app/helpers/seo_helper.rb
@@ -1,27 +1,25 @@
 module SeoHelper
-
-	def home_schema
-		{
-        "@context": "https://schema.org",
-        "@type": "WebPage",
-        "name": "#{t('landings.landings.home.meta.title')}",
-        "url": "#{canonical_url}",
-        "description": "#{t('landings.landings.home.meta.description')}",
-        "image": "#{image_url('logo-ce.png')}",
-        "isPartOf":{
-          "@type": "WebSite",
-          "name": "Conseillers-Entreprises",
-          "url": "#{canonical_url}",
-          "image": "#{image_url('logo-ce.png')}",
-          "publisher":{
-            "@type": "GovernmentOrganization",
-            "name": "Conseillers-Entreprises",
-            "url": "#{canonical_url}",
-            "logo": "#{image_url('logo-ce.png')}",
-            "email": "#{ENV['APPLICATION_EMAIL']}"
+  def home_schema
+    {
+      '@context': "https://schema.org",
+        '@type': "WebPage",
+        name: "#{t('landings.landings.home.meta.title')}",
+        url: "#{canonical_url}",
+        description: "#{t('landings.landings.home.meta.description')}",
+        image: "#{image_url('logo-ce.png')}",
+        isPartOf: {
+          '@type': "WebSite",
+          name: "Conseillers-Entreprises",
+          url: "#{canonical_url}",
+          image: "#{image_url('logo-ce.png')}",
+          publisher: {
+            '@type': "GovernmentOrganization",
+            name: "Conseillers-Entreprises",
+            url: "#{canonical_url}",
+            logo: "#{image_url('logo-ce.png')}",
+            email: "#{ENV['APPLICATION_EMAIL']}"
           }
         }
-      }.to_json
-	end
-
+    }.to_json
+  end
 end
