18c91cd503e1c2a702847aba3e60582476a807a7
Claire Zuliani <clairezuliani@gmail.com>
2024-05-21 16:05:23 +0200

    Fix Email sending with

--- app/jobs/quarterly_reports/notify_managers_job.rb
+++ app/jobs/quarterly_reports/notify_managers_job.rb
@@ -4,7 +4,7 @@ class QuarterlyReports::NotifyManagersJob < ApplicationJob
   def perform
     User.managers.find_each do |user|
       next unless user.managed_antennes.map { |antenne| antenne.quarterly_reports.find_by(start_date: 3.months.ago.beginning_of_month) }.any?
-      UserMailer.quarterly_report(user).deliver_later(queue: 'low_priority')
+      UserMailer.with(user: user).quarterly_report.deliver_later
     end
   end
 end
