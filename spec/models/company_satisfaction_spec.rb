87db114fb0bf0020d7168915bb0d847f322a54a6
Claire Zuliani <clairezuliani@gmail.com>
2024-05-30 09:39:50 +0200

    Improve company_satisfactions tests

--- spec/models/company_satisfaction_spec.rb
+++ spec/models/company_satisfaction_spec.rb
@@ -33,16 +33,14 @@ RSpec.describe CompanySatisfaction do
     end
 
     context 'satisfaction ok' do
-      let(:user1) { create :user }
-      let(:expert1) { create :expert, users: [user1] }
-      let(:user2) { create :user }
-      let(:expert2) { create :expert, users: [user2] }
+      let(:expert1) { create :expert_with_users }
+      let(:expert2) { create :expert_with_users }
       let(:need) do
-  create :need, matches: [
-    create(:match, status: :done, expert: expert1),
-    create(:match, status: :not_for_me, expert: expert2)
-  ]
-end
+        create :need, matches: [
+          create(:match, status: :done, expert: expert1),
+          create(:match, status: :not_for_me, expert: expert2)
+        ]
+      end
       let(:company_satisfaction) { create :company_satisfaction, need: need }
 
       it 'shares to done_users' do
@@ -56,7 +54,18 @@ end
         expect { company_satisfaction.share }.to change(SharedSatisfaction.all, :count).by(1)
         expect{ company_satisfaction.share }.not_to change(SharedSatisfaction.all, :count)
       end
-    end
 
+      context 'multiple satisfction for an expert' do
+        let(:company_satisfaction2) { create :company_satisfaction, need: create(:need, matches: [ create(:match, status: :done, expert: expert1)]) }
+
+        before { company_satisfaction.share }
+
+        it 'can share multiple satisfactions for the same expert' do
+          expect(expert1.shared_satisfactions.count).to eq(1)
+          expect { company_satisfaction2.share }.to change(expert1.shared_satisfactions, :count).by(1)
+
+        end
+      end
+    end
   end
 end
