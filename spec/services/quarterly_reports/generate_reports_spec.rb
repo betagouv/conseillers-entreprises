9069c7ef03af61a4c7d81e4e9146aef55cc6f0cb
Claire Zuliani <clairezuliani@gmail.com>
2024-06-17 17:25:50 +0200

    Add test for quarters

--- spec/services/quarterly_reports/generate_reports_spec.rb
+++ spec/services/quarterly_reports/generate_reports_spec.rb
@@ -38,6 +38,7 @@ describe QuarterlyReports::GenerateReports do
 
         it('return one past quarters') { expect(quarters.length).to eq 1 }
       end
+      
 
       context 'with first national matches a year ago' do
         let!(:expert) { create :expert_with_users, antenne: antenne }
@@ -47,18 +48,51 @@ describe QuarterlyReports::GenerateReports do
         it('return only current and last year quarters') { expect(quarters.length).to eq 4 }
       end
 
-      context 'with first national matches three years ago' do
+      context 'with old first national matches' do
         let!(:expert) { create :expert_with_users, antenne: antenne }
-        let!(:a_match) { create :match, expert: expert, need: create(:need, created_at: travel_to(Time.zone.local('2021', '06', '13'))) } # 3 years ago
-        let!(:other_match) { create :match, expert: create(:expert, antenne: local_antenne), need: create(:need, created_at: travel_to(Time.zone.local('2024', '02', '08'))) } # 5 months ago
+        let!(:a_match) { create :match, expert: expert, need: create(:need, created_at: Time.zone.local('2021', '06', '13')) }
+        let!(:other_match) { create :match, expert: create(:expert, antenne: local_antenne), need: create(:need, created_at: Time.zone.local('2024', '04', '30')) } # 5 months ago
 
-        let(:quarters) do
-          travel_to(Time.zone.local('2024', '07', '13')) do
-            described_class.new(antenne).send(:last_quarters)
+        context 'calculating in T2 N-1' do
+          let(:quarters) do
+            travel_to(Time.zone.local('2023', '04', '20')) do
+              described_class.new(antenne).send(:last_quarters)
+            end
           end
+
+          it('returns 5 quarters') { expect(quarters.length).to eq 5 }
         end
 
-        it('return only current and last year quarters') { expect(quarters.length).to eq 6 }
+        context 'calculating in T3 N-1' do
+          let(:quarters) do
+            travel_to(Time.zone.local('2023', '07', '20')) do
+              described_class.new(antenne).send(:last_quarters)
+            end
+          end
+
+          it('returns 6 quarters') { expect(quarters.length).to eq 6 }
+        end
+
+        context 'calculating in T1 N' do
+          let(:quarters) do
+            travel_to(Time.zone.local('2024', '01', '20')) do
+              described_class.new(antenne).send(:last_quarters)
+            end
+          end
+
+          it('returns 8 quarters') { expect(quarters.length).to eq 8 }
+        end
+
+        context 'calculating in T2 N' do
+          let(:quarters) do
+            travel_to(Time.zone.local('2024', '04', '20')) do
+              described_class.new(antenne).send(:last_quarters)
+            end
+          end
+
+          it('returns 5 quarters') { expect(quarters.length).to eq 5 }
+        end
+
       end
     end
   end
