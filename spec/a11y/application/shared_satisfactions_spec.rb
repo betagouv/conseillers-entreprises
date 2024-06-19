b5249875e679f8503c35f45275d041c5a3fa312c
Claire Zuliani <clairezuliani@gmail.com>
2024-06-19 16:06:39 +0200

    lint

--- spec/a11y/application/shared_satisfactions_spec.rb
+++ spec/a11y/application/shared_satisfactions_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 
 describe 'shared_satisfactions', :js, type: :feature do
   before { create_home_landing }
+
   login_user
 
   subject { page }
@@ -11,17 +12,17 @@ describe 'shared_satisfactions', :js, type: :feature do
   let(:expert) { create :expert, users: [current_user] }
   let(:need) { create :need, matches: [ create(:match, status: :done, expert: expert) ] }
   let(:company_satisfaction) { create :company_satisfaction, need: need }
-  
+
   describe '/conseiller/retours/nouveaux' do
     let!(:unseen_satisfaction) { create :shared_satisfaction, company_satisfaction: company_satisfaction, user: current_user, seen_at: nil }
-    
+
     before { visit '/conseiller/retours/nouveaux' }
 
     it { is_expected.to be_accessible }
   end
 
   describe '/conseiller/retours/vus' do
-    let!(:seen_satisfaction) { create :shared_satisfaction, company_satisfaction: company_satisfaction,  user: current_user, seen_at: Time.zone.now }
+    let!(:seen_satisfaction) { create :shared_satisfaction, company_satisfaction: company_satisfaction, user: current_user, seen_at: Time.zone.now }
 
     before { visit '/conseiller/retours/vus' }
 
