6569be55a1fff50723c26ff55c01941cf55d90a0
Lucien <mollard.lucien@gmail.com>
2024-06-25 11:22:22 +0200

    add occasional subjects to subjects tab

--- app/services/xlsx_export/antenne_stats_worksheet_generator/by_subject.rb
+++ app/services/xlsx_export/antenne_stats_worksheet_generator/by_subject.rb
@@ -4,20 +4,39 @@ module XlsxExport
       def generate
         sheet.add_row
 
-        add_agglomerate_headers(:subject)
+        add_agglomerate_headers(:antenne_subjects)
 
         needs_by_subjects = {}
+        antenne_subjects_labels = @antenne.institution.subjects.map(&:label)
 
         @needs.map(&:subject).each do |subject|
           needs_by_subjects[subject.label] = @needs.where(subject: subject)
         end
 
+        needs_by_antenne_subjects = needs_by_subjects.select { |subject_label, _| antenne_subjects_labels.include?(subject_label) }
+        needs_by_occasional_subjects = needs_by_subjects.except(*antenne_subjects_labels)
+
+        generate_subjects_row(needs_by_antenne_subjects)
+
+        if needs_by_occasional_subjects.any?
+          sheet.add_row []
+          sheet.add_row [
+            I18n.t('antenne_stats_exporter.occasional_subjects'), '', '', '', '', ''
+          ], style: [@left_header, @right_header, @right_header, @right_header, @right_header, @right_header]
+
+          generate_subjects_row(needs_by_occasional_subjects)
+        end
+
+        finalise_agglomerate_style
+      end
+
+      private
+
+      def generate_subjects_row(needs_by_subjects)
         needs_by_subjects.sort_by { |_, needs| -needs.count }.each do |subject_label, needs|
           ratio = calculate_rate(needs.count, @needs)
           add_agglomerate_rows(needs, subject_label, @antenne, ratio)
         end
-
-        finalise_agglomerate_style
       end
     end
   end
