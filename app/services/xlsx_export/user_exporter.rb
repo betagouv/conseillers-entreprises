40ae3f9ea65909ca177b8c246b5676ec54faaf91
Lucien <mollard.lucien@gmail.com>
2024-08-14 15:57:40 +0200

    linter

--- app/services/xlsx_export/user_exporter.rb
+++ app/services/xlsx_export/user_exporter.rb
@@ -49,7 +49,7 @@ module XlsxExport
         lambda = -> {
           # This block is executed in the context of a User
           # (`self` is a User; See `object.instance_exec(&lambda)` in CsvExport::Base.)
-          return unless experts.first_expert_with_subject.present?
+          return if experts.first_expert_with_subject.blank?
 
           # We’re using `&` instead of .merge to use the preloaded relations instead of doing a new DB query.
           experts_subjects = first_expert_with_subject&.experts_subjects & institution_subject.experts_subjects
