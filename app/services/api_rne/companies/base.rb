2633d60e74ef5b492c50dc9c503edafb8a6bda46
Claire Zuliani <clairezuliani@gmail.com>
2024-08-19 12:19:19 +0200

    Données activités secondaires RNE

--- app/services/api_rne/companies/base.rb
+++ app/services/api_rne/companies/base.rb
@@ -28,9 +28,40 @@ module ApiRne::Companies
       registres = @http_request.data.dig('formality','content','registreAnterieur')
       {
         "forme_exercice" => @http_request.data.dig('formality', 'content', 'formeExerciceActivitePrincipale'),
+        "activites_secondaires" => grab_activites_secondaires(@http_request.data),
         "rne_rcs" => registres.present? ? registres['rncs'] : nil,
         "rne_rnm" => registres.present? ? registres['rnm'] : nil,
       }
+    end
+
+    private
+
+    def grab_activites_secondaires(data)
+      activites_secondaires = []
+      personne = data.dig('formality', 'content', 'personneMorale') || data.dig('formality', 'content', 'personnePhysique') || data.dig('formality', 'content', 'exploitation')
+
+      activites_etablissement_principal = personne.dig('etablissementPrincipal', 'activites')
+      activites_etablissement_principal&.each do |activite|
+        activites_secondaires << {
+          'formeExercice' => activite['formeExercice'],
+          "descriptionDetaillee" => activite['descriptionDetaillee'],
+          'codeApe' => activite['codeApe'],
+          'codeAprm' => activite['codeAprm']
+        }
+      end
+
+      personne['autresEtablissements']&.each do |etablissement|
+        activites_autres_etablissements = etablissement['activites']
+        activites_autres_etablissements&.each do |activite|
+          activites_secondaires << {
+            'formeExercice' => activite['formeExercice'],
+            "descriptionDetaillee" => activite['descriptionDetaillee'],
+            'codeApe' => activite['codeApe'],
+            'codeAprm' => activite['codeAprm']
+          }
+        end
+      end
+      activites_secondaires
     end
   end
 end
