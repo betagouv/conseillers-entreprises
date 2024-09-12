6d0f0b2edcb46885c1bddd680766668feef4a67c
Claire Zuliani <clairezuliani@gmail.com>
2024-09-12 14:59:22 +0200

    Script MaJ

--- db/migrate/20240912095206_display_partner_url_on_landings.rb
+++ db/migrate/20240912095206_display_partner_url_on_landings.rb
@@ -3,7 +3,7 @@ class DisplayPartnerUrlOnLandings < ActiveRecord::Migration[7.0]
     add_column :landings, :display_partner_url, :boolean, default: false
 
     up_only do
-      Landing.where(slug: ['team-rh-occitanie', 'cci-les-aides-fr', 'transition-ecologique-entreprises-api']).find_each do |landing|
+      Landing.where(slug: ['entreprendre-service-public-fr', 'team-rh-occitanie', 'cci-les-aides-fr', 'transition-ecologique-entreprises-api', 'zetwal', 'espace-fournisseurs']).find_each do |landing|
         landing.update_attribute(:display_partner_url, true)
       end
     end
