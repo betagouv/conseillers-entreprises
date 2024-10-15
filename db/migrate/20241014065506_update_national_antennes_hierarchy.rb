245975b25de249602bcfdc798ec08ccfd28b2d38
Lucien <mollard.lucien@gmail.com>
2024-10-15 09:27:02 +0200

    desn't run too much jobs in migration

--- db/migrate/20241014065506_update_national_antennes_hierarchy.rb
+++ db/migrate/20241014065506_update_national_antennes_hierarchy.rb
@@ -3,9 +3,5 @@ class UpdateNationalAntennesHierarchy < ActiveRecord::Migration[7.0]
     Antenne.territorial_level_national.find_each do |antenne|
       UpdateAntenneHierarchyJob.perform_async(antenne.id)
     end
-
-    CompanySatisfaction.shared.find_each do |company_satisfaction|
-      CreateSharedSatisfactionJob.perform_later(company_satisfaction.id)
-    end
   end
 end
