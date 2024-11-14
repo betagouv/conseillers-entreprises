c37601ef1af47fc584d78c7f237278bb171d23d4
Lucien <mollard.lucien@gmail.com>
2024-11-14 08:26:41 +0100

    add not_sas? alerte and update to dsfr badges

--- app/models/solicitation.rb
+++ app/models/solicitation.rb
@@ -483,6 +483,12 @@ class Solicitation < ApplicationRecord
       .uniq
   end
 
+  def not_sas?
+    company.present? &&
+      landing_subject.subject.id == 261 &&
+      company.legal_form_code[0...2] != "57"
+  end
+
   def has_similar_abandonned_solicitations?
     similar_abandonned_solicitations.size >= 4
   end
