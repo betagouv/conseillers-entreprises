3b76044ab232d240fa008a8fde62bce185d48e8d
Claire Zuliani <clairezuliani@gmail.com>
2024-11-28 17:10:58 +0100

    Simplification des stats simples

--- app/services/stats/solicitations/completed.rb
+++ app/services/stats/solicitations/completed.rb
@@ -6,14 +6,17 @@ module Stats::Solicitations
       Solicitation.step_complete.where(completed_at: @start_date..@end_date)
     end
 
+    def filtered(query)
+      Stats::Filters::Solicitations.new(query, self).call
+    end
+
     def build_series
-      query = main_query
-      query = Stats::Filters::Solicitations.new(query, self).call
+      query = filtered(main_query)
 
       @solicitations = []
 
       search_range_by_month.each do |range|
-        month_query = query.where(completed_at: range.first..range.last)
+        month_query = query.where(completed_at: range.first.beginning_of_day..range.last.end_of_day)
         @solicitations.push(month_query.count)
       end
 
@@ -26,10 +29,6 @@ module Stats::Solicitations
 
     def chart
       'column-chart'
-    end
-
-    def count
-      Stats::Filters::Solicitations.new(main_query, self).call.size
     end
 
     def subtitle
