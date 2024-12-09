5424b110a7bf4e37e81b7fdcc64ed03693cb0961
Lucien <mollard.lucien@gmail.com>
2024-12-09 10:14:14 +0100

    fix tests

--- spec/views/mailers/expert_mailer/notify_company_needs.html.haml_spec.rb
+++ spec/views/mailers/expert_mailer/notify_company_needs.html.haml_spec.rb
@@ -4,18 +4,19 @@ require 'rails_helper'
 require 'api_helper'
 
 RSpec.describe 'mailers/expert_mailer/notify_company_needs' do
-  let!(:national_referent) { create :user, :national_referent }
 
   context 'hash with several information' do
     let(:contact) { create :contact }
     let(:user) { create :user }
     let(:expert) { create :expert }
     let(:need) { create(:need, matches: [create(:match, expert: expert)]) }
+    let(:support_user) { create :user, :national_referent }
 
     before do
       assign(:expert, expert)
       assign(:diagnosis, need.diagnosis)
       assign(:need, need)
+      assign(:support_user, support_user)
 
       render
     end
