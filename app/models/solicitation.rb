f2eb615bb3d6b08dea009194fd403848efc7bc4d
Lucien <mollard.lucien@gmail.com>
2024-12-16 07:57:02 +0100

    Style/SafeNavigationChainLength && Lint/RedundantSafeNavigation: Redundant safe navigation detected, use . instead

--- app/models/solicitation.rb
+++ app/models/solicitation.rb
@@ -471,7 +471,7 @@ class Solicitation < ApplicationRecord
   end
 
   def from_intermediary?
-    facility = self&.facility
+    facility = self.facility
     return false if facility.nil?
     intermediary_naf_codes = %w[7022Z 6920Z 9411Z 8299Z 7021Z 9499Z 8413Z]
     intermediary_naf_codes.include?(facility.naf_code&.delete('.'))
