0c3f160f7ee9b1f0b1cffaea3d63a4e96b0c8f8a
Claire Zuliani <clairezuliani@gmail.com>
2025-04-01 17:44:38 +0200

    Refacto

--- app/services/activity_reports/generate/antenne_stats.rb
+++ app/services/activity_reports/generate/antenne_stats.rb
@@ -2,24 +2,13 @@ module ActivityReports
   class Generate::AntenneStats < Generate::Base
     private
 
-    def generate_files(quarter)
-      return if reports.find_by(start_date: quarter.first).present?
-
-      ActiveRecord::Base.transaction do
-        exporter = XlsxExport::AntenneStatsExporter.new({
-          start_date: quarter.first,
+    def export_xls(quarter)
+      exporter = XlsxExport::AntenneStatsExporter.new({
+        start_date: quarter.first,
           end_date: quarter.last,
           antenne: antenne
-        })
-        result = exporter.export
-
-        filename = I18n.t('activity_report_service.stats_file_name', number: TimeDurationService.find_quarter_for_month(quarter.first.month), year: quarter.first.year, antenne: antenne.name.parameterize)
-        report = reports.create!(start_date: quarter.first, end_date: quarter.last)
-        report.file.attach(io: result.xlsx.to_stream(true),
-                           key: "activity_report_stats/#{antenne.name.parameterize}/#{filename}",
-                           filename: filename,
-                           content_type: 'application/xlsx')
-      end
+      })
+      exporter.export
     end
 
     def antenne
@@ -28,6 +17,10 @@ module ActivityReports
 
     def last_periods
       last_quarters
+    end
+
+    def report_type
+      :stats
     end
 
     def reports
--- app/services/activity_reports/generate/cooperation.rb
+++ app/services/activity_reports/generate/cooperation.rb
@@ -2,24 +2,13 @@ module ActivityReports
   class Generate::Cooperation < Generate::Base
     private
 
-    def generate_files(quarter)
-      return if reports.find_by(start_date: quarter.first).present?
-
-      ActiveRecord::Base.transaction do
-        exporter = XlsxExport::CooperationExporter.new({
-          start_date: quarter.first,
+    def export_xls(quarter)
+      exporter = XlsxExport::CooperationExporter.new({
+        start_date: quarter.first,
           end_date: quarter.last,
           cooperation: cooperation
-        })
-        result = exporter.export
-
-        filename = I18n.t('activity_report_service.cooperation_file_name', number: TimeDurationService.find_quarter_for_month(quarter.first.month), year: quarter.first.year, cooperation: cooperation.name.parameterize)
-        report = reports.create!(start_date: quarter.first, end_date: quarter.last)
-        report.file.attach(io: result.xlsx.to_stream(true),
-                           key: "activity_report_cooperation/#{cooperation.name.parameterize}/#{filename}",
-                           filename: filename,
-                           content_type: 'application/xlsx')
-      end
+      })
+      exporter.export
     end
 
     def cooperation
@@ -32,6 +21,10 @@ module ActivityReports
 
     def reports
       cooperation.activity_reports
+    end
+
+    def report_type
+      :cooperation
     end
   end
 end
