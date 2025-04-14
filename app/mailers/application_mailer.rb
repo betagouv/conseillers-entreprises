3a942d3ab959ed8fa3a2b1c13018d802972694fb
Lucien <mollard.lucien@gmail.com>
2025-04-14 14:15:34 +0100

    adds encryption header for emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -5,7 +5,8 @@ class ApplicationMailer < ActionMailer::Base
   REPLY_TO_EMAIL = ENV['APPLICATION_EMAIL']
   SENDER = "#{I18n.t('app_name')} <#{SENDER_EMAIL}>"
   REPLY_TO = "#{I18n.t('app_name')} <#{REPLY_TO_EMAIL}>"
-  default from: SENDER, reply_to: REPLY_TO
+  default from: SENDER, reply_to: REPLY_TO, 'X-Encryption': 'TLS'
+
   helper :mailto
 
   layout 'mailers'
