4ee0032456aa8465514da95fe2cd2737c8eb4b82
Lucien <mollard.lucien@gmail.com>
2025-04-15 08:49:56 +0100

    moves migrations for codes regions

--- db/migrate/20250402091841_add_regions_codes_to_territorial_zones.rb
+++
@@ -1,5 +0,0 @@
-class AddRegionsCodesToTerritorialZones < ActiveRecord::Migration[7.2]
-  def change
-    add_column :territorial_zones, :regions_codes, :string, array: true, default: []
-  end
-end
