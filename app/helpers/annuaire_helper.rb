9058b169165078773cd51d3401273b30cf64c537
Lucien <mollard.lucien@gmail.com>
2025-05-07 15:24:49 +0200

    move icon to expert

--- app/helpers/annuaire_helper.rb
+++ app/helpers/annuaire_helper.rb
@@ -15,7 +15,6 @@ module AnnuaireHelper
     html = link_to(user.full_name, admin_user_path(user), title: t('annuaire_helper.build_user_name_cell.user', user_name: user.full_name, antenne: antenne))
     html << tag.span(class: 'ri-mail-add-fill blue fr-ml-1v', aria: { hidden: true }, title: t('annuaire_helper.build_user_name_cell.not_invited')) if user.invitation_sent_at.nil?
     html << tag.span(class: 'ri-nurse-fill blue fr-ml-1v', aria: { hidden: true }, title: t('annuaire_helper.build_user_name_cell.antenne_manager')) if user.is_manager?
-    html << tag.span(class: 'ri-map-2-line blue fr-ml-1v', aria: { hidden: true }, title: t('annuaire_helper.build_user_name_cell.specific_territories')) if user.experts.any? && user.experts.first.communes.any?
     html
   end
 
