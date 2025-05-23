7f7bdbf4d4ae445f63fb07b90b252308ba63728d
Claire Zuliani <clairezuliani@gmail.com>
2025-05-23 16:28:48 +0200

    Change taking_care_duration stats

---
+++ app/services/stats/needs/taken_care_in_three_days.rb
@@ -0,0 +1,17 @@
+module Stats::Needs
+  class TakenCareInThreeDays
+    include ::Stats::Needs::TakingCareTime
+
+    def number_of_days
+      @number_of_days ||= 3
+    end
+
+    def taken_care_after_label
+      I18n.t('stats.taken_care_in_three_days.taken_care_after')
+    end
+
+    def taken_care_before_label
+      I18n.t('stats.taken_care_in_three_days.taken_care_before')
+    end
+  end
+end
