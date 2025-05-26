77089a6f5dda3ad6d71d5455f419fa82cd1c7343
Claire Zuliani <clairezed@users.noreply.github.com>
2025-05-26 09:39:58 +0200

    Revert "Stats temps de prise en charge pour partenaires et coopérations"

--- app/services/stats/solicitations/taking_care_time.rb
+++ app/services/stats/solicitations/taking_care_time.rb
@@ -1,73 +1,69 @@
-module Stats::Solicitations::TakingCareTime
-  include ::Stats::BaseStats
+module Stats::Solicitations
+  class TakingCareTime
+    include ::Stats::BaseStats
 
-  def main_query
-    Solicitation
-      .step_complete
-      .joins(diagnosis: [needs: :matches])
-      .where(completed_at: @start_date..@end_date)
-      .merge(Need.with_exchange)
-      .distinct
-  end
-
-  def delay
-    @delay ||= 5.days
-  end
-
-  def group_by_date(query)
-    pp "delay"
-    pp delay
-    query.includes(:matches).group_by do |solicitation|
-      solicitation.matches.pluck(:taken_care_of_at).compact.min&.between?(solicitation.completed_at, solicitation.completed_at + delay)
-    end
-  end
-
-  def group_by_date_in_range(query, range)
-    query_range = query.created_between(range.first, range.last)
-    group_by_date(query_range)
-  end
-
-  def build_series
-    query = main_query
-    query = Stats::Filters::Needs.new(query, self).call
-
-    @taken_care_before = []
-    @taken_care_after = []
-
-    search_range_by_month.each do |range|
-      grouped_result = group_by_date_in_range(query, range)
-      @taken_care_before.push(grouped_result[true]&.size || 0)
-      @taken_care_after.push(grouped_result[false]&.size || 0)
+    def main_query
+      Solicitation
+        .step_complete
+        .joins(diagnosis: [needs: :matches])
+        .where(completed_at: @start_date..@end_date)
+        .merge(Need.with_exchange)
+        .distinct
     end
 
-    as_series(@taken_care_before, @taken_care_after)
-  end
+    def group_by_date(query)
+      query.includes(:matches).group_by do |solicitation|
+        solicitation.matches.pluck(:taken_care_of_at).compact.min&.between?(solicitation.completed_at, solicitation.completed_at + 5.days)
+      end
+    end
 
-  def max_value
-    100
-  end
+    def group_by_date_in_range(query, range)
+      query_range = query.created_between(range.first, range.last)
+      group_by_date(query_range)
+    end
 
-  def category_order_attribute
-    Arel.sql('true')
-  end
+    def build_series
+      query = main_query
+      query = Stats::Filters::Needs.new(query, self).call
 
-  def count
-    series
-    @count ||= percentage_two_numbers(@taken_care_before, @taken_care_after)
-  end
+      @taken_care_before = []
+      @taken_care_after = []
 
-  private
+      search_range_by_month.each do |range|
+        grouped_result = group_by_date_in_range(query, range)
+        @taken_care_before.push(grouped_result[true]&.size || 0)
+        @taken_care_after.push(grouped_result[false]&.size || 0)
+      end
 
-  def as_series(taken_care_before, taken_care_after)
-    [
-      {
-        name: taken_care_after_label,
-          data: taken_care_after
-      },
-      {
-        name: taken_care_before_label,
-          data: taken_care_before
-      }
-    ]
+      as_series(@taken_care_before, @taken_care_after)
+    end
+
+    def max_value
+      100
+    end
+
+    def category_order_attribute
+      Arel.sql('true')
+    end
+
+    def count
+      series
+      @count ||= percentage_two_numbers(@taken_care_before, @taken_care_after)
+    end
+
+    private
+
+    def as_series(taken_care_before, taken_care_after)
+      [
+        {
+          name: I18n.t('stats.taken_care_after'),
+            data: taken_care_after
+        },
+        {
+          name: I18n.t('stats.taken_care_before'),
+            data: taken_care_before
+        }
+      ]
+    end
   end
 end
