814093fa6f2d60aca0a1bfe897d09056bfe4d5a8
Claire Zuliani <clairezuliani@gmail.com>
2025-06-05 14:46:57 +0200

    Fignolage

--- app/services/xlsx_export/antenne_stats_worksheet_generator/by_subject.rb
+++ app/services/xlsx_export/antenne_stats_worksheet_generator/by_subject.rb
@@ -1,12 +1,14 @@
 module XlsxExport
   module AntenneStatsWorksheetGenerator
     class BySubject < Base
+      include XlsxExport::AntenneStatsWorksheetGenerator::BySubjectMethods
+
       def generate
         sheet.add_row
 
-        add_agglomerate_headers(:antenne_subjects)
+        add_subject_table_header(:antenne_subjects)
 
-        generate_by_subjects_stats(@needs)
+        generate_by_subject_table(@needs)
       end
     end
   end
