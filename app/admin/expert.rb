ed2f2b2cda17804ce411c7e6743187d1f00a26cc
Lucien <mollard.lucien@gmail.com>
2025-06-23 16:17:35 +0200

    changes for custom_territories and fix typo

--- app/admin/expert.rb
+++ app/admin/expert.rb
@@ -153,7 +153,7 @@ ActiveAdmin.register Expert do
     column_count :antenne_territories
     column_count :antenne_communes
     column :is_global_zone
-    column :custome_territories?
+    column :custom_territories?
     column_count :territories
     column_count :communes
     column_count :users
@@ -176,7 +176,7 @@ ActiveAdmin.register Expert do
         if e.is_global_zone
           status_tag t('activerecord.attributes.expert.is_global_zone'), class: 'yes'
         else
-          # if e.custome_territories?
+          # if e.custom_territories?
           #   status_tag t('attributes.custom_communes'), class: 'yes'
           # end
           # div intervention_zone_description(e)
