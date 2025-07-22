9563ca7275d9782d8856350ed28e95dc536e116d
Lucien <mollard.lucien@gmail.com>
2025-07-22 16:17:13 +0200

    update shema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_04_084013) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_08_080921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
