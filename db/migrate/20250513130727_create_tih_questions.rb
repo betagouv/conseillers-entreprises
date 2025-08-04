96b923ac29bb1a53aa2cb5345dc04b0e3f80f977
LucienMLD <lucien.mollard@beta.gouv.fr>
2025-08-04 10:52:39 +0200

    Add safe retrieval for institutions and themes in cooperation creation methods

--- db/migrate/20250513130727_create_tih_questions.rb
+++ db/migrate/20250513130727_create_tih_questions.rb
@@ -2,7 +2,7 @@ class CreateTihQuestions < ActiveRecord::Migration[7.2]
   def up
     change_column :subject_answers, :filter_value, :string
 
-    tih_theme = Theme.find(54)
+    tih_theme = Theme.find_by(id: 54)
     if tih_theme.present?
       tih_theme.subjects.each do |subject|
         tih_question = subject.subject_questions.create(key: 'mode_contact_privilegie')
@@ -13,7 +13,7 @@ class CreateTihQuestions < ActiveRecord::Migration[7.2]
   def down
     change_column :subject_answers, :filter_value, :boolean
 
-    tih_theme = Theme.find(54)
+    tih_theme = Theme.find_by(id: 54)
     if tih_theme.present?
       SubjectQuestion.where(subject_id: tih_theme.subject_ids).destroy_all
     end
