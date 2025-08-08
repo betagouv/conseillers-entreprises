13f17c837a4e696df9982f209eed327c014e3c28
LucienMLD <lucien.mollard@beta.gouv.fr>
2025-08-08 14:55:54 +0200

    Remove unused includes from expert model

--- app/admin/expert.rb
+++ app/admin/expert.rb
@@ -61,7 +61,6 @@ ActiveAdmin.register Expert do
     init_subjects_filter
   end
 
-  includes :institution, :antenne, :users, :subjects, :received_matches
   config.sort_order = 'full_name_asc'
 
   scope :active, default: true
