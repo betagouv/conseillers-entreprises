6da2fd39ca4bbe111a699ede77125accf479fa99
Nicolas Bouilleaud <nico@bou.io>
2025-08-11 09:51:47 +0200

    Remove frozen_string_literal magic comments

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 module Users
   # Custom RegistrationsController
   ## This overrides some Devise features and adds new actions.
--- spec/services/activity_reports/generate/antenne_stats_spec.rb
+++ spec/services/activity_reports/generate/antenne_stats_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 describe ActivityReports::Generate::AntenneStats do
   describe 'generate_files' do
