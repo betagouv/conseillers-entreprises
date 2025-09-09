7b7f997622f4d3c4795a6ea29b7221364a1dac49
LucienMLD <lucien.mollard@beta.gouv.fr>
2025-09-09 16:11:54 +0200

    Replace `facility_region` in filters and models, implement `facility_region_eq` scope, and clean up unused associations.

--- app/admin/need.rb
+++ app/admin/need.rb
@@ -48,7 +48,7 @@ ActiveAdmin.register Need do
   filter :experts, as: :ajax_select, data: { url: :admin_experts_path, search_fields: [:full_name] }
   filter :expert_antennes, as: :ajax_select, data: { url: :admin_antennes_path, search_fields: [:name] }
   filter :expert_institutions, as: :ajax_select, data: { url: :admin_institutions_path, search_fields: [:name] }
-  filter :facility_regions, as: :select, collection: -> { RegionOrderingService.call.map { |r| [r.nom, r.code] }.sort_by(&:first) }
+  filter :facility_regions, as: :select, collection: -> { RegionOrderingService.call.map { |r| [r.nom, r.code] } }
 
   ## CSV
   #
