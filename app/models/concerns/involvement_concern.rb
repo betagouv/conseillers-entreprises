12a55e398bfdf4feb7b90b3c2597bab6a366a0db
Nicolas Bouilleaud <nico@bou.io>
2025-10-08 16:08:39 +0200

    DRY InvolvementConcern
    - We’re already going through received_needs, which is based on received_matches; there is no need to use it again in the where.

--- app/models/concerns/involvement_concern.rb
+++ app/models/concerns/involvement_concern.rb
@@ -5,51 +5,43 @@ module InvolvementConcern
   # i.e. User, Experts, Antennes and Institutions.
 
   def needs_quo
-    received_needs
-      .where(matches: received_matches.status_quo)
-      .where(matches: { archived_at: nil })
-      .distinct
+    received_needs.distinct
+      .merge(Match.not_archived)
+      .merge(Match.status_quo)
   end
 
   def needs_quo_active
-    received_needs
-      .where(matches: received_matches.with_status_quo_active)
-      .where(matches: { archived_at: nil })
-      .distinct
+    received_needs.distinct
+      .merge(Match.not_archived)
+      .merge(Match.with_status_quo_active)
   end
 
   def needs_taking_care
-    received_needs
-      .where(matches: received_matches.status_taking_care)
-      .where(matches: { archived_at: nil })
-      .distinct
+    received_needs.distinct
+      .merge(Match.not_archived)
+      .merge(Match.status_taking_care)
   end
 
   def needs_done
-    received_needs
-      .where(matches: received_matches.with_status_done)
-      .where(matches: { archived_at: nil })
-      .distinct
+    received_needs.distinct
+      .merge(Match.not_archived)
+      .merge(Match.with_status_done)
   end
 
   def needs_not_for_me
-    received_needs
-      .where(matches: received_matches.status_not_for_me)
-      .where(matches: { archived_at: nil })
-      .distinct
+    received_needs.distinct
+      .merge(Match.not_archived)
+      .merge(Match.status_not_for_me)
   end
 
   def needs_expired
-    received_needs
-      .where(matches: received_matches.with_status_expired.not_archived)
-      .distinct
+    received_needs.distinct
+      .merge(Match.not_archived)
+      .merge(Match.with_status_expired)
   end
 
   def needs_others_taking_care
-    received_needs
+    needs_quo
       .status_taking_care
-      .where(matches: received_matches.status_quo)
-      .where(matches: { archived_at: nil })
-      .distinct
   end
 end
