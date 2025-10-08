86c51a7fd93a2838581d65dcba150943858d4c76
Nicolas Bouilleaud <nico@bou.io>
2025-10-08 16:31:07 +0200

    DRY more, declare the match_sent_at attribute in a module

--- app/models/concerns/involvement_concern.rb
+++ app/models/concerns/involvement_concern.rb
@@ -5,38 +5,32 @@ module InvolvementConcern
   # i.e. User, Experts, Antennes and Institutions.
 
   def needs_quo
-    received_needs.distinct
-      .merge(Match.not_archived)
+    received_need_with_match
       .merge(Match.status_quo)
   end
 
   def needs_quo_active
-    received_needs.distinct
-      .merge(Match.not_archived)
+    received_need_with_match
       .merge(Match.with_status_quo_active)
   end
 
   def needs_taking_care
-    received_needs.distinct
-      .merge(Match.not_archived)
+    received_need_with_match
       .merge(Match.status_taking_care)
   end
 
   def needs_done
-    received_needs.distinct
-      .merge(Match.not_archived)
+    received_need_with_match
       .merge(Match.with_status_done)
   end
 
   def needs_not_for_me
-    received_needs.distinct
-      .merge(Match.not_archived)
+    received_need_with_match
       .merge(Match.status_not_for_me)
   end
 
   def needs_expired
-    received_needs.distinct
-      .merge(Match.not_archived)
+    received_need_with_match
       .merge(Match.with_status_expired)
   end
 
@@ -44,4 +38,22 @@ module InvolvementConcern
     needs_quo
       .status_taking_care
   end
+
+  ## Helpers
+  #
+  def received_need_with_match
+    received_needs.distinct
+      .merge(Match.not_archived)
+      .select("needs.*, matches.sent_at as match_sent_at")
+  end
+
+  module MatchSentAtAttribute
+    extend ActiveSupport::Concern
+
+    included do
+      # Let Rails know about match_sent_at, so that it’s converted to an ActiveSupport::TimeWithZone object.
+      attribute :match_sent_at, :datetime
+    end
+  end
+  ::Need.include MatchSentAtAttribute
 end
