d628e4e949fe9dafc446465955752c7cb89f4f95
Nicolas Bouilleaud <nico@bou.io>
2025-10-14 18:24:16 +0200

    Add ActiveRecord#find_random

--- spec/mailers/previews/company_mailer_preview.rb
+++ spec/mailers/previews/company_mailer_preview.rb
@@ -1,45 +1,45 @@
 class CompanyMailerPreview < ActionMailer::Preview
   def confirmation_solicitation_from_pde
-    CompanyMailer.confirmation_solicitation(Solicitation.where(cooperation: nil).where.associated(:landing_subject).sample)
+    CompanyMailer.confirmation_solicitation(Solicitation.where(cooperation: nil).where.associated(:landing_subject).find_random)
   end
 
   def confirmation_solicitation_from_iframe_or_api
-    CompanyMailer.confirmation_solicitation(Solicitation.where.not(cooperation: nil).where.associated(:landing_subject).sample)
+    CompanyMailer.confirmation_solicitation(Solicitation.where.not(cooperation: nil).where.associated(:landing_subject).find_random)
   end
 
   def taking_care_solicitation
-    CompanyMailer.notify_taking_care(Diagnosis.completed.sample.matches.sample)
+    CompanyMailer.notify_taking_care(Diagnosis.completed.find_random.matches.find_random)
   end
 
   def not_reachable
-    CompanyMailer.notify_not_reachable(Diagnosis.completed.sample.matches.sample)
+    CompanyMailer.notify_not_reachable(Diagnosis.completed.find_random.matches.find_random)
   end
 
   def satisfaction
-    CompanyMailer.satisfaction(Need.where(status: :done).sample)
+    CompanyMailer.satisfaction(Need.where(status: :done).find_random)
   end
 
   def retention
-    CompanyMailer.retention(Need.where(status: :done).sample)
+    CompanyMailer.retention(Need.where(status: :done).find_random)
   end
 
   def failed_need
-    CompanyMailer.failed_need(Diagnosis.completed.from_solicitation.sample.needs.sample)
+    CompanyMailer.failed_need(Diagnosis.completed.from_solicitation.find_random.needs.find_random)
   end
 
   def solicitation_relaunch_company
-    CompanyMailer.solicitation_relaunch_company(Solicitation.status_step_company.where.not(uuid: nil).sample)
+    CompanyMailer.solicitation_relaunch_company(Solicitation.status_step_company.where.not(uuid: nil).find_random)
   end
 
   def solicitation_relaunch_description
-    CompanyMailer.solicitation_relaunch_description(Solicitation.status_step_description.where.not(uuid: nil).sample)
+    CompanyMailer.solicitation_relaunch_description(Solicitation.status_step_description.where.not(uuid: nil).find_random)
   end
 
   def intelligent_retention
-    CompanyMailer.intelligent_retention(Need.where(status: :done).where.associated(:solicitation).sample, EmailRetention.all.sample)
+    CompanyMailer.intelligent_retention(Need.where(status: :done).where.associated(:solicitation).find_random, EmailRetention.all.find_random)
   end
 
   def not_yet_taken_care
-    CompanyMailer.not_yet_taken_care(Diagnosis.completed.sample.solicitation)
+    CompanyMailer.not_yet_taken_care(Diagnosis.completed.find_random.solicitation)
   end
 end
--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,13 +1,13 @@
 class UserMailerPreview < ActionMailer::Preview
   def antenne_activity_report
-    UserMailer.with(user: User.active.managers.sample).antenne_activity_report
+    UserMailer.with(user: User.active.managers.find_random).antenne_activity_report
   end
 
   def cooperation_activity_report
-    UserMailer.with(user: User.active.cooperation_managers.sample).cooperation_activity_report
+    UserMailer.with(user: User.active.cooperation_managers.find_random).cooperation_activity_report
   end
 
   def invite_to_demo
-    UserMailer.with(user: User.active.joins(:experts).merge(Expert.not_deleted.with_subjects).sample).invite_to_demo
+    UserMailer.with(user: User.active.joins(:experts).merge(Expert.not_deleted.with_subjects).find_random).invite_to_demo
   end
 end
