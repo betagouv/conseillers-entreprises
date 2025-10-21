e3650588847dbf43ff11aba49b59f38420014c24
Nicolas Bouilleaud <nico@bou.io>
2025-10-21 15:17:57 +0200

    Simplify requests in TerritoryNeedsStatus

--- app/models/concerns/territory_needs_status.rb
+++ app/models/concerns/territory_needs_status.rb
@@ -5,35 +5,35 @@ module TerritoryNeedsStatus
 
   def territory_needs_quo
     perimeter_received_needs
-      .where(matches: perimeter_received_matches_from_needs(perimeter_received_needs).status_quo)
+      .merge(Match.status_quo)
       .where(matches: { archived_at: nil })
       .distinct
   end
 
   def territory_needs_quo_active
     perimeter_received_needs
-      .where(matches: perimeter_received_matches_from_needs(perimeter_received_needs).with_status_quo_active)
+      .merge(Match.with_status_quo_active)
       .where(matches: { archived_at: nil })
       .distinct
   end
 
   def territory_needs_taking_care
     perimeter_received_needs
-      .where(matches: perimeter_received_matches_from_needs(perimeter_received_needs).status_taking_care)
+      .merge(Match.status_taking_care)
       .where(matches: { archived_at: nil })
       .distinct
   end
 
   def territory_needs_done
     perimeter_received_needs
-      .where(matches: perimeter_received_matches_from_needs(perimeter_received_needs).with_status_done)
+      .merge(Match.with_status_done)
       .where(matches: { archived_at: nil })
       .distinct
   end
 
   def territory_needs_not_for_me
     perimeter_received_needs
-      .where(matches: perimeter_received_matches_from_needs(perimeter_received_needs).status_not_for_me)
+      .merge(Match.status_not_for_me)
       .where(matches: { archived_at: nil })
       .distinct
   end
@@ -42,7 +42,8 @@ module TerritoryNeedsStatus
   # Sinon on a des besoins avec 2 MERS (une quo_archived et une refusé par exemple) dans les expirés
   def territory_needs_expired
     perimeter_received_needs
-      .where(matches: perimeter_received_matches_from_needs(perimeter_received_needs).with_status_expired.not_archived)
+      .merge(Match.with_status_expired)
+      .merge(Match.not_archived)
       .distinct
   end
 end
