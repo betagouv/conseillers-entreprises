3e788a76787a93382359dc328b2f0e5d740b4f87
Nicolas Bouilleaud <nico@bou.io>
2025-10-21 15:17:58 +0200

    DRY TerritoryNeedsStatus

--- app/models/concerns/territory_needs_status.rb
+++ app/models/concerns/territory_needs_status.rb
@@ -4,46 +4,40 @@ module TerritoryNeedsStatus
   # These methods can be called on any object that implements #perimeter_received_needs
 
   def territory_needs_quo
-    perimeter_received_needs
+    territory_needs_match_not_archived
       .merge(Match.status_quo)
-      .where(matches: { archived_at: nil })
-      .distinct
   end
 
   def territory_needs_quo_active
-    perimeter_received_needs
+    territory_needs_match_not_archived
       .merge(Match.with_status_quo_active)
-      .where(matches: { archived_at: nil })
-      .distinct
   end
 
   def territory_needs_taking_care
-    perimeter_received_needs
+    territory_needs_match_not_archived
       .merge(Match.status_taking_care)
-      .where(matches: { archived_at: nil })
-      .distinct
   end
 
   def territory_needs_done
-    perimeter_received_needs
+    territory_needs_match_not_archived
       .merge(Match.with_status_done)
-      .where(matches: { archived_at: nil })
-      .distinct
   end
 
   def territory_needs_not_for_me
-    perimeter_received_needs
+    territory_needs_match_not_archived
       .merge(Match.status_not_for_me)
-      .where(matches: { archived_at: nil })
-      .distinct
   end
 
-  # Il faut les conditions statut + not_archived dans la même conditions
-  # Sinon on a des besoins avec 2 MERS (une quo_archived et une refusé par exemple) dans les expirés
   def territory_needs_expired
-    perimeter_received_needs
+    territory_needs_match_not_archived
       .merge(Match.with_status_expired)
-      .merge(Match.not_archived)
+  end
+
+  ## Helpers
+  #
+  def territory_needs_match_not_archived
+    perimeter_received_needs
       .distinct
+      .merge(Match.not_archived)
   end
 end
