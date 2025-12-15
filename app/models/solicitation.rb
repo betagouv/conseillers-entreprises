90ee2df7dd8d43fc0959eaad360bfac9312b9ebe
LucienMLD <lucien.mollard@beta.gouv.fr>
2025-12-15 16:50:12 +0100

    adds no_expert_agri email to solicitations GENERIC_EMAILS_TYPES

--- app/models/solicitation.rb
+++ app/models/solicitation.rb
@@ -81,7 +81,7 @@ class Solicitation < ApplicationRecord
 
   GENERIC_EMAILS_TYPES = [
     %i[bad_quality no_expert moderation creation intermediary],
-    %i[sie_tva_and_others sie_sip_declare_and_pay formalites_asso_agri_sci tns_training],
+    %i[sie_tva_and_others sie_sip_declare_and_pay formalites_asso_agri_sci tns_training no_expert_agri],
     %i[carsat retirement_liberal_professions employee_labor_law recruitment_foreign_worker],
     %i[administrations_collectivites siret mediateurs kbis_extract],
   ]
