fc86748c6170018e6487ba9184d020699beb8b1e
LucienMLD <lucien.mollard@beta.gouv.fr>
2025-12-15 11:25:34 +0100

    Improve `with_insee_codes` scope and fix parent-child relationships in `antenne_hierarchy`

--- app/services/antenne_hierarchy.rb
+++ app/services/antenne_hierarchy.rb
@@ -8,17 +8,27 @@ class AntenneHierarchy
       # On prend les antennes régionales
       # Et les antennes locales qui n'ont pas d'antennes régionales
       regional_antennes = Antenne.not_deleted.where(institution: @antenne.institution, territorial_level: :regional)
+      # Réinitialise d'abord toutes les relations régionales incorrectes
+      clean_parent_antennes(@antenne.id, regional_antennes, :regional)
       regional_antennes.update_all(parent_antenne_id: @antenne.id)
+
       local_antennes_with_regional_ids = regional_antennes.flat_map { |ra| get_associated_antennes(:local, ra).ids }
       local_antennes_without_regional = Antenne.where(territorial_level: :local, institution: @antenne.institution)
         .where.not(id: local_antennes_with_regional_ids.flatten)
+      # Réinitialise d'abord toutes les relations locales incorrectes
+      clean_parent_antennes(@antenne.id, local_antennes_without_regional)
       local_antennes_without_regional.update_all(parent_antenne_id: @antenne.id)
+
     elsif @antenne.regional?
       # Va chercher les antennes locales et l'antenne nationale associées
       territorial_antennes = get_associated_antennes(Antenne.territorial_levels[:local], @antenne)
+      # Réinitialise d'abord toutes les relations enfants incorrectes
+      clean_parent_antennes(@antenne.id, territorial_antennes)
+      # Puis met à jour les bonnes relations
       territorial_antennes.update_all(parent_antenne_id: @antenne.id)
       national_antenne = Antenne.not_deleted.where(institution_id: @antenne.institution_id, territorial_level: :national).first
       @antenne.update(parent_antenne: national_antenne)
+
     else # antenne locale
       # Va chercher l'antenne régionale associée
       regional_antenne = get_associated_antennes(Antenne.territorial_levels[:regional], @antenne)&.first
@@ -28,11 +38,17 @@ class AntenneHierarchy
 
   private
 
+  def clean_parent_antennes(antenne_id, antennes_to_keep, level = :local)
+    Antenne.where(parent_antenne_id: antenne_id, territorial_level: level)
+      .where.not(id: antennes_to_keep)
+      .update_all(parent_antenne_id: nil)
+  end
+
   def get_associated_antennes(targeted_territorial_level, antenne)
-    antennes_with_territories = Antenne.not_deleted.where(institution_id: antenne.institution_id, territorial_level: targeted_territorial_level)
-      .with_insee_codes(antenne.insee_codes)
-    antenne_global_zone = Antenne.not_deleted.joins(:experts).where(institution_id: antenne.institution_id, territorial_level: targeted_territorial_level)
-      .where(experts: { is_global_zone: true })
-    Antenne.where(id: antennes_with_territories.pluck(:id) + antenne_global_zone.pluck(:id))
+    base_relation = Antenne.not_deleted.where(institution_id: antenne.institution_id, territorial_level: targeted_territorial_level)
+    antennes_with_territories_ids = base_relation.with_insee_codes(antenne.insee_codes).pluck(:id)
+    global_zone_ids = base_relation.joins(:experts).where(experts: { is_global_zone: true }).pluck(:id)
+
+    Antenne.where(id: (antennes_with_territories_ids + global_zone_ids))
   end
 end
--- spec/services/antenne_hierarchy_spec.rb
+++ spec/services/antenne_hierarchy_spec.rb
@@ -37,6 +37,24 @@ describe AntenneHierarchy do
         expect(antenne.child_antennes).to contain_exactly(local_antenne_1, local_antenne_2)
         expect(antenne.parent_antenne).to eq(national_antenne)
       end
+
+      context 'when local antennes outside regional perimeter have wrong parent' do
+        # Test pour le bug où des antennes locales en dehors du périmètre
+        # de l'antenne régionale avaient quand même cette antenne comme parent
+        let!(:local_antenne_outside_perimeter) do
+          create :antenne, :local, institution: institution,
+                 territorial_zones: [create(:territorial_zone, :departement, code: "75")],
+                 parent_antenne: regional_antenne
+        end
+
+        before { described_class.new(antenne).call }
+
+        it "removes parent_id for local antennes outside perimeter" do
+          expect(local_antenne_outside_perimeter.reload.parent_antenne_id).to be_nil
+          expect(antenne.child_antennes).to contain_exactly(local_antenne_1, local_antenne_2)
+          expect(antenne.child_antennes).not_to include(local_antenne_outside_perimeter)
+        end
+      end
     end
 
     context "Local antenne" do
@@ -47,6 +65,52 @@ describe AntenneHierarchy do
 
       it "updates parent with regional antenne" do
         expect(antenne.parent_antenne).to eq(regional_antenne)
+      end
+    end
+
+    context 'with global zone experts' do
+      let!(:local_antenne_with_global_expert) do
+  create :antenne, :local, institution: institution,
+                                                 territorial_zones: [create(:territorial_zone, :departement, code: "75")],
+                                                 experts: [create(:expert, antenne: antenne, is_global_zone: true)]
+end
+      let(:antenne) { regional_antenne }
+
+      before { described_class.new(antenne).call }
+
+      it "includes antennes with global zone experts even outside perimeter" do
+        expect(local_antenne_with_global_expert.reload.parent_antenne).to eq(regional_antenne)
+        expect(antenne.child_antennes).to include(local_antenne_with_global_expert)
+      end
+    end
+
+    context 'when cleaning wrong parent relationships for national antenne' do
+      let!(:regional_from_other_institution) { create :antenne, :regional, institution: create(:institution) }
+      let!(:regional_with_wrong_parent) do
+        create :antenne, :regional, institution: institution,
+               territorial_zones: [create(:territorial_zone, :region, code: "75")],
+               parent_antenne: regional_from_other_institution
+      end
+      let(:antenne) { national_antenne }
+
+      before { described_class.new(antenne).call }
+
+      it "cleans wrong parent for regional antennes" do
+        expect(regional_with_wrong_parent.reload.parent_antenne).to eq(national_antenne)
+      end
+    end
+
+    context 'with multiple institutions' do
+      let(:other_institution) { create :institution }
+      let!(:other_national) { create :antenne, :national, institution: other_institution }
+      let!(:other_regional) { create :antenne, :regional, institution: other_institution, territorial_zones: [create(:territorial_zone, :region, code: "53")] }
+      let!(:local_from_other) { create :antenne, :local, institution: other_institution, territorial_zones: [create(:territorial_zone, :departement, code: "22")] }
+      let(:antenne) { regional_antenne }
+
+      before { described_class.new(antenne).call }
+
+      it "does not affect antennes from other institutions" do
+        expect(local_from_other.reload.parent_antenne).to be_nil
       end
     end
   end
