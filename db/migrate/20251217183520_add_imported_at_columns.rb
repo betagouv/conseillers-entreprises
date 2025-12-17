3b6adc37b021a080cc923b2b1c430ddcd70b8ae8
Nicolas Bouilleaud <nico@bou.io>
2025-12-17 19:45:58 +0100

    Set the imported_at value

--- db/migrate/20251217183520_add_imported_at_columns.rb
+++ db/migrate/20251217183520_add_imported_at_columns.rb
@@ -1,5 +1,5 @@
 class AddImportedAtColumns < ActiveRecord::Migration[7.2]
-  DEFAULT_IMPORTED_AT = Time.utc(2001, 1, 1)
+  DEFAULT_IMPORTED_AT = Time.utc(2001, 1, 1) # move this to somewhere permanent
   def change
     add_column :users, :imported_at, :datetime, null: false, default: DEFAULT_IMPORTED_AT
     add_column :antennes, :imported_at, :datetime, null: false, default: DEFAULT_IMPORTED_AT
