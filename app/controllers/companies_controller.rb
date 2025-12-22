44a10fbfdfd7a466c9d268272e9dd6a958e8ab40
Nicolas Bouilleaud <nico@bou.io>
2025-12-22 10:58:00 +0100

    Do not use subqueries with orders

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -56,23 +56,12 @@ class CompaniesController < ApplicationController
   def needs
     @facility = authorize Facility.find(params.permit(:id)[:id])
 
-    @needs_in_progress = policy_scope(@facility.needs)
-      .in_progress
-      .order(created_at: :desc)
-    @needs_done = policy_scope(@facility.needs)
-      .done
-      .order(created_at: :desc)
+    @needs_in_progress = policy_scope(@facility.needs).in_progress.order(created_at: :desc)
+    @needs_done = policy_scope(@facility.needs).done.order(created_at: :desc)
 
     email_needs = Need.for_emails(@facility.company.contacts.pluck(:email))
-
-    @contact_needs_in_progress = policy_scope(email_needs)
-      .in_progress
-      .where.not(id: @needs_in_progress)
-      .order(created_at: :desc)
-    @contact_needs_done = policy_scope(email_needs)
-      .done
-      .where.not(id: @needs_done)
-      .order(created_at: :desc)
+    @contact_needs_in_progress = policy_scope(email_needs).in_progress.order(created_at: :desc) - @needs_in_progress
+    @contact_needs_done = policy_scope(email_needs).done.order(created_at: :desc) - @needs_done
   end
 
   private
