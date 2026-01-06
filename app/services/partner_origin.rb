fdfdb3fe4bcbe9ebddbe9662b6bc72223968d0c6
Nicolas Bouilleaud <nico@bou.io>
2026-01-06 18:09:20 +0100

    Move SolicitationHelper.partner_url and friends to PartnerOrigin

--- app/services/partner_origin.rb
+++ app/services/partner_origin.rb
@@ -8,11 +8,35 @@ module PartnerOrigin
 
   def self.from_entreprendre?(solicitation: nil, campaign: nil, kwd: nil)
     if solicitation.present?
-      solicitation.cooperation&.mtm_campaign == 'entreprendre' || from_entreprendre?(campaign: self.campaign, kwd: self.kwd)
+      solicitation.cooperation&.mtm_campaign == 'entreprendre' || from_entreprendre?(campaign: solicitation.campaign, kwd: solicitation.kwd)
     elsif campaign.present? || kwd.present?
       campaign == 'entreprendre' || !!(kwd =~ ENTREPRENDRE_PAGE_PATTERN)
     else
       false
     end
+  end
+
+  def self.partner_url(solicitation, full: false)
+    return if solicitation.nil?
+
+    if solicitation.origin_url.present?
+      solicitation.origin_url
+    elsif (solicitation.from_entreprendre && solicitation.kwd.present?)
+      entreprendre_url(solicitation, full: full)
+    else
+      landing_partner_url(solicitation, full: full)
+    end
+  end
+
+  def self.entreprendre_url(solicitation, full: false)
+    if full && (solicitation&.kwd =~ ENTREPRENDRE_PAGE_PATTERN)
+      "#{ENTREPRENDRE_PAGE_BASE_URL}#{solicitation.kwd}"
+    else
+      ENTREPRENDRE_HOME_URL
+    end
+  end
+
+  def self.landing_partner_url(solicitation, full: false)
+    full ? solicitation.landing.partner_full_url : solicitation.landing.partner_url
   end
 end
--- spec/services/partner_origin_spec.rb
+++ spec/services/partner_origin_spec.rb
@@ -41,4 +41,63 @@ describe PartnerOrigin do
       it { is_expected.to be(true) }
     end
   end
+
+  describe 'partner_url' do
+    subject { described_class.partner_url(solicitation, full: full) }
+
+    let(:cooperation) { build :cooperation, root_url: 'https://exemple.fr' }
+
+    context 'without full display' do
+      let(:full) { false }
+
+      context 'entreprendre solicitation' do
+        let(:solicitation) { build :solicitation, mtm_campaign: 'entreprendre', mtm_kwd: 'F1111' }
+
+        it { is_expected.to eq "https://entreprendre.service-public.gouv.fr" }
+      end
+
+      context 'with landing url' do
+        let(:solicitation) do
+          build :solicitation,
+                landing: build(:landing, cooperation: cooperation, url_path: '/aide-1')
+        end
+
+        it { is_expected.to eq 'https://exemple.fr' }
+      end
+
+      context 'with origin_url' do
+        let(:solicitation) do
+          build :solicitation, form_info: { "origin_url" => "exemple.fr/super-aide", "origin_title" => "Super aide" },
+                           landing: build(:landing, url_path: '/aide-1')
+        end
+
+        it { is_expected.to eq "exemple.fr/super-aide" }
+      end
+    end
+
+    context 'with full display' do
+      let(:full) { true }
+
+      context 'partenaire entreprendre' do
+        let(:solicitation) { build :solicitation, mtm_campaign: 'entreprendre', mtm_kwd: 'F1111' }
+
+        it { is_expected.to eq "https://entreprendre.service-public.gouv.fr/vosdroits/F1111" }
+      end
+
+      context 'with landing url' do
+        let(:solicitation) { build :solicitation, landing: create(:landing, cooperation: cooperation, url_path: '/aide-1') }
+
+        it { is_expected.to eq 'https://exemple.fr/aide-1' }
+      end
+
+      context 'with origin_url' do
+        let(:solicitation) do
+          build :solicitation, form_info: { "origin_url" => "exemple-bis.fr/super-aide", "origin_title" => "Super aide" },
+                           landing: build(:landing, cooperation: cooperation, url_path: '/aide-1')
+        end
+
+        it { is_expected.to eq "exemple-bis.fr/super-aide" }
+      end
+    end
+  end
 end
