a9d30a150fd27b69cc236898185324c5aaf595cc
Nicolas Bouilleaud <nico@bou.io>
2026-01-07 11:05:40 +0100

    wip

--- app/services/query_from_entreprendre.rb
+++ app/services/query_from_entreprendre.rb
@@ -18,3 +18,28 @@ class QueryFromEntreprendre
     @kwd.present? && !!@kwd.match(/^F+[0-9]+/)
   end
 end
+
+module PartnerOrigin
+  # Helper methods for partner origin
+  # specific hardcoded behaviour for Entreprendre
+  class << self
+
+    def from_entreprendre?(solicitation: nil, campaign: nil, kwd: nil)
+      if solicitation.present?
+        solicitation.cooperation&.mtm_campaign == 'entreprendre' || from_entreprendre?(campaign: self.campaign, kwd: self.kwd)
+      else if campaign.present? && kwd.present?
+        campaign == 'entreprendre' || kwd =~ /^F+[0-9]+/
+      end
+      end
+
+
+      def entreprendre_url(solicitation, full: false)
+        full && solicitation.kwd.match(/^F+[0-9]+/) ? "https://entreprendre.service-public.gouv.fr/vosdroits/#{solicitation.kwd}" : "https://entreprendre.service-public.gouv.fr"
+      end
+
+      def landing_partner_url(solicitation, full: false)
+        full ? solicitation.landing.partner_full_url : solicitation.landing.partner_url
+      end
+
+    end
+end
