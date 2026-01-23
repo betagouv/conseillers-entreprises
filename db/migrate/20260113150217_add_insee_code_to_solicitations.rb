fe430131dce2fe79925794bcd6ece2efccfe6269
LucienMLD <lucien.mollard@beta.gouv.fr>
2026-01-23 15:39:16 +0100

    Refactor INSEE code migration to use `change` method

--- db/migrate/20260113150217_add_insee_code_to_solicitations.rb
+++ db/migrate/20260113150217_add_insee_code_to_solicitations.rb
@@ -1,20 +1,22 @@
 class AddInseeCodeToSolicitations < ActiveRecord::Migration[7.2]
-  def up
+  def change
     add_column :solicitations, :insee_code, :string
 
-    # Backfill insee_code for in_progress solicitations that have a location but no insee_code
-    solicitations_to_update = Solicitation.where(status: :in_progress)
-      .where.not(location: [nil, ''])
-      .where(insee_code: nil)
+    up_only do
+      # Backfill insee_code for in_progress solicitations that have a location but no insee_code
+      solicitations_to_update = Solicitation.where(status: :in_progress)
+        .where.not(location: [nil, ''])
+        .where(insee_code: nil)
 
-    say "Backfilling insee_code for #{solicitations_to_update.count} in_progress solicitations..."
+      say "Backfilling insee_code for #{solicitations_to_update.count} in_progress solicitations..."
 
-    solicitations_to_update.find_each do |solicitation|
-      normalized_location = normalize_string(solicitation.location)
-      commune = DecoupageAdministratif::Commune.all.find do |c|
-        normalize_string(c.nom).include?(normalized_location)
+      solicitations_to_update.find_each do |solicitation|
+        normalized_location = normalize_string(solicitation.location)
+        commune = DecoupageAdministratif::Commune.all.find do |c|
+          normalize_string(c.nom).include?(normalized_location)
+        end
+        solicitation.update_column(:insee_code, commune.code) if commune.present?
       end
-      solicitation.update_column(:insee_code, commune.code) if commune.present?
     end
   end
 
@@ -25,9 +27,5 @@ class AddInseeCodeToSolicitations < ActiveRecord::Migration[7.2]
       .downcase
       .gsub(/[^a-z0-9]+/, ' ')
       .strip
-  end
-
-  def down
-    remove_column :solicitations, :insee_code
   end
 end
