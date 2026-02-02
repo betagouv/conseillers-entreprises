d326a6ed89c37f95219e293582fde2e687c3a760
LucienMLD <lucien.mollard@beta.gouv.fr>
2026-02-02 23:03:07 +0100

    fix

--- spec/services/activity_reports/generate/antenne_matches_spec.rb
+++ spec/services/activity_reports/generate/antenne_matches_spec.rb
@@ -3,7 +3,7 @@ describe ActivityReports::Generate::AntenneMatches do
   describe 'generate_files' do
     let(:antenne) { create :antenne }
     let!(:expert) { create :expert_with_users, antenne: antenne }
-    let!(:a_match) { create :match, expert: expert, need: create(:need, created_at: 1.month.ago) }
+    let!(:a_match) { create :match, expert: expert, need: create(:need, created_at: 2.months.ago) }
     let(:quarters) { described_class.new(antenne).send(:last_periods) }
     let(:generate_files) { described_class.new(antenne).send(:generate_files, quarters.first) }
 
