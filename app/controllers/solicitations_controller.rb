4c7134cd8d09992117bf306e13ef2a414577607d
Nicolas Bouilleaud <nico@bou.io>
2026-02-19 16:02:31 +0100

    Fix returning from step3 to step2 in location mode
    fixes #4241

--- app/controllers/solicitations_controller.rb
+++ app/controllers/solicitations_controller.rb
@@ -190,6 +190,7 @@ class SolicitationsController < PagesController
   def retrieve_company_step_path
     @solicitation.landing_subject.fields_mode_siret? ? step_company_search_solicitation_path(@solicitation.uuid, anchor: 'section-breadcrumbs') : step_company_solicitation_path(@solicitation.uuid, anchor: 'section-breadcrumbs')
   end
+  helper_method :retrieve_company_step_path
 
   def siret_is_set?
     siret_params_present? && solicitation_params[:siret].length == 14
