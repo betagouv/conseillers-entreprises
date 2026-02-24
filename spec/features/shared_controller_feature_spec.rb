d94d389121ea7b09ac2bec8f7014818985712e5f
Nicolas Bouilleaud <nico@bou.io>
2026-02-24 17:25:41 +0100

    Add failing test for #4293

--- spec/features/shared_controller_feature_spec.rb
+++ spec/features/shared_controller_feature_spec.rb
@@ -2,29 +2,43 @@ require 'rails_helper'
 
 describe 'SharedController features' do
   describe 'render_error' do
-    login_admin
-
     before do
       ENV['TEST_ERROR_RENDERING'] = 'true'
-      create_home_landing
-      allow_any_instance_of(User).to receive(:received_needs).and_raise(raised_error)
-      visit quo_active_needs_path
     end
 
     after do
       ENV['TEST_ERROR_RENDERING'] = 'false'
     end
 
-    describe '404 error' do
-      let(:raised_error) { ActiveRecord::RecordNotFound }
+    context 'on html pages' do
+      login_admin
 
-      it { expect(page.html).to include(I18n.t('shared.errors.404.message')) }
+      before do
+        create_home_landing
+        allow_any_instance_of(User).to receive(:received_needs).and_raise(raised_error)
+        visit quo_active_needs_path
+      end
+
+      describe '404 error' do
+        let(:raised_error) { ActiveRecord::RecordNotFound }
+
+        it { expect(page.html).to include(I18n.t('shared.errors.404.message')) }
+      end
+
+      describe '500 error' do
+        let(:raised_error) { ArgumentError }
+
+        it { expect(page.html).to include I18n.t('shared.errors.500.message') }
+      end
     end
 
-    describe '500 error' do
-      let(:raised_error) { ArgumentError }
+    context 'on resources' do
+      before { visit '/nonexistant.png' }
 
-      it { expect(page.html).to include I18n.t('shared.errors.500.message') }
+      it do
+        expect(page.status_code).to eq 404
+        expect(page.body).to be_empty
+      end
     end
   end
 end
