ea0c7b3d00730f9d1de8924737c44f0489d68581
LucienMLD <lucien.mollard@beta.gouv.fr>
2026-03-09 16:53:49 +0100

    Remove production environment restriction for `Rack::Timeout.register_state_change_observer`

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -3,16 +3,14 @@
 # Rack::Timeout pour détecter les timeouts avant Scalingo (30s)
 ENV['RACK_TIMEOUT_SERVICE_TIMEOUT'] ||= '28'
 
-if Rails.env.production?
-  Rack::Timeout.register_state_change_observer(:appsignal_context) do |env|
-    info = env['rack-timeout.info']
+Rack::Timeout.register_state_change_observer(:appsignal_context) do |env|
+  info = env['rack-timeout.info']
 
-    if defined?(Appsignal) && info&.state == :timed_out
-      Appsignal.set_tags(
-        timeout: true,
-        timeout_type: 'rack_timeout',
-        service_time_ms: info.service
-      )
-    end
+  if defined?(Appsignal) && info&.state == :timed_out
+    Appsignal.set_tags(
+      timeout: true,
+      timeout_type: 'rack_timeout',
+      service_time_ms: info.service
+    )
   end
 end
