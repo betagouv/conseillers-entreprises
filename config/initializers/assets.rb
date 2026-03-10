aad688b80b5b5b8a429cfa52d8d094d0545e28a1
Nicolas Bouilleaud <nico@bou.io>
2026-03-10 18:16:48 +0100

    Revert "Run rails app:update (undo these changes)"
    b2708c8320737acccd0754c94ab909fab1793935

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,4 +4,15 @@
 Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('node_modules', '@gouvfr', 'dsfr', 'dist')
+Rails.application.config.assets.paths << Rails.root.join('node_modules', '@gouvfr', 'dsfr', 'dist', 'utility', 'icons')
+Rails.application.config.assets.paths << Rails.root.join('node_modules', '@gouvfr', 'dsfr', 'dist', 'utility', 'icons', 'icons-system.min')
+Rails.application.config.assets.paths << Rails.root.join('node_modules', '@gouvfr', 'dsfr', 'dist', 'utility', 'icons', 'icons-communication.min')
+Rails.application.config.assets.paths << Rails.root.join('node_modules', 'remixicon', 'fonts')
+Rails.application.config.assets.paths << Rails.root.join('node_modules', 'slim-select', 'dist')
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+# Rails.application.config.assets.precompile += %w[ admin.js admin.css ]
