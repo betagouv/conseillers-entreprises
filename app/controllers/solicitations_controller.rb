f18fd18769872b3385a3c61dc33c9c71f7e584ed
Nicolas Bouilleaud <nico@bou.io>
2026-03-10 17:17:07 +0100

    Make rubocop happy

--- app/controllers/solicitations_controller.rb
+++ app/controllers/solicitations_controller.rb
@@ -170,10 +170,12 @@ class SolicitationsController < PagesController
   private
 
   def solicitation_params
-    params.require(:solicitation)
-      .permit(:landing_id, :landing_subject_id, :description, :code_region, :status, :insee_code,
-              *Solicitation::FIELD_TYPES.keys,
-              subject_answers_attributes: [:id, :subject_question_id, :filter_value])
+    params
+      .expect(solicitation: [
+        :landing_id, :landing_subject_id, :description, :code_region, :status, :insee_code,
+        *Solicitation::FIELD_TYPES.keys,
+        subject_answers_attributes: [:id, :subject_question_id, :filter_value]
+      ])
   end
 
   def search_params
