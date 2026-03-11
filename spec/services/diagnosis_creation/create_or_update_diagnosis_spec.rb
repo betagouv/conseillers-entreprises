085c180dd13146fce3c75fc35bdfd1dd4a47a606
Nicolas Bouilleaud <nico@bou.io>
2026-03-11 15:36:40 +0100

    Remove failing (and unneeded) “require 'api'”
    “add_autoload_paths_to_load_path” is now false (in rails 7.1 defaults): it’s not possible to *require* the files in the autoloaded paths; however, they are properly eager loaded.

--- spec/services/diagnosis_creation/create_or_update_diagnosis_spec.rb
+++ spec/services/diagnosis_creation/create_or_update_diagnosis_spec.rb
@@ -1,7 +1,5 @@
 require 'rails_helper'
 require 'api_helper'
-# Nécessaire pour que la constante Api::BasicError soit initialisée
-require 'api'
 
 describe DiagnosisCreation::CreateOrUpdateDiagnosis do
   describe 'call' do
