d248da70a5e6a58ba33144fb281668ba3c272722
LucienMLD <lucien.mollard@beta.gouv.fr>
2026-03-13 14:24:29 +0100

    add complete schema.org

--- app/helpers/seo_helper.rb
+++ app/helpers/seo_helper.rb
@@ -1,25 +1,94 @@
 module SeoHelper
-  def home_schema
+  def page_schema(title:, description:, url: nil)
+    page_url = url || request.original_url
+    {
+      "@type": "WebPage",
+      "@id": "#{page_url}#webpage",
+      "name": title,
+      "url": page_url,
+      "description": description,
+      "image": image_url('logo-ce.png'),
+      "inLanguage": "fr-FR",
+      "isPartOf": { "@id": "#{root_url}#website" }
+    }
+  end
+
+  def add_page_schema(schema)
+    @page_schemas ||= []
+    @page_schemas << schema
+  end
+
+  def schema_org_tag(data)
+    content_tag(:script, type: 'application/ld+json') do
+      raw(data.to_json.gsub('</', '<\/"'))
+    end
+  end
+
+  def schema_graph(*schemas)
     {
       '@context': "https://schema.org",
-        '@type': "WebPage",
-        name: "#{t('landings.landings.home.meta.title')}",
-        url: "#{canonical_url}",
-        description: "#{t('landings.landings.home.meta.description')}",
-        image: "#{image_url('logo-ce.png')}",
-        isPartOf: {
-          '@type': "WebSite",
-          name: "Conseillers-Entreprises",
-          url: "#{canonical_url}",
-          image: "#{image_url('logo-ce.png')}",
-          publisher: {
-            '@type': "GovernmentOrganization",
-            name: "Conseillers-Entreprises",
-            url: "#{canonical_url}",
-            logo: "#{image_url('logo-ce.png')}",
-            email: "#{ENV['APPLICATION_EMAIL']}"
-          }
-        }
-    }.to_json
+      '@graph': schemas.flatten.compact
+    }
+  end
+
+  def schema_org
+    # Collecter tous les schémas : organization en premier (pour les références @id), puis les autres
+    all_schemas = [government_organization_schema, website_schema, government_service_schema]
+    all_schemas += Array.wrap(@page_schemas) if defined?(@page_schemas) && @page_schemas.present?
+
+    schema_org_tag(schema_graph(*all_schemas))
+  end
+
+  def government_service_schema
+    {
+      "@type": "GovernmentService",
+      "@id": "#{root_url}#service",
+      "name": "Conseillers-Entreprises Service Public",
+      "description": "Service public d'accompagnement gratuit pour les TPE et PME en France",
+      "url": root_url,
+      "serviceType": "Conseil aux entreprises",
+      "provider": { "@id": "#{root_url}#organization" },
+      "areaServed": {
+        "@type": "Country",
+        "name": "France"
+      },
+      "audience": {
+        "@type": "BusinessAudience",
+        "audienceType": "TPE et PME"
+      },
+      "availableChannel": {
+        "@type": "ServiceChannel",
+        "serviceUrl": root_url,
+        "availableLanguage": "fr"
+      }
+    }
+  end
+
+  def government_organization_schema
+    {
+      "@type": "GovernmentOrganization",
+      "@id": "#{root_url}#organization",
+      "name": "Conseillers-Entreprises Service Public",
+      "url": root_url,
+      "logo": image_url('logo-ce.png'),
+      "contactPoint": {
+        "@type": "ContactPoint",
+        "email": ENV['APPLICATION_EMAIL'],
+        "contactType": "customer service",
+        "availableLanguage": "French"
+      }
+    }
+  end
+
+  def website_schema
+    {
+      '@type': "WebSite",
+      '@id': "#{root_url}#website",
+      name: "Conseillers-Entreprises Service Public",
+      url: root_url,
+      image: image_url('logo-ce.png'),
+      inLanguage: "fr-FR",
+      publisher: { '@id': "#{root_url}#organization" }
+    }
   end
 end
