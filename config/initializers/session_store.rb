557c7db9018c7179dba52522f9db0f6c2668b892
Nicolas Bouilleaud <nico@bou.io>
2026-03-23 12:43:43 +0100

    Allow setting cookies in iframes
    This is needed to use nonce in <style> and <script> elements in iframes, because we store the nonce in the session; and we do that for the nonce to be stable when navigating with turbo. (see #4373)
    
    Additionally, SameSite=None can only be set if the cookie is also Secure, and Secure requires https.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_ConseillersEntreprises_session', same_site: :strict
+Rails.application.config.session_store :cookie_store, key: '_ConseillersEntreprises_session', same_site: :none, secure: true
