27e232df2392664ddd8c0a91fbb2761f5faea5d6
Nicolas Bouilleaud <nico@bou.io>
2026-04-09 16:07:12 +0200

    Only display filtered institutions in stats

--- app/policies/stats/team_policy.rb
+++ app/policies/stats/team_policy.rb
@@ -7,4 +7,15 @@ class Stats::TeamPolicy < ApplicationPolicy
   def acquisition? = index?
   def load_data? = index?
   def load_filter_options? = index?
+
+  def visible_institutions
+    institutions = if @user&.is_admin?
+      Institution.all
+    elsif @user&.is_sponsor?
+      @user.sponsored_institutions
+    else
+      Institution.none
+    end
+    institutions.not_deleted.expert_provider
+  end
 end
