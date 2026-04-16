0c6c69bf4b2d10de048fdb6982bd21c71a6e78d5
Nicolas Bouilleaud <nico@bou.io>
2026-04-16 10:24:57 +0200

    Add helper #navbar_item and use it
    - path and title
    - optional icon
    - optional additional content block

---
+++ app/helpers/navbar_helper.rb
@@ -0,0 +1,9 @@
+module NavbarHelper
+  def navbar_item(path, title, icon = nil, **, &)
+    active_link_to path, class: 'fr-nav__link', wrap_tag: :li, wrap_class: 'fr-nav__item', class_active: 'fr-nav__item--active', data: { turbo: false }, **options do
+      icon_tag = tag.span(class: class_names('fr-mr-1w', icon), 'aria-hidden': 'true') if icon
+      additional_content = capture(&) if block_given?
+      [icon_tag, title, additional_content].join.html_safe
+    end
+  end
+end
