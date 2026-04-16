59d5befd74afff54b1cd03136115d1d8574695d7
Nicolas Bouilleaud <nico@bou.io>
2026-04-16 12:41:56 +0200

    Use a proper badge class to avoid css clash in active admin
    fixup #4417

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -3,13 +3,13 @@ module BadgesHelper
     tag.style(nonce: content_security_policy_nonce) do
       Rails.cache.fetch(Badge.all) do
         Badge.colored.map do |badge|
-          ".label--#{badge.long_name} { border: 1px solid #{badge.color}; color: #{badge.color} }"
+          ".badge--#{badge.long_name} { border: 1px solid #{badge.color}; color: #{badge.color} }"
         end.join(' ').html_safe
       end
     end
   end
 
   def badge_label(badge)
-    tag.div(badge.title, class: "label label--#{badge.long_name}")
+    tag.div(badge.title, class: "label badge badge--#{badge.long_name}")
   end
 end
