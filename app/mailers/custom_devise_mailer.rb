8a64554237203c6fef4996bb1f25a3229adc75a1
Nicolas Bouilleaud <nico@bou.io>
2026-04-22 16:31:16 +0200

    Simplify CustomDeviseMailer not that we can rely on the defaults

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -1,31 +1,24 @@
 class CustomDeviseMailer < Devise::Mailer
   layout 'expert_mailers'
 
-  def headers_for(action, opts)
-    if action == :invitation_instructions
-      super.merge!({
-        subject: invitation_instructions_subject(resource),
-          from: email_address_with_name(ApplicationMailer::SENDER, resource_item.support_user_name),
-          reply_to: resource_item.support_user_email_with_name,
-          'X-Encryption': 'TLS'
-      })
-    else
-      super.merge!({
-        from: ApplicationMailer::SENDER,
-        reply_to: ApplicationMailer::REPLY_TO,
-        'X-Encryption': 'TLS'
-      })
-    end
-  end
-
   def invitation_instructions(record, token, opts = {})
-    if cooperation_invitation?(record)
+    if record.is_cooperation_manager?
       @cooperation = record.managed_cooperations.first
       @cooperation_logo_name = @cooperation.logo&.filename
+      @support_user = record.support_user
+
+      opts[:subject] = I18n.t('devise.mailer.invitation_instructions.subject_cooperation', cooperation: @cooperation.name)
+      opts[:from] = email_address_with_name(ApplicationMailer::SENDER.call, @cooperation.support_user_name)
+      opts[:reply_to] = @cooperation.support_user_email_with_name
     else
       @institution_logo_name = record.institution.logo&.filename
+      @support_user = record.support_user
+
+      opts[:subject] = I18n.t('devise.mailer.invitation_instructions.subject', institution: record.institution.name)
+      opts[:from] = email_address_with_name(ApplicationMailer::SENDER.call, record.antenne.support_user_name)
+      opts[:reply_to] = record.antenne.support_user_email_with_name
     end
-    @support_user = record.support_user
+
     super
   end
 
@@ -33,27 +26,5 @@ class CustomDeviseMailer < Devise::Mailer
     @institution_logo_name = record.institution.logo&.filename
     @support_user = record.support_user
     super
-  end
-
-  private
-
-  def cooperation_invitation?(resource)
-    @cooperation_invitation ||= resource.managed_cooperations.any?
-  end
-
-  def invitation_instructions_subject(resource)
-    if cooperation_invitation?(resource)
-      I18n.t('devise.mailer.invitation_instructions.subject_cooperation', cooperation: resource_item.name)
-    else
-      I18n.t('devise.mailer.invitation_instructions.subject', institution: resource.institution.name)
-    end
-  end
-
-  def resource_item
-    if cooperation_invitation?(resource)
-      resource.managed_cooperations.first
-    else
-      resource.antenne
-    end
   end
 end
