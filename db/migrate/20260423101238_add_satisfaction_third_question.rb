5932be9c1d653a6338916b1147e5e7fccc6b7599
Nicolas Bouilleaud <nico@bou.io>
2026-04-23 16:56:52 +0200

    Redo the model with individual booleans

--- db/migrate/20260423101238_add_satisfaction_third_question.rb
+++ db/migrate/20260423101238_add_satisfaction_third_question.rb
@@ -1,6 +1,8 @@
 class AddSatisfactionThirdQuestion < ActiveRecord::Migration[8.1]
   def change
-    create_enum :company_satisfaction_outcome, %i[find_institution find_measure start_action help_choice other]
-    add_column :company_satisfactions, :outcome, :company_satisfaction_outcome
+    add_column :company_satisfactions, :outcome_discover_support, :boolean
+    add_column :company_satisfactions, :outcome_start_action, :boolean
+    add_column :company_satisfactions, :outcome_help_choice, :boolean
+    add_column :company_satisfactions, :outcome_other, :boolean
   end
 end
