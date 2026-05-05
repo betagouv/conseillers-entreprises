99265e597f5f35aec1c7d26757fe9e656060272e
Nicolas Bouilleaud <nico@bou.io>
2026-05-05 16:46:51 +0200

    Make sure to always sort the subject answers the same way
    fixes #4461

--- app/admin/institution.rb
+++ app/admin/institution.rb
@@ -114,7 +114,7 @@ ActiveAdmin.register Institution do
     attributes_table title: I18n.t('active_admin.institution.subject_questions') do
       institution.subject_answer_groupings.map.with_index do |sag, index|
         panel I18n.t('active_admin.subject_answer_grouping.title_with_index', index: index + 1) do
-          table_for sag.subject_answers do
+          table_for sag.subject_answers.ordered do
             column(:subject) { |answer| admin_link_to(answer.subject_question, :subject) }
             column(:label) { |answer| question_label(answer.key, :long) }
             column(:key)
