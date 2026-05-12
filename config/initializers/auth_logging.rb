d250a8503e43722bd61345a79d07df154a951c81
LucienMLD <lucien.mollard@beta.gouv.fr>
2026-05-12 15:04:16 +0200

    Fix email parameter extraction in `auth_logging.rb` during authentication failure

--- config/initializers/auth_logging.rb
+++ config/initializers/auth_logging.rb
@@ -9,7 +9,7 @@ Rails.application.config.after_initialize do
   end
 
   Warden::Manager.before_failure do |env, opts|
-    email = env.dig('rack.request.form_hash', 'user', 'email').to_s.downcase.strip
+    email = ActionDispatch::Request.new(env).params.dig('user', 'email').to_s.downcase.strip
     ip = Rack::FullRemoteIpAndPort.call(env)
     Rails.logger.error("[AUTH] failure scope=#{opts[:scope]} reason=#{opts[:message]} email=#{email} ip=#{ip}")
   end
