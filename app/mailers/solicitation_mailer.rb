4f6de7e2b546496ce83472decd4cdf01d7430a24
LucienMLD <lucien.mollard@beta.gouv.fr>
2026-05-22 13:25:44 +0200

    Refactor `solicitation_mailer` by dynamically defining methods for generic email types excluding `bad_quality`.

--- app/mailers/solicitation_mailer.rb
+++ app/mailers/solicitation_mailer.rb
@@ -4,11 +4,6 @@ class SolicitationMailer < ApplicationMailer
 
   helper :solicitation, :images
 
-  def administrations_collectivites(solicitation)
-    @solicitation_mail_template = SolicitationMailTemplate.find_by!(email_type: 'administrations_collectivites')
-    mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
-  end
-
   def bad_quality(solicitation)
     @solicitation = solicitation
     @cooperation_logo_name = @solicitation.cooperation&.logo&.filename
@@ -18,83 +13,10 @@ class SolicitationMailer < ApplicationMailer
     mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
   end
 
-  def carsat(solicitation)
-    @solicitation_mail_template = SolicitationMailTemplate.find_by!(email_type: 'carsat')
-    mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
-  end
-
-  def creation(solicitation)
-    @solicitation_mail_template = SolicitationMailTemplate.find_by!(email_type: 'creation')
-    mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
-  end
-
-  def employee_labor_law(solicitation)
-    @solicitation_mail_template = SolicitationMailTemplate.find_by!(email_type: 'employee_labor_law')
-    mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
-  end
-
-  def formalites_asso_agri_sci(solicitation)
-    @solicitation_mail_template = SolicitationMailTemplate.find_by!(email_type: 'formalites_asso_agri_sci')
-    mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
-  end
-
-  def intermediary(solicitation)
-    @solicitation_mail_template = SolicitationMailTemplate.find_by!(email_type: 'intermediary')
-    mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
-  end
-
-  def kbis_extract(solicitation)
-    @solicitation_mail_template = SolicitationMailTemplate.find_by!(email_type: 'kbis_extract')
-    mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
-  end
-
-  def mediateurs(solicitation)
-    @solicitation_mail_template = SolicitationMailTemplate.find_by!(email_type: 'mediateurs')
-    mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
-  end
-
-  def moderation(solicitation)
-    @solicitation_mail_template = SolicitationMailTemplate.find_by!(email_type: 'moderation')
-    mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
-  end
-
-  def no_expert(solicitation)
-    @solicitation_mail_template = SolicitationMailTemplate.find_by!(email_type: 'no_expert')
-    mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
-  end
-
-  def no_expert_agri(solicitation)
-    @solicitation_mail_template = SolicitationMailTemplate.find_by!(email_type: 'no_expert_agri')
-    mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
-  end
-
-  def recruitment_foreign_worker(solicitation)
-    @solicitation_mail_template = SolicitationMailTemplate.find_by!(email_type: 'recruitment_foreign_worker')
-    mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
-  end
-
-  def retirement_liberal_professions(solicitation)
-    @solicitation_mail_template = SolicitationMailTemplate.find_by!(email_type: 'retirement_liberal_professions')
-    mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
-  end
-
-  def sie_sip_declare_and_pay(solicitation)
-    @solicitation_mail_template = SolicitationMailTemplate.find_by!(email_type: 'sie_sip_declare_and_pay')
-    mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
-  end
-
-  def sie_tva_and_others(solicitation)
-    @solicitation_mail_template = SolicitationMailTemplate.find_by!(email_type: 'sie_tva_and_others')
-    mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
-  end
-
-  def siret(solicitation)
-    @solicitation_mail_template = SolicitationMailTemplate.find_by!(email_type: 'siret')
-    mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
-  end
-
-  def tns_training(solicitation)
-    @solicitation_mail_template = SolicitationMailTemplate.find_by!(email_type: 'tns_training')
-    mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
+  Solicitation::GENERIC_EMAILS_TYPES.flatten.without(:bad_quality).each do |email_type|
+    define_method(email_type) do |solicitation|
+      @solicitation_mail_template = SolicitationMailTemplate.find_by!(email_type: email_type.to_s)
+      mail(to: solicitation.email, subject: t('mailers.solicitation.subject'))
+    end
   end
 end
