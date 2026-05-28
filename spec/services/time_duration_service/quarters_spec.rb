22990e288fd523c7ec54c0a968c326949bd87e3f
Nicolas Bouilleaud <nico@bou.io>
2026-05-28 17:38:24 +0200

    Simplify TimeDuration specs
    use Rails’ facilities

--- spec/services/time_duration_service/quarters_spec.rb
+++ spec/services/time_duration_service/quarters_spec.rb
@@ -6,7 +6,16 @@ describe TimeDurationService::Quarters do
     context '1rst quarter' do
       it do
         travel_to('2025-01-20') do
-          is_expected.to contain_exactly(('Tue, 01 Oct 2024'.to_date)..('Tue, 31 Dec 2024'.to_date), ('Mon, 01 Jul 2024'.to_date)..('Mon, 30 Sep 2024'.to_date), ('Mon, 01 Apr 2024'.to_date)..('Sun, 30 Jun 2024'.to_date), ('Mon, 01 Jan 2024'.to_date)..('Sun, 31 Mar 2024'.to_date), ('Sun, 01 Oct 2023'.to_date)..('Sun, 31 Dec 2023'.to_date), ('Sat, 01 Jul 2023'.to_date)..('Sat, 30 Sep 2023'.to_date), ('Sat, 01 Apr 2023'.to_date)..('Fri, 30 Jun 2023'.to_date), ('Sun, 01 Jan 2023'.to_date)..('Fri, 31 Mar 2023'.to_date))
+          is_expected.to contain_exactly(
+            '10/2024'.to_date.all_quarter,
+                           '07/2024'.to_date.all_quarter,
+                           '04/2024'.to_date.all_quarter,
+                           '01/2024'.to_date.all_quarter,
+                           '10/2023'.to_date.all_quarter,
+                           '07/2023'.to_date.all_quarter,
+                           '04/2023'.to_date.all_quarter,
+                           '01/2023'.to_date.all_quarter
+          )
         end
       end
     end
@@ -14,7 +23,13 @@ describe TimeDurationService::Quarters do
     context '2nd quarter' do
       it do
         travel_to('2025-05-20') do
-          is_expected.to contain_exactly(('Mon, 01 Jan 2024'.to_date)..('Sun, 31 Mar 2024'.to_date), ('Mon, 01 Apr 2024'.to_date)..('Sun, 30 Jun 2024'.to_date), ('Mon, 01 Jul 2024'.to_date)..('Mon, 30 Sep 2024'.to_date), ('Tue, 01 Oct 2024'.to_date)..('Tue, 31 Dec 2024'.to_date), ('Wed, 01 Jan 2025'.to_date)..('Mon, 31 Mar 2025'.to_date))
+          is_expected.to contain_exactly(
+            '01/2025'.to_date.all_quarter,
+                           '10/2024'.to_date.all_quarter,
+                           '07/2024'.to_date.all_quarter,
+                           '04/2024'.to_date.all_quarter,
+                           '01/2024'.to_date.all_quarter
+          )
         end
       end
     end
@@ -22,7 +37,14 @@ describe TimeDurationService::Quarters do
     context '3rd quarter' do
       it do
         travel_to('2025-08-20') do
-          is_expected.to contain_exactly(('Mon, 01 Jan 2024'.to_date)..('Sun, 31 Mar 2024'.to_date), ('Mon, 01 Apr 2024'.to_date)..('Sun, 30 Jun 2024'.to_date), ('Mon, 01 Jul 2024'.to_date)..('Mon, 30 Sep 2024'.to_date), ('Tue, 01 Oct 2024'.to_date)..('Tue, 31 Dec 2024'.to_date), ('Wed, 01 Jan 2025'.to_date)..('Mon, 31 Mar 2025'.to_date), ('Tue, 01 Apr 2025'.to_date)..('Mon, 30 Jun 2025'.to_date))
+          is_expected.to contain_exactly(
+            '04/2025'.to_date.all_quarter,
+                           '01/2025'.to_date.all_quarter,
+                           '10/2024'.to_date.all_quarter,
+                           '07/2024'.to_date.all_quarter,
+                           '04/2024'.to_date.all_quarter,
+                           '01/2024'.to_date.all_quarter
+          )
         end
       end
     end
@@ -30,7 +52,15 @@ describe TimeDurationService::Quarters do
     context 'last quarter' do
       it do
         travel_to('2025-11-20') do
-          is_expected.to contain_exactly(('Mon, 01 Jan 2024'.to_date)..('Sun, 31 Mar 2024'.to_date), ('Mon, 01 Apr 2024'.to_date)..('Sun, 30 Jun 2024'.to_date), ('Mon, 01 Jul 2024'.to_date)..('Mon, 30 Sep 2024'.to_date), ('Tue, 01 Oct 2024'.to_date)..('Tue, 31 Dec 2024'.to_date), ('Wed, 01 Jan 2025'.to_date)..('Mon, 31 Mar 2025'.to_date), ('Tue, 01 Apr 2025'.to_date)..('Mon, 30 Jun 2025'.to_date), ('Wed, 01 Jul 2025'.to_date)..('Tue, 30 Sep 2025'.to_date))
+          is_expected.to contain_exactly(
+            '07/2025'.to_date.all_quarter,
+                           '04/2025'.to_date.all_quarter,
+                           '01/2025'.to_date.all_quarter,
+                           '10/2024'.to_date.all_quarter,
+                           '07/2024'.to_date.all_quarter,
+                           '04/2024'.to_date.all_quarter,
+                           '01/2024'.to_date.all_quarter
+          )
         end
       end
     end
