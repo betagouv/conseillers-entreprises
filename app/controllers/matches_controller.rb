71ab9f46d43cb3c6284ec32ab377b0c6b6c0facf
Nicolas Bouilleaud <nico@bou.io>
2026-06-23 14:36:47 +0200

    Replace the match#update js implementation with turbo streams

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -9,7 +9,13 @@ class MatchesController < ApplicationController
       MatchMailerService.new(@match).deduplicated_notify_status(previous_status)
     end
     respond_to do |format|
-      format.js
+      format.turbo_stream do
+        stream = [turbo_stream.replace("row-match-#{@match.id}", partial: 'match', locals: { match: @match })]
+        if current_user.experts.include?(@match.expert)
+          stream << turbo_stream.replace('match-actions', partial: 'needs/match_actions', locals: { match: @match, display_feedback_form: true })
+        end
+        render turbo_stream: stream
+      end
       format.html { redirect_to need_path(@match.need, anchor: "match-#{@match.id}") }
     end
   end
