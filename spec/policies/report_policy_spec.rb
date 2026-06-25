7224ec5f7ca6c752e9a21d9785dd13a529202da4
Nicolas Bouilleaud <nico@bou.io>
2026-06-25 17:30:40 +0200

    Only allow sponsors to view the stats reports, not the matches
    refs #4553

--- spec/policies/report_policy_spec.rb
+++ spec/policies/report_policy_spec.rb
@@ -85,4 +85,33 @@ RSpec.describe ReportPolicy, type: :policy do
       it { is_expected.not_to permit(user, activity_report) }
     end
   end
+
+  permissions :matches? do
+    let(:antenne) { create :antenne }
+
+    context "grant access if user is an admin" do
+      let(:user) { create :user, :admin }
+
+      it { is_expected.to permit(user, antenne) }
+    end
+
+    context "grants access if antenne is in user managed antennes" do
+      let(:user) { create :user, :manager, managed_antennes: [antenne] }
+
+      it { is_expected.to permit(user, antenne) }
+    end
+
+    context "denies access if user is a sponsor" do
+      let(:user) { create :user, :sponsor, sponsored_institutions: [antenne.institution] }
+
+      it { is_expected.not_to permit(user) }
+    end
+
+
+    context "denies access if user is another user" do
+      let(:user) { create :user }
+
+      it { is_expected.not_to permit(user, antenne) }
+    end
+  end
 end
