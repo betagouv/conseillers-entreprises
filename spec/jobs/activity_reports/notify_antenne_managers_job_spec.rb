96aea2408d3e0c3c346c2863a5204d7bd6697f05
Nicolas Bouilleaud <nico@bou.io>
2026-07-10 17:08:20 +0200

    Remove pointless test (unrelated)

--- spec/jobs/activity_reports/notify_antenne_managers_job_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-RSpec.describe ActivityReports::NotifyAntenneManagersJob do
-  describe 'enqueue a job' do
-    it { assert_enqueued_jobs(1) { described_class.perform_later } }
-  end
-end
