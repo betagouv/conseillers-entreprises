94692d0392aa47bb3fa6d25ca555522a311bc339
Nicolas Bouilleaud <nico@bou.io>
2026-07-24 10:13:00 +0200

    Make view spec happy

--- app/helpers/cooperations_helper.rb
+++ app/helpers/cooperations_helper.rb
@@ -3,7 +3,8 @@ module CooperationsHelper
   # Each option uses direct-submit-controller to automatically switch to its url.
   def options_for_managed_cooperations_select_menu(current_cooperation, cooperations)
     options = cooperations.map do |cooperation|
-      action = policy(cooperation).send("#{action_name}?") ? action_name : "needs"
+      current_action = action_name || "needs"
+      action = policy(cooperation).send("#{current_action}?") ? current_action : "needs"
       path = polymorphic_path([action.to_sym, :conseiller, cooperation])
       [cooperation.name, cooperation.id, { data: { 'direct-submit-url': path } }]
     end
