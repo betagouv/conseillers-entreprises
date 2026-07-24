1712519632b2b31b68ac49ad02a61f73d688e87d
Nicolas Bouilleaud <nico@bou.io>
2026-07-24 12:25:42 +0200

    Avoid including archived landing subjects in the response

--- app/serializers/api/v1/subject_serializer.rb
+++ app/serializers/api/v1/subject_serializer.rb
@@ -5,5 +5,7 @@ class Api::V1::SubjectSerializer < ActiveModel::Serializer
     attributes :id, :title, :slug, :description, :description_explanation
   end
 
-  has_many :landing_subjects, serializer: LandingSubjectSerializer
+  has_many :landing_subjects, serializer: LandingSubjectSerializer do
+    object.landing_subjects.filter(&:not_archived?)
+  end
 end
