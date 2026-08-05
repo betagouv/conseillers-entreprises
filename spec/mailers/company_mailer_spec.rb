87f58835f4c258d9050f2ef7d545210eb4c43fec
LucienMLD <lucien.mollard@beta.gouv.fr>
2026-08-05 11:32:58 +0200

    update not_yet_taken_care email to use requalified subject

--- spec/mailers/company_mailer_spec.rb
+++ spec/mailers/company_mailer_spec.rb
@@ -33,6 +33,41 @@ describe CompanyMailer do
     it { expect(mail.header[:from].value).to eq ExpertMailer::SENDER.call }
   end
 
+  describe '#not_yet_taken_care' do
+    subject(:mail) { described_class.not_yet_taken_care(solicitation).deliver_now }
+
+    let!(:initial_subject) { create :subject, label: "Sujet initial" }
+    let!(:initial_landing_subject) { create :landing_subject, subject: initial_subject, title: "Sujet initial" }
+    let!(:solicitation) { create :solicitation, landing_subject: initial_landing_subject }
+    let!(:need) { create :need_with_matches, solicitation: solicitation, subject: need_subject }
+
+    context 'when need has not been requalified' do
+      let(:need_subject) { initial_subject }
+
+      it_behaves_like 'an email'
+
+      it 'uses the original subject in the body' do
+        expect(mail.body.parts.first.body).to match(/#{initial_subject.label.split.join('\s*')}/)
+      end
+    end
+
+    context 'when need has been requalified with a different subject' do
+      let(:need_subject) { create :subject, label: "Sujet requalifié" }
+
+      it_behaves_like 'an email'
+
+      it 'uses the requalified subject in the body, not the original one' do
+        expect(mail.body.parts.first.body).to match(/#{need_subject.label.split.join('\s*')}/)
+        expect(mail.body.parts.first.body).not_to include initial_landing_subject.title
+      end
+
+      it 'shows the same subject in the mail subject line and in the body' do
+        expect(mail.subject).to eq I18n.t('mailers.company_mailer.not_yet_taken_care.subject', subject: solicitation.final_subject_title)
+        expect(mail.body.parts.first.body).to match(/#{solicitation.final_subject_title.split.join('\s*')}/)
+      end
+    end
+  end
+
   describe '#intelligent_retention' do
     subject(:mail) { described_class.intelligent_retention(need, email_retention).deliver_now }
 
