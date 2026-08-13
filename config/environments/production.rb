080d17710228996fa67e418a2530858d1704d045
Nicolas Bouilleaud <nico@bou.io>
2026-08-13 10:16:11 +0200

    Revert PR #4608 (switch back to redis for application cache)
    refs #4352

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,16 @@ Rails.application.configure do
   # want to log everything, set the level to "debug".
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
+  # Use a different cache store in production.
+  config.cache_store =
+    :redis_cache_store, { url: ENV['REDIS_URL'], reconnect_attempts: 3,
+                                                 error_handler: -> (method:, returning:, exception:) {
+                                                   Appsignal.send_error(exception) do |transaction|
+                                                     transaction.set_tags(method: method, returning: returning)
+                                                   end
+                                                 }
+      }
+
   # Prevent health checks from clogging up the logs.
   config.silence_healthcheck_path = "/up"
   # Use a real queuing backend for Active Job (and separate queues per environment).
@@ -95,7 +105,14 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :file_store, "#{root}/tmp/cache"
+  config.cache_store =
+    :redis_cache_store, { url: ENV['REDIS_URL'], reconnect_attempts: 3,
+                          error_handler: -> (method:, returning:, exception:) {
+                            Appsignal.send_error(exception) do |transaction|
+                              transaction.set_tags(method: method, returning: returning)
+                            end
+                          }
+    }
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   # config.active_job.queue_adapter = :resque
