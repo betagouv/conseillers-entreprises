23cd1184360a2e19a686d63a67b08da83177e4b0
Nicolas Bouilleaud <nico@bou.io>
2026-09-01 14:26:14 +0200

    Adjust pseudonymization email creation
    avoid blanking diacritics characters and limit the domain length

--- app/services/anonymization.rb
+++ app/services/anonymization.rb
@@ -104,15 +104,15 @@ module Anonymization
   def pseudonymize_expert(expert)
     ensure_can_anonymize!
 
-    domain = expert.institution.name.parameterize(separator: ".")
+    domain = "#{expert.institution.slug}.demo"
     if expert.with_one_user?
       name = Faker::Name.name
-      email = Faker::Internet.email(name: name, domain: domain)
+      email = "#{name.parameterize}@#{domain}"
       expert.update(full_name: name, email: email, phone_number: Faker::PhoneNumber.phone_number)
       expert.users.first.update(full_name: name, email: email, phone_number: Faker::PhoneNumber.phone_number, current_sign_in_ip: Faker::Internet.ip_v4_address, last_sign_in_ip: Faker::Internet.ip_v4_address)
     else
       team_name = Faker::Team.name
-      team_email = Faker::Internet.email(name: team_name, domain: domain)
+      team_email = "#{team_name.parameterize}@#{domain}"
 
       expert.update(full_name: team_name, email: team_email, phone_number: Faker::PhoneNumber.phone_number)
       expert.users.each do |user|
@@ -130,7 +130,7 @@ module Anonymization
     description = Faker::Lorem.paragraph(sentence_count: 20)
     name = Faker::Name.name
     company_name = Faker::Company.name
-    email = Faker::Internet.email(name: name, domain: company_name.parameterize(separator: "."))
+    email = "#{name.parameterize}@#{company_name.parameterize(separator: ".")}"
     phone_number = Faker::PhoneNumber.phone_number
 
     solicitation.update(siret: siret, description: description, email: email, full_name: name, phone_number: phone_number)
