dd93e43464e362cfb436b1238f4ecea3ec85e1f9
Nicolas Bouilleaud <nico@bou.io>
2026-09-03 14:59:45 +0200

    Update last_active_at when doing something

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -4,6 +4,7 @@ class MatchesController < ApplicationController
     authorize @match
     previous_status = @match.status
     @match.update status: params[:status]
+    current_user.update(last_active_at: DateTime.now)
     # Don't send emails when the current user is an administrator and therefore is not present in @match.contacted_users
     if @match.contacted_users.include?(current_user)
       MatchMailerService.new(@match).deduplicated_notify_status(previous_status)
